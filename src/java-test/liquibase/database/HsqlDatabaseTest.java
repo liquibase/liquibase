0ab46fb255d367515693dd758e69026e3351e23c
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2007-08-01 19:18:14 +0000

    Applied patch for multi-value concat's in HSQL via Matthieu Brouillard
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@228 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/java-test/liquibase/database/HsqlDatabaseTest.java
+++ src/java-test/liquibase/database/HsqlDatabaseTest.java
@@ -11,4 +11,15 @@ public class HsqlDatabaseTest extends TestCase {
         assertNull(database.getDefaultDriver("jdbc:oracle://localhost;databaseName=liquibase"));
     }
 
+    public void testGetConcatSql() {
+        Database database = new HsqlDatabase();
+        String expectedResult = "CONCAT(str1, CONCAT(str2, CONCAT(str3, str4)))";
+        String value = "v";
+        String[] values = new String[]{"str1", "str2", "str3", "str4"};
+
+        assertEquals(database.getConcatSql(value), value);
+        assertEquals(database.getConcatSql(values), expectedResult);
+        assertNull(database.getConcatSql((String[]) null));
+    }
+
 }
