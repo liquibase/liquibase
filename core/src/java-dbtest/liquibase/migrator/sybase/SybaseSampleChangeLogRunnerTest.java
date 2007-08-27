879955c62eaf611c6ff944605bd929915bce46fe
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2007-08-27 19:11:03 +0000

    sybase test needs to run with autocommit = true
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@282 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java-dbtest/liquibase/migrator/sybase/SybaseSampleChangeLogRunnerTest.java
+++ core/src/java-dbtest/liquibase/migrator/sybase/SybaseSampleChangeLogRunnerTest.java
@@ -10,4 +10,10 @@ public class SybaseSampleChangeLogRunnerTest extends AbstractSimpleChangeLogRunn
     public SybaseSampleChangeLogRunnerTest() throws Exception {
         super("sybase", "com.sybase.jdbc3.jdbc.SybDriver", "jdbc:sybase:Tds:"+ InetAddress.getLocalHost().getHostName()+":5000/liquibase");
     }
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        connection.setAutoCommit(true);        
+    }
 }
