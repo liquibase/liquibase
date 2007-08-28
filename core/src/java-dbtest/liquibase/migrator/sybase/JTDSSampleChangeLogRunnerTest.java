149b05d658e3edc4c9b725ddca4eb1ec813a0b09
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2007-08-28 19:21:10 +0000

    false autocommit for JTDS Sybase test
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@288 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java-dbtest/liquibase/migrator/sybase/JTDSSampleChangeLogRunnerTest.java
+++ core/src/java-dbtest/liquibase/migrator/sybase/JTDSSampleChangeLogRunnerTest.java
@@ -8,4 +8,10 @@ public class JTDSSampleChangeLogRunnerTest extends AbstractSimpleChangeLogRunner
     public JTDSSampleChangeLogRunnerTest() throws Exception {
         super("sybase", "net.sourceforge.jtds.jdbc.Driver", "jdbc:jtds:sybase://localhost/nathan:5000");
     }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        connection.setAutoCommit(true);
+    }
+
 }
