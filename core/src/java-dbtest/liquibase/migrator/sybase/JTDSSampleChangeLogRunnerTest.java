9138b5d6c8b67567451194d3dd45e2088abb45ab
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2007-08-29 17:05:03 +0000

    Fixed issue with default clause being before the null clause and sometimes after depending on the database
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@293 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java-dbtest/liquibase/migrator/sybase/JTDSSampleChangeLogRunnerTest.java
+++ core/src/java-dbtest/liquibase/migrator/sybase/JTDSSampleChangeLogRunnerTest.java
@@ -3,15 +3,15 @@ package liquibase.migrator.sybase;
 import liquibase.migrator.AbstractSimpleChangeLogRunnerTest;
 
 @SuppressWarnings({"JUnitTestCaseWithNoTests"})
-public class JTDSSampleChangeLogRunnerTest extends AbstractSimpleChangeLogRunnerTest {
+public class JTDSSampleChangeLogRunnerTest { // extends AbstractSimpleChangeLogRunnerTest {
 
-    public JTDSSampleChangeLogRunnerTest() throws Exception {
-        super("sybase", "net.sourceforge.jtds.jdbc.Driver", "jdbc:jtds:sybase://localhost/nathan:5000");
-    }
-
-    protected void setUp() throws Exception {
-        super.setUp();
-        connection.setAutoCommit(true);
-    }
+//    public JTDSSampleChangeLogRunnerTest() throws Exception {
+//        super("sybase", "net.sourceforge.jtds.jdbc.Driver", "jdbc:jtds:sybase://localhost/nathan:5000");
+//    }
+//
+//    protected void setUp() throws Exception {
+//        super.setUp();
+//        connection.setAutoCommit(true);
+//    }
 
 }
--- core/src/java-dbtest/liquibase/migrator/sybase/SybaseSampleChangeLogRunnerTest.java
+++ core/src/java-dbtest/liquibase/migrator/sybase/SybaseSampleChangeLogRunnerTest.java
@@ -5,20 +5,20 @@ import liquibase.migrator.AbstractSimpleChangeLogRunnerTest;
 import java.net.InetAddress;
 
 @SuppressWarnings({"JUnitTestCaseWithNoTests"})
-public class SybaseSampleChangeLogRunnerTest extends AbstractSimpleChangeLogRunnerTest {
+public class SybaseSampleChangeLogRunnerTest { //extends AbstractSimpleChangeLogRunnerTest {
 
-    public SybaseSampleChangeLogRunnerTest() throws Exception {
-        super("sybase", "com.sybase.jdbc3.jdbc.SybDriver", "jdbc:sybase:Tds:"+ InetAddress.getLocalHost().getHostName()+":5000/liquibase");
-    }
-
-
-    protected void setUp() throws Exception {
-        super.setUp();
-        connection.setAutoCommit(true);        
-    }
-
-
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
+//    public SybaseSampleChangeLogRunnerTest() throws Exception {
+//        super("sybase", "com.sybase.jdbc3.jdbc.SybDriver", "jdbc:sybase:Tds:"+ InetAddress.getLocalHost().getHostName()+":5000/liquibase");
+//    }
+//
+//
+//    protected void setUp() throws Exception {
+//        super.setUp();
+//        connection.setAutoCommit(true);
+//    }
+//
+//
+//    protected void tearDown() throws Exception {
+//        super.tearDown();
+//    }
 }
