e459779c831f0ed24e5f79938859490f83671325
csuml <csuml@e6edf6fb-f266-4316-afb4-e53d95876a76>
2007-09-25 20:43:46 +0000

    Added common ancestry for SQL and SqlFile Change so that the defaults and splitting/comment behaviour are now identical. Added necessary unit test changes to ensure that this the major combinations work as intended together.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@366 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ core/src/java-test/liquibase/change/AbstractSQLChangeTest.java
@@ -0,0 +1,157 @@
+package liquibase.change;
+
+import static org.junit.Assert.assertEquals;
+
+import liquibase.database.MSSQLDatabase;
+import liquibase.database.OracleDatabase;
+import liquibase.database.sql.SqlStatement;
+import liquibase.exception.UnsupportedChangeException;
+import liquibase.util.XMLUtil;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class AbstractSQLChangeTest extends AbstractChangeTest {
+
+    private RawSQLChange refactoring;
+    
+    @Before
+    public void setUp() throws Exception {
+        refactoring = new RawSQLChange();
+    }
+
+    @Test
+    public void getRefactoringName() throws Exception {
+        assertEquals("Custom SQL", new RawSQLChange().getChangeName());
+    }
+
+    @Test
+    public void generateStatement() throws Exception {
+        refactoring.setSql("SQL STATEMENT HERE");
+        OracleDatabase database = new OracleDatabase();
+        assertEquals("SQL STATEMENT HERE", refactoring.generateStatements(database)[0].getSqlStatement(database));
+    }
+
+    @Test
+    public void getConfirmationMessage() throws Exception {
+        assertEquals("Custom SQL executed", refactoring.getConfirmationMessage());
+    }
+
+    @Test
+    public void createNode() throws Exception {
+        refactoring.setSql("SOME SQL HERE");
+
+        Element element = refactoring.createNode(DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
+        assertEquals("sql", element.getTagName());
+
+        assertEquals("SOME SQL HERE", XMLUtil.getTextContent(element));
+    }
+    
+    @Test
+    public void multiLineSQLFileSemiColon() throws Exception {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM customer;" +
+                "SELECT * from table;");
+        OracleDatabase database = new OracleDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        
+        assertEquals(2,statements.length);
+        assertEquals("SELECT * FROM customer",statements[0].getSqlStatement(database));
+        assertEquals("SELECT * from table",statements[1].getSqlStatement(database));
+    }
+    
+    @Test
+    public void singleLineEndInSemiColon() throws Exception {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM customer;");
+        OracleDatabase database = new OracleDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        assertEquals(1,statements.length);
+        assertEquals("SELECT * FROM customer",statements[0].getSqlStatement(database));
+    }
+    
+    @Test
+    public void singleLineEndGo() throws Exception {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM customer\ngo");
+        MSSQLDatabase database = new MSSQLDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        assertEquals(1,statements.length);
+        assertEquals("SELECT * FROM customer",statements[0].getSqlStatement(database));
+    }
+    
+    @Test
+    public void singleLineBeginGo() throws Exception {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("goSELECT * FROM customer\ngo");
+        MSSQLDatabase database = new MSSQLDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        assertEquals(1,statements.length);
+        assertEquals("goSELECT * FROM customer",statements[0].getSqlStatement(database));
+    }
+    
+    @Test
+    public void multiLineSQLFileGoShouldFind() throws Exception {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM customer\ngo\n" +
+                "SELECT * from table\ngo");
+        MSSQLDatabase database = new MSSQLDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        assertEquals(2,statements.length);
+        assertEquals("SELECT * FROM customer",statements[0].getSqlStatement(database));
+        assertEquals("SELECT * from table",statements[1].getSqlStatement(database));
+    }
+    
+    @Test
+    public void multiLineSQLFileGoShouldNotFind() throws Exception {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM go\ngo\n" +
+                "SELECT * from gogo\ngo\n");
+        MSSQLDatabase database = new MSSQLDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        
+        assertEquals(2,statements.length);
+        assertEquals("SELECT * FROM go",statements[0].getSqlStatement(database));
+        assertEquals("SELECT * from gogo",statements[1].getSqlStatement(database));
+    }
+    
+    @Test
+    public void stripComments() throws UnsupportedChangeException {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM x\n -- A comment");
+        change2.setStripComments(true);
+        MSSQLDatabase database = new MSSQLDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        
+        assertEquals(1,statements.length);
+        assertEquals("SELECT * FROM x\n",statements[0].getSqlStatement(database));
+    }
+    
+    @Test
+    public void turnOffSplitting() throws UnsupportedChangeException {
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM x;SELECT * FROM x;");
+        change2.setSplitStatements(false);
+        MSSQLDatabase database = new MSSQLDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        
+        assertEquals(1,statements.length);
+        assertEquals("SELECT * FROM x;SELECT * FROM x;",statements[0].getSqlStatement(database));
+    }
+    
+    @Test
+    public void defaultSplittingAndNoStripping() throws UnsupportedChangeException{
+        SQLFileChange change2 = new SQLFileChange();
+        change2.setSql("SELECT * FROM x;/*A Comment*/SELECT * FROM x;");
+
+        MSSQLDatabase database = new MSSQLDatabase();
+        SqlStatement[] statements = change2.generateStatements(database);
+        
+        assertEquals(2,statements.length);
+        assertEquals("SELECT * FROM x",statements[0].getSqlStatement(database));
+        assertEquals("/*A Comment*/SELECT * FROM x",statements[1].getSqlStatement(database));
+    }
+}
