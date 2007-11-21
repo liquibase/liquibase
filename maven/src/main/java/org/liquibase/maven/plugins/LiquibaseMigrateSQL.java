ae6421f00b83ebb7242f6e63f57c7d662c442c42
plm31 <plm31@e6edf6fb-f266-4316-afb4-e53d95876a76>
2007-11-21 18:41:54 +0000

    - Added a migrateSQL mojo and refactored the AbstractLiquibaseMojo to cater for configuring the Migrator before executing it. - Cleaned up Maven documentation so it can be used in the site documentation.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@476 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ maven/src/main/java/org/liquibase/maven/plugins/LiquibaseMigrateSQL.java
@@ -0,0 +1,66 @@
+// Version:   $Id: $
+// Copyright: Copyright(c) 2007 Trace Financial Limited
+package org.liquibase.maven.plugins;
+
+import java.io.*;
+import liquibase.exception.LiquibaseException;
+import liquibase.migrator.Migrator;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Creates an SQL migration script using the provided DatabaseChangeLog(s) comparing what
+ * already exists in the database to what is defined in the DataBaseChangeLog(s).
+ * @author Peter Murray
+ * @description Liquibase Migrate SQL Maven plugin
+ * @goal migrateSQL
+ */
+public class LiquibaseMigrateSQL extends AbstractLiquibaseMojo {
+
+  /**
+   * The file to output the Migration SQL script to, if it exists it will be overwritten.
+   * @parameter expression=${liquibase.migrationSqlOutputFile}
+   * @required
+   */
+  private File migrationSqlOutputFile;
+
+  /**
+   * Output a change log SQL for the DatabaseChangeLog table only, not the actual database
+   * changes that are required.
+   * @parameter expression=${liquibase.outputChangeLogSQLOnly} default-value="false"
+   */
+  private boolean changeLogSqlOnly;
+
+  @Override
+  protected void performMigratorConfiguration(Migrator migrator) throws MojoExecutionException {
+    Writer w = null;
+    try {
+      if (!migrationSqlOutputFile.exists()) {
+        // Ensure the parent directories exist
+        migrationSqlOutputFile.getParentFile().mkdirs();
+      }
+      w = new FileWriter(migrationSqlOutputFile);
+    }
+    catch (IOException e) {
+      getLog().error(e);
+      throw new MojoExecutionException("Failed to create SQL output writer", e);
+    }
+    
+    getLog().info("Output SQL Migration File: " + migrationSqlOutputFile.getAbsolutePath());
+    if (changeLogSqlOnly) {
+      getLog().info("Change Log only SQL");
+      migrator.setMode(Migrator.Mode.OUTPUT_CHANGELOG_ONLY_SQL_MODE);
+    } else {
+      migrator.setMode(Migrator.Mode.OUTPUT_SQL_MODE);
+    }
+    migrator.setOutputSQLWriter(w);
+  }
+
+  protected void performLiquibaseTask(Migrator migrator) throws MojoExecutionException {
+    try {
+      migrator.migrate();
+    }
+    catch (LiquibaseException e) {
+      throw new MojoExecutionException(e.getMessage(), e);
+    }
+  }
+}
