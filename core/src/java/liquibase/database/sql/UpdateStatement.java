59203d1a0bb4a79c2c9e0ac3683cfd9c8ada0b19
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-02-25 20:14:34 +0000

    Additional patch from Thilo Tanner to include column escaping for MySQL
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@589 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/database/sql/UpdateStatement.java
+++ core/src/java/liquibase/database/sql/UpdateStatement.java
@@ -79,7 +79,7 @@ public class UpdateStatement implements SqlStatement {
         if (newValue == null) {
             sqlString = "NULL";
         } else if (newValue instanceof String && database.shouldQuoteValue(((String) newValue))) {
-            sqlString = "'"+newValue+"'";
+            sqlString = "'" + newValue + "'";
         } else if (newValue instanceof Date) {
             sqlString = database.getDateLiteral(((Date) newValue));
         } else if (newValue instanceof Boolean) {
