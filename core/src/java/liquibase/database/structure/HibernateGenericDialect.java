b431ff5a9061ef58ce6085a303e20e3e48f5e6a2
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-03-24 19:09:49 +0000

    generify types from hibernate
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@601 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ core/src/java/liquibase/database/structure/HibernateGenericDialect.java
@@ -0,0 +1,38 @@
+package liquibase.database.structure;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.HibernateException;
+
+import java.sql.Types;
+
+public class HibernateGenericDialect extends Dialect {
+    private Dialect realDialect;
+
+    public HibernateGenericDialect(String dialectClass) throws Exception {
+        realDialect = (Dialect) Class.forName(dialectClass).newInstance();
+    }
+
+    public String getTypeName(int code, int length, int precision, int scale) throws HibernateException {
+        if (code == Types.BIGINT) {
+            return "bigint";
+        } else if (code == Types.BOOLEAN) {
+            return "boolean";
+        } else if (code == Types.BLOB) {
+            return "blob";
+        } else if (code == Types.CLOB) {
+            return "clob";
+        } else if (code == Types.DATE) {
+            return "date";
+        } else if (code == Types.FLOAT) {
+            return "float";
+        } else if (code == Types.TIME) {
+            return "time";
+        } else if (code == Types.TIMESTAMP) {
+            return "datetime";
+        } else if (code == Types.VARCHAR) {
+            return "varchar";
+        } else {
+            return realDialect.getTypeName(code, length, precision, scale);
+        }
+    }
+}
