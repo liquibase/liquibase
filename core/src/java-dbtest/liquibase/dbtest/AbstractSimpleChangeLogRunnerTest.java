f21c27b0c35183f3478e4dac6cc8b66530e58ac5
michael_tauer <michael_tauer@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-06-30 15:31:53 +0000

    Last changes for SQLite support.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@689 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java-dbtest/liquibase/dbtest/AbstractSimpleChangeLogRunnerTest.java
+++ core/src/java-dbtest/liquibase/dbtest/AbstractSimpleChangeLogRunnerTest.java
@@ -130,7 +130,6 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
         };
     }
 
-    /*
     public void testOutputChangeLog() throws Exception {
         if (database == null) {
             return;
@@ -145,9 +144,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
 
 //        System.out.println(output.getBuffer().toString());
     }
-    */
 
-    /*
     public void testRollbackableChangeLog() throws Exception {
         if (database == null) {
             return;
@@ -168,9 +165,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
         liquibase = createLiquibase(rollbackChangeLog);
         liquibase.rollback(new Date(0), this.contexts);
     }
-    */
 
-    /*
     public void testRollbackableChangeLogScriptOnExistingDatabase() throws Exception {
         if (database == null) {
             return;
@@ -189,9 +184,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
 
 //        System.out.println("Rollback SQL for "+driverName+StreamUtil.getLineSeparator()+StreamUtil.getLineSeparator()+writer.toString());
     }
-    */
 
-    /*
     public void testRollbackableChangeLogScriptOnFutureDatabase() throws Exception {
         if (database == null) {
             return;
@@ -207,9 +200,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
 
 //        System.out.println("Rollback SQL for future "+driverName+"\n\n"+writer.toString());
     }
-    */
 
-    /*
     public void testTag() throws Exception {
         if (database == null) {
             return;
@@ -223,9 +214,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
 
         liquibase.tag("Test Tag");
     }
-    */
 
-    /*
     public void testDiff() throws Exception {
         if (database == null) {
             return;
@@ -254,9 +243,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
         assertEquals(0, diffResult.getUnexpectedTables().size());
         assertEquals(0, diffResult.getUnexpectedViews().size());
     }
-    */
 
-    /*
     public void testRerunDiffChangeLog() throws Exception {
         if (database == null) {
             return;
@@ -331,9 +318,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
         assertEquals(0, emptyAgainSnapshot.getTables().size());
         assertEquals(0, emptyAgainSnapshot.getViews().size());
     }
-    */
 
-    /*
     public void testRerunDiffChangeLogAltSchema() throws Exception {
         if (database == null) {
             return;
@@ -403,9 +388,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
         assertEquals(0, finalDiffResult.getUnexpectedTables().size());
         assertEquals(0, finalDiffResult.getUnexpectedViews().size());
     }
-    */
 
-    /*
     public void testClearChecksums() throws Exception {
         if (database == null) {
             return;
@@ -422,9 +405,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
 
         liquibase.clearCheckSums();
     }
-    */
 
-    /*
     public void testTagEmptyDatabase() throws Exception {
         if (database == null) {
             return;
@@ -442,9 +423,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
         }
 
     }
-    */
 
-    /*
     public void testUnrunChangeSetsEmptyDatabase() throws Exception {
         if (database == null) {
             return;
@@ -459,9 +438,7 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
         assertTrue(list.size() > 0);
 
     }
-    */
 
-    /*
     public void testAbsolutePathChangeLog() throws Exception {
         if (database == null) {
             return;
@@ -487,7 +464,6 @@ public abstract class AbstractSimpleChangeLogRunnerTest extends TestCase {
 
         liquibase.dropAll(getSchemasToDrop());
     }
-    */
 
 
 //    public void testRerunChangeLogOnDifferentSchema() throws Exception {
