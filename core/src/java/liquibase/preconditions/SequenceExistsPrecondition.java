696207d08eafc562f90ff4381b2be77464b324e5
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-09-08 22:20:05 +0000

    bunch more preconditions
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@715 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ core/src/java/liquibase/preconditions/SequenceExistsPrecondition.java
@@ -0,0 +1,45 @@
+package liquibase.preconditions;
+
+import liquibase.database.Database;
+import liquibase.database.structure.DatabaseSnapshot;
+import liquibase.DatabaseChangeLog;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.exception.PreconditionErrorException;
+import liquibase.exception.JDBCException;
+
+public class SequenceExistsPrecondition implements Precondition {
+    private String schemaName;
+    private String sequenceName;
+
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    public void setSchemaName(String schemaName) {
+        this.schemaName = schemaName;
+    }
+
+    public String getSequenceName() {
+        return sequenceName;
+    }
+
+    public void setSequenceName(String sequenceName) {
+        this.sequenceName = sequenceName;
+    }
+
+    public void check(Database database, DatabaseChangeLog changeLog) throws PreconditionFailedException, PreconditionErrorException {
+        DatabaseSnapshot databaseSnapshot;
+        try {
+            databaseSnapshot = database.createDatabaseSnapshot(getSchemaName(), null);
+        } catch (JDBCException e) {
+            throw new PreconditionErrorException(e, changeLog, this);
+        }
+        if (databaseSnapshot.getSequence(getSequenceName()) == null) {
+            throw new PreconditionFailedException("Sequence "+database.escapeSequenceName(getSchemaName(), getSequenceName())+" does not exist", changeLog, this);
+        }
+    }
+
+    public String getTagName() {
+        return "sequenceExists";
+    }
+}
