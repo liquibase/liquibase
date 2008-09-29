7a72279daa68f6243a6efaa4755931d734b88805
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-09-29 17:32:09 +0000

    [ 2112165 ] MySQL: Tag command not working.  Added support for mysql 4
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@746 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/database/sql/TagDatabaseStatement.java
+++ core/src/java/liquibase/database/sql/TagDatabaseStatement.java
@@ -3,6 +3,7 @@ package liquibase.database.sql;
 import liquibase.database.Database;
 import liquibase.database.MySQLDatabase;
 import liquibase.exception.StatementNotSupportedOnDatabaseException;
+import liquibase.exception.JDBCException;
 
 public class TagDatabaseStatement implements SqlStatement {
 
@@ -20,6 +21,17 @@ public class TagDatabaseStatement implements SqlStatement {
         UpdateStatement statement = new UpdateStatement(database.getDefaultSchemaName(), database.getDatabaseChangeLogTableName());
         statement.addNewColumnValue("TAG", tag);
         if (database instanceof MySQLDatabase) {
+            long version = 5;
+            try {
+                version = Long.parseLong(database.getDatabaseProductVersion().substring(0,1));
+
+                if (version < 5) {
+                    return new RawSqlStatement("UPDATE DATABASECHANGELOG C LEFT JOIN (SELECT MAX(DATEEXECUTED) as MAXDATE FROM (SELECT DATEEXECUTED FROM`DATABASECHANGELOG`) AS X) D ON C.DATEEXECUTED = D.MAXDATE SET C.TAG = '"+tag+"'").getSqlStatement(database);
+                }
+
+            } catch (Throwable e) {
+                ; //assume it is version 5
+            }
             statement.setWhereClause("DATEEXECUTED = (SELECT MAX(DATEEXECUTED) FROM (SELECT DATEEXECUTED FROM " + database.escapeTableName(database.getDefaultSchemaName(), database.getDatabaseChangeLogTableName())+") AS X)");
         } else {
             statement.setWhereClause("DATEEXECUTED = (SELECT MAX(DATEEXECUTED) FROM " + database.escapeTableName(database.getDefaultSchemaName(), database.getDatabaseChangeLogTableName())+")");
