2955b0baeb12a19c98ca795101f906bccc533843
csuml <csuml@e6edf6fb-f266-4316-afb4-e53d95876a76>
2008-11-16 15:59:47 +0000

    Fixed Bug 2272005 - Changed the way the SwingUIFacade was loaded so that it is only done when its actual called which should stop the failure in a headless environment.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@768 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/UIFactory.java
+++ core/src/java/liquibase/UIFactory.java
@@ -1,16 +1,33 @@
 package liquibase;
 
-public class UIFactory {
+import java.lang.reflect.Constructor;
 
+public class UIFactory {
+	private static final String UI_IMPL_CLASSNAME="liquibase.SwingUIFacade";
     private static UIFactory instance = new UIFactory();
 
-    private UIFacade facade = new SwingUIFacade();
+    private UIFacade facade;// = new SwingUIFacade();
 
     public static UIFactory getInstance() {
         return instance;
     }
 
-    public UIFacade getFacade() {
+    @SuppressWarnings("unchecked")
+	public UIFacade getFacade() {
+    	if(facade==null) {
+    		System.out.println("XXX - Loaded UIFacade");
+    		ClassLoader cl = UIFacade.class.getClassLoader();
+    		try {
+				Class<UIFacade> swingUIClazz = (Class<UIFacade>)cl.loadClass(UI_IMPL_CLASSNAME);
+				Constructor<UIFacade> con = swingUIClazz.getConstructor(new Class[0]);
+				facade = con.newInstance(new Object[0]);
+				
+			} catch (Exception e) {
+				// Should never happen as class exists
+				throw new RuntimeException(e); 
+			}
+    	}
+    	
         return facade;
     }
 
