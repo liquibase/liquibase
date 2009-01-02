b81a58a32fcae2dac9df2a74d334cfbe01c848b3
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-01-02 20:14:10 +0000

    Feature request 2472812: rollback can be empty now for no rollback
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@784 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ core/src/java/liquibase/change/EmptyChange.java
@@ -0,0 +1,37 @@
+package liquibase.change;
+
+import liquibase.database.sql.SqlStatement;
+import liquibase.database.Database;
+import liquibase.database.structure.DatabaseObject;
+import liquibase.exception.UnsupportedChangeException;
+import liquibase.exception.InvalidChangeDefinitionException;
+import org.w3c.dom.Node;
+import org.w3c.dom.Document;
+
+import java.util.Set;
+
+public class EmptyChange extends AbstractChange {
+    public EmptyChange() {
+        super("empty", "empty");
+    }
+
+    public SqlStatement[] generateStatements(Database database) throws UnsupportedChangeException {
+        return new SqlStatement[0];
+    }
+
+    public String getConfirmationMessage() {
+        return "Empty change did nothing";
+    }
+
+    public Node createNode(Document currentChangeLogDOM) {
+        return currentChangeLogDOM.createElement("empty");
+    }
+
+    public Set<DatabaseObject> getAffectedDatabaseObjects() {
+        return null;
+    }
+
+    public void validate(Database database) throws InvalidChangeDefinitionException {
+        
+    }
+}
