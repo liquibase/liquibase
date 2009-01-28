0ce15fadf707e9343d77cccf3bb89c295c011a41
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-01-28 16:46:06 +0000

    Applied patch 2526097:  Support for length semantics on Oracle
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@822 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/database/structure/OracleDatabaseSnapshot.java
+++ core/src/java/liquibase/database/structure/OracleDatabaseSnapshot.java
@@ -4,6 +4,14 @@ import liquibase.database.Database;
 import liquibase.exception.JDBCException;
 import liquibase.diff.DiffStatusListener;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class OracleDatabaseSnapshot extends SqlDatabaseSnapshot {
@@ -25,4 +33,22 @@ public class OracleDatabaseSnapshot extends SqlDatabaseSnapshot {
     public OracleDatabaseSnapshot(Database database, Set<DiffStatusListener> statusListeners, String requestedSchema) throws JDBCException {
         super(database, statusListeners, requestedSchema);
     }
+    
+    /**
+     * Oracle specific implementation
+     */
+    protected void getColumnTypeAndDefValue(Column columnInfo, ResultSet rs, Database database) throws SQLException, JDBCException {
+      super.getColumnTypeAndDefValue(columnInfo, rs, database);
+
+        String columnTypeName = rs.getString("TYPE_NAME");
+        if("VARCHAR2".equals(columnTypeName)) {          
+          int charOctetLength = rs.getInt("CHAR_OCTET_LENGTH");
+          int columnSize = rs.getInt("COLUMN_SIZE");
+          if(columnSize == charOctetLength) {
+            columnInfo.setLengthSemantics(Column.LengthSemantics.BYTE);
+          }else {
+            columnInfo.setLengthSemantics(Column.LengthSemantics.CHAR);            
+          }
+        }
+    }    
 }
