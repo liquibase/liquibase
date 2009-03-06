b57e8d171c1dbea17832eb61c5c5e88647e3db12
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-03-06 19:58:38 +0000

    Only call reorg on DB2 databases >= 9
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@843 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/database/sql/ReorganizeTableStatement.java
+++ core/src/java/liquibase/database/sql/ReorganizeTableStatement.java
@@ -3,6 +3,7 @@ package liquibase.database.sql;
 import liquibase.database.DB2Database;
 import liquibase.database.Database;
 import liquibase.exception.StatementNotSupportedOnDatabaseException;
+import liquibase.exception.JDBCException;
 
 public class ReorganizeTableStatement implements SqlStatement {
     private String schemaName;
@@ -25,7 +26,15 @@ public class ReorganizeTableStatement implements SqlStatement {
         if (!supportsDatabase(database)) {
             throw new StatementNotSupportedOnDatabaseException("Cannot reorganize table", this, database);
         }
-        return "CALL SYSPROC.ADMIN_CMD ('REORG TABLE " + database.escapeTableName(getSchemaName(), getTableName()) + "')";
+        try {
+            if (database.getDatabaseMajorVersion() >= 9) {
+                return "CALL SYSPROC.ADMIN_CMD ('REORG TABLE " + database.escapeTableName(getSchemaName(), getTableName()) + "')";
+            } else {
+                return null;
+            }
+        } catch (JDBCException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getEndDelimiter(Database database) {
