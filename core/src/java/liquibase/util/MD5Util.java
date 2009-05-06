224e9f74d27f3a4aa3b45ec46d75e05cb3d2a5dc
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-05-06 17:57:45 +0000

    close CORE-158: Took md5 code from jakarta commons codec
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@888 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/util/MD5Util.java
+++ core/src/java/liquibase/util/MD5Util.java
@@ -1,5 +1,7 @@
 package liquibase.util;
 
+import liquibase.exception.UnexpectedLiquibaseException;
+
 import java.io.InputStream;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
@@ -8,21 +10,26 @@ import java.security.MessageDigest;
  * Generates md5-sums based on a string.
  */
 public class MD5Util {
+
+    /**
+     * Used to build output as Hex
+     */
+    private static final char[] DIGITS_LOWER = {
+        '0', '1', '2', '3', '4', '5', '6', '7',
+           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+
     public static String computeMD5(String input) {
         MessageDigest digest;
         try {
             digest = MessageDigest.getInstance("MD5");
             digest.update(input.getBytes("UTF-8"));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new UnexpectedLiquibaseException(e);
         }
         byte[] digestBytes = digest.digest();
 
-        StringBuffer hexString = new StringBuffer();
-        for (byte digestByte : digestBytes) {
-            hexString.append(Integer.toHexString(0xFF & digestByte));
-        }
-        return hexString.toString();
+        return new String(encodeHex(digestBytes));
 
     }
 
@@ -40,10 +47,46 @@ public class MD5Util {
         }
         byte[] digestBytes = digest.digest();
 
-        StringBuffer hexString = new StringBuffer();
-        for (byte digestByte : digestBytes) {
-            hexString.append(Integer.toHexString(0xFF & digestByte));
-        }
-        return hexString.toString();
+        return new String(encodeHex(digestBytes));
     }
+
+    /**
+     * Converts a hexadecimal character to an integer.
+     *
+     * @param ch A character to convert to an integer digit
+     * @param index The index of the character in the source
+     * @return An integer
+     */
+    private static int toDigit(char ch, int index) {
+        int digit = Character.digit(ch, 16);
+        if (digit == -1) {
+            throw new UnexpectedLiquibaseException("Illegal hexadecimal charcter " + ch + " at index " + index);
+        }
+        return digit;
+    }
+
+    /**
+     * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
+     * The returned array will be double the length of the passed array, as it takes two characters to represent any
+     * given byte.
+     *
+     * @param data
+     *            a byte[] to convert to Hex characters
+     * @return A char[] containing hexadecimal characters
+     */
+    private static char[] encodeHex(byte[] data) {
+
+        int l = data.length;
+
+        char[] out = new char[l << 1];
+
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = DIGITS_LOWER[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS_LOWER[0x0F & data[i]];
+        }
+
+        return out;
+    }
+
 }
