70c9246d0650c1be1ab19e62ad12c3d2a379c946
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-05-06 17:57:45 +0000

    close CORE-158: Took md5 code from jakarta commons codec
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@888 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java-test/liquibase/util/MD5UtilTest.java
+++ core/src/java-test/liquibase/util/MD5UtilTest.java
@@ -10,10 +10,16 @@ public class MD5UtilTest {
 	private static final String TEST_STRING = "foo";
 	private static final String TEST_STRING_MD5_HASH = "acbd18db4cc2f85cedef654fccc4a4d8";
 
+    private static final String TEST_STRING2 = "abc";
+    private static final String TEST_STRING2_MD5_HASH = "900150983cd24fb0d6963f7d28e17f72";
+
 	@Test
 	public void testComputeMD5() throws Exception {
 		String hash = MD5Util.computeMD5(TEST_STRING);
 		assertEquals(TEST_STRING_MD5_HASH, hash);
+
+        String hash2 = MD5Util.computeMD5(TEST_STRING2);
+        assertEquals(TEST_STRING2_MD5_HASH, hash2);
 	}
 
 	@Test
