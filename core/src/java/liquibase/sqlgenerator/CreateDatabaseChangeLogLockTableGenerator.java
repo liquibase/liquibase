d90887ef6a33a7e14accf541296e7bfd01206bae
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-05-08 16:51:40 +0000

    created statement and generator for create databasechangeloglock
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@896 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ core/src/java/liquibase/sqlgenerator/CreateDatabaseChangeLogLockTableGenerator.java
@@ -0,0 +1,32 @@
+package liquibase.sqlgenerator;
+
+import liquibase.statement.CreateDatabaseChangeLogLockTableStatement;
+import liquibase.statement.CreateTableStatement;
+import liquibase.statement.NotNullConstraint;
+import liquibase.database.Database;
+import liquibase.exception.ValidationErrors;
+import liquibase.sql.Sql;
+
+public class CreateDatabaseChangeLogLockTableGenerator implements SqlGenerator<CreateDatabaseChangeLogLockTableStatement> {
+    public int getPriority() {
+        return PRIORITY_DEFAULT;
+    }
+
+    public boolean supports(CreateDatabaseChangeLogLockTableStatement statement, Database database) {
+        return true;
+    }
+
+    public ValidationErrors validate(CreateDatabaseChangeLogLockTableStatement createDatabaseChangeLogLockTableStatement, Database database) {
+        return new ValidationErrors();
+    }
+
+    public Sql[] generateSql(CreateDatabaseChangeLogLockTableStatement statement, Database database) {
+        CreateTableStatement createTableStatement = new CreateTableStatement(database.getDefaultSchemaName(), database.getDatabaseChangeLogLockTableName())
+                .addPrimaryKeyColumn("ID", "INT", null, null, new NotNullConstraint())
+                .addColumn("LOCKED", "BOOLEAN", new NotNullConstraint())
+                .addColumn("LOCKGRANTED", "DATETIME")
+                .addColumn("LOCKEDBY", "VARCHAR(255)");
+
+        return SqlGeneratorFactory.getInstance().generateSql(createTableStatement, database);
+    }
+}
