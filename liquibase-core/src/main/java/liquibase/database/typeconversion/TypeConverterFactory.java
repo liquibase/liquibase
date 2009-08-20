4447c8b9975704462fb2fae75f9cb6737b06bfa4
damian.golda <damian.golda@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-08-20 11:20:47 +0000

    CORE-446: Fixing tests on maven * added specific converters
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1072 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/typeconversion/TypeConverterFactory.java
+++ liquibase-core/src/main/java/liquibase/database/typeconversion/TypeConverterFactory.java
@@ -1,7 +1,21 @@
 package liquibase.database.typeconversion;
 
 import liquibase.database.Database;
+import liquibase.database.core.DerbyDatabase;
+import liquibase.database.core.InformixDatabase;
+import liquibase.database.core.MySQLDatabase;
+import liquibase.database.core.OracleDatabase;
+import liquibase.database.core.PostgresDatabase;
+import liquibase.database.core.SybaseASADatabase;
+import liquibase.database.core.SybaseDatabase;
 import liquibase.database.typeconversion.core.DefaultTypeConverter;
+import liquibase.database.typeconversion.core.DerbyTypeConverter;
+import liquibase.database.typeconversion.core.InformixTypeConverter;
+import liquibase.database.typeconversion.core.MSSQLTypeConverter;
+import liquibase.database.typeconversion.core.OracleTypeConverter;
+import liquibase.database.typeconversion.core.PostgresTypeConverter;
+import liquibase.database.typeconversion.core.SybaseASATypeConverter;
+import liquibase.database.typeconversion.core.SybaseTypeConverter;
 
 public class TypeConverterFactory {
     private static TypeConverterFactory instance;
@@ -14,6 +28,27 @@ public class TypeConverterFactory {
     }
 
     public TypeConverter findTypeConverter(Database database) {
+        if(database instanceof OracleDatabase) {
+            return new OracleTypeConverter();
+        }
+        if(database instanceof InformixDatabase) {
+            return new InformixTypeConverter();
+        }
+        if(database instanceof PostgresDatabase) {
+            return new PostgresTypeConverter();
+        }
+        if(database instanceof MySQLDatabase) {
+            return new MSSQLTypeConverter();
+        }
+        if(database instanceof SybaseDatabase) {
+            return new SybaseTypeConverter();
+        }
+        if(database instanceof SybaseASADatabase) {
+            return new SybaseASATypeConverter();
+        }
+        if(database instanceof DerbyDatabase) {
+            return new DerbyTypeConverter();
+        }
         return new DefaultTypeConverter();
     }
 }
