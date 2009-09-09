6511271998fbaedd76f9009d0f5aeef0c96692f2
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-09 15:17:29 +0000

    pass parameters correctly
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1129 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/structure/type/CustomType.java
+++ liquibase-core/src/main/java/liquibase/database/structure/type/CustomType.java
@@ -3,6 +3,6 @@ package liquibase.database.structure.type;
 public class CustomType extends DataType {
 
     public CustomType(String dataTypeName, int minParams, int maxParams) {
-        super(dataTypeName, 0, 0);
+        super(dataTypeName, minParams, maxParams);
     }
 }
