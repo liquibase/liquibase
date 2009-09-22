648a2c7766afa6f5bd7c10587224ba334be2b3b9
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-22 19:07:39 +0000

    CORE-438 User friendly messages for failed preconditions
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1166 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXHandler.java
+++ liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXHandler.java
@@ -181,6 +181,8 @@ class XMLChangeLogSAXHandler extends DefaultHandler {
                 rootPrecondition = new PreconditionContainer();
                 rootPrecondition.setOnFail(StringUtils.trimToNull(atts.getValue("onFail")));
                 rootPrecondition.setOnError(StringUtils.trimToNull(atts.getValue("onError")));
+                rootPrecondition.setOnFailMessage(StringUtils.trimToNull(atts.getValue("onFailMessage")));
+                rootPrecondition.setOnErrorMessage(StringUtils.trimToNull(atts.getValue("onErrorMessage")));
                 preconditionLogicStack.push(rootPrecondition);
             } else if (currentPrecondition != null && currentPrecondition instanceof CustomPreconditionWrapper && qName.equals("param")) {
                 ((CustomPreconditionWrapper) currentPrecondition).setParam(atts.getValue("name"), atts.getValue("value"));
