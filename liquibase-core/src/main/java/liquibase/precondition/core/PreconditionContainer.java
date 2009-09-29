78979c411f47b60c252341fe2fcdebad501f1cf4
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-09-29 05:52:03 +0000

    CORE-161 updateSQL ignores preCondition added onSqlOutput attribute to preconditions tag
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1184 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/precondition/core/PreconditionContainer.java
+++ liquibase-core/src/main/java/liquibase/precondition/core/PreconditionContainer.java
@@ -5,6 +5,10 @@ import liquibase.database.Database;
 import liquibase.changelog.DatabaseChangeLog;
 import liquibase.exception.PreconditionFailedException;
 import liquibase.exception.PreconditionErrorException;
+import liquibase.exception.MigrationFailedException;
+import liquibase.exception.UnexpectedLiquibaseException;
+import liquibase.executor.Executor;
+import liquibase.executor.ExecutorService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,8 +51,26 @@ public class PreconditionContainer extends AndPrecondition {
         }
     }
 
+
+    public enum OnSqlOutputOption {
+        IGNORE("IGNORE"),
+        TEST("TEST"),
+        FAIL("FAIL");
+
+        String key;
+
+        OnSqlOutputOption(String key) {
+            this.key = key;
+        }
+
+        public String toString() {
+            return key;
+        }
+    }
+
     private FailOption onFail = FailOption.HALT;
     private ErrorOption onError = ErrorOption.HALT;
+    private OnSqlOutputOption onSqlOutput = OnSqlOutputOption.IGNORE;
     private String onFailMessage;
     private String onErrorMessage;
 
@@ -96,6 +118,37 @@ public class PreconditionContainer extends AndPrecondition {
         }
     }
 
+    public OnSqlOutputOption getOnSqlOutput() {
+        return onSqlOutput;
+    }
+
+    public void setOnSqlOutput(String onSqlOutput) {
+        if (onSqlOutput == null) {
+            setOnSqlOutput((OnSqlOutputOption)null);
+            return;
+        }
+        
+        for (OnSqlOutputOption option : OnSqlOutputOption.values()) {
+            if (option.key.equalsIgnoreCase(onSqlOutput)) {
+                setOnSqlOutput(option);
+                return;
+            }
+        }
+        List<String> possibleOptions = new ArrayList<String>();
+        for (OnSqlOutputOption option : OnSqlOutputOption.values()) {
+            possibleOptions.add(option.key);
+        }
+        throw new RuntimeException("Unknown onSqlOutput attribute value '" + onSqlOutput + "'.  Possible values: " + StringUtils.join(possibleOptions, ", "));
+    }
+
+    public void setOnSqlOutput(OnSqlOutputOption onSqlOutput) {
+        if (onSqlOutput == null) {
+            this.onSqlOutput = OnSqlOutputOption.IGNORE;
+        } else {
+            this.onSqlOutput = onSqlOutput;
+        }
+    }
+
     public String getOnFailMessage() {
         return onFailMessage;
     }
@@ -114,8 +167,28 @@ public class PreconditionContainer extends AndPrecondition {
 
     @Override
     public void check(Database database, DatabaseChangeLog changeLog) throws PreconditionFailedException, PreconditionErrorException {
+        Executor executor = ExecutorService.getInstance().getExecutor(database);
         try {
-            super.check(database, changeLog);
+            // Three cases for preConditions onUpdateSQL:
+            // 1. TEST: preConditions should be run, as in regular update mode
+            // 2. FAIL: the preConditions should fail if there are any
+            // 3. IGNORE: act as if preConditions don't exist
+            boolean testPrecondition = false;
+                if (executor.updatesDatabase()) {
+                    testPrecondition = true;
+                } else {
+                    if (this.getOnSqlOutput().equals(PreconditionContainer.OnSqlOutputOption.TEST)) {
+                        testPrecondition = true;
+                    } else if (this.getOnSqlOutput().equals(PreconditionContainer.OnSqlOutputOption.FAIL)) {
+                        throw new PreconditionFailedException("Unexpected precondition in updateSQL mode with onUpdateSQL value: "+this.getOnSqlOutput(), changeLog, this);
+                    } else if (this.getOnSqlOutput().equals(PreconditionContainer.OnSqlOutputOption.IGNORE)) {
+                        testPrecondition = false;
+                    }
+                }
+
+            if (testPrecondition) {
+                super.check(database, changeLog);
+            }
         } catch (PreconditionFailedException e) {
             if (getOnFailMessage() == null) {
                 throw e;
--- liquibase-integration-tests/src/test/java/liquibase/dbtest/h2/H2IntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/h2/H2IntegrationTest.java
@@ -7,4 +7,9 @@ public class H2IntegrationTest extends AbstractIntegrationTest {
     public H2IntegrationTest() throws Exception {
         super("h2", "jdbc:h2:mem:liquibase");
     }
+
+    @Override
+    public void testOutputChangeLog() throws Exception {
+        super.testOutputChangeLog();    //To change body of overridden methods use File | Settings | File Templates.
+    }
 }
