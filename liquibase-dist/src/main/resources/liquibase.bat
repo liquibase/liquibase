edfdfc76f86e73f0aa1c51dbc4ee65c5e4da51db
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-05 04:50:04 +0000

    CORE-350 Lib-Detection in Windows-Batch-Script grabs only last jar
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1202 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-dist/src/main/resources/liquibase.bat
+++ liquibase-dist/src/main/resources/liquibase.bat
@@ -1,12 +1,14 @@
 @echo off
 if "%OS%" == "Windows_NT" setlocal
 
+setlocal enabledelayedexpansion
+
 rem %~dp0 is expanded pathname of the current script under NT
 set LIQUIBASE_HOME="%~dp0"
 
 set CP=.
-for /R %LIQUIBASE_HOME% %%f in (liquibase*.jar) do set CP=%CP%;%%f
-for /R %LIQUIBASE_HOME%\lib %%f in (*.jar) do set CP=%CP%;%%f
+for /R %LIQUIBASE_HOME% %%f in (liquibase*.jar) do set CP=!CP!;%%f
+for /R %LIQUIBASE_HOME%\lib %%f in (*.jar) do set CP=!CP!;%%f
 
 rem get command line args into a variable
 set CMD_LINE_ARGS=%1
