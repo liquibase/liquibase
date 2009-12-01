8f76c6c0f14daacfe7e8bab2d96a0395592bfbe3
tbassett22030 <tbassett22030@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-12-01 13:53:03 +0000

    CORE-524 Add logic to incorporate the schema name for the rolling back of a view
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1253 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-integration-tests/src/test/java/liquibase/dbtest/oracle/OracleIntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/oracle/OracleIntegrationTest.java
@@ -18,10 +18,12 @@ import java.util.Date;
 public class OracleIntegrationTest extends AbstractIntegrationTest {
 
     private String indexOnSchemaChangeLog;
+    private String viewOnSchemaChangeLog;
 
     public OracleIntegrationTest() throws Exception {
         super("oracle", "jdbc:oracle:thin:@" + getDatabaseServerHostname() + "/XE");
         this.indexOnSchemaChangeLog = "changelogs/oracle/complete/indexOnSchema.xml";
+        this.viewOnSchemaChangeLog = "changelogs/oracle/complete/viewOnSchema.xml";
     }
 
     @Test
@@ -63,4 +65,38 @@ public class OracleIntegrationTest extends AbstractIntegrationTest {
 
     }
 
+    @Test
+    public void viewCreatedOnCorrectSchema() throws Exception {
+         if (this.getDatabase() == null) {
+            return;
+        }
+
+        Liquibase liquibase = createLiquibase(this.viewOnSchemaChangeLog);
+        clearDatabase(liquibase);
+
+        try {
+            liquibase.update(this.contexts);
+        } catch (ValidationFailedException e) {
+            e.printDescriptiveError(System.out);
+            throw e;
+        }
+
+        Statement queryIndex = ((JdbcConnection) this.getDatabase().getConnection()).getUnderlyingConnection().createStatement();
+
+        ResultSet indexOwner = queryIndex.executeQuery("SELECT owner FROM ALL_VIEWS WHERE view_name = 'V_BOOK2'");
+
+        assertTrue(indexOwner.next());
+
+        String owner = indexOwner.getString("owner");
+
+        assertEquals("LIQUIBASEB",owner);
+
+        // check that the automatically rollback now works too
+        try {
+            liquibase.rollback( new Date(0),this.contexts);
+        } catch (ValidationFailedException e) {
+            e.printDescriptiveError(System.out);
+            throw e;
+        }
+    }
 }
