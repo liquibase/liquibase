c807773244a28361c3c69f0c436bc604eefdf153
tbassett22030 <tbassett22030@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-12-11 01:00:50 +0000

    CORE-528 - added support for MSSQL
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1259 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGenerator.java
@@ -1,12 +1,140 @@
 package liquibase.sqlgenerator.core;
 
+import liquibase.database.Database;
+import liquibase.database.typeconversion.TypeConverterFactory;
+import liquibase.exception.ValidationErrors;
 import liquibase.sqlgenerator.SqlGenerator;
+import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.statement.core.InsertOrUpdateStatement;
+import liquibase.statement.core.UpdateStatement;
+import liquibase.sql.Sql;
+import liquibase.sql.UnparsedSql;
 
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bassettt
+ * Date: Dec 2, 2009
+ * Time: 12:15:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
 public abstract class InsertOrUpdateGenerator implements SqlGenerator<InsertOrUpdateStatement> {
 
-    public int getPriority() {
-        return PRIORITY_DATABASE;
+    protected abstract String getRecordCheck(InsertOrUpdateStatement insertOrUpdateStatement, Database database, String whereClause);
+
+    protected abstract String getElse(Database database);
+
+    protected String getPostUpdateStatements(){
+        return "";
     }
 
+    public int getPriority() {
+        return PRIORITY_DATABASE;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public ValidationErrors validate(InsertOrUpdateStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        ValidationErrors validationErrors = new ValidationErrors();
+        validationErrors.checkRequiredField("tableName", statement.getTableName());
+        validationErrors.checkRequiredField("columns", statement.getColumnValues());
+        validationErrors.checkRequiredField("primaryKey", statement.getPrimaryKey());
+
+        return validationErrors;
+    }
+
+    protected String getWhereClause(InsertOrUpdateStatement insertOrUpdateStatement, Database database) {
+        StringBuffer where = new StringBuffer();
+
+        String[] pkColumns = insertOrUpdateStatement.getPrimaryKey().split(",");
+
+        for(String thisPkColumn:pkColumns)
+        {
+            where.append(database.escapeColumnName(insertOrUpdateStatement.getSchemaName(), insertOrUpdateStatement.getTableName(), thisPkColumn)  + " = " );
+            Object newValue = insertOrUpdateStatement.getColumnValues().get(thisPkColumn);
+            if (newValue == null || newValue.toString().equals("NULL")) {
+                where.append("NULL");
+            } else if (newValue instanceof String && database.shouldQuoteValue(((String) newValue))) {
+                where.append("'").append(database.escapeStringForDatabase((String) newValue)).append("'");
+            } else if (newValue instanceof Date) {
+                where.append(database.getDateLiteral(((Date) newValue)));
+            } else if (newValue instanceof Boolean) {
+                if (((Boolean) newValue)) {
+                    where.append(TypeConverterFactory.getInstance().findTypeConverter(database).getBooleanType().getTrueBooleanValue());
+                } else {
+                    where.append(TypeConverterFactory.getInstance().findTypeConverter(database).getBooleanType().getFalseBooleanValue());
+                }
+            } else {
+                where.append(newValue);
+            }
+
+            where.append(" AND ");
+        }
+
+        where.delete(where.lastIndexOf(" AND "),where.lastIndexOf(" AND ") + " AND ".length());
+        return where.toString();
+    }
+
+    protected String getInsertStatement(InsertOrUpdateStatement insertOrUpdateStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        StringBuffer insertBuffer = new StringBuffer();
+        InsertGenerator insert = new InsertGenerator();
+        Sql[] insertSql = insert.generateSql(insertOrUpdateStatement,database,sqlGeneratorChain);
+
+        for(Sql s:insertSql)
+        {
+            insertBuffer.append(s.toSql());
+            insertBuffer.append(";");
+        }
+
+        insertBuffer.append("\n");
+
+        return insertBuffer.toString();
+    }
+
+    protected String getUpdateStatement(InsertOrUpdateStatement insertOrUpdateStatement,Database database, String whereClause, SqlGeneratorChain sqlGeneratorChain){
+
+        StringBuffer updateSqlString = new StringBuffer();
+
+        UpdateGenerator update = new UpdateGenerator();
+        UpdateStatement updateStatement = new UpdateStatement(insertOrUpdateStatement.getSchemaName(),insertOrUpdateStatement.getTableName());
+        updateStatement.setWhereClause(whereClause + ";\n");
+
+
+        for(String columnKey:insertOrUpdateStatement.getColumnValues().keySet())
+        {
+            updateStatement.addNewColumnValue(columnKey,insertOrUpdateStatement.getColumnValue(columnKey));
+        }
+
+        Sql[] updateSql = update.generateSql(updateStatement, database, sqlGeneratorChain);
+
+        for(Sql s:updateSql)
+        {
+            updateSqlString.append(s.toSql());
+            updateSqlString.append(";");
+        }
+
+        updateSqlString.deleteCharAt(updateSqlString.lastIndexOf(";"));
+        updateSqlString.append("\n");
+
+        return updateSqlString.toString();
+
+    }
+
+    public Sql[] generateSql(InsertOrUpdateStatement insertOrUpdateStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        StringBuffer completeSql = new StringBuffer();
+        String whereClause = getWhereClause(insertOrUpdateStatement, database);
+
+        completeSql.append( getRecordCheck(insertOrUpdateStatement, database, whereClause));
+
+        completeSql.append(getInsertStatement(insertOrUpdateStatement, database, sqlGeneratorChain));
+
+        completeSql.append(getElse(database));
+
+        completeSql.append(getUpdateStatement(insertOrUpdateStatement,database,whereClause,sqlGeneratorChain));
+
+        completeSql.append(getPostUpdateStatements());
+
+        return new Sql[]{
+                new UnparsedSql(completeSql.toString())
+        };
+    }
 }
--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGeneratorOracle.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGeneratorOracle.java
@@ -13,104 +13,38 @@ import liquibase.statement.core.UpdateStatement;
 import java.util.Date;
 
 public class InsertOrUpdateGeneratorOracle extends InsertOrUpdateGenerator {
+
+
     public boolean supports(InsertOrUpdateStatement statement, Database database) {
         return database instanceof OracleDatabase;
     }
 
-    public ValidationErrors validate(InsertOrUpdateStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        ValidationErrors validationErrors = new ValidationErrors();
-        validationErrors.checkRequiredField("tableName", statement.getTableName());
-        validationErrors.checkRequiredField("columns", statement.getColumnValues());
-        validationErrors.checkRequiredField("primaryKey", statement.getPrimaryKey());
+    protected String getRecordCheck(InsertOrUpdateStatement insertOrUpdateStatement, Database database, String whereClause) {
 
-        return validationErrors;
+        StringBuffer recordCheckSql = new StringBuffer();
+
+        recordCheckSql.append("DECLARE\n");
+        recordCheckSql.append("\tv_reccount NUMBER := 0;\n");
+        recordCheckSql.append("BEGIN\n");
+        recordCheckSql.append("\tSELECT COUNT(*) INTO v_reccount FROM " + database.escapeTableName(insertOrUpdateStatement.getSchemaName(), insertOrUpdateStatement.getTableName()) + " WHERE ");
+
+        recordCheckSql.append(whereClause);
+        recordCheckSql.append(";\n");
+
+        recordCheckSql.append("\tIF v_reccount = 0 THEN\n");
+
+        return recordCheckSql.toString();
     }
 
-    public Sql[] generateSql(InsertOrUpdateStatement insertOrUpdateStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        StringBuffer completeSql = new StringBuffer();
-        String whereClause = getWhereClause(insertOrUpdateStatement, database);
-
-        completeSql.append("DECLARE\n");
-        completeSql.append("\tv_reccount NUMBER := 0;\n");
-        completeSql.append("BEGIN\n");
-        completeSql.append("\tSELECT COUNT(*) INTO v_reccount FROM " + database.escapeTableName(insertOrUpdateStatement.getSchemaName(), insertOrUpdateStatement.getTableName()) + " WHERE ");
-
-        completeSql.append(whereClause);
-        completeSql.append(";\n");
-
-        completeSql.append("\tIF v_reccount = 0 THEN\n");
-
-        InsertGenerator insert = new InsertGenerator();
-        Sql[] insertSql = insert.generateSql(insertOrUpdateStatement,database,sqlGeneratorChain);
-
-        for(Sql s:insertSql)
-        {
-            completeSql.append(s.toSql());
-            completeSql.append(";");
-        }
-
-        completeSql.append("\n");
-
-        completeSql.append("\tELSIF v_reccount = 1 THEN\n");
-
-        UpdateGenerator update = new UpdateGenerator();
-        UpdateStatement updateStatement = new UpdateStatement(insertOrUpdateStatement.getSchemaName(),insertOrUpdateStatement.getTableName());
-        updateStatement.setWhereClause(whereClause + ";\n");
-
-
-        for(String columnKey:insertOrUpdateStatement.getColumnValues().keySet())
-        {
-            updateStatement.addNewColumnValue(columnKey,insertOrUpdateStatement.getColumnValue(columnKey));
-        }
-
-        Sql[] updateSql = update.generateSql(updateStatement, database, sqlGeneratorChain);
-
-        for(Sql s:updateSql)
-        {
-            completeSql.append(s.toSql());
-            completeSql.append(";");
-        }
-
-        completeSql.deleteCharAt(completeSql.lastIndexOf(";"));
-        completeSql.append("\n");
-
-        completeSql.append("END IF;\n");
-        completeSql.append("END;\n");
-
-        return new Sql[]{
-                new UnparsedSql(completeSql.toString())
-        };
+    protected String getElse(Database database){
+               return "\tELSIF v_reccount = 1 THEN\n";
     }
 
-    private String getWhereClause(InsertOrUpdateStatement insertOrUpdateStatement, Database database) {
-        StringBuffer where = new StringBuffer();
+    protected String getPostUpdateStatements(){
+        StringBuffer endStatements = new StringBuffer();
+        endStatements.append("END IF;\n");
+        endStatements.append("END;\n");
+        return endStatements.toString();
 
-        String[] pkColumns = insertOrUpdateStatement.getPrimaryKey().split(",");
-
-        for(String thisPkColumn:pkColumns)
-        {
-            where.append(database.escapeColumnName(insertOrUpdateStatement.getSchemaName(), insertOrUpdateStatement.getTableName(), thisPkColumn)  + " = " );
-            Object newValue = insertOrUpdateStatement.getColumnValues().get(thisPkColumn);
-            if (newValue == null || newValue.toString().equals("NULL")) {
-                where.append("NULL");
-            } else if (newValue instanceof String && database.shouldQuoteValue(((String) newValue))) {
-                where.append("'").append(database.escapeStringForDatabase((String) newValue)).append("'");
-            } else if (newValue instanceof Date) {
-                where.append(database.getDateLiteral(((Date) newValue)));
-            } else if (newValue instanceof Boolean) {
-                if (((Boolean) newValue)) {
-                    where.append(TypeConverterFactory.getInstance().findTypeConverter(database).getBooleanType().getTrueBooleanValue());
-                } else {
-                    where.append(TypeConverterFactory.getInstance().findTypeConverter(database).getBooleanType().getFalseBooleanValue());
-                }
-            } else {
-                where.append(newValue);
-            }
-
-            where.append(" AND ");
-        }
-
-        where.delete(where.lastIndexOf(" AND "),where.lastIndexOf(" AND ") + " AND ".length());
-        return where.toString();
     }
 }
