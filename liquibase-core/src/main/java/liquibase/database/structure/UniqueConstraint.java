c37185f21bf7c5b0a4138ac53c8543f0a68c5852
nick <nick@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-02-02 10:37:02 +0000

    Added null checks to equals and compareTo as Postgres metadata gives null table names on occasion.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1282 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/structure/UniqueConstraint.java
+++ liquibase-core/src/main/java/liquibase/database/structure/UniqueConstraint.java
@@ -1,122 +1,148 @@
 package liquibase.database.structure;
 
-import liquibase.util.StringUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class UniqueConstraint implements DatabaseObject, Comparable<UniqueConstraint> {
-    private String name;
-    private Table table;
-    private List<String> columns = new ArrayList<String>();
+import liquibase.util.StringUtils;
 
-    private boolean deferrable;
-    private boolean initiallyDeferred;
-    private boolean disabled;
+public class UniqueConstraint implements DatabaseObject,
+		Comparable<UniqueConstraint> {
+	private String name;
+	private Table table;
+	private List<String> columns = new ArrayList<String>();
 
-    public DatabaseObject[] getContainingObjects() {
-        List<DatabaseObject> columns = new ArrayList<DatabaseObject>();
-        for (String column : this.columns) {
-            columns.add(new Column().setName(column).setTable(table));
-        }
+	private boolean deferrable;
+	private boolean initiallyDeferred;
+	private boolean disabled;
 
-        return columns.toArray(new DatabaseObject[columns.size()]);
-    }
+	public DatabaseObject[] getContainingObjects() {
+		List<DatabaseObject> columns = new ArrayList<DatabaseObject>();
+		for (String column : this.columns) {
+			columns.add(new Column().setName(column).setTable(table));
+		}
 
-    public String getName() {
-        return name;
-    }
+		return columns.toArray(new DatabaseObject[columns.size()]);
+	}
 
-    public void setName(String constraintName) {
-        this.name = constraintName;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Table getTable() {
-        return table;
-    }
+	public void setName(String constraintName) {
+		this.name = constraintName;
+	}
 
-    public void setTable(Table table) {
-        this.table = table;
-    }
+	public Table getTable() {
+		return table;
+	}
 
-    public List<String> getColumns() {
-        return columns;
-    }
+	public void setTable(Table table) {
+		this.table = table;
+	}
 
-    public boolean isDeferrable() {
-        return deferrable;
-    }
+	public List<String> getColumns() {
+		return columns;
+	}
 
-    public void setDeferrable(boolean deferrable) {
-        this.deferrable = deferrable;
-    }
+	public boolean isDeferrable() {
+		return deferrable;
+	}
 
-    public boolean isInitiallyDeferred() {
-        return initiallyDeferred;
-    }
+	public void setDeferrable(boolean deferrable) {
+		this.deferrable = deferrable;
+	}
 
-    public void setInitiallyDeferred(boolean initiallyDeferred) {
-        this.initiallyDeferred = initiallyDeferred;
-    }
+	public boolean isInitiallyDeferred() {
+		return initiallyDeferred;
+	}
 
-    public String getColumnNames() {
-        return StringUtils.join(columns, ", ");
-    }
+	public void setInitiallyDeferred(boolean initiallyDeferred) {
+		this.initiallyDeferred = initiallyDeferred;
+	}
 
-    public void setDisabled(boolean disabled) {
-        this.disabled = disabled;
-    }
+	public String getColumnNames() {
+		return StringUtils.join(columns, ", ");
+	}
 
-    public boolean isDisabled() {
-        return disabled;
-    }
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
+	public boolean isDisabled() {
+		return disabled;
+	}
 
-        UniqueConstraint that = (UniqueConstraint) o;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		if (null == this.getColumnNames())
+			return false;
+		UniqueConstraint that = (UniqueConstraint) o;
+		boolean result = false;
+		result = !(getColumnNames() != null ? !getColumnNames()
+				.equalsIgnoreCase(that.getColumnNames()) : that
+				.getColumnNames() != null)
+				&& isDeferrable() == that.isDeferrable()
+				&& isInitiallyDeferred() == that.isInitiallyDeferred()
+				&& isDisabled() == that.isDisabled();
+		// Need check for nulls here due to NullPointerException using
+		// Postgres
+		if (result) {
+			if (null == this.getTable()) {
+				result = null == that.getTable();
+			} else if (null == this.getTable()) {
+				result = false;
+			} else {
+				result = this.getTable().getName().equals(
+						that.getTable().getName());
+			}
+		}
 
-        return !(getColumnNames() != null ? !getColumnNames().equalsIgnoreCase(that.getColumnNames()) : that.getColumnNames() != null)
-                && !(getTable().getName() != null ? !getTable().getName().equalsIgnoreCase(that.getTable().getName()) : that.getTable().getName() != null)
-                && isDeferrable() == that.isDeferrable() && isInitiallyDeferred() == that.isInitiallyDeferred() && isDisabled() == that.isDisabled();
+		return result;
 
-    }
+	}
 
-    /**
-     * @see java.lang.Comparable#compareTo(java.lang.Object)
-     */
-    public int compareTo(UniqueConstraint o) {
-        int returnValue = this.getTable().getName().compareTo(o.getTable().getName());
-        if (returnValue == 0) {
-            returnValue = this.getName().compareTo(o.getName());
-        }
-        if (returnValue == 0) {
-            returnValue = this.getColumnNames().compareTo(o.getColumnNames());
-        }
-        return returnValue;
-    }
+	/**
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	public int compareTo(UniqueConstraint o) {
+		// Need check for nulls here due to NullPointerException using Postgres
+		String thisTableName;
+		String thatTableName;
+		thisTableName = null == this.getTable() ? "" : this.getTable()
+				.getName();
+		thatTableName = null == o.getTable() ? "" : o.getTable().getName();
+		int returnValue = thisTableName.compareTo(thatTableName);
+		if (returnValue == 0) {
+			returnValue = this.getName().compareTo(o.getName());
+		}
+		if (returnValue == 0) {
+			returnValue = this.getColumnNames().compareTo(o.getColumnNames());
+		}
+		return returnValue;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = 0;
-        if (this.table != null) {
-            result = this.table.hashCode();
-        }
-        if (this.name != null) {
-            result = 31 * result + this.name.toUpperCase().hashCode();
-        }
-        if (getColumnNames() != null) {
-            result = 31 * result + getColumnNames().hashCode();
-        }
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = 0;
+		if (this.table != null) {
+			result = this.table.hashCode();
+		}
+		if (this.name != null) {
+			result = 31 * result + this.name.toUpperCase().hashCode();
+		}
+		if (getColumnNames() != null) {
+			result = 31 * result + getColumnNames().hashCode();
+		}
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return getName() + " on " + getTable().getName() + "(" + getColumnNames() + ")";
-    }
+	@Override
+	public String toString() {
+		return getName() + " on " + getTable().getName() + "("
+				+ getColumnNames() + ")";
+	}
 }
