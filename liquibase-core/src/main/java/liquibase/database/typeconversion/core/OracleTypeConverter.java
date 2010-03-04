b8467de979081aad29bf051bcf50b980164fc1b5
gadget <gadget@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-04 10:18:21 +0000

    Fix: Setting default value for columns
    There are two equals ways to set up columns default value in oracle.
    1. DEFAULT (0)
    2. DEFAULT 0
    
    In both cases it will work as insertion '0' number-type value into cell.
    But in [1] Liquibase recognizes '(0)' as VARCHAR2-type value, and then it wrapped it by [ ' ].
    
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1297 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/typeconversion/core/OracleTypeConverter.java
+++ liquibase-core/src/main/java/liquibase/database/typeconversion/core/OracleTypeConverter.java
@@ -9,79 +9,99 @@ import java.sql.Types;
 
 public class OracleTypeConverter extends AbstractTypeConverter {
 
-    public int getPriority() {
-        return PRIORITY_DATABASE;
-    }
+	public int getPriority() {
+		return PRIORITY_DATABASE;
+	}
 
-    public boolean supports(Database database) {
-        return database instanceof OracleDatabase;
-    }
+	public boolean supports(Database database) {
+		return database instanceof OracleDatabase;
+	}
 
 
-    @Override
-    public Object convertDatabaseValueToObject(Object defaultValue, int dataType, int columnSize, int decimalDigits, Database database) throws ParseException {
-        if (defaultValue != null) {
-            if (defaultValue instanceof String) {
-                if (dataType == Types.DATE || dataType == Types.TIME || dataType == Types.TIMESTAMP) {
-                    if (((String) defaultValue).indexOf("YYYY-MM-DD HH") > 0) {
-                        defaultValue = ((String) defaultValue).replaceFirst("^to_date\\('","").replaceFirst("', 'YYYY-MM-DD HH24:MI:SS'\\)$","");
-                    } else if (((String) defaultValue).indexOf("YYYY-MM-DD") > 0) {
-                        defaultValue = ((String) defaultValue).replaceFirst("^to_date\\('","").replaceFirst("', 'YYYY-MM-DD'\\)$","");
-                    } else {
-                        defaultValue = ((String) defaultValue).replaceFirst("^to_date\\('","").replaceFirst("', 'HH24:MI:SS'\\)$","");
-                    }
-                }
-                defaultValue = ((String) defaultValue).replaceFirst("'\\s*$", "'"); //sometimes oracle adds an extra space after the trailing ' (see http://sourceforge.net/tracker/index.php?func=detail&aid=1824663&group_id=187970&atid=923443).
-            }
-        }
-        return super.convertDatabaseValueToObject(defaultValue, dataType, columnSize, decimalDigits, database);
-    }
+	@Override
+	public Object convertDatabaseValueToObject(Object defaultValue, int dataType, int columnSize, int decimalDigits, Database database) throws ParseException {
+		if (defaultValue != null) {
+			if (defaultValue instanceof String) {
+				if (dataType == Types.DATE || dataType == Types.TIME || dataType == Types.TIMESTAMP) {
+					if (((String) defaultValue).indexOf("YYYY-MM-DD HH") > 0) {
+						defaultValue = ((String) defaultValue).replaceFirst("^to_date\\('", "").replaceFirst("', 'YYYY-MM-DD HH24:MI:SS'\\)$", "");
+					} else if (((String) defaultValue).indexOf("YYYY-MM-DD") > 0) {
+						defaultValue = ((String) defaultValue).replaceFirst("^to_date\\('", "").replaceFirst("', 'YYYY-MM-DD'\\)$", "");
+					} else {
+						defaultValue = ((String) defaultValue).replaceFirst("^to_date\\('", "").replaceFirst("', 'HH24:MI:SS'\\)$", "");
+					}
+				} else if (
+						dataType == Types.BIGINT ||
+						dataType == Types.NUMERIC ||
+						dataType == Types.BIT ||
+						dataType == Types.SMALLINT ||
+						dataType == Types.DECIMAL ||
+						dataType == Types.INTEGER ||
+						dataType == Types.TINYINT ||
+						dataType == Types.FLOAT ||
+						dataType == Types.REAL
+						) {
+					/*
+					* if dataType is numeric-type then cut "(" , ")" symbols
+					* Cause: Column's default value option may be set by both ways:
+					* DEFAULT 0
+					* DEFAULT (0)
+					* */
+					defaultValue = ((String) defaultValue).replaceFirst("\\(", "").replaceFirst("\\)", "");
+				}
+				defaultValue = ((String) defaultValue).replaceFirst("'\\s*$", "'"); //sometimes oracle adds an extra space after the trailing ' (see http://sourceforge.net/tracker/index.php?func=detail&aid=1824663&group_id=187970&atid=923443).
+			}
+		}
+		return super.convertDatabaseValueToObject(defaultValue, dataType, columnSize, decimalDigits, database);
+	}
 
-    @Override
-    public BooleanType getBooleanType() {
-        return new BooleanType.NumericBooleanType("NUMBER(1)");
-    }
+	@Override
+	public BooleanType getBooleanType() {
+		return new BooleanType.NumericBooleanType("NUMBER(1)");
+	}
 
-    @Override
-    public CurrencyType getCurrencyType() {
-        return new CurrencyType("NUMBER(15, 2)");
-    }
+	@Override
+	public CurrencyType getCurrencyType() {
+		return new CurrencyType("NUMBER(15, 2)");
+	}
 
-    @Override
-    public UUIDType getUUIDType() {
-        return new UUIDType("RAW(16)");
-    }
-    @Override
-    public TimeType getTimeType() {
-        return new TimeType("DATE");
-    }
-    @Override
-    public DateTimeType getDateTimeType() {
-        return new DateTimeType("TIMESTAMP");
-    }
+	@Override
+	public UUIDType getUUIDType() {
+		return new UUIDType("RAW(16)");
+	}
 
-    @Override
-    public BigIntType getBigIntType() {
-        return new BigIntType("NUMBER(38,0)");
-    }
+	@Override
+	public TimeType getTimeType() {
+		return new TimeType("DATE");
+	}
 
-    @Override
-    public IntType getIntType() {
-        return new IntType("NUMBER(10,0)");
-    }
+	@Override
+	public DateTimeType getDateTimeType() {
+		return new DateTimeType("TIMESTAMP");
+	}
 
-    @Override
-    public VarcharType getVarcharType() {
-        return new VarcharType("VARCHAR2");
-    }
+	@Override
+	public BigIntType getBigIntType() {
+		return new BigIntType("NUMBER(38,0)");
+	}
 
-    @Override
-    public DoubleType getDoubleType() {
-        return new DoubleType("FLOAT(24)");
-    }
+	@Override
+	public IntType getIntType() {
+		return new IntType("NUMBER(10,0)");
+	}
 
-    @Override
-    public TinyIntType getTinyIntType() {
-        return new TinyIntType("NUMBER(3)");
-    }
+	@Override
+	public VarcharType getVarcharType() {
+		return new VarcharType("VARCHAR2");
+	}
+
+	@Override
+	public DoubleType getDoubleType() {
+		return new DoubleType("FLOAT(24)");
+	}
+
+	@Override
+	public TinyIntType getTinyIntType() {
+		return new TinyIntType("NUMBER(3)");
+	}
 }
