b7f3e203a0e0bbe8113bd78279bb3042f8ff09aa
jeroen <jeroen@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-03-26 14:17:25 +0000

    Fix NullPointerException
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1315 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/structure/type/DateTimeType.java
+++ liquibase-core/src/main/java/liquibase/database/structure/type/DateTimeType.java
@@ -4,29 +4,29 @@ import liquibase.statement.DatabaseFunction;
 import liquibase.database.Database;
 
 public class DateTimeType extends DataType {
-	public DateTimeType() {
-		super("DATETIME", 0, 1);
-	}
+    public DateTimeType() {
+        super("DATETIME", 0, 1);
+    }
 
-	public DateTimeType(String dataTypeName) {
-		super(dataTypeName, 0, 1);
-	}
+    public DateTimeType(String dataTypeName) {
+        super(dataTypeName, 0, 1);
+    }
 
-	@Override
-	public boolean getSupportsPrecision() {
-		return true;
-	}
+    @Override
+    public boolean getSupportsPrecision() {
+        return true;
+    }
 
-	@Override
-	public String convertObjectToString(Object value, Database database) {
-		if (value == null) {
-			return null;
-		} else if (value instanceof DatabaseFunction) {
-			return ((DatabaseFunction) value).getValue();
-		} else if (database.getDatabaseFunctions().contains(new DatabaseFunction(value.toString()))) {
-			return value.toString();
-		}
+    @Override
+    public String convertObjectToString(Object value, Database database) {
+        if (value == null) {
+            return null;
+        } else if (value instanceof DatabaseFunction) {
+            return ((DatabaseFunction) value).getValue();
+        } else if (database.getDatabaseFunctions() != null && database.getDatabaseFunctions().contains(new DatabaseFunction(value.toString()))) {
+            return value.toString();
+        }
 
-		return database.getDateTimeLiteral(((java.sql.Timestamp) value));
-	}
+        return database.getDateTimeLiteral(((java.sql.Timestamp) value));
+    }
 }
