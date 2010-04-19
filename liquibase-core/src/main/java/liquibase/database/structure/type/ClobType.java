97924cfa6a13c5b8c05b9c3000b86c16fa67b62a
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-04-19 21:19:47 +0000

    CORE-593 Postgres 8.4 text column defaults unquoted in 2.0 RC2
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1362 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/structure/type/ClobType.java
+++ liquibase-core/src/main/java/liquibase/database/structure/type/ClobType.java
@@ -1,5 +1,7 @@
 package liquibase.database.structure.type;
 
+import liquibase.database.Database;
+
 public class ClobType extends DataType {
 
     public ClobType() {
@@ -9,4 +11,16 @@ public class ClobType extends DataType {
     public ClobType(String dataTypeName) {
         super(dataTypeName,0,0);
     }
+
+    @Override
+    public String convertObjectToString(Object value, Database database) {
+        if (value == null) {
+            return null;
+        }
+        if ("null".equalsIgnoreCase(((String) value))) {
+            return null;
+        }
+        return "'" + ((String) value).replaceAll("'", "''") + "'";
+    }
+    
 }
