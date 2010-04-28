42b4c4990abfdde33959f193ce23eb818e0aced1
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-04-28 05:30:34 +0000

    CORE-567 RC2 creates indexes incorrectly in Postgres 8.4 in non-public schemas
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1388 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/core/PostgresDatabase.java
+++ liquibase-core/src/main/java/liquibase/database/core/PostgresDatabase.java
@@ -318,4 +318,9 @@ public class PostgresDatabase extends AbstractDatabase {
 
         return count != null && count > 0;
     }
+
+    @Override
+    public String escapeIndexName(String schemaName, String indexName) {
+        return indexName;
+    }
 }
