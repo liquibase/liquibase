40d91523e5bb89ced9278389c10e6287cdd3ef89
lujop <lujop@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-09 15:16:41 +0000

    Set default encoding for loadData to UTF-8 as documentation says (CORE 605) Add integration tests about encoding when updating to DB or to SQL file Minor refactoring adding a method to see if a DiffResult has diferences.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1412 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ liquibase-core/src/test/java/liquibase/util/RegexMatcherTest.java
@@ -0,0 +1,49 @@
+package liquibase.util;
+
+import java.util.regex.PatternSyntaxException;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lujop
+ */
+public class RegexMatcherTest {
+    private RegexMatcher matcher;
+    private static final String text="Pulp Fiction\n" +
+                "Reservoir Dogs\n" +
+                "Kill Bill\n";
+
+    @After
+    public void tearDown() {
+        matcher=null;
+    }
+
+    @Test(expected=PatternSyntaxException.class)
+    public void testBadPatternFails() {
+        matcher=new RegexMatcher(text,new String[]{"a(j"});
+    }
+
+    @Test
+    public void testMatchingInSequentialOrder() {
+        matcher=new RegexMatcher(text,new String[]{"Pulp","Reservoir","Kill"});
+        assertTrue("All matched",matcher.allMatchedInSequentialOrder());
+
+        matcher=new RegexMatcher(text,new String[]{"Pulp","ion"});
+        assertTrue("All matched",matcher.allMatchedInSequentialOrder());
+
+        matcher=new RegexMatcher(text,new String[]{"Pu.p","^Ki.+ll$"});
+        assertTrue("All matched",matcher.allMatchedInSequentialOrder());
+
+        matcher=new RegexMatcher(text,new String[]{"pulP","kiLL"});
+        assertTrue("Case insensitive",matcher.allMatchedInSequentialOrder());
+
+        matcher=new RegexMatcher(text,new String[]{"Reservoir","Pulp","Dogs"});
+        assertFalse("Not in order",matcher.allMatchedInSequentialOrder());
+
+        matcher=new RegexMatcher(text,new String[]{"Memento"});
+        assertFalse("Not found",matcher.allMatchedInSequentialOrder());
+    }
+
+}
