1b55c942064fefab6e51560da65343c22c9f544e
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-05-12 21:20:30 +0000

    added logging
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1435 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core-jvm/src/main/java/liquibase/executor/core/jdbc/JdbcExecutor.java
+++ liquibase-core-jvm/src/main/java/liquibase/executor/core/jdbc/JdbcExecutor.java
@@ -59,7 +59,7 @@ public class JdbcExecutor extends AbstractExecutor implements Executor {
             // in the case when the exception translator hasn't been initialized yet.
             JdbcUtils.closeStatement(stmt);
             stmt = null;
-            throw new DatabaseException("Error executing SQL " + StringUtils.join(applyVisitors(action.getStatement(), sqlVisitors), "; "), ex);
+            throw new DatabaseException("Error executing SQL " + StringUtils.join(applyVisitors(action.getStatement(), sqlVisitors), "; on "+ con.getURL()), ex);
         }
         finally {
             JdbcUtils.closeStatement(stmt);
