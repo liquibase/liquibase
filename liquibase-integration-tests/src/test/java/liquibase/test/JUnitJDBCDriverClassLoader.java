dfa625443a0fe844036040737523c54fcafd79a7
lujop <lujop@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-06-06 20:49:28 +0000

    Ability to specify jdbc integration test drivers per Java version to be fun with Microsoft JDBC Driver requirements. New MSSQL JDBC v3 driver has one version for Java5 that isn't compatible in Java6 and another for Java6 that isn't compatible with Java5. That way we can run in Build Server with Java 5 and at home with Java 6
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1470 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-integration-tests/src/test/java/liquibase/test/JUnitJDBCDriverClassLoader.java
+++ liquibase-integration-tests/src/test/java/liquibase/test/JUnitJDBCDriverClassLoader.java
@@ -9,6 +9,7 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import liquibase.util.SystemUtils;
 
 /**
  * Class Loader for loading JDBC drivers in unit tests.  It was orginally not a singleton, but
@@ -34,9 +35,25 @@ public class JUnitJDBCDriverClassLoader extends URLClassLoader {
         try {
             List<URL> urls = new ArrayList<URL>();
 
+            addUrlsFromPath(urls,  "jdbc-drivers/all");
+
+            //Add drivers by Java version. Only jars from the biggest matching version are taken
+            if(SystemUtils.isJavaVersionAtLeast(1.6f)) {
+                addUrlsFromPath(urls,  "jdbc-drivers/byJavaVersion/1.6");
+            } else if(SystemUtils.isJavaVersionAtLeast(1.5f)) {
+                addUrlsFromPath(urls,  "jdbc-drivers/byJavaVersion/1.5");
+            }
+
+            return urls.toArray(new URL[urls.size()]);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void addUrlsFromPath(List<URL> addTo,String path) throws Exception{
             File thisClassFile = new File(new URI(Thread.currentThread().getContextClassLoader().getResource("liquibase/test/JUnitJDBCDriverClassLoader.class")
                     .toExternalForm()));
-            File jdbcLib = new File(thisClassFile.getParentFile().getParentFile().getParentFile(), "jdbc-drivers");
+            File jdbcLib = new File(thisClassFile.getParentFile().getParentFile().getParentFile(),path);
             if (!jdbcLib.exists()) {
                 throw new RuntimeException("JDBC driver directory "+jdbcLib.getAbsolutePath()+" does not exist");
             }
@@ -46,7 +63,6 @@ public class JUnitJDBCDriverClassLoader extends URLClassLoader {
                 }
             });
             if(files == null) {
-                //throw new RuntimeException("No files found in "+jdbcLib);
                 files = new File[]{};
             }
             for (File driverDir : files) {
@@ -57,13 +73,9 @@ public class JUnitJDBCDriverClassLoader extends URLClassLoader {
                 });
 
                 for (File jar : driverJars) {
-                    urls.add(jar.toURL());
+                    addTo.add(jar.toURL());
                 }
 
             }
-            return urls.toArray(new URL[urls.size()]);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
