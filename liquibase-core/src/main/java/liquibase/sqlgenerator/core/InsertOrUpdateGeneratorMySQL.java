8b100d3122fd24a8d4e34b509eeb15f0de318c4e
koss3982 <koss3982@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-07-15 07:58:08 +0000

    Support to loadUpdateDate for MySQL using "INSERT .... ON DUPLICATE KEY UPDATE" syntax.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1572 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGeneratorMySQL.java
@@ -0,0 +1,80 @@
+package liquibase.sqlgenerator.core;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import liquibase.database.Database;
+import liquibase.database.core.MySQLDatabase;
+import liquibase.database.typeconversion.TypeConverterFactory;
+import liquibase.sqlgenerator.SqlGeneratorChain;
+import liquibase.statement.core.InsertOrUpdateStatement;
+
+/**
+ *
+ * @author Carles
+ */
+public class InsertOrUpdateGeneratorMySQL extends InsertOrUpdateGenerator {
+    @Override
+    public boolean supports(InsertOrUpdateStatement statement, Database database) {
+         return database instanceof MySQLDatabase;
+    }
+
+    @Override
+    protected String getInsertStatement(InsertOrUpdateStatement insertOrUpdateStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        StringBuffer sql = new StringBuffer(super.getInsertStatement(insertOrUpdateStatement, database, sqlGeneratorChain));
+        String updateSql = super.getUpdateStatement(insertOrUpdateStatement, database, "", sqlGeneratorChain);
+        
+        sql.deleteCharAt(sql.lastIndexOf(";"));
+        sql.append("ON DUPLICATE KEY UPDATE ");
+        String[] pkFields=insertOrUpdateStatement.getPrimaryKey().split(",");
+        HashSet<String> hashPkFields = new HashSet<String>(Arrays.asList(pkFields));
+        for(String columnKey:insertOrUpdateStatement.getColumnValues().keySet())
+        {
+            if (!hashPkFields.contains(columnKey)) {
+                sql.append(columnKey).append(" = ");
+                sql.append(convertToString(insertOrUpdateStatement.getColumnValue(columnKey),database));
+                sql.append(",");
+            }
+        }
+        sql.deleteCharAt(sql.lastIndexOf(","));
+
+        System.out.println("SQL :" + sql.toString());
+
+        return sql.toString();
+    }
+
+    @Override
+    protected String getUpdateStatement(InsertOrUpdateStatement insertOrUpdateStatement, Database database, String whereClause, SqlGeneratorChain sqlGeneratorChain) {
+        return "";
+    }
+
+    @Override
+    protected String getRecordCheck(InsertOrUpdateStatement insertOrUpdateStatement, Database database, String whereClause) {
+        return "";
+    }
+
+    @Override
+    protected String getElse(Database database) {
+        return "";
+    }
+
+    private String convertToString(Object newValue, Database database) {
+        String sqlString;
+        if (newValue == null || newValue.toString().equals("") || newValue.toString().equalsIgnoreCase("NULL")) {
+            sqlString = "NULL";
+        } else if (newValue instanceof String && database.shouldQuoteValue(((String) newValue))) {
+            sqlString = "'" + database.escapeStringForDatabase(newValue.toString()) + "'";
+        } else if (newValue instanceof Date) {
+            sqlString = database.getDateLiteral(((Date) newValue));
+        } else if (newValue instanceof Boolean) {
+            if (((Boolean) newValue)) {
+                sqlString = TypeConverterFactory.getInstance().findTypeConverter(database).getBooleanType().getTrueBooleanValue();
+            } else {
+                sqlString = TypeConverterFactory.getInstance().findTypeConverter(database).getBooleanType().getFalseBooleanValue();
+            }
+        } else {
+            sqlString = newValue.toString();
+        }
+        return sqlString;
+    }
+}
