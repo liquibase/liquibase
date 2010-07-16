107dfb45c7e1dfaf4430d38268b1f13f7c37d1b7
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-07-16 04:45:13 +0000

    CORE-663 sqlFile should ignore whitespace after end delimiter
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1575 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/change/AbstractSQLChange.java
+++ liquibase-core/src/main/java/liquibase/change/AbstractSQLChange.java
@@ -75,7 +75,7 @@ public abstract class AbstractSQLChange extends AbstractChange {
      * The raw SQL to use for this change.
      */
     public void setSql(String sql) {
-       this.sql = sql;
+       this.sql = StringUtils.trimToNull(sql);
     }
 
     public String getEndDelimiter() {
@@ -90,10 +90,10 @@ public abstract class AbstractSQLChange extends AbstractChange {
      * Generates one or more statements depending on how the SQL should be parsed.
      * If split statements is set to true then the SQL is split on the ; and go\n entries
      * found in the sql text and each is made a separate statement.
-     * 
+     *
      * If stripping comments is true then any comments after -- on a line and any comments
      * between /* and \*\/ will be stripped before the splitting is executed.
-     * 
+     *
      * The end result is one or more SQL statements split in the way the user requested
      */
     public SqlStatement[] generateStatements(Database database) {
