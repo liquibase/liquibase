9a26a1b8cce69b8a89024a6a14cb94e8725bd127
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-07-27 03:53:27 +0000

    ensure databaseClass parameter still works
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1603 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-integration-tests/src/test/java/liquibase/database/example/ExampleCustomDatabase.java
+++ liquibase-integration-tests/src/test/java/liquibase/database/example/ExampleCustomDatabase.java
@@ -6,6 +6,11 @@ import liquibase.exception.DatabaseException;
 public class ExampleCustomDatabase extends H2Database {
 
     @Override
+    public int getPriority() {
+        return super.getPriority()+5;
+    }
+
+    @Override
     public void tag(String tagString) throws DatabaseException {
         System.out.println("Custom tagging");
         super.tag(tagString);
