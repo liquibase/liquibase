a6821605096f1c9975b5e4130616dc82d585e1ef
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-07-30 17:48:07 +0000

    undid websphere fix
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1638 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/servicelocator/ResolverUtil.java
+++ liquibase-core/src/main/java/liquibase/servicelocator/ResolverUtil.java
@@ -30,7 +30,7 @@ import java.util.regex.Pattern;
  * methods.</p>
  *
  * <p>General searches are initiated by calling the
- * {@link #find(ResolverUtil.Test, String)} ()} method and supplying
+ * {@link #find(Test, String)} ()} method and supplying
  * a package name and a Test instance. This will cause the named package <b>and all sub-packages</b>
  * to be scanned for classes that meet the test. There are also utility methods for the common
  * use cases of scanning multiple packages for extensions of particular classes, or classes
