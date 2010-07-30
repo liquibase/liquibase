7e215ec9eab92537dc31298c8a57f37515e8ed40
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-07-30 20:18:49 +0000

    Trying classes from camel instead of stripes
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1639 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ liquibase-core/src/main/java/liquibase/servicelocator/AssignableToPackageScanFilter.java
@@ -0,0 +1,47 @@
+package liquibase.servicelocator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Package scan filter for testing if a given class is assignable to another class.
+ */
+public class AssignableToPackageScanFilter implements PackageScanFilter {
+    private final Set<Class<?>> parents = new HashSet<Class<?>>();
+
+    public AssignableToPackageScanFilter() {
+    }
+
+    public AssignableToPackageScanFilter(Class<?> parentType) {
+        parents.add(parentType);
+    }
+
+    public AssignableToPackageScanFilter(Set<Class<?>> parents) {
+        this.parents.addAll(parents);
+    }
+
+    public void addParentType(Class<?> parentType) {
+        parents.add(parentType);
+    }
+
+    public boolean matches(Class<?> type) {
+        if (parents != null && parents.size() > 0) {
+            for (Class<?> parent : parents) {
+                if (parent.isAssignableFrom(type)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Class<?> parent : parents) {
+            sb.append(parent.getSimpleName()).append(", ");
+        }
+        sb.setLength(sb.length() > 0 ? sb.length() - 2 : 0);
+        return "is assignable to any of " + sb;
+    }
+}
---
+++ liquibase-core/src/main/java/liquibase/servicelocator/PackageScanClassResolver.java
@@ -0,0 +1,67 @@
+package liquibase.servicelocator;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+/**
+ * A resolver that can find resources based on package scanning.
+ */
+public interface PackageScanClassResolver {
+
+    /**
+     * Sets the ClassLoader instances that should be used when scanning for
+     * classes. If none is set then the context classloader will be used.
+     *
+     * @param classLoaders loaders to use when scanning for classes
+     */
+    void setClassLoaders(Set<ClassLoader> classLoaders);
+
+    /**
+     * Gets the ClassLoader instances that should be used when scanning for classes.
+     *
+     * @return the class loaders to use
+     */
+    Set<ClassLoader> getClassLoaders();
+
+    /**
+     * Adds the class loader to the existing loaders
+     *
+     * @param classLoader the loader to add
+     */
+    void addClassLoader(ClassLoader classLoader);
+
+    /**
+     * Attempts to discover classes that are assignable to the type provided. In
+     * the case that an interface is provided this method will collect
+     * implementations. In the case of a non-interface class, subclasses will be
+     * collected.
+     *
+     * @param parent       the class of interface to find subclasses or implementations of
+     * @param packageNames one or more package names to scan (including subpackages) for classes
+     * @return the classes found, returns an empty set if none found
+     */
+    Set<Class<?>> findImplementations(Class<?> parent, String... packageNames);
+
+    /**
+     * Attempts to discover classes filter by the provided filter
+     *
+     * @param filter  filter to filter desired classes.
+     * @param packageNames one or more package names to scan (including subpackages) for classes
+     * @return the classes found, returns an empty set if none found
+     */
+    Set<Class<?>> findByFilter(PackageScanFilter filter, String... packageNames);
+
+    /**
+     * Add a filter that will be applied to all scan operations
+     *
+     * @param filter filter to filter desired classes in all scan operations
+     */
+    void addFilter(PackageScanFilter filter);
+
+    /**
+     * Removes the filter
+     *
+     * @param filter filter to filter desired classes in all scan operations
+     */
+    void removeFilter(PackageScanFilter filter);
+}
