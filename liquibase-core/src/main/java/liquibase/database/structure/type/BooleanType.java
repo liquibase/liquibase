e4cb59a93b6d0091ea419d898c3023da7a2a49f0
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-09-28 04:55:17 +0000

    CORE-702 Classcast exception: java.lang.String cannot be cast to java.lang.Boolean with Oracle
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1739 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/structure/type/BooleanType.java
+++ liquibase-core/src/main/java/liquibase/database/structure/type/BooleanType.java
@@ -3,6 +3,7 @@ package liquibase.database.structure.type;
 import liquibase.database.Database;
 import liquibase.database.typeconversion.TypeConverterFactory;
 import liquibase.database.typeconversion.TypeConverter;
+import liquibase.exception.UnexpectedLiquibaseException;
 
 public class BooleanType extends DataType {
 
@@ -24,10 +25,19 @@ public class BooleanType extends DataType {
 
         String returnValue;
         TypeConverter converter = TypeConverterFactory.getInstance().findTypeConverter(database);
-        if (((Boolean) value)) {
-            returnValue = converter.getBooleanType().getTrueBooleanValue();
+        BooleanType booleanType = converter.getBooleanType();
+        if (value instanceof String) {
+            if (((String) value).equalsIgnoreCase("true") || value.equals("1") || ((String) value).equalsIgnoreCase(booleanType.getTrueBooleanValue())) {
+                returnValue = booleanType.getTrueBooleanValue();
+            } else if (((String) value).equalsIgnoreCase("false") || value.equals("0") || ((String) value).equalsIgnoreCase(booleanType.getFalseBooleanValue())) {
+                returnValue = booleanType.getTrueBooleanValue();
+            } else {
+                throw new UnexpectedLiquibaseException("Unknown boolean value: "+value);
+            }
+        } else if (((Boolean) value)) {
+            returnValue = booleanType.getTrueBooleanValue();
         } else {
-            returnValue = converter.getBooleanType().getFalseBooleanValue();
+            returnValue = booleanType.getFalseBooleanValue();
         }
         if (returnValue.matches("\\d+")) {
             return returnValue;
