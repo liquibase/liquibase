28aead972761392b63333f9784fe6f428c5eccea
donsmith <donsmith@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-09-30 19:14:14 +0000

    CORE-708: modified JDBC DatabaseMetaData query to mimic that used to check for the databasechangelog and databasechangeloglock tables, fixing a bug on some platforms
    git-svn-id: http://liquibase.jira.com/svn/CORE/branches/1_9@1754 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/preconditions/TableExistsPrecondition.java
+++ core/src/java/liquibase/preconditions/TableExistsPrecondition.java
@@ -1,17 +1,22 @@
 package liquibase.preconditions;
 
-import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import liquibase.DatabaseChangeLog;
+import liquibase.log.LogFactory;
 import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
+import liquibase.exception.JDBCException;
 import liquibase.exception.PreconditionErrorException;
 import liquibase.exception.PreconditionFailedException;
 import liquibase.util.StringUtils;
 
 public class TableExistsPrecondition implements Precondition {
+    static final protected Logger log = LogFactory.getLogger();
+    
     private String schemaName;
     private String tableName;
 
@@ -32,15 +37,32 @@ public class TableExistsPrecondition implements Precondition {
     }
 
     public void check(Database database, DatabaseChangeLog changeLog) throws PreconditionFailedException, PreconditionErrorException {
-        Connection conn = database.getConnection().getUnderlyingConnection();
+        // Use DatabaseMetaData to query db's data dictionary
+        DatabaseConnection conn = database.getConnection();
+        ResultSet tables = null;
         try {
             DatabaseMetaData dbm = conn.getMetaData();
-            ResultSet tables = dbm.getTables(null, schemaName, database.escapeTableName(getSchemaName(), getTableName()), null);
+            tables = dbm.getTables(
+                    database.convertRequestedSchemaToCatalog(getSchemaName()),
+                    database.convertRequestedSchemaToSchema(getSchemaName()),
+                    getTableName(),
+                    new String[]{"TABLE"}
+            );
             if (!tables.next()) {
                 throw new PreconditionFailedException("Table "+database.escapeTableName(getSchemaName(), getTableName())+" does not exist", changeLog, this);
             }
+        } catch (JDBCException je) {
+            throw new PreconditionErrorException(je, changeLog, this);
         } catch (SQLException se) {
             throw new PreconditionErrorException(se, changeLog, this);
+        } finally {
+            if (tables != null) {
+                try {
+                    tables.close();
+                } catch (SQLException e) {
+                    log.warning("Error closing result set: " + e.getMessage());
+                }
+            }
         }
     }
 
