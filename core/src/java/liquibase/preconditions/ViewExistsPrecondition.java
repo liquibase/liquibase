2c8e7188a80701a8563f46b1d650266ed0963487
donsmith <donsmith@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-09-30 22:50:51 +0000

    CORE-717: modified CheckViewExistsPrecondition to use JDBC DatabaseMetaData to query if a view exists rather than taking a full database snapshot
    git-svn-id: http://liquibase.jira.com/svn/CORE/branches/1_9@1764 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/preconditions/ViewExistsPrecondition.java
+++ core/src/java/liquibase/preconditions/ViewExistsPrecondition.java
@@ -1,14 +1,22 @@
 package liquibase.preconditions;
 
-import liquibase.database.Database;
-import liquibase.database.structure.DatabaseSnapshot;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 import liquibase.DatabaseChangeLog;
-import liquibase.util.StringUtils;
-import liquibase.exception.PreconditionFailedException;
-import liquibase.exception.PreconditionErrorException;
+import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
 import liquibase.exception.JDBCException;
+import liquibase.exception.PreconditionErrorException;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.log.LogFactory;
+import liquibase.util.StringUtils;
 
 public class ViewExistsPrecondition implements Precondition {
+    static final protected Logger log = LogFactory.getLogger();
+
     private String schemaName;
     private String viewName;
 
@@ -29,14 +37,32 @@ public class ViewExistsPrecondition implements Precondition {
     }
 
     public void check(Database database, DatabaseChangeLog changeLog) throws PreconditionFailedException, PreconditionErrorException {
-        DatabaseSnapshot databaseSnapshot;
+        // Use DatabaseMetaData to query db's data dictionary
+        DatabaseConnection conn = database.getConnection();
+        ResultSet views = null;
         try {
-            databaseSnapshot = database.createDatabaseSnapshot(getSchemaName(), null);
-        } catch (JDBCException e) {
-            throw new PreconditionErrorException(e, changeLog, this);
-        }
-        if (databaseSnapshot.getView(getViewName()) == null) {
-            throw new PreconditionFailedException("View "+database.escapeStringForDatabase(getViewName())+" does not exist", changeLog, this);
+            DatabaseMetaData dbm = conn.getMetaData();
+            views = dbm.getTables(
+                    database.convertRequestedSchemaToCatalog(getSchemaName()),
+                    database.convertRequestedSchemaToSchema(getSchemaName()),
+                    getViewName(),
+                    new String[]{"VIEW"}
+            );
+            if (!views.next()) {
+                throw new PreconditionFailedException("View "+database.escapeStringForDatabase(getViewName())+" does not exist", changeLog, this);
+            }
+        } catch (JDBCException je) {
+            throw new PreconditionErrorException(je, changeLog, this);
+        } catch (SQLException se) {
+            throw new PreconditionErrorException(se, changeLog, this);
+        } finally {
+            if (views != null) {
+                try {
+                    views.close();
+                } catch (SQLException e) {
+                    log.warning("Error closing result set: " + e.getMessage());
+                }
+            }
         }
     }
 
