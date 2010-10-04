31258c62c1fc38c6d8a3caf252cacad1a9313cd2
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-10-04 20:38:51 +0000

    Add column/table remarks support for postgres
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1772 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetTableRemarksGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetTableRemarksGenerator.java
@@ -3,6 +3,7 @@ package liquibase.sqlgenerator.core;
 import liquibase.database.Database;
 import liquibase.database.core.MySQLDatabase;
 import liquibase.database.core.OracleDatabase;
+import liquibase.database.core.PostgresDatabase;
 import liquibase.exception.ValidationErrors;
 import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
@@ -14,7 +15,7 @@ public class SetTableRemarksGenerator extends AbstractSqlGenerator<SetTableRemar
 
     @Override
     public boolean supports(SetTableRemarksStatement statement, Database database) {
-        return database instanceof MySQLDatabase || database instanceof OracleDatabase;
+        return database instanceof MySQLDatabase || database instanceof OracleDatabase || database instanceof PostgresDatabase;
     }
 
     public ValidationErrors validate(SetTableRemarksStatement setTableRemarksStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
@@ -27,10 +28,10 @@ public class SetTableRemarksGenerator extends AbstractSqlGenerator<SetTableRemar
     public Sql[] generateSql(SetTableRemarksStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
         String sql;
         String remarks = database.escapeStringForDatabase(statement.getRemarks());
-        if (database instanceof OracleDatabase) {
-            sql = "COMMENT ON TABLE "+database.escapeTableName(statement.getSchemaName(), statement.getTableName())+" IS '"+remarks+"'";
-        } else {
+        if (database instanceof MySQLDatabase) {
             sql = "ALTER TABLE "+database.escapeTableName(statement.getSchemaName(), statement.getTableName())+" COMMENT = '"+remarks+"'";
+        } else {
+            sql = "COMMENT ON TABLE "+database.escapeTableName(statement.getSchemaName(), statement.getTableName())+" IS '"+remarks+"'";
         }
 
         return new Sql[] {
