30d691392765d1e05efc33723df3363864d13f83
donsmith <donsmith@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-10-04 17:25:52 +0000

    CORE-718: modified ColumnExistsPrecondition to use JDBC DatabaseMetaData to query the data dictionary for the existence of a column
    git-svn-id: http://liquibase.jira.com/svn/CORE/branches/1_9@1771 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/preconditions/ColumnExistsPrecondition.java
+++ core/src/java/liquibase/preconditions/ColumnExistsPrecondition.java
@@ -1,14 +1,21 @@
 package liquibase.preconditions;
 
-import liquibase.database.Database;
-import liquibase.database.structure.DatabaseSnapshot;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 import liquibase.DatabaseChangeLog;
-import liquibase.util.StringUtils;
-import liquibase.exception.PreconditionFailedException;
-import liquibase.exception.PreconditionErrorException;
+import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
 import liquibase.exception.JDBCException;
+import liquibase.exception.PreconditionErrorException;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.log.LogFactory;
+import liquibase.util.StringUtils;
 
 public class ColumnExistsPrecondition implements Precondition {
+    static final protected Logger log = LogFactory.getLogger();
     private String schemaName;
     private String tableName;
     private String columnName;
@@ -38,14 +45,32 @@ public class ColumnExistsPrecondition implements Precondition {
     }
 
     public void check(Database database, DatabaseChangeLog changeLog) throws PreconditionFailedException, PreconditionErrorException {
-        DatabaseSnapshot databaseSnapshot;
+        // Use DatabaseMetaData to query db's data dictionary
+        DatabaseConnection conn = database.getConnection();
+        ResultSet columns = null;
         try {
-            databaseSnapshot = database.createDatabaseSnapshot(getSchemaName(), null);
-        } catch (JDBCException e) {
-            throw new PreconditionErrorException(e, changeLog, this);
-        }
-        if (databaseSnapshot.getColumn(getTableName(), getColumnName()) == null) {
-            throw new PreconditionFailedException("Column "+database.escapeColumnName(getSchemaName(), getTableName(), getColumnName())+" does not exist", changeLog, this);
+            DatabaseMetaData dbm = conn.getMetaData();
+            columns = dbm.getColumns(
+                    database.convertRequestedSchemaToCatalog(getSchemaName()),
+                    database.convertRequestedSchemaToSchema(getSchemaName()),
+                    getTableName(),
+                    getColumnName()
+            );
+            if (!columns.next()) {
+                throw new PreconditionFailedException("Column "+database.escapeColumnName(getSchemaName(), getTableName(), getColumnName())+" does not exist", changeLog, this);
+            }
+        } catch (JDBCException je) {
+            throw new PreconditionErrorException(je, changeLog, this);
+        } catch (SQLException se) {
+            throw new PreconditionErrorException(se, changeLog, this);
+        } finally {
+            if (columns != null) {
+                try {
+                    columns.close();
+                } catch (SQLException e) {
+                    log.warning("Error closing result set: " + e.getMessage());
+                }
+            }
         }
     }
 
