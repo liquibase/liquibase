62687ab54a81c2fd3412bf93ef112a968d2287e4
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-10-04 20:38:51 +0000

    Add column/table remarks support for postgres
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1772 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetColumnRemarksGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetColumnRemarksGenerator.java
@@ -2,6 +2,7 @@ package liquibase.sqlgenerator.core;
 
 import liquibase.database.Database;
 import liquibase.database.core.OracleDatabase;
+import liquibase.database.core.PostgresDatabase;
 import liquibase.exception.ValidationErrors;
 import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
@@ -9,15 +10,15 @@ import liquibase.sqlgenerator.SqlGenerator;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.statement.core.SetColumnRemarksStatement;
 
-public class SetColumnRemarksGeneratorOracle extends AbstractSqlGenerator<SetColumnRemarksStatement> {
+public class SetColumnRemarksGenerator extends AbstractSqlGenerator<SetColumnRemarksStatement> {
     @Override
     public int getPriority() {
-        return PRIORITY_DATABASE;
+        return PRIORITY_DEFAULT;
     }
 
     @Override
     public boolean supports(SetColumnRemarksStatement statement, Database database) {
-        return database instanceof OracleDatabase;
+        return database instanceof OracleDatabase || database instanceof PostgresDatabase;
     }
 
     public ValidationErrors validate(SetColumnRemarksStatement setColumnRemarksStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
