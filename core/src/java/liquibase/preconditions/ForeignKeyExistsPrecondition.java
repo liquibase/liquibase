217c747b7df40a3b7f8a3836fc3b385ebe1af075
donsmith <donsmith@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-10-20 14:33:50 +0000

    CORE-752: modified ForeignKeyExistsPrecondition to use a JDBC DatabaseMetaData query instead of Database.snapshot() to determine if a foriegn key exists to improve performance
    git-svn-id: http://liquibase.jira.com/svn/CORE/branches/1_9@1788 e6edf6fb-f266-4316-afb4-e53d95876a76

--- core/src/java/liquibase/preconditions/ForeignKeyExistsPrecondition.java
+++ core/src/java/liquibase/preconditions/ForeignKeyExistsPrecondition.java
@@ -1,14 +1,20 @@
 package liquibase.preconditions;
 
-import liquibase.database.Database;
-import liquibase.database.structure.DatabaseSnapshot;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 import liquibase.DatabaseChangeLog;
-import liquibase.util.StringUtils;
-import liquibase.exception.PreconditionFailedException;
+import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
 import liquibase.exception.PreconditionErrorException;
-import liquibase.exception.JDBCException;
+import liquibase.exception.PreconditionFailedException;
+import liquibase.log.LogFactory;
+import liquibase.util.StringUtils;
 
 public class ForeignKeyExistsPrecondition implements Precondition {
+    static final protected Logger log = LogFactory.getLogger();
     private String schemaName;
     private String foreignKeyName;
 
@@ -29,15 +35,39 @@ public class ForeignKeyExistsPrecondition implements Precondition {
     }
 
     public void check(Database database, DatabaseChangeLog changeLog) throws PreconditionFailedException, PreconditionErrorException {
-        DatabaseSnapshot databaseSnapshot;
+        // Use DatabaseMetaData to query db's data dictionary
+        DatabaseConnection conn = database.getConnection();
+        ResultSet foreignKeys = null;
         try {
-            databaseSnapshot = database.createDatabaseSnapshot(getSchemaName(), null);
-        } catch (JDBCException e) {
+            String schemaName = getSchemaName();
+            DatabaseMetaData dbm = conn.getMetaData();
+            foreignKeys = dbm.getCrossReference(
+                    schemaName,
+                    schemaName,
+                    null,
+                    schemaName,
+                    schemaName,
+                    null
+            );
+            while (foreignKeys.next()) {
+                String fk = foreignKeys.getString("FK_NAME");
+                if (getForeignKeyName().equalsIgnoreCase(fk)) {
+                    return;
+                }
+            }
+        } catch (SQLException e) {
             throw new PreconditionErrorException(e, changeLog, this);
+        } finally {
+            if (foreignKeys != null) {
+                try {
+                    foreignKeys.close();
+                } catch (SQLException e) {
+                    log.warning("Error closing result set: " + e.getMessage());
+                }
+            }
         }
-        if (databaseSnapshot.getForeignKey(getForeignKeyName()) == null) {
-            throw new PreconditionFailedException("Foreign Key "+database.escapeStringForDatabase(getForeignKeyName())+" does not exist", changeLog, this);
-        }
+        // If we got here, the foreign key was not found.
+        throw new PreconditionFailedException("Foreign Key "+database.escapeStringForDatabase(getForeignKeyName())+" does not exist", changeLog, this);
     }
 
     public String getTagName() {
