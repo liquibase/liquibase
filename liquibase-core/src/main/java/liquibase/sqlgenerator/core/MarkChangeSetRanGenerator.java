748f69467b99a33fa8be38f528653c2f490a0dea
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-10-22 20:15:22 +0000

    CORE-754 changeLogSync does not re-insert DATABASECHANGELOG rows with TAG column correctly
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1793 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/MarkChangeSetRanGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/MarkChangeSetRanGenerator.java
@@ -1,16 +1,19 @@
 package liquibase.sqlgenerator.core;
 
+import java.util.List;
+
+import liquibase.change.Change;
+import liquibase.change.core.TagDatabaseChange;
 import liquibase.changelog.ChangeSet;
 import liquibase.database.Database;
 import liquibase.exception.LiquibaseException;
 import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.exception.ValidationErrors;
 import liquibase.sql.Sql;
-import liquibase.sqlgenerator.SqlGenerator;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.SqlGeneratorFactory;
-import liquibase.statement.SqlStatement;
 import liquibase.statement.DatabaseFunction;
+import liquibase.statement.SqlStatement;
 import liquibase.statement.core.InsertStatement;
 import liquibase.statement.core.MarkChangeSetRanStatement;
 import liquibase.statement.core.UpdateStatement;
@@ -54,6 +57,19 @@ public class MarkChangeSetRanGenerator extends AbstractSqlGenerator<MarkChangeSe
                         .addColumnValue("COMMENTS", limitSize(StringUtils.trimToEmpty(changeSet.getComments())))
                         .addColumnValue("EXECTYPE", statement.getExecType().value)
                         .addColumnValue("LIQUIBASE", LiquibaseUtil.getBuildVersion().replaceAll("SNAPSHOT", "SNP"));
+
+                String tag = null;
+                List<Change> changes = changeSet.getChanges();
+                if (changes != null && changes.size() == 1) {
+                    Change change = changes.get(0);
+                    if (change instanceof TagDatabaseChange) {
+                        TagDatabaseChange tagChange = (TagDatabaseChange) change;
+                        tag = tagChange.getTag();
+                    }
+                }
+                if (tag != null) {
+                    ((InsertStatement) runStatement).addColumnValue("TAG", tag);
+                }
             }
         } catch (LiquibaseException e) {
             throw new UnexpectedLiquibaseException(e);
