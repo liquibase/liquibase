8b1dbe0bb80f728d4338713e03a2f013522a1948
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2010-11-05 20:16:18 +0000

    Added register() method to TypeConverterFactory
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@1815 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/typeconversion/TypeConverterFactory.java
+++ liquibase-core/src/main/java/liquibase/database/typeconversion/TypeConverterFactory.java
@@ -35,18 +35,28 @@ import liquibase.database.typeconversion.core.SybaseTypeConverter;
 import liquibase.servicelocator.ServiceLocator;
 import liquibase.exception.UnexpectedLiquibaseException;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.Comparator;
+import java.util.*;
 
 public class TypeConverterFactory {
     private static TypeConverterFactory instance;
+    private Set<TypeConverter> allConverters;
 
     public static TypeConverterFactory getInstance() {
         if (instance == null) {
             instance = new TypeConverterFactory();
         }
         return instance;
+    }
+
+    private TypeConverterFactory() {
+        allConverters = new HashSet<TypeConverter>();
+        try {
+            for (Class<? extends TypeConverter> converterClass : ServiceLocator.getInstance().findClasses(TypeConverter.class)) {
+                register(converterClass.newInstance());
+            }
+        } catch (Exception e) {
+            throw new UnexpectedLiquibaseException(e);
+        }
     }
 
     public TypeConverter findTypeConverter(Database database) {
@@ -57,17 +67,25 @@ public class TypeConverterFactory {
         });
 
         //noinspection unchecked
-        for (Class<TypeConverter> converterClass : ServiceLocator.getInstance().findClasses(TypeConverter.class)) {
-            try {
-                TypeConverter converter = converterClass.newInstance();
-                if (converter.supports(database)) {
-                    converters.add(converter);
-                }
-            } catch (Exception e) {
-                throw new UnexpectedLiquibaseException(e);
+
+        for (TypeConverter converter : allConverters) {
+            if (converter.supports(database)) {
+                converters.add(converter);
             }
         }
 
         return converters.last();
+    }
+
+    public void register(TypeConverter typeConverter) {
+        allConverters.add(typeConverter);
+    }
+
+    public void register(Class<? extends TypeConverter> typeConverterClass) {
+        try {
+            allConverters.add(typeConverterClass.newInstance());
+        } catch (Exception e) {
+            throw new UnexpectedLiquibaseException(e);
+        }
     }
 }
