ec4e1ce1758149fa12a152bd1568df305ae3cc4e
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-01-14 06:47:05 +0000

    CORE-817 Mojo for listLocks, releaseLocks, changelogSync, changelogSyncSQL, and status.
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@2016 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseChangeLogSyncSQLMojo.java
@@ -0,0 +1,100 @@
+package org.liquibase.maven.plugins;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ResourceAccessor;
+
+/**
+ * A Maven Mojo for generating the SQL that marks all unapplied changes as applied.
+ * 
+ * @author JAmes Atwill
+ * @goal changelogSyncSQL
+ */
+public class LiquibaseChangeLogSyncSQLMojo extends
+		AbstractLiquibaseChangeLogMojo {
+
+	/**
+	 * The file to output the Migration SQL script to, if it exists it will be
+	 * overwritten.
+	 * 
+	 * @parameter expression="${liquibase.migrationSqlOutputFile}"
+	 *            default-value=
+	 *            "${project.build.directory}/liquibase/migrate.sql"
+	 */
+	protected File migrationSqlOutputFile;
+
+	/** The writer for writing the migration SQL. */
+	private Writer outputWriter;
+
+	@Override
+	protected void performLiquibaseTask(Liquibase liquibase)
+			throws LiquibaseException {
+		liquibase.changeLogSync(contexts, outputWriter);
+	}
+
+	@Override
+	protected void printSettings(String indent) {
+		super.printSettings(indent);
+		getLog().info(
+				indent + "migrationSQLOutputFile: " + migrationSqlOutputFile);
+
+	}
+
+	@Override
+	protected boolean isPromptOnNonLocalDatabase() {
+		// Always run on an non-local database as we are not actually modifying
+		// the database
+		// when run on it.
+		return false;
+	}
+
+	@Override
+	protected Liquibase createLiquibase(ResourceAccessor fo, Database db)
+			throws MojoExecutionException {
+		Liquibase liquibase = super.createLiquibase(fo, db);
+
+		// Setup the output file writer
+		try {
+			if (!migrationSqlOutputFile.exists()) {
+				// Ensure the parent directories exist
+				migrationSqlOutputFile.getParentFile().mkdirs();
+				// Create the actual file
+				if (!migrationSqlOutputFile.createNewFile()) {
+					throw new MojoExecutionException(
+							"Cannot create the migration SQL file; "
+									+ migrationSqlOutputFile.getAbsolutePath());
+				}
+			}
+			outputWriter = new FileWriter(migrationSqlOutputFile);
+		} catch (IOException e) {
+			getLog().error(e);
+			throw new MojoExecutionException(
+					"Failed to create SQL output writer", e);
+		}
+		getLog().info(
+				"Output SQL Migration File: "
+						+ migrationSqlOutputFile.getAbsolutePath());
+		return liquibase;
+	}
+
+	@Override
+	protected void cleanup(Database db) {
+		super.cleanup(db);
+		if (outputWriter != null) {
+			try {
+				outputWriter.close();
+			} catch (IOException e) {
+				getLog().error(e);
+			}
+		}
+	}
+
+}
