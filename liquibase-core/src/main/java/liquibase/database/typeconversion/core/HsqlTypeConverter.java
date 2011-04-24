ee8f89c16ca6c390aac4ee5598a7ca141ec893aa
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2011-04-24 03:18:39 +0000

    CORE-889 Wrong mapping of type NUMERIC/NUMBER to NUMBER for HSQLDB databases
    git-svn-id: http://liquibase.jira.com/svn/CORE/trunk@2066 e6edf6fb-f266-4316-afb4-e53d95876a76

--- liquibase-core/src/main/java/liquibase/database/typeconversion/core/HsqlTypeConverter.java
+++ liquibase-core/src/main/java/liquibase/database/typeconversion/core/HsqlTypeConverter.java
@@ -4,6 +4,7 @@ import liquibase.database.structure.type.BlobType;
 import liquibase.database.structure.type.ClobType;
 import liquibase.database.structure.type.DateTimeType;
 import liquibase.database.structure.type.NVarcharType;
+import liquibase.database.structure.type.NumberType;
 import liquibase.database.Database;
 import liquibase.database.core.H2Database;
 import liquibase.database.core.HsqlDatabase;
@@ -33,9 +34,12 @@ public class HsqlTypeConverter extends AbstractTypeConverter {
     public DateTimeType getDateTimeType() {
         return new DateTimeType("DATETIME");
     }
-
     @Override
     public NVarcharType getNVarcharType() {
         return new NVarcharType("VARCHAR");
+    }
+    @Override
+    public NumberType getNumberType() {
+        return new NumberType("NUMERIC");
     }
 }
