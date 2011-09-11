c4dd14a649b78c1098111d55a624b9c0b7b15127
Nathan Voxland <nathan@voxland.net>
2011-09-11 00:16:47 -0500

    CORE-969 "All" is not escaped when creating a database snapshot on postgresql

--- liquibase-core/src/main/java/liquibase/database/core/PostgresDatabase.java
+++ liquibase-core/src/main/java/liquibase/database/core/PostgresDatabase.java
@@ -276,7 +276,7 @@ public class PostgresDatabase extends AbstractDatabase {
     /*
     * Reserved words from postgresql documentation
     */
-    private TreeSet<String> reservedWords = new TreeSet<String>(Arrays.asList(
+    private Set<String> reservedWords = new HashSet<String>(Arrays.asList(
             "USER", "LIKE", "GROUP", "DATE", "ALL"
 //            "ALL", "ANALYSE", "ANALYZE", "AND", "ANY", "ARRAY", "AS", "ASC", "ASYMMETRIC", "AUTHORIZATION", "BETWEEN", "BINARY", "BOTH", "CASE", "CAST", "CHECK", "COLLATE", "COLUMN", "CONSTRAINT", "CORRESPONDING", "CREATE", "CROSS", "CURRENT_DATE", "CURRENT_ROLE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURRENT_USER", "DEFAULT", "DEFERRABLE", "DESC", "DISTINCT", "DO", "ELSE", "END", "EXCEPT", "FALSE", "FOR", "FOREIGN", "FREEZE", "FROM", "FULL", "GRANT", "GROUP", "HAVING",
 //            "ILIKE", "IN", "INITIALLY", "INNER", "INTERSECT", "INTO", "IS", "ISNULL", "JOIN", "LEADING", "LEFT", "LIKE", "LIMIT", "LOCALTIME", "LOCALTIMESTAMP", "NATURAL", "NEW", "NOT", "NOTNULL", "NULL", "OFF", "OFFSET", "OLD", "ON", "ONLY", "OPEN", "OR", "ORDER", "OUTER", "OVERLAPS", "PLACING", "PRIMARY", "REFERENCES", "RETURNING", "RIGHT", "SELECT", "SESSION_USER", "SIMILAR", "SOME", "SYMMETRIC", "TABLE", "THEN", "TO", "TRAILING", "TRUE", "UNION", "UNIQUE", "USER", "USING", "VERBOSE", "WHEN", "WHERE"
