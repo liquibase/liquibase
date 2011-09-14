525981c3a530b736c6ce7bc3d105f992cd511086
Nathan Voxland <nathan@voxland.net>
2011-09-14 00:27:49 -0500

    CORE-868 Unable to create changelog tables in DB2i (since moving to 2.0)

---
+++ liquibase-core/src/main/java/liquibase/database/core/DB2iDatabase.java
@@ -0,0 +1,34 @@
+package liquibase.database.core;
+
+import java.sql.Connection;
+
+import liquibase.database.DatabaseConnection;
+import liquibase.database.structure.type.DataType;
+import liquibase.database.structure.type.FloatType;
+import liquibase.exception.DatabaseException;
+
+public class DB2iDatabase extends DB2Database {
+
+    @Override
+    public int getPriority() {
+        return super.getPriority()+5;
+    }
+
+    @Override
+    public boolean isCorrectDatabaseImplementation(DatabaseConnection conn) throws DatabaseException {
+        return conn.getDatabaseProductName().startsWith("DB2 UDB for AS/400");
+    }
+
+    @Override
+    public String getDefaultDriver(String url) {
+        if (url.startsWith("jdbc:as400")) {
+            return "com.ibm.as400.access.AS400JDBCDriver";
+        }
+        return null;
+    }
+
+    @Override
+    public String getTypeName() {
+        return "db2i";
+    }
+}
