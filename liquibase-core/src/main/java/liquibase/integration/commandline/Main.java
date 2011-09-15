b5f5ac9922a46a09de4798d2c59a7dbcf36c1c30
Nathan Voxland <nathan@voxland.net>
2011-09-15 10:41:53 -0500

    System.console() is java 6 only

--- liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
+++ liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
@@ -534,12 +534,12 @@ public class Main {
                 } catch (Exception e) {
                     throw new CommandLineParsingException("Unknown parameter: '" + attributeName + "'");
                 }
-            } else if(arg.equals("-p")) {
-            	//Prompt for password
-            	password = new String(System.console().readPassword("DB Password:"));
-            } else if(arg.equals("-rp")) {
-            	//Prompt for reference password
-            	referencePassword = new String(System.console().readPassword("Reference DB Password:"));
+//            } else if(arg.equals("-p")) {
+//            	//Prompt for password
+//            	password = new String(System.console().readPassword("DB Password:"));
+//            } else if(arg.equals("-rp")) {
+//            	//Prompt for reference password
+//            	referencePassword = new String(System.console().readPassword("Reference DB Password:"));
             } else {
                 throw new CommandLineParsingException("Unexpected value " + arg + ": parameters must start with a '--'");
             }
