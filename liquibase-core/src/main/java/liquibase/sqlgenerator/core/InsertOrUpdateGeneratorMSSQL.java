4ad45f86f26e9258d001c210a4fe6350daf01244
Nathan Voxland <nathan@voxland.net>
2011-10-24 00:29:30 -0500

    removed stdout statement

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGeneratorMSSQL.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGeneratorMSSQL.java
@@ -50,8 +50,6 @@ public class InsertOrUpdateGeneratorMSSQL extends InsertOrUpdateGenerator {
 
     @Override
     public Sql[] generateSql(InsertOrUpdateStatement insertOrUpdateStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        System.out.println("generating");
-
         return super.generateSql(insertOrUpdateStatement, database, sqlGeneratorChain);    //To change body of overridden methods use File | Settings | File Templates.
     }
 }
