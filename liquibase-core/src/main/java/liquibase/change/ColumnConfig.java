4298046db6650d55e3042eace483d32bb1567ba3
Bart Cappon <bart.cappon@gmail.com>
2012-01-27 14:50:19 +0100

    add support for prepared statement, blob and clob on insert

--- liquibase-core/src/main/java/liquibase/change/ColumnConfig.java
+++ liquibase-core/src/main/java/liquibase/change/ColumnConfig.java
@@ -22,6 +22,8 @@ public class ColumnConfig {
     private Number valueNumeric;
     private Date valueDate;
     private Boolean valueBoolean;
+    private String valueBlob;
+    private String valueClob;
     private DatabaseFunction valueComputed;
 
     private String defaultValue;
@@ -189,6 +191,9 @@ public class ColumnConfig {
         return this;
     }
 
+    // KEEP THIS A POJO!
+    // It's up to the user of this class to decide what value object to use
+    @Deprecated
     public Object getValueObject() {
         if (getValue() != null) {
             return getValue();
@@ -369,5 +374,21 @@ public class ColumnConfig {
     public ColumnConfig setRemarks(String remarks) {
         this.remarks = remarks;
         return this;
+    }
+
+    public String getValueBlob() {
+        return valueBlob;
+    }
+
+    public void setValueBlob(String valueBlob) {
+        this.valueBlob = valueBlob;
+    }
+
+    public String getValueClob() {
+        return valueClob;
+    }
+
+    public void setValueClob(String valueClob) {
+        this.valueClob = valueClob;
     }
 }
