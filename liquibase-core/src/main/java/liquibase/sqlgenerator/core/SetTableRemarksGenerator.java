111ca7eb982f3aa8f9e8bbe9579b5ce6c6a40cd3
Nathan Voxland <nathan@voxland.net>
2012-04-23 08:58:09 -0500

    implement set column/table remarks for Db2i database (cherry picked from commit 7c2930f)

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetTableRemarksGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetTableRemarksGenerator.java
@@ -1,41 +1,49 @@
 package liquibase.sqlgenerator.core;
 
 import liquibase.database.Database;
+import liquibase.database.core.DB2Database;
+import liquibase.database.core.DB2iDatabase;
 import liquibase.database.core.MySQLDatabase;
 import liquibase.database.core.OracleDatabase;
 import liquibase.database.core.PostgresDatabase;
 import liquibase.exception.ValidationErrors;
 import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
-import liquibase.sqlgenerator.SqlGenerator;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.statement.core.SetTableRemarksStatement;
 
 public class SetTableRemarksGenerator extends AbstractSqlGenerator<SetTableRemarksStatement> {
 
-    @Override
-    public boolean supports(SetTableRemarksStatement statement, Database database) {
-        return database instanceof MySQLDatabase || database instanceof OracleDatabase || database instanceof PostgresDatabase;
-    }
+	@Override
+	public boolean supports(SetTableRemarksStatement statement, Database database) {
+		return database instanceof MySQLDatabase || database instanceof OracleDatabase || database instanceof PostgresDatabase
+				|| database instanceof DB2Database;
+	}
 
-    public ValidationErrors validate(SetTableRemarksStatement setTableRemarksStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        ValidationErrors validationErrors = new ValidationErrors();
-        validationErrors.checkRequiredField("tableName", setTableRemarksStatement.getTableName());
-        validationErrors.checkRequiredField("remarks", setTableRemarksStatement.getRemarks());
-        return validationErrors;
-    }
+	public ValidationErrors validate(SetTableRemarksStatement setTableRemarksStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+		ValidationErrors validationErrors = new ValidationErrors();
+		validationErrors.checkRequiredField("tableName", setTableRemarksStatement.getTableName());
+		validationErrors.checkRequiredField("remarks", setTableRemarksStatement.getRemarks());
+		return validationErrors;
+	}
 
-    public Sql[] generateSql(SetTableRemarksStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        String sql;
-        String remarks = database.escapeStringForDatabase(statement.getRemarks());
-        if (database instanceof MySQLDatabase) {
-            sql = "ALTER TABLE "+database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName())+" COMMENT = '"+remarks+"'";
-        } else {
-            sql = "COMMENT ON TABLE "+database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName())+" IS '"+remarks+"'";
-        }
+	public Sql[] generateSql(SetTableRemarksStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+		String sql;
+		String remarks = database.escapeStringForDatabase(statement.getRemarks());
+		if (database instanceof MySQLDatabase) {
+			sql = "ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " COMMENT = '" + remarks
+					+ "'";
+		} else {
+			String command = "COMMENT";
 
-        return new Sql[] {
-                new UnparsedSql(sql)
-        };
-    }
+			if (database instanceof DB2iDatabase) {
+				command = "LABEL";
+			}
+
+			sql = command + " ON TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " IS '"
+					+ database.escapeStringForDatabase(remarks) + "'";
+		}
+
+		return new Sql[] { new UnparsedSql(sql) };
+	}
 }
