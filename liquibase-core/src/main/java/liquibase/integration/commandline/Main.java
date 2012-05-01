69925c065a61f0a4be568ba9621502db8e0c21f8
Nathan Voxland <nathan@voxland.net>
2012-05-01 11:18:50 -0500

    CORE-1149 updateSQL generates an empty file using standard output

--- liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
+++ liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
@@ -970,7 +970,7 @@ public class Main {
     }
 
     private Writer getOutputWriter() {
-        return new OutputStreamWriter(System.err);
+        return new OutputStreamWriter(System.out);
     }
 
     public boolean isWindows() {
