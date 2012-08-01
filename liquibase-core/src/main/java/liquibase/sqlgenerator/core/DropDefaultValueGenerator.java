11d895c4769dd77201ac4f528c2d56781fc68021
Laird Nelson <ljnelson@gmail.com>
2012-08-01 08:14:10 -0700

    Fixing https://liquibase.jira.com/browse/CORE-1141

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/DropDefaultValueGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/DropDefaultValueGenerator.java
@@ -40,7 +40,7 @@ public class DropDefaultValueGenerator extends AbstractSqlGenerator<DropDefaultV
              } catch (DatabaseException e) {
                  throw new UnexpectedLiquibaseException(e);
              }
-             if(productVersion.startsWith("9") || productVersion.startsWith("10")) { // SQL Server 2005/2008
+             if(productVersion.startsWith("9") || productVersion.startsWith("10") || productVersion.startsWith("11")) { // SQL Server 2005/2008/2012
 			      // SQL Server 2005 does not often work with the simpler query shown below
         			String query = "DECLARE @default sysname\n";
         			query += "SELECT @default = object_name(default_object_id) FROM sys.columns WHERE object_id=object_id('" + statement.getSchemaName() + "." + statement.getTableName() + "') AND name='" + statement.getColumnName() + "'\n";
