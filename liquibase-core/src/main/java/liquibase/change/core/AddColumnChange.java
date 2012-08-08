6083378877c89ac28ee93c4aa779224334e61197
Alex Wenckus <alex.wenckus@cedexis.com>
2012-08-08 14:49:26 -0700

    Rollback of a column with a default value where the schema was specified was failing. The schema was not being added to the DropDefaultValue call.

--- liquibase-core/src/main/java/liquibase/change/core/AddColumnChange.java
+++ liquibase-core/src/main/java/liquibase/change/core/AddColumnChange.java
@@ -150,6 +150,7 @@ public class AddColumnChange extends AbstractChange implements ChangeWithColumns
                 DropDefaultValueChange dropChange = new DropDefaultValueChange();
                 dropChange.setTableName(getTableName());
                 dropChange.setColumnName(aColumn.getName());
+                dropChange.setSchemaName(getSchemaName());
 
                 inverses.add(dropChange);
             }
