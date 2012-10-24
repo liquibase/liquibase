47a2cf6d7b51ecd3fed18b3bb01bcf3efaf37ded
Steve Suehs <suehs@datical.com>
2012-10-24 14:45:54 -0500

    Mock out ChangeSet.getId to fix test on visitor.

--- liquibase-core/src/test/java/liquibase/changelog/visitor/UpdateVisitorTest.java
+++ liquibase-core/src/test/java/liquibase/changelog/visitor/UpdateVisitorTest.java
@@ -13,6 +13,7 @@ public class UpdateVisitorTest {
         Database database = createMock(Database.class);
 
         ChangeSet changeSet = createMock(ChangeSet.class);
+        expect(changeSet.getId()).andReturn("A1");
         expect(changeSet.execute(new DatabaseChangeLog("test.xml"), database)).andReturn(ChangeSet.ExecType.EXECUTED);
 
 
