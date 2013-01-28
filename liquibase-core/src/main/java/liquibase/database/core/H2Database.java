22e1b224bd6fa2169e37138291c16fbeeeab937c
Damien Biggs <dbiggs@vmware.com>
2013-01-28 15:22:43 -0500

    Fix for CORE-1251
    Overide doGetDefaultSchmea instead of getDefaultSchema so an alternative default schema can actually be set.

--- liquibase-core/src/main/java/liquibase/database/core/H2Database.java
+++ liquibase-core/src/main/java/liquibase/database/core/H2Database.java
@@ -144,9 +144,8 @@ public class H2Database extends AbstractJdbcDatabase {
         return true;
     }
 
-
     @Override
-    public String getDefaultSchemaName() {
+    protected String doGetDefaultSchemaName() {
         return "PUBLIC";
     }
 
