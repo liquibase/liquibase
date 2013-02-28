539fc1def13136805f43693b4fb08a71f964b5d0
nathan.voxland <nathan@voxland.net>
2013-02-28 00:33:46 -0600

    CORE-1263 markNextChangeSetRan wrong EXECTYPE

--- liquibase-core/src/test/java/liquibase/sqlgenerator/core/MarkChangeSetRanGeneratorTest.java
+++ liquibase-core/src/test/java/liquibase/sqlgenerator/core/MarkChangeSetRanGeneratorTest.java
@@ -1,8 +1,17 @@
 package liquibase.sqlgenerator.core;
 
+import junit.framework.Assert;
 import liquibase.changelog.ChangeSet;
+import liquibase.database.core.MockDatabase;
+import liquibase.database.core.MySQLDatabase;
+import liquibase.sql.Sql;
 import liquibase.sqlgenerator.AbstractSqlGeneratorTest;
+import liquibase.sqlgenerator.MockSqlGeneratorChain;
 import liquibase.statement.core.MarkChangeSetRanStatement;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 public class MarkChangeSetRanGeneratorTest extends AbstractSqlGeneratorTest<MarkChangeSetRanStatement> {
     public MarkChangeSetRanGeneratorTest() throws Exception {
@@ -11,5 +20,12 @@ public class MarkChangeSetRanGeneratorTest extends AbstractSqlGeneratorTest<Mark
 
     protected MarkChangeSetRanStatement createSampleSqlStatement() {
         return new MarkChangeSetRanStatement(new ChangeSet("1", "a", false, false, "c", null, null), ChangeSet.ExecType.EXECUTED);
+    }
+
+    @Test
+    public void generateSql_markRan() {
+        Sql[] sqls = new MarkChangeSetRanGenerator().generateSql(new MarkChangeSetRanStatement(new ChangeSet("1", "a", false, false, "c", null, null), ChangeSet.ExecType.MARK_RAN), new MockDatabase(), new MockSqlGeneratorChain());
+        assertEquals(1, sqls.length);
+        assertTrue(sqls[0].toSql(), sqls[0].toSql().contains("MARK_RAN"));
     }
 }
