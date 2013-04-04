fa3c06bd070edfa8b55830fb378db3b34c2fa7dc
nathan.voxland <nathan@voxland.net>
2013-04-04 00:08:29 -0500

    Moved DiffResultAssertion to more standard package

---
+++ liquibase-core/src/test/java/liquibase/test/DiffResultAssert.java
@@ -0,0 +1,38 @@
+package liquibase.test;
+
+import liquibase.structure.core.ForeignKey;
+import liquibase.diff.DiffResult;
+import static org.junit.Assert.*;
+
+/**
+ * Utility class to make asserts on diffresults
+ * @author lujop
+ */
+public class DiffResultAssert {
+    private DiffResult diff;
+
+    private DiffResultAssert() {        
+    }
+    
+    /**
+     * Constructs a DiffResultAssert to make assertions on a diffresult     
+     */
+    public static DiffResultAssert assertThat(DiffResult diffResult) {
+        DiffResultAssert da=new DiffResultAssert();
+        da.diff=diffResult;
+        return da;
+    }
+
+    /**
+     * Checks that diffresult contains a foreign key with the given name
+     * @param fkName Foreign key name
+     */
+    public DiffResultAssert containsMissingForeignKeyWithName(String fkName) {
+        for(ForeignKey fk:diff.getMissingObjects(ForeignKey.class)) {
+            if(fk.getName().equalsIgnoreCase(fkName))
+                return this;
+        }
+        fail("Foreign key with name "+fkName+" not found");
+        return this;
+    }
+}
