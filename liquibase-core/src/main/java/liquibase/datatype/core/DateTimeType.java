1e9d598fd69a934d491f9a0e0d4bbdfe883e4834
Nathan Voxland <nathan@voxland.net>
2013-08-15 15:51:27 -0500

    CORE-1370 Oracle doesnt have DATETIME datatype (cherry picked from commit b519a22)

--- liquibase-core/src/main/java/liquibase/datatype/core/DateTimeType.java
+++ liquibase-core/src/main/java/liquibase/datatype/core/DateTimeType.java
@@ -31,6 +31,10 @@ public class DateTimeType extends LiquibaseDataType {
             return new DatabaseDataType("TEXT");
         }
 
+        if (database instanceof OracleDatabase) {
+            return new DatabaseDataType("DATE");
+        }
+
         return new DatabaseDataType(getName());
     }
 
