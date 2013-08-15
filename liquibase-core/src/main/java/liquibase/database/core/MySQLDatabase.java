3b908c16d5eca4c6d683d5634bc9ac48c4458f58
Nathan Voxland <nathan@voxland.net>
2013-08-15 15:50:08 -0500

    CORE-1371 MySQL syntax error is thrown if schema name contains hyphens

--- liquibase-core/src/main/java/liquibase/database/core/MySQLDatabase.java
+++ liquibase-core/src/main/java/liquibase/database/core/MySQLDatabase.java
@@ -147,6 +147,11 @@ public class MySQLDatabase extends AbstractJdbcDatabase {
     }
 
     @Override
+    public String escapeObjectName(String objectName, Class<? extends DatabaseObject> objectType) {
+        return quotingStartCharacter+objectName+quotingEndCharacter;
+    }
+
+    @Override
     public boolean supportsForeignKeyDisable() {
         return true;
     }
