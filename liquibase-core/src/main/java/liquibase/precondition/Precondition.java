a98b4e1596cf69285dccf0fc452aa7087b54a45d
adriens <Adrien.Sales@gmail.com>
2013-08-16 12:53:50 +1100

    Removed unused import

--- liquibase-core/src/main/java/liquibase/precondition/Precondition.java
+++ liquibase-core/src/main/java/liquibase/precondition/Precondition.java
@@ -7,7 +7,6 @@ import liquibase.exception.PreconditionErrorException;
 import liquibase.exception.PreconditionFailedException;
 import liquibase.exception.ValidationErrors;
 import liquibase.exception.Warnings;
-import liquibase.serializer.LiquibaseSerializable;
 
 /**
  * Marker interface for preconditions.  May become an annotation in the future.
