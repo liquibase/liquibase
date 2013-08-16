f7175a2b8a74a414aecf5a13fa31472a17166446
adriens <Adrien.Sales@gmail.com>
2013-08-16 11:41:57 +1100

    Removed all unused imports

--- liquibase-core/src/main/java/liquibase/change/DatabaseChangeNote.java
+++ liquibase-core/src/main/java/liquibase/change/DatabaseChangeNote.java
@@ -1,8 +1,5 @@
 package liquibase.change;
 
-import liquibase.database.Database;
-import liquibase.database.core.UnsupportedDatabase;
-
 public @interface DatabaseChangeNote {
     public String database() default "";
     public String notes() default "";
