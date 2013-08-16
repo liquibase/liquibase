d3be34857affd89095345d86bf1f90e2cf4b0d4e
adriens <Adrien.Sales@gmail.com>
2013-08-16 12:48:13 +1100

    Removed unused import

--- liquibase-core/src/main/java/liquibase/integration/ant/AntResourceAccessor.java
+++ liquibase-core/src/main/java/liquibase/integration/ant/AntResourceAccessor.java
@@ -2,17 +2,12 @@ package liquibase.integration.ant;
 
 import liquibase.resource.ClassLoaderResourceAccessor;
 import liquibase.resource.CompositeResourceAccessor;
-import liquibase.resource.ResourceAccessor;
 import org.apache.tools.ant.AntClassLoader;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.Path;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.util.Enumeration;
 
 /**
  * An implementation of FileOpener that is specific to how Ant works.
