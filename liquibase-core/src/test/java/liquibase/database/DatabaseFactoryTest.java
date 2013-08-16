f2d4c4731e83a7b4e8a9e4870076901529ed5332
adriens <Adrien.Sales@gmail.com>
2013-08-16 13:22:39 +1100

    Removed all unused imports

--- liquibase-core/src/test/java/liquibase/database/DatabaseFactoryTest.java
+++ liquibase-core/src/test/java/liquibase/database/DatabaseFactoryTest.java
@@ -2,10 +2,6 @@ package liquibase.database;
 
 import static org.junit.Assert.*;
 
-import liquibase.database.core.H2Database;
-import liquibase.database.core.MSSQLDatabase;
-import liquibase.database.core.MySQLDatabase;
-import liquibase.database.core.OracleDatabase;
 import org.junit.Test;
 
 public class DatabaseFactoryTest {
