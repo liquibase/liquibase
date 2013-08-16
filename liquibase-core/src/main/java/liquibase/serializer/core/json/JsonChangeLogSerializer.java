aa19045bcf9a8a00c38beb7343ba30fcaafb6ec0
adriens <Adrien.Sales@gmail.com>
2013-08-16 12:57:06 +1100

    Removed unused import

--- liquibase-core/src/main/java/liquibase/serializer/core/json/JsonChangeLogSerializer.java
+++ liquibase-core/src/main/java/liquibase/serializer/core/json/JsonChangeLogSerializer.java
@@ -4,9 +4,7 @@ import liquibase.serializer.LiquibaseSerializable;
 import liquibase.serializer.core.yaml.YamlChangeLogSerializer;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.Tag;
-import org.yaml.snakeyaml.representer.Represent;
 
 public class JsonChangeLogSerializer extends YamlChangeLogSerializer {
 
