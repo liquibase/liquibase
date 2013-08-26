44986c4f9eeb2f268bb11ad4c27908ee9954f396
Nathan Voxland <nathan@voxland.net>
2013-08-26 09:58:13 -0500

    CORE-1408NVarcharType on Derby Fails

--- liquibase-core/src/main/java/liquibase/datatype/core/NVarcharType.java
+++ liquibase-core/src/main/java/liquibase/datatype/core/NVarcharType.java
@@ -1,10 +1,7 @@
 package liquibase.datatype.core;
 
 import liquibase.database.Database;
-import liquibase.database.core.HsqlDatabase;
-import liquibase.database.core.MSSQLDatabase;
-import liquibase.database.core.OracleDatabase;
-import liquibase.database.core.PostgresDatabase;
+import liquibase.database.core.*;
 import liquibase.datatype.DataTypeInfo;
 import liquibase.datatype.DatabaseDataType;
 import liquibase.datatype.LiquibaseDataType;
@@ -14,7 +11,7 @@ public class NVarcharType extends CharType {
 
     @Override
     public DatabaseDataType toDatabaseDataType(Database database) {
-        if (database instanceof HsqlDatabase || database instanceof PostgresDatabase) {
+        if (database instanceof HsqlDatabase || database instanceof PostgresDatabase|| database instanceof DerbyDatabase) {
             return new DatabaseDataType("VARCHAR", getParameters());
         }
         if (database instanceof OracleDatabase) {
