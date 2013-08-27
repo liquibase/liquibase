921adca4cb468bb7efada34baf580aae0f7348b9
Nathan Voxland <nathan@voxland.net>
2013-08-27 09:16:12 -0500

    CORE-1418 Foreign Key changes not detected in snapshot process

--- liquibase-core/src/main/java/liquibase/diff/compare/core/ForeignKeyComparator.java
+++ liquibase-core/src/main/java/liquibase/diff/compare/core/ForeignKeyComparator.java
@@ -8,6 +8,8 @@ import liquibase.diff.compare.DatabaseObjectComparatorFactory;
 import liquibase.structure.DatabaseObject;
 import liquibase.structure.core.Column;
 import liquibase.structure.core.ForeignKey;
+import liquibase.structure.core.Table;
+import liquibase.util.StringUtils;
 
 public class ForeignKeyComparator implements DatabaseObjectComparator {
     public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
@@ -18,7 +20,33 @@ public class ForeignKeyComparator implements DatabaseObjectComparator {
     }
 
     public boolean isSameObject(DatabaseObject databaseObject1, DatabaseObject databaseObject2, Database accordingTo, DatabaseObjectComparatorChain chain) {
-        return chain.isSameObject(databaseObject1, databaseObject2, accordingTo);
+        if (!(databaseObject1 instanceof ForeignKey && databaseObject2 instanceof ForeignKey)) {
+            return false;
+        }
+
+        ForeignKey thisForeignKey = (ForeignKey) databaseObject1;
+        ForeignKey otherForeignKey = (ForeignKey) databaseObject2;
+
+        if (thisForeignKey.getName() != null && databaseObject2.getName() != null) {
+            if (chain.isSameObject(thisForeignKey, databaseObject2, accordingTo)) {
+                return true;
+            }
+        }
+
+        boolean columnsTheSame;
+        if (accordingTo.isCaseSensitive()) {
+            columnsTheSame = StringUtils.trimToEmpty(((ForeignKey) databaseObject1).getForeignKeyColumns()).equals(StringUtils.trimToEmpty(((ForeignKey) databaseObject2).getForeignKeyColumns())) &&
+                    StringUtils.trimToEmpty(((ForeignKey) databaseObject1).getPrimaryKeyColumns()).equals(StringUtils.trimToEmpty(((ForeignKey) databaseObject2).getPrimaryKeyColumns()));
+        } else {
+            columnsTheSame = ((ForeignKey) databaseObject1).getForeignKeyColumns().equalsIgnoreCase(((ForeignKey) databaseObject2).getForeignKeyColumns()) &&
+                    ((ForeignKey) databaseObject1).getPrimaryKeyColumns().equalsIgnoreCase(((ForeignKey) databaseObject2).getPrimaryKeyColumns());
+
+        }
+
+        return columnsTheSame &&
+                DatabaseObjectComparatorFactory.getInstance().isSameObject(thisForeignKey.getForeignKeyTable(), otherForeignKey.getForeignKeyTable(), accordingTo) &&
+                DatabaseObjectComparatorFactory.getInstance().isSameObject(thisForeignKey.getPrimaryKeyTable(), otherForeignKey.getPrimaryKeyTable(), accordingTo);
+
     }
 
 
@@ -28,7 +56,10 @@ public class ForeignKeyComparator implements DatabaseObjectComparator {
         differences.removeDifference("backingIndex");
 
         differences.removeDifference("columnNames");
-        differences.compare("columnNames", databaseObject1, databaseObject2, new ObjectDifferences.DatabaseObjectNameCompareFunction(Column.class, accordingTo));
+        differences.compare("foreignKeyColumns", databaseObject1, databaseObject2, new ObjectDifferences.DatabaseObjectNameCompareFunction(Column.class, accordingTo));
+        differences.compare("primaryKeyColumns", databaseObject1, databaseObject2, new ObjectDifferences.DatabaseObjectNameCompareFunction(Column.class, accordingTo));
+        differences.compare("foreignKeyTable", databaseObject1, databaseObject2, new ObjectDifferences.DatabaseObjectNameCompareFunction(Table.class, accordingTo));
+        differences.compare("primaryKeyTable", databaseObject1, databaseObject2, new ObjectDifferences.DatabaseObjectNameCompareFunction(Table.class, accordingTo));
         return differences;
     }
 }
---
+++ liquibase-core/src/main/java/liquibase/diff/output/changelog/core/ChangedForeignKeyChangeGenerator.java
@@ -0,0 +1,65 @@
+package liquibase.diff.output.changelog.core;
+
+import liquibase.change.Change;
+import liquibase.change.core.AddForeignKeyConstraintChange;
+import liquibase.change.core.DropForeignKeyConstraintChange;
+import liquibase.database.Database;
+import liquibase.diff.ObjectDifferences;
+import liquibase.diff.output.DiffOutputControl;
+import liquibase.diff.output.changelog.ChangeGeneratorChain;
+import liquibase.diff.output.changelog.ChangedObjectChangeGenerator;
+import liquibase.structure.DatabaseObject;
+import liquibase.structure.core.Index;
+import liquibase.structure.core.ForeignKey;
+import liquibase.structure.core.UniqueConstraint;
+
+public class ChangedForeignKeyChangeGenerator implements ChangedObjectChangeGenerator {
+    public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
+        if (ForeignKey.class.isAssignableFrom(objectType)) {
+            return PRIORITY_DEFAULT;
+        }
+        return PRIORITY_NONE;
+    }
+
+    public Class<? extends DatabaseObject>[] runBeforeTypes() {
+        return new Class[] {Index.class, UniqueConstraint.class };
+    }
+
+    public Class<? extends DatabaseObject>[] runAfterTypes() {
+        return null;
+    }
+
+    public Change[] fixChanged(DatabaseObject changedObject, ObjectDifferences differences, DiffOutputControl control, Database referenceDatabase, Database comparisonDatabase, ChangeGeneratorChain chain) {
+        ForeignKey fk = (ForeignKey) changedObject;
+
+        DropForeignKeyConstraintChange dropFkChange = new DropForeignKeyConstraintChange();
+        dropFkChange.setConstraintName(fk.getName());
+        dropFkChange.setBaseTableName(fk.getForeignKeyTable().getName());
+
+        AddForeignKeyConstraintChange addFkChange = new AddForeignKeyConstraintChange();
+        addFkChange.setConstraintName(fk.getName());
+        addFkChange.setBaseTableName(fk.getForeignKeyTable().getName());
+        addFkChange.setBaseColumnNames(fk.getForeignKeyColumns());
+        addFkChange.setReferencedTableName(fk.getPrimaryKeyTable().getName());
+        addFkChange.setReferencedColumnNames(fk.getPrimaryKeyColumns());
+
+        if (control.isIncludeCatalog()) {
+            dropFkChange.setBaseTableCatalogName(fk.getForeignKeyTable().getSchema().getCatalogName());
+
+            addFkChange.setBaseTableCatalogName(fk.getForeignKeyTable().getSchema().getCatalogName());
+            addFkChange.setReferencedTableCatalogName(fk.getPrimaryKeyTable().getSchema().getCatalogName());
+        }
+        if (control.isIncludeSchema()) {
+            dropFkChange.setBaseTableSchemaName(fk.getForeignKeyTable().getSchema().getName());
+
+            addFkChange.setBaseTableSchemaName(fk.getForeignKeyTable().getSchema().getName());
+            addFkChange.setReferencedTableSchemaName(fk.getPrimaryKeyTable().getSchema().getName());
+        }
+
+        if (fk.getBackingIndex() != null) {
+            control.setAlreadyHandledChanged(fk.getBackingIndex());
+        }
+
+        return new Change[] { dropFkChange, addFkChange };
+    }
+}
