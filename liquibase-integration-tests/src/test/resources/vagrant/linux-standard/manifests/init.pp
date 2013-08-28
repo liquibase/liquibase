841ab558b3ce26b269d196103a98eb89345247be
Nathan Voxland <nathan@voxland.net>
2013-08-28 09:32:26 -0500

    CORE-1398 Missing or unexpected unique constraint not found on postgres

--- liquibase-integration-tests/src/test/resources/vagrant/linux-standard/manifests/init.pp
+++ liquibase-integration-tests/src/test/resources/vagrant/linux-standard/manifests/init.pp
@@ -17,3 +17,19 @@ mysql::db { 'liquibaseb':
   host     => '%',
   grant    => ['all'],
 }
+
+
+class { 'postgresql::server':
+  config_hash => {
+    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+    'ip_mask_allow_all_users'    => '0.0.0.0/0',
+    'listen_addresses'           => '*',
+    'ipv4acls'                   => ['host all liquibase 0.0.0.0/0 password'],
+    'postgres_password'          => 'postgres'
+  },
+}
+
+postgresql::db { 'liquibase':
+  user     => 'liquibase',
+  password => 'liquibase'
+}
