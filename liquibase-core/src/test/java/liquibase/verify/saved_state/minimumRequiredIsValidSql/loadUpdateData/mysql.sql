d8a278e12252a57ac21a1921f62f94ca691ec192
Nathan Voxland <nathan@voxland.net>
2013-10-22 16:33:25 -0500

    Handle object names that end up with spaces around them

--- liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/loadUpdateData/mysql.sql
+++ liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/loadUpdateData/mysql.sql
@@ -6,7 +6,7 @@
 -- Change Parameter: file=com/example/users.csv
 -- Change Parameter: primaryKey=pk_id
 -- Change Parameter: tableName=person
-INSERT INTO person (username,  fullname) VALUES ('nvoxland', ' Nathan Voxland')
+INSERT INTO person (username, fullname) VALUES ('nvoxland', ' Nathan Voxland')
 ON DUPLICATE KEY UPDATE username = 'nvoxland', fullname = ' Nathan Voxland';
-INSERT INTO person (username,  fullname) VALUES ('bob', ' Bob Bobson')
+INSERT INTO person (username, fullname) VALUES ('bob', ' Bob Bobson')
 ON DUPLICATE KEY UPDATE username = 'bob', fullname = ' Bob Bobson';
