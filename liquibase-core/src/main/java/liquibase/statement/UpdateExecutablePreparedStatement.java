6cf4a684ae12950f735d3b544a8b6b36875eb54a
dmitrygusev <dmitry.gusev@gmail.com>
2013-11-07 12:18:34 -0600

    Add WHERE clause to UpdateExecutablePreparedStatement (cherry picked from commit 33a2b19)

--- liquibase-core/src/main/java/liquibase/statement/UpdateExecutablePreparedStatement.java
+++ liquibase-core/src/main/java/liquibase/statement/UpdateExecutablePreparedStatement.java
@@ -1,12 +1,21 @@
 package liquibase.statement;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import liquibase.change.ColumnConfig;
 import liquibase.changelog.ChangeSet;
 import liquibase.database.Database;
+import liquibase.datatype.DataTypeFactory;
+import liquibase.structure.core.Column;
 
 public class UpdateExecutablePreparedStatement extends ExecutablePreparedStatementBase {
+
+    private String whereClause;
+
+    private List<String> whereColumnNames = new ArrayList<String>();
+    private List<Object> whereParameters = new ArrayList<Object>();
 
 	public UpdateExecutablePreparedStatement(Database database, String catalogName, String schemaName, String tableName, List<ColumnConfig> columns, ChangeSet changeSet) {
 		super(database, catalogName, schemaName, tableName, columns, changeSet);
@@ -27,6 +36,58 @@ public class UpdateExecutablePreparedStatement extends ExecutablePreparedStateme
 	    params.deleteCharAt(params.lastIndexOf(" "));
 	    params.deleteCharAt(params.lastIndexOf(","));
 	    sql.append(params);
+        if (getWhereClause() != null) {
+            String fixedWhereClause = "WHERE " + getWhereClause().trim();
+            for (String columnName : getWhereColumnNames()) {
+                if (columnName == null) {
+                    continue;
+                }
+                fixedWhereClause = fixedWhereClause.replaceFirst(":name",
+                        database.escapeObjectName(columnName, Column.class));
+            }
+            for (Object param : getWhereParameters()) {
+                fixedWhereClause = fixedWhereClause.replaceFirst("\\?|:value", DataTypeFactory.getInstance().fromObject(param, database).objectToSql(param, database));
+            }
+            sql.append(" ").append(fixedWhereClause);
+        }
+
 		return sql.toString();		
 	}
+	
+
+    public String getWhereClause() {
+        return whereClause;
+    }
+
+    public UpdateExecutablePreparedStatement setWhereClause(String whereClause) {
+        this.whereClause = whereClause;
+
+        return this;
+    }
+
+    public UpdateExecutablePreparedStatement addWhereParameter(Object value) {
+        this.whereParameters.add(value);
+
+        return this;
+    }
+
+    public UpdateExecutablePreparedStatement addWhereParameters(Object... value) {
+        this.whereParameters.addAll(Arrays.asList(value));
+
+        return this;
+    }
+
+    public UpdateExecutablePreparedStatement addWhereColumnName(String value) {
+        this.whereColumnNames.add(value);
+
+        return this;
+    }
+
+    public List<Object> getWhereParameters() {
+        return whereParameters;
+    }
+
+    public List<String> getWhereColumnNames() {
+        return whereColumnNames;
+    }
 }
