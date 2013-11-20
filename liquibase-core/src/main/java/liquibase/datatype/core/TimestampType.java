85fe3c47f307bb309a04cd51856c1dff32dc1483
Nathan Voxland <nathan@voxland.net>
2013-11-20 15:13:54 -0600

    CORE-1607 MSSQL java.sql.Types.TIMESTAMP should map to DateTime

--- liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
+++ liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
@@ -1,6 +1,7 @@
 package liquibase.datatype.core;
 
 import liquibase.database.Database;
+import liquibase.database.core.MSSQLDatabase;
 import liquibase.database.core.MySQLDatabase;
 import liquibase.datatype.DataTypeInfo;
 import liquibase.datatype.DatabaseDataType;
@@ -12,6 +13,9 @@ public class TimestampType extends DateTimeType {
     public DatabaseDataType toDatabaseDataType(Database database) {
         if (database instanceof MySQLDatabase) {
             return new DatabaseDataType("TIMESTAMP");
+        }
+        if (database instanceof MSSQLDatabase) {
+            return new DatabaseDataType("DATETIME");
         }
         return super.toDatabaseDataType(database);
     }
