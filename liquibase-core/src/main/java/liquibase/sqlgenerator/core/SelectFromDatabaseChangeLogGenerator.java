26a9a1e9f8ed7d9e1d492ad84be641ef118c8a4f
Nathan Voxland <nathan@voxland.net>
2013-11-26 13:47:02 -0600

    Need to escape databaasechangelog columns in select, in particular for redshift where "TAG" is a keyword

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/SelectFromDatabaseChangeLogGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/SelectFromDatabaseChangeLogGenerator.java
@@ -10,6 +10,9 @@ import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.statement.core.SelectFromDatabaseChangeLogStatement;
 import liquibase.util.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SelectFromDatabaseChangeLogGenerator extends AbstractSqlGenerator<SelectFromDatabaseChangeLogStatement> {
 
     @Override
@@ -22,13 +25,17 @@ public class SelectFromDatabaseChangeLogGenerator extends AbstractSqlGenerator<S
 
     @Override
     public Sql[] generateSql(SelectFromDatabaseChangeLogStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        String sql = "SELECT " + StringUtils.join(statement.getColumnsToSelect(), ",").toUpperCase() + " FROM " +
+        List<String> columnsToSelect = Arrays.asList(statement.getColumnsToSelect());
+        for (int i=0; i<columnsToSelect.size(); i++) {
+            columnsToSelect.set(i, database.escapeColumnName(null, null, null, columnsToSelect.get(i)));
+        }
+        String sql = "SELECT " + StringUtils.join(columnsToSelect, ",").toUpperCase() + " FROM " +
                 database.escapeTableName(database.getLiquibaseCatalogName(), database.getLiquibaseSchemaName(), database.getDatabaseChangeLogTableName());
 
         SelectFromDatabaseChangeLogStatement.WhereClause whereClause = statement.getWhereClause();
         if (whereClause != null) {
             if (whereClause instanceof SelectFromDatabaseChangeLogStatement.ByTag) {
-                sql += " WHERE TAG='" + ((SelectFromDatabaseChangeLogStatement.ByTag) whereClause).getTagName() + "'";
+                sql += " WHERE "+database.escapeColumnName(null, null, null, "TAG")+"='" + ((SelectFromDatabaseChangeLogStatement.ByTag) whereClause).getTagName() + "'";
             } else if (whereClause instanceof SelectFromDatabaseChangeLogStatement.ByNotNullCheckSum) {
                     sql += " WHERE MD5SUM IS NOT NULL";
             } else {
