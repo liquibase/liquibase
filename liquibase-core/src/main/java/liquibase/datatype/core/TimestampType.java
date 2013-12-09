7016c86af3e26f006d05ad19b4b5ae2865c1328e
Nathan Voxland <nathan@voxland.net>
2013-12-09 15:07:36 -0600

    CORE-1581 modifyDataType ignores additional info of newDataType when using something like "TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"

--- liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
+++ liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
@@ -9,14 +9,24 @@ import liquibase.datatype.LiquibaseDataType;
 
 @DataTypeInfo(name = "timestamp", aliases = {"timestamp", "java.sql.Types.TIMESTAMP", "java.sql.Timestamp"}, minParameters = 0, maxParameters = 1, priority = LiquibaseDataType.PRIORITY_DEFAULT)
 public class TimestampType extends DateTimeType {
+    private String originalDefinition;
+
     @Override
     public DatabaseDataType toDatabaseDataType(Database database) {
         if (database instanceof MySQLDatabase) {
+            if (originalDefinition.contains(" ")) {
+                return new DatabaseDataType(originalDefinition);
+            }
             return new DatabaseDataType("TIMESTAMP");
         }
         if (database instanceof MSSQLDatabase) {
             return new DatabaseDataType("DATETIME");
         }
         return super.toDatabaseDataType(database);
+    }
+
+    @Override
+    public void finishInitialization(String originalDefinition) {
+        this.originalDefinition = originalDefinition.trim();
     }
 }
