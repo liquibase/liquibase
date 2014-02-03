4a36034ac3d84f5b030bd29b396a655934c88d4f
Nathan Voxland <nathan@voxland.net>
2014-02-03 11:45:06 -0600

    CORE-1757 Oracle: do not assume NUMBER(38,0) for columns defined as NUMBER without params

--- liquibase-core/src/main/java/liquibase/snapshot/jvm/ColumnSnapshotGenerator.java
+++ liquibase-core/src/main/java/liquibase/snapshot/jvm/ColumnSnapshotGenerator.java
@@ -187,13 +187,13 @@ public class ColumnSnapshotGenerator extends JdbcSnapshotGenerator {
 //            type.setDataTypeId(dataType);
             if (dataType.equalsIgnoreCase("NUMBER")) {
                 type.setColumnSize(columnMetadataResultSet.getInt("DATA_PRECISION"));
-                if (type.getColumnSize() == null) {
-                    type.setColumnSize(38);
-                }
+//                if (type.getColumnSize() == null) {
+//                    type.setColumnSize(38);
+//                }
                 type.setDecimalDigits(columnMetadataResultSet.getInt("DATA_SCALE"));
-                if (type.getDecimalDigits() == null) {
-                    type.setDecimalDigits(0);
-                }
+//                if (type.getDecimalDigits() == null) {
+//                    type.setDecimalDigits(0);
+//                }
 //            type.setRadix(10);
             } else {
                 type.setColumnSize(columnMetadataResultSet.getInt("DATA_LENGTH"));
