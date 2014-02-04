caf4f4623537e09e12899620c46ae7b109fe8f92
Nathan Voxland <nathan@voxland.net>
2014-02-04 16:02:51 -0600

    Updated expected sql

--- liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/addNotNullConstraint/db2.sql
+++ liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/addNotNullConstraint/db2.sql
@@ -2,3 +2,5 @@
 -- Change Parameter: columnName=id
 -- Change Parameter: tableName=person
 ALTER TABLE person ALTER COLUMN  id SET NOT NULL;
+CALL SYSPROC.ADMIN_CMD ('REORG TABLE person');
+CALL SYSPROC.ADMIN_CMD ('REORG TABLE person');
