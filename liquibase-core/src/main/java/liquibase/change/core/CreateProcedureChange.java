4c1a5cb1513830bf1e689aa16d980e206f63c5da
Björn Kautler <Bjoern@Kautler.net>
2014-02-09 19:38:38 +0100

    make the check sum generation of the createProcedure change independent of the default encoding which is different on different OS

--- liquibase-core/src/main/java/liquibase/change/core/CreateProcedureChange.java
+++ liquibase-core/src/main/java/liquibase/change/core/CreateProcedureChange.java
@@ -14,6 +14,7 @@ import liquibase.util.StringUtils;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 @DatabaseChange(name = "createProcedure",
         description = "Defines the definition for a stored procedure. This command is better to use for creating procedures than the raw sql command because it will not attempt to strip comments or break up lines.\n\nOften times it is best to use the CREATE OR REPLACE syntax along with setting runOnChange='true' on the enclosing changeSet tag. That way if you need to make a change to your procedure you can simply change your existing code rather than creating a new REPLACE PROCEDURE call. The advantage to this approach is that it keeps your change log smaller and allows you to more easily see what has changed in your procedure code through your source control system's diff command.",
@@ -178,7 +179,11 @@ public class CreateProcedureChange extends AbstractChange implements DbmsTargete
         }
 
         if (procedureText != null) {
-            stream = new ByteArrayInputStream(procedureText.getBytes());
+            try {
+                stream = new ByteArrayInputStream(procedureText.getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                throw new AssertionError("UTF-8 is not supported by the JVM, this should not happen according to the JavaDoc of the Charset class");
+            }
         }
 
         CheckSum checkSum = CheckSum.compute(new AbstractSQLChange.NormalizingStream(";", false, false, stream), false);
