61e99faf1a3147f70f47b2014884f4e4fa92d49e
unknown <c.henry@EDEV-037-L.EDEV.local>
2014-02-28 10:37:31 +0100

    Database tagging improvement. Solved bug : if two tagging operations are done in the same datetime, the last one overrides the first and a tagging version is lost

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/TagDatabaseGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/TagDatabaseGenerator.java
@@ -13,46 +13,95 @@ import liquibase.statement.SqlStatement;
 import liquibase.statement.core.TagDatabaseStatement;
 import liquibase.statement.core.UpdateStatement;
 
-public class TagDatabaseGenerator extends AbstractSqlGenerator<TagDatabaseStatement> {
+public class TagDatabaseGenerator extends AbstractSqlGenerator<TagDatabaseStatement>
+{
 
     @Override
-    public ValidationErrors validate(TagDatabaseStatement tagDatabaseStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+    public ValidationErrors validate(
+    		TagDatabaseStatement tagDatabaseStatement,
+    		Database database,
+    		SqlGeneratorChain sqlGeneratorChain)
+    {
         ValidationErrors validationErrors = new ValidationErrors();
         validationErrors.checkRequiredField("tag", tagDatabaseStatement.getTag());
         return validationErrors;
     }
 
     @Override
-    public Sql[] generateSql(TagDatabaseStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+    public Sql[] generateSql(
+    		TagDatabaseStatement statement,
+    		Database database,
+    		SqlGeneratorChain sqlGeneratorChain)
+    {
     	String liquibaseSchema = null;
    		liquibaseSchema = database.getLiquibaseSchemaName();
-        UpdateStatement updateStatement = new UpdateStatement(database.getLiquibaseCatalogName(), liquibaseSchema, database.getDatabaseChangeLogTableName());
+        UpdateStatement updateStatement = new UpdateStatement(
+        		database.getLiquibaseCatalogName(),
+        		liquibaseSchema,
+        		database.getDatabaseChangeLogTableName());
         updateStatement.addNewColumnValue("TAG", statement.getTag());
+        
         if (database instanceof MySQLDatabase) {
             try {
                 long version = Long.parseLong(database.getDatabaseProductVersion().substring(0, 1));
 
                 if (version < 5) {
-                    return new Sql[]{
-                            new UnparsedSql("UPDATE "+database.escapeTableName(database.getLiquibaseCatalogName(), database.getLiquibaseSchemaName(), database.getDatabaseChangeLogTableName())+" C LEFT JOIN (SELECT MAX(DATEEXECUTED) as MAXDATE FROM (SELECT DATEEXECUTED FROM `DATABASECHANGELOG`) AS X) D ON C.DATEEXECUTED = D.MAXDATE SET C.TAG = '" + statement.getTag() + "' WHERE D.MAXDATE IS NOT NULL")
+                    return new Sql[] {
+                        new UnparsedSql(
+                        		"UPDATE " + 
+            					database.escapeTableName(
+                    				database.getLiquibaseCatalogName(),
+                    				database.getLiquibaseSchemaName(),
+                    				database.getDatabaseChangeLogTableName()) + 
+                				" C LEFT JOIN (SELECT MAX(DATEEXECUTED) as MAXDATE" + 
+                				"FROM (SELECT DATEEXECUTED FROM `DATABASECHANGELOG`) AS X) D" + 
+                				"ON C.DATEEXECUTED = D.MAXDATE" + 
+                				"SET C.TAG = '" + 
+                				statement.getTag() + 
+                				"' WHERE D.MAXDATE IS NOT NULL" + 
+                				"AND TAG IS NULL")
                     };
                 }
 
-            } catch (Throwable e) {
-                ; //assume it is version 5
             }
-            updateStatement.setWhereClause("DATEEXECUTED = (SELECT MAX(DATEEXECUTED) FROM (SELECT DATEEXECUTED FROM " + database.escapeTableName(database.getLiquibaseCatalogName(), liquibaseSchema, database.getDatabaseChangeLogTableName()) + ") AS X)");
-        } else if (database instanceof InformixDatabase) {
-            return new Sql[]{
-                    new UnparsedSql("SELECT MAX(dateexecuted) max_date FROM " + database.escapeTableName(database.getLiquibaseCatalogName(), liquibaseSchema, database.getDatabaseChangeLogTableName()) + " INTO TEMP max_date_temp WITH NO LOG"),
-                    new UnparsedSql("UPDATE "+database.escapeTableName(database.getLiquibaseCatalogName(), liquibaseSchema, database.getDatabaseChangeLogTableName())+" SET TAG = '"+statement.getTag()+"' WHERE DATEEXECUTED = (SELECT max_date FROM max_date_temp);"),
+            catch (Throwable e)
+            	{; /* assume it is version 5 */}
+            
+            updateStatement.setWhereClause(
+            		"DATEEXECUTED = (SELECT MAX(DATEEXECUTED)" + 
+    				"FROM (SELECT DATEEXECUTED FROM " + 
+    				database.escapeTableName(
+    						database.getLiquibaseCatalogName(),
+    						liquibaseSchema,
+    						database.getDatabaseChangeLogTableName()) + 
+					") AS X) AND TAG IS NULL");
+        } 
+        else if (database instanceof InformixDatabase){
+            return new Sql[] {
+                    new UnparsedSql("SELECT MAX(dateexecuted) max_date FROM " +
+                			database.escapeTableName(database.getLiquibaseCatalogName(),
+                					liquibaseSchema,
+                					database.getDatabaseChangeLogTableName()) +
+        					" INTO TEMP max_date_temp WITH NO LOG"),
+                    new UnparsedSql("UPDATE "+ 
+        					database.escapeTableName(
+        							database.getLiquibaseCatalogName(),
+        							liquibaseSchema,
+        							database.getDatabaseChangeLogTableName()) +
+							" SET TAG = '"+statement.getTag() + 
+							"' WHERE DATEEXECUTED = (SELECT max_date FROM max_date_temp);"),
                     new UnparsedSql("DROP TABLE max_date_temp;")
             };
-        } else {
-            updateStatement.setWhereClause("DATEEXECUTED = (SELECT MAX(DATEEXECUTED) FROM " + database.escapeTableName(database.getLiquibaseCatalogName(), liquibaseSchema, database.getDatabaseChangeLogTableName()) + ")");
         }
-
+        else {
+            updateStatement.setWhereClause(
+            		"DATEEXECUTED = (SELECT MAX(DATEEXECUTED) FROM " +
+    				database.escapeTableName(
+						database.getLiquibaseCatalogName(),
+						liquibaseSchema,
+						database.getDatabaseChangeLogTableName()) +
+					") AND TAG IS NULL");
+        }
         return SqlGeneratorFactory.getInstance().generateSql(updateStatement, database);
-
     }
 }
