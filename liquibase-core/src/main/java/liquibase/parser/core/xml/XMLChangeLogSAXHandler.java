c4d7e65c3e2a38b251a756384dae31849f9db6a4
Nathan Voxland <nathan@voxland.net>
2014-05-12 16:36:37 -0500

    CORE-1874 Improve XML and other parsers for better extensiblity
    Support changelog parameters again

--- liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXHandler.java
+++ liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXHandler.java
@@ -12,11 +12,14 @@ import liquibase.parser.core.ParsedNodeException;
 import liquibase.precondition.PreconditionFactory;
 import liquibase.resource.ResourceAccessor;
 import liquibase.sql.visitor.SqlVisitorFactory;
+import liquibase.util.StreamUtil;
 import liquibase.util.StringUtils;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 class XMLChangeLogSAXHandler extends DefaultHandler {
@@ -44,7 +47,11 @@ class XMLChangeLogSAXHandler extends DefaultHandler {
 		databaseChangeLog.setPhysicalFilePath(physicalChangeLogLocation);
 		databaseChangeLog.setChangeLogParameters(changeLogParameters);
 
-		this.changeLogParameters = changeLogParameters;
+        if (changeLogParameters == null) {
+            this.changeLogParameters = new ChangeLogParameters();
+        } else {
+            this.changeLogParameters = changeLogParameters;
+        }
 
         changeFactory = ChangeFactory.getInstance();
         preconditionFactory = PreconditionFactory.getInstance();
@@ -70,9 +77,38 @@ class XMLChangeLogSAXHandler extends DefaultHandler {
     public void startElement(String uri, String localName, String qualifiedName, Attributes attributes) throws SAXException {
         ParsedNode node = new ParsedNode(null, localName);
         try {
+            if (localName.equals("property")) {
+                String context = changeLogParameters.expandExpressions(StringUtils.trimToNull(attributes.getValue("context")));
+                String dbms = changeLogParameters.expandExpressions(StringUtils.trimToNull(attributes.getValue("dbms")));
+
+                if (StringUtils.trimToNull(attributes.getValue("file")) == null) {
+                    this.changeLogParameters.set(attributes.getValue("name"), changeLogParameters.expandExpressions(attributes.getValue("value")), context, dbms);
+                } else {
+                    Properties props = new Properties();
+                    InputStream propertiesStream = StreamUtil.singleInputStream(attributes.getValue("file"), resourceAccessor);
+                    if (propertiesStream == null) {
+                        log.info("Could not open properties file " + attributes.getValue("file"));
+                    } else {
+                        props.load(propertiesStream);
+
+                        for (Map.Entry entry : props.entrySet()) {
+                            this.changeLogParameters.set(entry.getKey().toString(), entry.getValue().toString(), context, dbms);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new SAXException(e);
+        }
+
+        try {
             if (attributes != null) {
                 for (int i=0; i< attributes.getLength(); i++) {
-                    node.addChild(null, attributes.getLocalName(i), attributes.getValue(i));
+                    try {
+                        node.addChild(null, attributes.getLocalName(i), changeLogParameters.expandExpressions(attributes.getValue(i)));
+                    } catch (NullPointerException e) {
+                        throw e;
+                    }
                 }
             }
             if (!nodeStack.isEmpty()) {
@@ -94,13 +130,13 @@ class XMLChangeLogSAXHandler extends DefaultHandler {
         try {
             String seenText = this.textStack.pop().toString();
             if (!StringUtils.trimToEmpty(seenText).equals("")) {
-                node.setValue(seenText);
+                node.setValue(changeLogParameters.expandExpressions(seenText));
             }
         } catch (ParsedNodeException e) {
             throw new SAXException(e);
         }
     }
-
+}
 //    public void startElementOld(String uri, String localName, String qName, Attributes baseAttributes) throws SAXException {
 //		Attributes atts = new ExpandingAttributes(baseAttributes);
 //		try {
