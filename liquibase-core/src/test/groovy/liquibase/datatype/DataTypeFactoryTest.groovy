8f122f7bb0a50c9e78503f32b655eb056972ec57
Nathan Voxland <nathan@voxland.net>
2014-05-27 13:39:10 -0500

    CORE-1797 Autoincrement on type Serial8 fails

--- liquibase-core/src/test/groovy/liquibase/datatype/DataTypeFactoryTest.groovy
+++ liquibase-core/src/test/groovy/liquibase/datatype/DataTypeFactoryTest.groovy
@@ -1,16 +1,19 @@
 package liquibase.datatype;
 
-import liquibase.database.core.H2Database;
+import liquibase.database.core.H2Database
+import liquibase.datatype.core.BigIntType;
 import liquibase.datatype.core.IntType;
 import liquibase.datatype.core.VarcharType;
 import org.junit.Test
-import spock.lang.Specification;
+import spock.lang.Specification
+import spock.lang.Unroll;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 public class DataTypeFactoryTest extends Specification {
 
+    @Unroll("#featureName: #liquibaseString")
     public void parse() throws Exception {
         when:
         def parsed = DataTypeFactory.getInstance().fromDescription(liquibaseString)
@@ -32,5 +35,6 @@ public class DataTypeFactoryTest extends Specification {
         "varchar COLLATE Latin1_General_BIN"      | null           | VarcharType.class | false
         "varchar(255) COLLATE Latin1_General_BIN" | null           | VarcharType.class | false
         "character varying(256)"                  | "varchar(256)" | VarcharType.class | false
+        "serial8"                                 | "bigint"       | BigIntType        | true
     }
 }
