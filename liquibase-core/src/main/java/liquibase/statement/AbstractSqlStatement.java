edb4179ae99d72b47b99b00ce8bf66d1a201d884
Nathan Voxland <nathan@voxland.net>
2014-10-10 16:13:20 -0500

    Should not add dependency to org.apache.commons

--- liquibase-core/src/main/java/liquibase/statement/AbstractSqlStatement.java
+++ liquibase-core/src/main/java/liquibase/statement/AbstractSqlStatement.java
@@ -1,18 +1,9 @@
 package liquibase.statement;
 
-import org.apache.commons.lang.builder.ReflectionToStringBuilder;
-
 public abstract class AbstractSqlStatement implements SqlStatement {
 
     @Override
     public boolean skipOnUnsupported() {
         return false;
     }
-
-    @Override
-    public String toString() {
-      return ReflectionToStringBuilder.toString(this);
-    }
-    
-    
 }
