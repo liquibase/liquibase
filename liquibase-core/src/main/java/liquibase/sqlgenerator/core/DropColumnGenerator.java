53c7f2ebff6a65fe891b79a71207a48ebf60293a
Andreas Dangel <adangel@users.sourceforge.net>
2014-10-10 20:05:43 +0200

    CORE-1833 Extend DropColumnStatement to support multiple columns

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/DropColumnGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/DropColumnGenerator.java
@@ -1,11 +1,13 @@
 package liquibase.sqlgenerator.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import liquibase.database.Database;
 import liquibase.database.core.*;
 import liquibase.exception.ValidationErrors;
 import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
-import liquibase.sqlgenerator.SqlGenerator;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.statement.core.DropColumnStatement;
 import liquibase.structure.core.Column;
@@ -15,6 +17,26 @@ public class DropColumnGenerator extends AbstractSqlGenerator<DropColumnStatemen
 
     @Override
     public ValidationErrors validate(DropColumnStatement dropColumnStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        if (dropColumnStatement.isMultiple()) {
+            ValidationErrors validationErrors = new ValidationErrors();
+            DropColumnStatement firstColumn = dropColumnStatement.getColumns().get(0);
+
+            for (DropColumnStatement drop : dropColumnStatement.getColumns()) {
+                validationErrors.addAll(validateSingleColumn(drop));
+                if (drop.getTableName() != null && !drop.getTableName().equals(firstColumn.getTableName())) {
+                    validationErrors.addError("All columns must be targeted at the same table");
+                }
+                if (drop.isMultiple()) {
+                    validationErrors.addError("Nested multiple drop column statements are not supported");
+                }
+            }
+            return validationErrors;
+        } else {
+            return validateSingleColumn(dropColumnStatement);
+        }
+    }
+
+    private ValidationErrors validateSingleColumn(DropColumnStatement dropColumnStatement) {
         ValidationErrors validationErrors = new ValidationErrors();
         validationErrors.checkRequiredField("tableName", dropColumnStatement.getTableName());
         validationErrors.checkRequiredField("columnName", dropColumnStatement.getColumnName());
@@ -23,12 +45,47 @@ public class DropColumnGenerator extends AbstractSqlGenerator<DropColumnStatemen
 
     @Override
     public Sql[] generateSql(DropColumnStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-        if (database instanceof DB2Database) {
-            return new Sql[] { new UnparsedSql("ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " DROP COLUMN " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()), getAffectedColumn(statement)) };
-        } else if (database instanceof SybaseDatabase || database instanceof SybaseASADatabase || database instanceof FirebirdDatabase || database instanceof InformixDatabase) {
-            return new Sql[] { new UnparsedSql("ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " DROP " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()), getAffectedColumn(statement)) };
+        if (statement.isMultiple()) {
+            return generateMultipleColumnSql(statement.getColumns(), database);
+        } else {
+            return generateSingleColumnSql(statement, database);
         }
-        return new Sql[] { new UnparsedSql("ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " DROP COLUMN " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()), getAffectedColumn(statement)) };
+    }
+
+    private Sql[] generateMultipleColumnSql(List<DropColumnStatement> columns, Database database) {
+        List<Sql> result = new ArrayList<Sql>();
+        if (database instanceof MySQLDatabase) {
+            String alterTable = "ALTER TABLE " + database.escapeTableName(columns.get(0).getCatalogName(), columns.get(0).getSchemaName(), columns.get(0).getTableName());
+            for (int i = 0; i < columns.size(); i++) {
+                alterTable +=  " DROP " + database.escapeColumnName(columns.get(i).getCatalogName(), columns.get(i).getSchemaName(), columns.get(i).getTableName(), columns.get(i).getColumnName());
+                if (i < columns.size() - 1) {
+                    alterTable += ",";
+                }
+            }
+            result.add(new UnparsedSql(alterTable, getAffectedColumns(columns)));
+        } else {
+            for (DropColumnStatement column : columns) {
+                result.add(generateSingleColumnSql(column, database)[0]);
+            }
+        }
+        return result.toArray(new Sql[result.size()]);
+    }
+
+    private Sql[] generateSingleColumnSql(DropColumnStatement statement, Database database) {
+        if (database instanceof DB2Database) {
+            return new Sql[] {new UnparsedSql("ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " DROP COLUMN " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()), getAffectedColumn(statement))};
+        } else if (database instanceof SybaseDatabase || database instanceof SybaseASADatabase || database instanceof FirebirdDatabase || database instanceof InformixDatabase) {
+            return new Sql[] {new UnparsedSql("ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " DROP " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()), getAffectedColumn(statement))};
+        }
+        return new Sql[] {new UnparsedSql("ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " DROP COLUMN " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()), getAffectedColumn(statement))};
+    }
+
+    private Column[] getAffectedColumns(List<DropColumnStatement> columns) {
+        List<Column> affected = new ArrayList<Column>();
+        for (DropColumnStatement column : columns) {
+            affected.add(getAffectedColumn(column));
+        }
+        return affected.toArray(new Column[affected.size()]);
     }
 
     protected Column getAffectedColumn(DropColumnStatement statement) {
--- liquibase-core/src/main/java/liquibase/statement/core/DropColumnStatement.java
+++ liquibase-core/src/main/java/liquibase/statement/core/DropColumnStatement.java
@@ -1,5 +1,8 @@
 package liquibase.statement.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import liquibase.statement.AbstractSqlStatement;
 
 public class DropColumnStatement extends AbstractSqlStatement {
@@ -9,11 +12,25 @@ public class DropColumnStatement extends AbstractSqlStatement {
     private String tableName;
     private String columnName;
 
+    private List<DropColumnStatement> columns = new ArrayList<DropColumnStatement>();
+
     public DropColumnStatement(String catalogName, String schemaName, String tableName, String columnName) {
         this.catalogName = catalogName;
         this.schemaName = schemaName;
         this.tableName = tableName;
         this.columnName = columnName;
+    }
+
+    public DropColumnStatement(List<DropColumnStatement> columns) {
+        this.columns.addAll(columns);
+    }
+
+    public boolean isMultiple() {
+        return !columns.isEmpty();
+    }
+
+    public List<DropColumnStatement> getColumns() {
+        return columns;
     }
 
     public String getCatalogName() {
