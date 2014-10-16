af3e41f58b47ba105e94d8801ebe9d7ff8382f36
Nathan Voxland <nathan@voxland.net>
2014-10-16 13:50:39 -0500

    "DESC" is an oracle keywords

--- liquibase-core/src/main/java/liquibase/database/core/OracleDatabase.java
+++ liquibase-core/src/main/java/liquibase/database/core/OracleDatabase.java
@@ -59,7 +59,7 @@ public class OracleDatabase extends AbstractJdbcDatabase {
             method.invoke(sqlConn, true);
 
             reservedWords.addAll(Arrays.asList(sqlConn.getMetaData().getSQLKeywords().toUpperCase().split(",\\s*")));
-            reservedWords.addAll(Arrays.asList("GROUP", "USER", "SESSION","PASSWORD", "RESOURCE", "START", "SIZE", "UID")); //more reserved words not returned by driver
+            reservedWords.addAll(Arrays.asList("GROUP", "USER", "SESSION","PASSWORD", "RESOURCE", "START", "SIZE", "UID", "DESC")); //more reserved words not returned by driver
         } catch (Exception e) {
             LogFactory.getLogger().info("Could not set remarks reporting on OracleDatabase: " + e.getMessage());
             ; //cannot set it. That is OK
