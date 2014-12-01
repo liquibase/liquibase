a64572445a26920323c5d44b36230df92c27f933
Nathan Voxland <nathan@voxland.net>
2014-12-01 17:00:19 -0600

    Updated versions to 4.0.0

--- liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXParser.java
+++ liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXParser.java
@@ -41,7 +41,7 @@ public class XMLChangeLogSAXParser extends AbstractChangeLogParser {
     }
 
     public static String getSchemaVersion() {
-        return "3.4";
+        return "4.0";
     }
 
     @Override
