bc22b3c546ec489fc629cceea74b79e55ac9c9bd
Brar Piening <brar@gmx.de>
2015-02-13 01:18:03 +0100

    Make createProcedure Change honor the procedureText Attribute
    The DatabaseChangeProperty metadata for
    CreateProcedureChange.getProcedureText contained 'isChangeProperty =
    false' which effectively made it unusable.

--- liquibase-core/src/main/java/liquibase/change/core/CreateProcedureChange.java
+++ liquibase-core/src/main/java/liquibase/change/core/CreateProcedureChange.java
@@ -121,7 +121,7 @@ public class CreateProcedureChange extends AbstractChange implements DbmsTargete
         this.procedureText = procedureText;
     }
 
-    @DatabaseChangeProperty(isChangeProperty = false)
+    @DatabaseChangeProperty
     public String getProcedureText() {
         return procedureText;
     }
