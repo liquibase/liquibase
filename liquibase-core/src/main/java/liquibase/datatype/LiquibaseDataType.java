eb79dd2399e4f0c1dca5219a5f6a0ff99263af94
Mark Chesney <mark.chesney@insight.com>
2015-03-24 22:44:35 -0700

    CORE-2318 Add support for converting BigDecimal objects to a SQL string via DataTypeFactory

--- liquibase-core/src/main/java/liquibase/datatype/LiquibaseDataType.java
+++ liquibase-core/src/main/java/liquibase/datatype/LiquibaseDataType.java
@@ -5,6 +5,7 @@ import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.servicelocator.PrioritizedService;
 import liquibase.statement.DatabaseFunction;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -123,6 +124,8 @@ public abstract class LiquibaseDataType implements PrioritizedService {
             return null;
         } else if (value instanceof DatabaseFunction) {
             return database.generateDatabaseFunctionValue((DatabaseFunction) value);
+        } else if (value instanceof BigDecimal) {
+            return formatNumber(((BigDecimal) value).toPlainString());
         } else if (value instanceof Number) {
             return formatNumber(value.toString());
         }
