30fd36b58ab1a8d18adf14366a72627222b9bc80
Mark Chesney <mark.chesney@insight.com>
2015-04-01 12:45:15 -0500

    CORE-2321 Modify logic so that MarkChangeSetRanGenerator can be solely responsible for tagging when executing a change log, while still allowing the database to be tagged outside of that scenario using the necessary update statement with subqueries and aggregate expressions. This can improve performance of executing change logs by eliminating the need to execute unnecessary update statements with subqueries and aggregate expressions. Improve the update statement to use the more-unique ORDEREXECUTED column rather than potentially less unique DATEEXECUTED to ensure that only the minimum number of records get updated. (cherry picked from commit 4014314)

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/UpdateChangeSetChecksumGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/UpdateChangeSetChecksumGenerator.java
@@ -9,6 +9,7 @@ import liquibase.sqlgenerator.SqlGeneratorFactory;
 import liquibase.statement.SqlStatement;
 import liquibase.statement.core.UpdateChangeSetChecksumStatement;
 import liquibase.statement.core.UpdateStatement;
+import liquibase.structure.core.Column;
 
 public class UpdateChangeSetChecksumGenerator extends AbstractSqlGenerator<UpdateChangeSetChecksumStatement> {
     @Override
@@ -26,9 +27,9 @@ public class UpdateChangeSetChecksumGenerator extends AbstractSqlGenerator<Updat
         SqlStatement runStatement = null;
         runStatement = new UpdateStatement(database.getLiquibaseCatalogName(), database.getLiquibaseSchemaName(), database.getDatabaseChangeLogTableName())
                 .addNewColumnValue("MD5SUM", changeSet.generateCheckSum().toString())
-                .setWhereClause(database.escapeColumnName(null, null, null, "ID") + " = ? " +
-                        "AND " + database.escapeColumnName(null, null, null, "AUTHOR") + " = ? " +
-                        "AND " + database.escapeColumnName(null, null, null, "FILENAME") + " = ?")
+                .setWhereClause(database.escapeObjectName("ID", Column.class) + " = ? " +
+                        "AND " + database.escapeObjectName("AUTHOR", Column.class) + " = ? " +
+                        "AND " + database.escapeObjectName("FILENAME", Column.class) + " = ?")
                 .addWhereParameters(changeSet.getId(), changeSet.getAuthor(), changeSet.getFilePath());
 
         return SqlGeneratorFactory.getInstance().generateSql(runStatement, database);
