041c34e6189704b0434470c0b06c54784f218fe1
Nathan Voxland <nathan@voxland.net>
2015-04-28 13:17:29 -0500

    CORE-2156 Resource loader can't load changelog file
    spring-boot jars sometimes do not return anything for getResources("")

--- liquibase-core/src/main/java/liquibase/integration/spring/SpringLiquibase.java
+++ liquibase-core/src/main/java/liquibase/integration/spring/SpringLiquibase.java
@@ -7,7 +7,9 @@ import liquibase.configuration.ConfigurationProperty;
 import liquibase.configuration.GlobalConfiguration;
 import liquibase.configuration.LiquibaseConfiguration;
 import liquibase.database.Database;
+import liquibase.database.DatabaseConnection;
 import liquibase.database.DatabaseFactory;
+import liquibase.database.OfflineConnection;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
@@ -34,6 +36,8 @@ import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 /**
  * A Spring-ified wrapper for Liquibase.
@@ -75,9 +79,48 @@ public class SpringLiquibase implements InitializingBean, BeanNameAware, Resourc
             super.init();
             try {
                 Resource[] resources = ResourcePatternUtils.getResourcePatternResolver(getResourceLoader()).getResources("");
-                for (Resource res : resources) {
-                    addRootPath(res.getURL());
-                }
+				if (resources.length == 0 || resources.length == 1 && !resources[0].exists()) { //sometimes not able to look up by empty string, try all the liquibase packages
+					Set<String> liquibasePackages = new HashSet<String>();
+					for (Resource manifest : ResourcePatternUtils.getResourcePatternResolver(getResourceLoader()).getResources("META-INF/MANIFEST.MF")) {
+						if (manifest.exists()) {
+							InputStream inputStream = null;
+							try {
+								inputStream = manifest.getInputStream();
+								Manifest manifestObj = new Manifest(inputStream);
+								Attributes attributes = manifestObj.getAttributes("Liquibase-Package");
+								if (attributes != null) {
+									for (Object attr : attributes.values()) {
+										String packages = "\\s*,\\s*";
+										;
+										for (String fullPackage : attr.toString().split(packages)) {
+											liquibasePackages.add(fullPackage.split("\\.")[0]);
+										}
+                                    }
+								}
+							} finally {
+								if (inputStream != null) {
+									inputStream.close();
+								}
+							}
+						}
+					}
+
+                    if (liquibasePackages.size() == 0) {
+                        LogFactory.getInstance().getLog().warning("No Liquibase-Packages entry found in MANIFEST.MF. Using fallback of entire 'liquibase' package");
+                        liquibasePackages.add("liquibase");
+                    }
+
+                    for (String foundPackage : liquibasePackages) {
+						resources = ResourcePatternUtils.getResourcePatternResolver(getResourceLoader()).getResources(foundPackage);
+						for (Resource res : resources) {
+							addRootPath(res.getURL());
+						}
+					}
+				} else {
+					for (Resource res : resources) {
+						addRootPath(res.getURL());
+					}
+				}
             } catch (IOException e) {
                 LogFactory.getInstance().getLog().warning("Error initializing SpringLiquibase", e);
             }
@@ -318,7 +361,7 @@ public class SpringLiquibase implements InitializingBean, BeanNameAware, Resourc
 		Liquibase liquibase = null;
 		try {
 			c = getDataSource().getConnection();
-			liquibase = createLiquibase(c);
+            liquibase = createLiquibase(c);
 			generateRollbackFile(liquibase);
 			performUpdate(liquibase);
 		} catch (SQLException e) {
@@ -384,7 +427,17 @@ public class SpringLiquibase implements InitializingBean, BeanNameAware, Resourc
 	 * @throws DatabaseException
 	 */
 	protected Database createDatabase(Connection c) throws DatabaseException {
-		Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(c));
+
+        DatabaseConnection liquibaseConnection;
+        if (c == null) {
+            log.warning("Null connection returned by liquibase datasource. Using offline unknown database");
+            liquibaseConnection = new OfflineConnection("offline:unknown");
+
+        } else {
+            liquibaseConnection = new JdbcConnection(c);
+        }
+
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(liquibaseConnection);
 		if (StringUtils.trimToNull(this.defaultSchema) != null) {
 			database.setDefaultSchemaName(this.defaultSchema);
 		}
