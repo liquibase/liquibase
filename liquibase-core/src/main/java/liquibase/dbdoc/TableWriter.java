6a61f692b16d28fcd574527de4c3cd98392eeb8e
juancarloscruz <jay@xenu.chickenkiller.com>
2015-06-02 20:36:16 +0200

    Display nullable column status in dbDoc

--- liquibase-core/src/main/java/liquibase/dbdoc/TableWriter.java
+++ liquibase-core/src/main/java/liquibase/dbdoc/TableWriter.java
@@ -36,6 +36,7 @@ public class TableWriter extends HTMLWriter {
         for (Column column : table.getColumns()) {
             String remarks = column.getRemarks();
             cells.add(Arrays.asList(column.getType().toString(),
+                    column.isNullable() ? "NULL" : "NOT NULL",
                     "<A HREF=\"../columns/" + table.getName().toLowerCase() + "." + column.getName().toLowerCase() + ".html" + "\">" + column.getName() + "</A>",
                     remarks != null ? remarks : ""));
             //todo: add foreign key info to columns?
