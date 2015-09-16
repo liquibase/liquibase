209ba6de204bc589d03f4eb461c1c4793dd11d8c
Nathan Voxland <nathan@voxland.net>
2015-09-16 16:24:31 -0500

    Removed unused columns from Oracle all columns snapshot query

--- liquibase-core/src/main/java/liquibase/snapshot/JdbcDatabaseSnapshot.java
+++ liquibase-core/src/main/java/liquibase/snapshot/JdbcDatabaseSnapshot.java
@@ -350,10 +350,10 @@ public class JdbcDatabaseSnapshot extends DatabaseSnapshot {
                             // note: oracle reports DATA_LENGTH=4*CHAR_LENGTH when using VARCHAR( <N> CHAR ), thus BYTEs
                             "DECODE( CHAR_USED, 'C',CHAR_LENGTH, DATA_LENGTH ) as DATA_LENGTH, " +
                             "DATA_PRECISION, DATA_SCALE, NULLABLE, COLUMN_ID, DEFAULT_LENGTH, " +
-                            "DATA_DEFAULT, NUM_DISTINCT, LOW_VALUE, HIGH_VALUE, DENSITY, NUM_NULLS, " +
-                            "NUM_BUCKETS, LAST_ANALYZED, SAMPLE_SIZE, CHARACTER_SET_NAME, " +
-                            "CHAR_COL_DECL_LENGTH, GLOBAL_STATS, USER_STATS, AVG_COL_LEN, CHAR_LENGTH, " +
-                            "CHAR_USED, V80_FMT_IMAGE, DATA_UPGRADED, HISTOGRAM, VIRTUAL_COLUMN\n" +
+                            "DATA_DEFAULT, " +
+                            "NUM_BUCKETS, CHARACTER_SET_NAME, " +
+                            "CHAR_COL_DECL_LENGTH, CHAR_LENGTH, " +
+                            "CHAR_USED, VIRTUAL_COLUMN " +
                             "FROM ALL_TAB_COLS c " +
                             "JOIN ALL_COL_COMMENTS cc USING ( OWNER, TABLE_NAME, COLUMN_NAME ) " +
                             "WHERE OWNER='" + ((AbstractJdbcDatabase) database).getJdbcSchemaName(catalogAndSchema) + "' AND hidden_column='NO'";
