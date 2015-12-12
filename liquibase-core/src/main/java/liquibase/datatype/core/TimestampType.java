7ba8974f0f95cae21c2adf40fb9d8ee5b2dd392a
Xiong He <xiong.he@sap.com>
2015-12-12 12:54:53 +0800

    In Sybase ASE database, "timestamp" is an internal type. When mapping in the changeset definition, it should be mapped into "datetime" column type. Fix it here.

--- liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
+++ liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
@@ -5,30 +5,37 @@ import liquibase.configuration.LiquibaseConfiguration;
 import liquibase.database.Database;
 import liquibase.database.core.MSSQLDatabase;
 import liquibase.database.core.MySQLDatabase;
+import liquibase.database.core.SybaseDatabase;
 import liquibase.datatype.DataTypeInfo;
 import liquibase.datatype.DatabaseDataType;
 import liquibase.datatype.LiquibaseDataType;
 import liquibase.util.StringUtils;
 
-@DataTypeInfo(name = "timestamp", aliases = {"java.sql.Types.TIMESTAMP", "java.sql.Timestamp", "timestamptz"}, minParameters = 0, maxParameters = 1, priority = LiquibaseDataType.PRIORITY_DEFAULT)
+@DataTypeInfo(name = "timestamp", aliases = { "java.sql.Types.TIMESTAMP", "java.sql.Timestamp", "timestamptz" }, minParameters = 0, maxParameters = 1, priority = LiquibaseDataType.PRIORITY_DEFAULT)
 public class TimestampType extends DateTimeType {
 
-    @Override
-    public DatabaseDataType toDatabaseDataType(Database database) {
-        String originalDefinition = StringUtils.trimToEmpty(getRawDefinition());
-        if (database instanceof MySQLDatabase) {
-            if (getRawDefinition().contains(" ") || getRawDefinition().contains("(")) {
-                return new DatabaseDataType(getRawDefinition());
-            }
-            return super.toDatabaseDataType(database);
-        }
-        if (database instanceof MSSQLDatabase) {
-            if (!LiquibaseConfiguration.getInstance().getProperty(GlobalConfiguration.class, GlobalConfiguration.CONVERT_DATA_TYPES).getValue(Boolean.class) && originalDefinition.toLowerCase().startsWith("timestamp")) {
-                return new DatabaseDataType(database.escapeDataTypeName("timestamp"));
-            }
+	@Override
+	public DatabaseDataType toDatabaseDataType(Database database) {
+		String originalDefinition = StringUtils.trimToEmpty(getRawDefinition());
+		if (database instanceof MySQLDatabase) {
+			if (getRawDefinition().contains(" ") || getRawDefinition().contains("(")) {
+				return new DatabaseDataType(getRawDefinition());
+			}
+			return super.toDatabaseDataType(database);
+		}
+		if (database instanceof MSSQLDatabase) {
+			if (!LiquibaseConfiguration.getInstance()
+					.getProperty(GlobalConfiguration.class, GlobalConfiguration.CONVERT_DATA_TYPES)
+					.getValue(Boolean.class)
+					&& originalDefinition.toLowerCase().startsWith("timestamp")) {
+				return new DatabaseDataType(database.escapeDataTypeName("timestamp"));
+			}
 
-            return new DatabaseDataType(database.escapeDataTypeName("datetime"));
-        }
-        return super.toDatabaseDataType(database);
-    }
+			return new DatabaseDataType(database.escapeDataTypeName("datetime"));
+		}
+		if (database instanceof SybaseDatabase) {
+			return new DatabaseDataType(database.escapeDataTypeName("datetime"));
+		}
+		return super.toDatabaseDataType(database);
+	}
 }
