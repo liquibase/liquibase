bdbea747ef840857d8fcc33e8d820473fb7d8747
Nathan Voxland <nathan@voxland.net>
2016-03-11 13:59:04 -0600

    CORE-2671 CORE-521 Handle Timestamp with Time Zone types where supported

--- liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
+++ liquibase-core/src/main/java/liquibase/datatype/core/TimestampType.java
@@ -3,9 +3,7 @@ package liquibase.datatype.core;
 import liquibase.configuration.GlobalConfiguration;
 import liquibase.configuration.LiquibaseConfiguration;
 import liquibase.database.Database;
-import liquibase.database.core.MSSQLDatabase;
-import liquibase.database.core.MySQLDatabase;
-import liquibase.database.core.SybaseDatabase;
+import liquibase.database.core.*;
 import liquibase.datatype.DataTypeInfo;
 import liquibase.datatype.DatabaseDataType;
 import liquibase.datatype.LiquibaseDataType;
@@ -31,8 +29,19 @@ public class TimestampType extends DateTimeType {
             return new DatabaseDataType(database.escapeDataTypeName("datetime"));
         }
         if (database instanceof SybaseDatabase) {
-			return new DatabaseDataType(database.escapeDataTypeName("datetime"));
-		}
+            return new DatabaseDataType(database.escapeDataTypeName("datetime"));
+        }
+
+
+        if (getAdditionalInformation() != null
+                && (database instanceof PostgresDatabase
+                || database instanceof OracleDatabase)
+                || database instanceof HsqlDatabase){
+            DatabaseDataType type = new DatabaseDataType("TIMESTAMP");
+            type.addAdditionalInformation(this.getAdditionalInformation());
+            return type;
+        }
+
         return super.toDatabaseDataType(database);
     }
 }
