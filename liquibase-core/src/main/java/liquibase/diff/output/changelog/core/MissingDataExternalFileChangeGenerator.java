d4f712ed3f54c62b7e8c3cb449a142010939a73f
Markus Bernhardt <markus.bernhardt@ing.nl>
2016-05-30 17:40:12 +0200

    Add option to compress unloaded CSV files

--- liquibase-core/src/main/java/liquibase/diff/output/changelog/core/MissingDataExternalFileChangeGenerator.java
+++ liquibase-core/src/main/java/liquibase/diff/output/changelog/core/MissingDataExternalFileChangeGenerator.java
@@ -3,8 +3,6 @@ package liquibase.diff.output.changelog.core;
 import liquibase.change.Change;
 import liquibase.change.core.LoadDataChange;
 import liquibase.change.core.LoadDataColumnConfig;
-import liquibase.configuration.GlobalConfiguration;
-import liquibase.configuration.LiquibaseConfiguration;
 import liquibase.database.Database;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.diff.output.DiffOutputControl;
@@ -21,6 +19,7 @@ import liquibase.util.csv.CSVWriter;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -28,14 +27,21 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 @LiquibaseService(skip = true)
 public class MissingDataExternalFileChangeGenerator extends MissingDataChangeGenerator {
 
     private String dataDir;
-
+    
+    private boolean dataDirCompress = false;
+    
     public MissingDataExternalFileChangeGenerator(String dataDir) {
         this.dataDir = dataDir;
+        if(dataDir.endsWith(":gz")) {
+        	this.dataDirCompress = true;
+        	this.dataDir = this.dataDir.substring(0,  this.dataDir.length() - 3);
+        }
     }
 
     @Override
@@ -70,6 +76,10 @@ public class MissingDataExternalFileChangeGenerator extends MissingDataChangeGen
             }
 
             String fileName = table.getName().toLowerCase() + ".csv";
+            if(dataDirCompress) {
+            	fileName += ".gz";
+            }
+            
             if (dataDir != null) {
                 fileName = dataDir + "/" + fileName;
             }
@@ -83,8 +93,13 @@ public class MissingDataExternalFileChangeGenerator extends MissingDataChangeGen
                         + " is not a directory");
             }
 
+            OutputStream outputStream = new FileOutputStream(fileName);
+            if(dataDirCompress) {
+            	outputStream = new GZIPOutputStream(outputStream);
+            }
+
             CSVWriter outputFile = new CSVWriter(
-                    new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8")));
+                    new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8")));
             String[] dataTypes = new String[columnNames.size()];
             String[] line = new String[columnNames.size()];
             for (int i = 0; i < columnNames.size(); i++) {
