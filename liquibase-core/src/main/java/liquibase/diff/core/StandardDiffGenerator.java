b8ce5acfdcc7d424940ca5dd5918267d59c6a425
Nathan Voxland <nathan@voxland.net>
2016-08-02 09:06:39 -0500

    Fixed null pointer exception

--- liquibase-core/src/main/java/liquibase/diff/core/StandardDiffGenerator.java
+++ liquibase-core/src/main/java/liquibase/diff/core/StandardDiffGenerator.java
@@ -111,7 +111,7 @@ public class StandardDiffGenerator implements DiffGenerator {
                         String schemaComparisonName2 = StringUtils.trimToEmpty(schemaComparison.getReferenceSchema().standardize(comparisonDatabase).getSchemaName());
 
                         if (comparisonObjectSchemaName.equals("") && !schemaComparisonName1.equals("") && !schemaComparisonName2.equals("")) {
-                            comparisonObjectSchemaName = StringUtils.trimToNull(comparisonObjectSchema.getName());
+                            comparisonObjectSchemaName = StringUtils.trimToEmpty(comparisonObjectSchema.getName());
                         }
                         if (!(comparisonObjectSchemaName.equalsIgnoreCase(schemaComparisonName1) || comparisonObjectSchemaName.equals(schemaComparisonName2))) {
                             continue;
