86970f705a1140e59295e057384bcb875da3379a
Andreas Buschka <kontakt@andreas-buschka.de>
2017-01-03 18:44:10 +0100

    * Fix testcases for MSSQL server: CHAR>[char] is intended according to https://liquibase.jira.com/browse/CORE-2217

--- liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/addLookupTable/mssql.sql
+++ liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/addLookupTable/mssql.sql
@@ -5,6 +5,6 @@
 -- Change Parameter: newColumnName=abbreviation
 -- Change Parameter: newTableName=state
 SELECT DISTINCT [state] AS [abbreviation] INTO [state] FROM [address] WHERE [state] IS NOT NULL;
-ALTER TABLE [state] ALTER COLUMN [abbreviation] CHAR(2) NOT NULL;
+ALTER TABLE [state] ALTER COLUMN [abbreviation] [char](2) NOT NULL;
 ALTER TABLE [state] ADD PRIMARY KEY ([abbreviation]);
 ALTER TABLE [address] ADD CONSTRAINT [FK_ADDRESS_STATE] FOREIGN KEY ([state]) REFERENCES [state] ([abbreviation]);
