9562c3a34e7bdf3536eef4421d97c30d03454804
Andreas Buschka <kontakt@andreas-buschka.de>
2017-01-04 18:37:16 +0100

    Fix testcases for MSSQL server: data_type>[data_type] is intended according to https://liquibase.jira.com/browse/CORE-2217

--- liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/addNotNullConstraint/mssql.sql
+++ liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/addNotNullConstraint/mssql.sql
@@ -2,4 +2,4 @@
 -- Change Parameter: columnDataType=int
 -- Change Parameter: columnName=id
 -- Change Parameter: tableName=person
-ALTER TABLE [person] ALTER COLUMN [id] INT NOT NULL;
+ALTER TABLE [person] ALTER COLUMN [id] [int] NOT NULL;
