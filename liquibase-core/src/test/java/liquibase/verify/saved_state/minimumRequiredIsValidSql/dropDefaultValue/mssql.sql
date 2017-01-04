deaa98642a0d11a8931083e7d50087e002f1a675
Andreas Buschka <kontakt@andreas-buschka.de>
2017-01-04 10:28:10 +0100

    Repaired test case for MSSQL->dropDefaultValue. SQL change is intended according to https://github.com/liquibase/liquibase/pull/440/files

--- liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/dropDefaultValue/mssql.sql
+++ liquibase-core/src/test/java/liquibase/verify/saved_state/minimumRequiredIsValidSql/dropDefaultValue/mssql.sql
@@ -1,6 +1,6 @@
 -- Database: mssql
 -- Change Parameter: columnName=id
 -- Change Parameter: tableName=person
-DECLARE @default sysname
-SELECT @default = object_name(default_object_id) FROM sys.columns WHERE object_id=object_id('[person]') AND name='id'
-EXEC ('ALTER TABLE [person] DROP CONSTRAINT ' + @default);
+DECLARE @sql [nvarchar](MAX)
+SELECT @sql = N'ALTER TABLE [person] DROP CONSTRAINT ' + QUOTENAME([df].[name]) FROM [sys].[columns] AS [c] INNER JOIN [sys].[default_constraints] AS [df] ON [df].[object_id] = [c].[default_object_id] WHERE [c].[object_id] = OBJECT_ID(N'[person]') AND [c].[name] = N'id'
+EXEC sp_executesql @sql;
