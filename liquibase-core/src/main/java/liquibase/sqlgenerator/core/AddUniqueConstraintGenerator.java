2759a814f6dd69fd5f82e84316443c20e7c872b1
isaki <scott@drawbridgenetworks.com>
2017-01-05 13:48:02 -0500

    Fixed warnings for AddUniqueConstraintGenerator
    - Eliminated unused import.
    - Used reflection to create a safe generic array (safe because its
      empty) to avoid the auto-vivification warning for vararg that uses
      generic types.

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/AddUniqueConstraintGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/AddUniqueConstraintGenerator.java
@@ -1,11 +1,12 @@
 package liquibase.sqlgenerator.core;
 
+import java.lang.reflect.Array;
+
 import liquibase.database.Database;
 import liquibase.database.core.*;
 import liquibase.exception.ValidationErrors;
 import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
-import liquibase.sqlgenerator.SqlGenerator;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.statement.core.AddUniqueConstraintStatement;
 import liquibase.structure.core.Column;
@@ -16,14 +17,18 @@ import liquibase.util.StringUtils;
 
 public class AddUniqueConstraintGenerator extends AbstractSqlGenerator<AddUniqueConstraintStatement> {
 
+    // This change ensures we don't have auto-vivification of generic
+    // arrays (which is generally a bad thing) due to varargs.
+    @SuppressWarnings("unchecked")
+    private static final Class<? extends Database>[] EMPTY_DATABASE_CLAZZ_ARRAY = (Class<? extends Database>[]) Array.newInstance(Class.class, 0);
+
     @Override
     public boolean supports(AddUniqueConstraintStatement statement, Database database) {
         return !(database instanceof SQLiteDatabase)
-        		&& !(database instanceof MSSQLDatabase)
-        		&& !(database instanceof SybaseDatabase)
-        		&& !(database instanceof SybaseASADatabase)
-        		&& !(database instanceof InformixDatabase)
-        ;
+            && !(database instanceof MSSQLDatabase)
+            && !(database instanceof SybaseDatabase)
+            && !(database instanceof SybaseASADatabase)
+            && !(database instanceof InformixDatabase);
     }
 
     @Override
@@ -33,43 +38,49 @@ public class AddUniqueConstraintGenerator extends AbstractSqlGenerator<AddUnique
         validationErrors.checkRequiredField("tableName", addUniqueConstraintStatement.getTableName());
 
         if (!(database instanceof OracleDatabase)) {
-            validationErrors.checkDisallowedField("forIndexName", addUniqueConstraintStatement.getForIndexName(), database);
+            validationErrors.checkDisallowedField("forIndexName", addUniqueConstraintStatement.getForIndexName(), database, EMPTY_DATABASE_CLAZZ_ARRAY);
         }
 
-//        if (!(database instanceof MSSQLDatabase) && addUniqueConstraintStatement.isClustered()) {
-//            validationErrors.checkDisallowedField("clustered", addUniqueConstraintStatement.isClustered(), database);
-//        }
+        // if (!(database instanceof MSSQLDatabase) &&
+        // addUniqueConstraintStatement.isClustered()) {
+        // validationErrors.checkDisallowedField("clustered",
+        // addUniqueConstraintStatement.isClustered(), database);
+        // }
         return validationErrors;
     }
 
     @Override
     public Sql[] generateSql(AddUniqueConstraintStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
 
-		String sql = null;
-		if (statement.getConstraintName() == null) {
-			sql = String.format("ALTER TABLE %s ADD UNIQUE (%s)"
-					, database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName())
-					, database.escapeColumnNameList(statement.getColumnNames())
-			);
-		} else {
-			sql = String.format("ALTER TABLE %s ADD CONSTRAINT %s UNIQUE (%s)"
-					, database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName())
-					, database.escapeConstraintName(statement.getConstraintName())
-					, database.escapeColumnNameList(statement.getColumnNames())
-			);
-		}
-		if(database instanceof OracleDatabase || database instanceof PostgresDatabase) {
+        String sql = null;
+        if (statement.getConstraintName() == null) {
+            sql = String.format("ALTER TABLE %s ADD UNIQUE (%s)"
+                , database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName())
+                , database.escapeColumnNameList(statement.getColumnNames())
+                );
+        } else {
+            sql = String.format("ALTER TABLE %s ADD CONSTRAINT %s UNIQUE (%s)"
+                , database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName())
+                , database.escapeConstraintName(statement.getConstraintName())
+                , database.escapeColumnNameList(statement.getColumnNames())
+                );
+        }
+
+        final boolean isOracle = database instanceof OracleDatabase;
+        final boolean isPostgres = database instanceof PostgresDatabase;
+
+        if (isOracle || isPostgres) {
             if (statement.isDeferrable()) {
                 sql += " DEFERRABLE";
             }
 
             if (statement.isInitiallyDeferred()) {
-                sql +=" INITIALLY DEFERRED";
+                sql += " INITIALLY DEFERRED";
             }
         }
 
         // Currently, only OracleDatabase supports the DISABLE command.
-        if (database instanceof OracleDatabase && statement.isDisabled()) {
+        if (isOracle && statement.isDisabled()) {
             sql += " DISABLE";
         }
 
@@ -79,26 +90,26 @@ public class AddUniqueConstraintGenerator extends AbstractSqlGenerator<AddUnique
             } else if (database instanceof DB2Database
                 || database instanceof SybaseASADatabase
                 || database instanceof InformixDatabase) {
-                ; //not supported
+                ; // not supported
             } else {
                 sql += " USING INDEX TABLESPACE " + statement.getTablespace();
             }
         }
 
         if (statement.getForIndexName() != null) {
-            sql += " USING INDEX "+database.escapeObjectName(statement.getForIndexCatalogName(), statement.getForIndexSchemaName(), statement.getForIndexName(), Index.class);
+            sql += " USING INDEX " + database.escapeObjectName(statement.getForIndexCatalogName(), statement.getForIndexSchemaName(), statement.getForIndexName(), Index.class);
         }
 
         return new Sql[] {
-                new UnparsedSql(sql, getAffectedUniqueConstraint(statement))
+            new UnparsedSql(sql, getAffectedUniqueConstraint(statement))
         };
 
     }
 
     protected UniqueConstraint getAffectedUniqueConstraint(AddUniqueConstraintStatement statement) {
         UniqueConstraint uniqueConstraint = new UniqueConstraint()
-                .setName(statement.getConstraintName())
-                .setTable((Table) new Table().setName(statement.getTableName()).setSchema(statement.getCatalogName(), statement.getSchemaName()));
+            .setName(statement.getConstraintName())
+            .setTable((Table) new Table().setName(statement.getTableName()).setSchema(statement.getCatalogName(), statement.getSchemaName()));
         int i = 0;
         for (Column column : Column.listFromNames(statement.getColumnNames())) {
             uniqueConstraint.addColumn(i++, column);
