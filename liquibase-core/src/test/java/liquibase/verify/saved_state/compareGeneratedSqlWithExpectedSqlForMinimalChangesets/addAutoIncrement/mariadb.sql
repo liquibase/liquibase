7a27468cf6f896cebb4756c7e696aeadba7edc36
Andreas Buschka <kontakt@andreas-buschka.de>
2017-01-05 13:17:37 +0100

    Created expected SQL testcases for MariaDB by copying the output from MySQL. Since MariaDB strives for compatibility with MySQL, I make the assumption that correct MySQL statements are correct MariaDB statements as well.

---
+++ liquibase-core/src/test/java/liquibase/verify/saved_state/compareGeneratedSqlWithExpectedSqlForMinimalChangesets/addAutoIncrement/mariadb.sql
@@ -0,0 +1,5 @@
+-- Database: mariadb
+-- Change Parameter: columnDataType=int
+-- Change Parameter: columnName=id
+-- Change Parameter: tableName=person
+ALTER TABLE person MODIFY id INT AUTO_INCREMENT;
---
+++ liquibase-core/src/test/java/liquibase/verify/saved_state/compareGeneratedSqlWithExpectedSqlForMinimalChangesets/createProcedure/mariadb.sql
@@ -0,0 +1,11 @@
+-- Database: mariadb
+-- Change Parameter: procedureBody=CREATE OR REPLACE PROCEDURE testHello
+--     IS
+--     BEGIN
+--       DBMS_OUTPUT.PUT_LINE('Hello From The Database!');
+--     END;
+CREATE OR REPLACE PROCEDURE testHello
+    IS
+    BEGIN
+      DBMS_OUTPUT.PUT_LINE('Hello From The Database!');
+    END;
