70688ced1b85c623250c33c202b8c43c3a222eb7
Gordon Dickens <gordon.dickens@cigna.com>
2017-02-13 10:15:50 -0500

    Corrected documentation type-o on line 203 "teh" is now "the". Corrected the skip documentation default value is false.  Lines 238 & 241

--- liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/AbstractLiquibaseMojo.java
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/AbstractLiquibaseMojo.java
@@ -200,7 +200,7 @@ public abstract class AbstractLiquibaseMojo extends AbstractMojo {
     protected boolean propertyFileWillOverride;
 
     /**
-     * Flag for forcing the checksums to be cleared from teh DatabaseChangeLog table.
+     * Flag for forcing the checksums to be cleared from the DatabaseChangeLog table.
      *
      * @parameter expression="${liquibase.clearCheckSums}" default-value="false"
      */
@@ -235,10 +235,10 @@ public abstract class AbstractLiquibaseMojo extends AbstractMojo {
     private Properties expressionVars;
 
     /**
-     * Set this to 'false' to skip running liquibase. Its use is NOT RECOMMENDED, but quite
+     * Set this to 'true' to skip running liquibase. Its use is NOT RECOMMENDED, but quite
      * convenient on occasion.
      *
-     * @parameter expression="${liquibase.skip}"
+     * @parameter expression="${liquibase.skip}" default-value="false"
      */
     protected boolean skip = false;
 
