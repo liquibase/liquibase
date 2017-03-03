e7c54297848f97317d02d0607d424b4b0394a543
Roman Fuerst <roman.fuerst@becompany.ch>
2017-03-03 17:15:07 +0100

    CORE-3022 Use classloader of initially passed ressource accessor to load the JDBC driver on derby close.

--- liquibase-core/src/main/java/liquibase/database/core/DerbyDatabase.java
+++ liquibase-core/src/main/java/liquibase/database/core/DerbyDatabase.java
@@ -1,26 +1,21 @@
 package liquibase.database.core;
 
-import java.sql.*;
-
 import liquibase.CatalogAndSchema;
 import liquibase.database.AbstractJdbcDatabase;
-import liquibase.database.Database;
 import liquibase.database.DatabaseConnection;
 import liquibase.database.OfflineConnection;
+import liquibase.database.jvm.DerbyConnection;
 import liquibase.database.jvm.JdbcConnection;
-import liquibase.executor.Executor;
-import liquibase.executor.ExecutorService;
-import liquibase.statement.SqlStatement;
-import liquibase.statement.core.CreateDatabaseChangeLogLockTableStatement;
-import liquibase.statement.core.DropTableStatement;
-import liquibase.statement.core.InitializeDatabaseChangeLogLockTableStatement;
-import liquibase.statement.core.RawSqlStatement;
-import liquibase.structure.DatabaseObject;
 import liquibase.exception.DatabaseException;
+import liquibase.executor.ExecutorService;
 import liquibase.logging.LogFactory;
 import liquibase.logging.Logger;
+import liquibase.statement.core.RawSqlStatement;
+import liquibase.structure.DatabaseObject;
 
 import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Enumeration;
 
@@ -150,9 +145,11 @@ public class DerbyDatabase extends AbstractJdbcDatabase {
                     url += ";shutdown=true";
                 }
                 LogFactory.getLogger().info("Shutting down derby connection: " + url);
+                // Get the classloader of the initially passed ResourceAccessor
+                JdbcConnection connection = (JdbcConnection) getConnection();
+                ClassLoader classLoader = connection.getWrappedConnection().getClass().getClassLoader();
+                Driver driver = (Driver) classLoader.loadClass(driverName).newInstance();
                 // this cleans up the lock files in the embedded derby database folder
-                ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-                Driver driver = (Driver) contextClassLoader.loadClass(driverName).newInstance();
                 driver.connect(url, null);
             } catch (Exception e) {
                 if (e instanceof SQLException) {
