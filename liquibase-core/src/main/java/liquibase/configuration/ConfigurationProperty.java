c68db68e1c0636c2ddcf340df151ac7a5be3aa19
Nathan Voxland <nathan@voxland.net>
2017-05-24 13:34:24 -0500

    Allow string->list property conversions

--- liquibase-core/src/main/java/liquibase/configuration/ConfigurationProperty.java
+++ liquibase-core/src/main/java/liquibase/configuration/ConfigurationProperty.java
@@ -1,6 +1,7 @@
 package liquibase.configuration;
 
 import liquibase.exception.UnexpectedLiquibaseException;
+import liquibase.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -97,8 +98,10 @@ public class ConfigurationProperty {
                 return new BigDecimal((String) value);
             } else if (type.equals(Long.class)) {
             	return Long.valueOf((String) value);
+            } else if (type.equals(List.class)) {
+                return StringUtils.splitAndTrim((String) value, ",");
             } else {
-                throw new UnexpectedLiquibaseException("Cannot parse property "+type.getSimpleName()+" to a "+type.getSimpleName());
+                throw new UnexpectedLiquibaseException("Cannot parse property "+value.getClass().getSimpleName()+" to a "+type.getSimpleName());
             }
         } else {
             throw new UnexpectedLiquibaseException("Could not convert "+value.getClass().getSimpleName()+" to a "+type.getSimpleName());
