354868b8550ec43f49dcbf7a238445d6ad1f87ab
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-05-24 12:44:27 +0200

    HSQLDB integration tests: Create schema LBCAT2 for running integration tests. Integration tests for HSQLDB now green :)

--- liquibase-integration-tests/src/test/java/liquibase/dbtest/hsqldb/HsqlIntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/hsqldb/HsqlIntegrationTest.java
@@ -1,13 +1,32 @@
 package liquibase.dbtest.hsqldb;
 
 import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
 import liquibase.dbtest.AbstractIntegrationTest;
-import liquibase.exception.MigrationFailedException;
-import org.junit.Test;
+
+import java.sql.SQLSyntaxErrorException;
 
 public class HsqlIntegrationTest extends AbstractIntegrationTest {
 
+    public static final String OBJECT_ALREADY_EXISTS = "42504";
+
     public HsqlIntegrationTest() throws Exception {
         super("hsqldb", DatabaseFactory.getInstance().getDatabase("hsqldb"));
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        try {
+            // Create schemas for tests testRerunDiffChangeLogAltSchema
+            ((JdbcConnection) getDatabase().getConnection()).getUnderlyingConnection().createStatement().executeUpdate(
+                    "CREATE SCHEMA LBCAT2"
+            );
+        } catch (SQLSyntaxErrorException e) {
+            if (e.getSQLState().equals(OBJECT_ALREADY_EXISTS)) {
+                ; // do nothing
+            }
+        }
+        getDatabase().commit();
     }
 }
