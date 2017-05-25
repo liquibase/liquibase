76e4dac8b0d1708710ea4895e58bf2c948c1634a
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-05-25 11:56:06 +0200

    Firebird integration test setup script: Added remark about the need to set WireCrypt=Enabled for JDBC driver 3.0.0

--- liquibase-integration-tests/setup/firebird/create_dbs_for_integration_tests.sql
+++ liquibase-integration-tests/setup/firebird/create_dbs_for_integration_tests.sql
@@ -1,6 +1,10 @@
 
 /* WARNING: You need to adjust the path in CREATE DATABASE for this script to work. This script needs to be
    execute inside an "isql" prompt.
+
+   Also, you must set "WireCrypt = Enabled" in your firebird.conf since the JDBC driver 3.0.0 does not
+   seem to support encrypted connections (Firebird default is to not allow unencrypted connections). After that,
+   restart your Firebird service.
  */
 
 CREATE DATABASE 'd:\temp\liquibase.fdb' page_size 8192 user 'sysdba' password 'liquibase';
