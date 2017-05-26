dcfbba287d0fb35e9634e216e8c0e4719290da5b
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-05-26 14:00:06 +0200

    Removed binary SQL Server JDBC driver from the source tree and replaced it with a Maven reference. Also removed the temporary exclusion for database-specific integration tests.

---
+++ liquibase-integration-tests/setup/mssql/create_dbs_for_integration_tests.sql
@@ -0,0 +1,42 @@
+
+/*
+ * WARNING: By default, new installations of Microsoft SQL Server only listen to named pipes. The default JDBC
+ * connection string for the integration tests expect it to listen on the default TCP port 1433. Please use the
+ * "SQL Server Configuration Manager" tool to enable TCP/IP on "SQL Server Network Configuration" ->
+ * Protocols for MSSQLSERVER. You do not need to enable it for all network interfaces; localhost (127.0.0.1) is
+ * sufficient for the tests.
+ *
+ * WARNING: You will probably want to adjust the path for the data files in the following script.
+ */
+USE [master]
+GO
+CREATE LOGIN [lbuser] WITH PASSWORD=N'lbuser', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+CREATE DATABASE [liquibase]
+ ON  PRIMARY 
+( NAME = N'liquibase', FILENAME = N'D:\MSSQL\MSSQL13.MSSQLSERVER\MSSQL\DATA\liquibase.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'liquibase_log', FILENAME = N'D:\MSSQL\MSSQL13.MSSQLSERVER\MSSQL\DATA\liquibase_log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+GO
+ALTER DATABASE [liquibase] SET COMPATIBILITY_LEVEL = 100
+GO
+
+USE [liquibase]
+GO
+USE [liquibase]
+GO
+IF NOT EXISTS (SELECT name FROM sys.filegroups WHERE is_default=1 AND name = N'PRIMARY') ALTER DATABASE [liquibase] MODIFY FILEGROUP [PRIMARY] DEFAULT
+GO
+
+USE [master]
+GO
+ALTER LOGIN [lbuser] WITH DEFAULT_DATABASE=[liquibase], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+USE [liquibase]
+GO
+CREATE USER [lbuser] FOR LOGIN [lbuser]
+GO
+USE [liquibase]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [lbuser]
+GO
