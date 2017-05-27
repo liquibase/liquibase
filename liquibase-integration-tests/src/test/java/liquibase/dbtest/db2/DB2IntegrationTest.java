ac041de18a75f97e74999219ed6e9a5ace71f434
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-05-27 12:18:40 +0200

    Revived IBM DB2 integration tests
    * Removed binary IBM DB2 JDBC driver from the source tree
    * Added instructions on how to manually create a local Maven repo artifact for the driver (no genuine Maven source available, driver is proprietary)

---
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/db2/DB2IntegrationTest.java
@@ -0,0 +1,15 @@
+package liquibase.dbtest.db2;
+
+import liquibase.database.DatabaseFactory;
+import liquibase.dbtest.AbstractIntegrationTest;
+
+/**
+ * Integration test für IBM's DB2 database.
+ */
+
+public class DB2IntegrationTest extends AbstractIntegrationTest {
+
+    public DB2IntegrationTest() throws Exception {
+        super("db2", DatabaseFactory.getInstance().getDatabase("db2"));
+    }
+}
