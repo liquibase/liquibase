98d19c46aca3de645c292c6b6393a99059cb035c
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-06-01 00:05:29 +0200

    (code documentation)

--- liquibase-core/src/main/java/liquibase/exception/UnknownFormatException.java
+++ liquibase-core/src/main/java/liquibase/exception/UnknownFormatException.java
@@ -1,5 +1,8 @@
 package liquibase.exception;
 
+/**
+ * Thrown by SnapshotParserFactory if it cannot find a parser for a given file format.
+ */
 public class UnknownFormatException extends LiquibaseException {
     public UnknownFormatException() {
     }
