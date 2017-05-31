ea338c82d8fd05283051d0ffc3aef048fccb5430
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-05-31 22:01:24 +0200

    Informix/IntegrationTests: Need to grant DBA privileges to enable tests in alternative schema

--- liquibase-integration-tests/setup/informix/create_dbs_for_integration_tests.sql
+++ liquibase-integration-tests/setup/informix/create_dbs_for_integration_tests.sql
@@ -5,6 +5,11 @@ CREATE DATABASE IF NOT EXISTS liquibase WITH LOG;
 
 GRANT CONNECT TO liquibase;
 GRANT RESOURCE to liquibase;
+/*
+ * The DBA privilege is needed to perform the integration tests that write into an alternative schema. Unfortunately,
+ * there seems to be no smaller privilege in Informix to make this possible.
+ */
+GRANT DBA to liquibase;
 
 -- (login as superuser (informix) into the dbadmin database!)
 
