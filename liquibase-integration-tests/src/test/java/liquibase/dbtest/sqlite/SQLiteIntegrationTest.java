6b3ec851f5d0fd674b08c22a292eebcab0cdb967
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-06-02 19:50:34 +0200

    Revived SQLite integration tests (they probably need a LOT of work)

--- liquibase-integration-tests/src/test/java/liquibase/dbtest/sqlite/SQLiteIntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/sqlite/SQLiteIntegrationTest.java
@@ -1,12 +1,65 @@
 package liquibase.dbtest.sqlite;
 
+import liquibase.Liquibase;
 import liquibase.database.DatabaseFactory;
 import liquibase.dbtest.AbstractIntegrationTest;
+import liquibase.exception.ValidationFailedException;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Date;
 
 public class SQLiteIntegrationTest extends AbstractIntegrationTest {
 
 	public SQLiteIntegrationTest() throws Exception {
         super("sqlite", DatabaseFactory.getInstance().getDatabase("sqlite"));
+        File f = new File("sqlite");
+        try {
+            if (f.mkdir()) {
+                System.out.println("Directory Created");
+            } else {
+                System.out.println("Directory is not created");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+
+    @Override
+    @Test
+    public void testRunChangeLog() throws Exception {
+        super.testRunChangeLog();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Test
+    public void smartDataLoad() throws Exception {
+        if (this.getDatabase() == null) {
+            return;
+        }
+
+        Liquibase liquibase = createLiquibase("changelogs/common/smartDataLoad.changelog.xml");
+        clearDatabase(liquibase);
+
+        try {
+            liquibase.update(this.contexts);
+        } catch (ValidationFailedException e) {
+            e.printDescriptiveError(System.out);
+            throw e;
+        }
+
+        // check that the automatically rollback now works too
+        try {
+            liquibase.rollback(new Date(0), this.contexts);
+        } catch (ValidationFailedException e) {
+            e.printDescriptiveError(System.out);
+            throw e;
+        }
+    }
+
+    @Override
+    @Test
+    public void testDiffExternalForeignKeys() throws Exception {
+        //cross-schema security for oracle is a bother, ignoring test for now
+    }
 }
