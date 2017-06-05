a79662b43c4051b75888c9f27d4749087563eb53
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-06-05 18:14:04 +0200

    Integration tests: Adjusted expected SQL statements for modern layout of DATABASECHANGELOG.

--- liquibase-integration-tests/src/test/java/liquibase/statementexecute/AddUniqueConstraintExecutorTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/statementexecute/AddUniqueConstraintExecutorTest.java
@@ -124,15 +124,34 @@ public class AddUniqueConstraintExecutorTest extends AbstractExecuteTest {
     @SuppressWarnings("unchecked")
     @Test
     public void execute_withSchema() throws Exception {
-        statementUnderTest = new AddUniqueConstraintStatement(DatabaseTestContext.ALT_CATALOG, DatabaseTestContext.ALT_SCHEMA, TABLE_NAME, new ColumnConfig[] { new ColumnConfig().setName(COLUMN_NAME)}, CONSTRAINT_NAME);
+        statementUnderTest = new AddUniqueConstraintStatement(
+                DatabaseTestContext.ALT_CATALOG,
+                DatabaseTestContext.ALT_SCHEMA,
+                TABLE_NAME,
+                new ColumnConfig[]
+                        {new ColumnConfig().setName(COLUMN_NAME)},
+                CONSTRAINT_NAME
+        );
 
-        // FIXME Syntax for mysql is correct, but exception "Table 'liquibaseb.adduqtest' doesn't exist" is thrown
-// 		assertCorrect("alter table `liquibaseb`.`adduqtest` add constraint `uq_test` unique (`coltomakeuq`)", MySQLDatabase.class);
-        assertCorrect("alter table liquibaseb.adduqtest add constraint unique (coltomakeuq) constraint uq_test", InformixDatabase.class);
+        assertCorrect("ALTER TABLE liquibasec.adduqtest ADD CONSTRAINT uq_test UNIQUE (coltomakeuq)", MySQLDatabase
+                .class);
+        /*
+         * In Informix, this test case is actually impossible. While it is allowed to cross-select data from
+          * different databases (using the database:schema.table notation), it is not allowed to send DDL to a
+          * different database (even if the database is on the same instance). So, even as the following
+          * statement is semantically false, it is syntactically correct.
+         */
+        assertCorrect("ALTER TABLE liquibasec:liquibaseb.adduqtest ADD CONSTRAINT UNIQUE (coltomakeuq) CONSTRAINT " +
+                "uq_test", InformixDatabase.class);
+
         assertCorrect("alter table liquibasec.adduqtest add constraint uq_test unique (coltomakeuq)", OracleDatabase.class);
         assertCorrect("alter table liquibaseb.\"adduqtest\" add constraint uq_test unique (\"coltomakeuq\")", PostgresDatabase.class);
-        assertCorrect("alter table liquibasec.adduqtest add constraint uq_test unique (coltomakeuq)", DerbyDatabase.class);
-        assertCorrect("alter table [liquibaseb].[adduqtest] add constraint [uq_test] unique ([coltomakeuq])", SybaseASADatabase.class, SybaseDatabase.class, MSSQLDatabase.class);
+        assertCorrect("alter table liquibasec.adduqtest add constraint uq_test unique (coltomakeuq)", DerbyDatabase
+                .class);
+        assertCorrect("alter table [liquibaseb].[adduqtest] add constraint [uq_test] unique ([coltomakeuq])",
+                SybaseASADatabase.class, SybaseDatabase.class);
+        assertCorrect("alter table [liquibasec].[liquibaseb].[adduqtest] add constraint [uq_test] unique " +
+                "([coltomakeuq])", MSSQLDatabase.class);
         assertCorrect("alter table [adduqtest] add constraint [uq_test] unique ([coltomakeuq])", FirebirdDatabase.class);
 
         assertCorrect("alter table [liquibaseb].[adduqtest] add constraint [uq_test] unique ([coltomakeuq])", HsqlDatabase.class);
@@ -163,8 +182,10 @@ public class AddUniqueConstraintExecutorTest extends AbstractExecuteTest {
         assertCorrect("alter table [adduqtest] add constraint [uq_test] unique ([coltomakeuq])", SybaseASADatabase.class);
         assertCorrect("alter table adduqtest add constraint uq_test unique (coltomakeuq)", MySQLDatabase.class);
         assertCorrect("alter table adduqtest add constraint unique (coltomakeuq) constraint uq_test", InformixDatabase.class);
-        assertCorrect("alter table adduqtest add constraint uq_test unique (coltomakeuq) DEFERRABLE INITIALLY DEFERRED DISABLE", OracleDatabase.class);
-        assertCorrect("alter table \"adduqtest\" add constraint uq_test unique (\"coltomakeuq\")", PostgresDatabase.class);
+        assertCorrect("alter table adduqtest add constraint uq_test unique (coltomakeuq) DEFERRABLE INITIALLY " +
+                "DEFERRED DISABLE", OracleDatabase.class);
+        assertCorrect("ALTER TABLE \"adduqtest\" ADD CONSTRAINT uq_test unique (\"coltomakeuq\") DEFERRABLE INITIALLY" +
+                " DEFERRED DISABLE", PostgresDatabase.class);
         assertCorrect("alter table adduqtest add constraint uq_test unique (coltomakeuq)", DerbyDatabase.class);
         assertCorrect("alter table [adduqtest] add constraint [uq_test] unique ([coltomakeuq])");
 	}
--- liquibase-integration-tests/src/test/java/liquibase/statementexecute/MarkChangeSetRanExecuteTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/statementexecute/MarkChangeSetRanExecuteTest.java
@@ -20,34 +20,109 @@ public class MarkChangeSetRanExecuteTest extends AbstractExecuteTest {
 
     @Test
     public void generateSql_insert() throws Exception {
-        this.statementUnderTest = new MarkChangeSetRanStatement(new ChangeSet("a", "b", false, false, "c", "e", "f", null), ChangeSet.ExecType.EXECUTED);
+        this.statementUnderTest = new MarkChangeSetRanStatement(new ChangeSet("a", "b", false, false, "c", "e", "f",
+                null), ChangeSet.ExecType.EXECUTED);
         String version = LiquibaseUtil.getBuildVersion().replaceAll("SNAPSHOT", "SNP");
-        assertCorrect("insert into [databasechangelog] ([id], [author], [filename], [dateexecuted], [orderexecuted]," +
-                " [md5sum], [description], [comments], [exectype], [contexts], [labels], [liquibase], " +
-                "[deployment_id]) values ('a', 'b', 'c', getdate(), 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty'," +
-                " '', 'executed', 'e', null, '"+version+"', null)", MSSQLDatabase.class);
-        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values ('a', 'b', 'c', systimestamp, 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '"+version+"', null)",OracleDatabase.class);
-        assertCorrect("insert into [databasechangelog] ([id], [author], [filename], [dateexecuted], [orderexecuted], [md5sum], [description], [comments], [exectype], [liquibase]) values ('a', 'b', 'c', getdate(), 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')",SybaseDatabase.class);
-        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, liquibase) values ('a', 'b', 'c', current year to fraction(5), 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')",InformixDatabase.class);
-        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, liquibase) values ('a', 'b', 'c', current timestamp, 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')",DB2Database.class);
-        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, liquibase) values ('a', 'b', 'c', current_timestamp, 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')",FirebirdDatabase.class, DerbyDatabase.class);
-        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, liquibase) values ('a', 'b', 'c', now, 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')",HsqlDatabase.class);
-        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, liquibase) values ('a', 'b', 'c', now(), 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')",SybaseASADatabase.class);
-        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, liquibase) values ('a', 'b', 'c', now(), 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')",MySQLDatabase.class,PostgresDatabase.class,H2Database.class);
-        assertCorrectOnRest("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, md5sum, description, comments, exectype, liquibase) values ('a', 'b', 'c', current timestamp, 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', '"+version+"')");
+        assertCorrect("insert into [databasechangelog] ([id], [author], [filename], [dateexecuted], " +
+                        "[orderexecuted], [md5sum], [description], [comments], [exectype], [contexts], [labels], " +
+                        "[liquibase], [deployment_id]) values ('a', 'b', 'c', getdate(), 1, " +
+                        "'7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '" + version + "'," +
+                        " null)",
+                MSSQLDatabase.class);
+        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, " +
+                        "md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values " +
+                        "('a', 'b', 'c', systimestamp, 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', " +
+                        "'executed', 'e', null, '" + version + "', null)",
+                OracleDatabase.class);
+        assertCorrect("insert into [databasechangelog] ([id], [author], [filename], [dateexecuted], " +
+                        "[orderexecuted], [md5sum], [description], [comments], [exectype], [contexts], [labels], " +
+                        "[liquibase], [deployment_id]) values ('a', 'b', 'c', getdate(), 1, " +
+                        "'7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '" + version + "'," +
+                        " null)",
+                SybaseDatabase.class);
+        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, " +
+                        "md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values " +
+                        "('a', 'b', 'c', " +
+                        "current year to fraction(5), 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', " +
+                        "'executed', " +
+                        "'e', null, '" + version + "', null)",
+                InformixDatabase.class);
+        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, " +
+                        "md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values " +
+                        "('a', 'b', 'c', current timestamp, 1, " +
+                        "'7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '" + version + "'," +
+                        " null)",
+                DB2Database.class);
+        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, " +
+                        "md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values " +
+                        "('a', 'b', 'c', current_timestamp, 1, " +
+                        "'7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '" + version + "'," +
+                        " null)",
+                FirebirdDatabase.class, DerbyDatabase.class);
+        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, " +
+                        "md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values " +
+                        "('a', 'b', 'c', now, 1, " +
+                        "'7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '" + version + "'," +
+                        " null)",
+                HsqlDatabase.class);
+        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, " +
+                        "md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values " +
+                        "('a', 'b', 'c', now(), 1, " +
+                        "'7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '" + version + "'," +
+                        " null)",
+                SybaseASADatabase.class);
+        assertCorrect("insert into databasechangelog (id, author, filename, dateexecuted, orderexecuted, " +
+                        "md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) values " +
+                        "('a', 'b', 'c', now(), 1, " +
+                        "'7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', 'e', null, '" + version + "'," +
+                        " null)",
+                MySQLDatabase.class, MariaDBDatabase.class, PostgresDatabase.class, H2Database.class);
+        assertCorrectOnRest("insert into databasechangelog (id, author, filename, dateexecuted, " +
+                "orderexecuted, md5sum, description, comments, exectype, contexts, labels, liquibase, deployment_id) " +
+                "values ('a', 'b', 'c', " +
+                "current timestamp, 1, '7:d41d8cd98f00b204e9800998ecf8427e', 'empty', '', 'executed', " +
+                "'" + version + "')");
     }
 
     @Test
     public void generateSql_update() throws Exception {
-        this.statementUnderTest = new MarkChangeSetRanStatement(new ChangeSet("a", "b", false, false, "c", "e", "f", null), ChangeSet.ExecType.RERAN);
-        assertCorrect("update [databasechangelog] set [dateexecuted] = getdate(), [deployment_id] = null, [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e', [orderexecuted] = 2 where [id] = 'a' and [author] = 'b' and [filename] = 'c'", MSSQLDatabase.class);
-        assertCorrect("update databasechangelog set dateexecuted = systimestamp, deployment_id = null, exectype = 'reran', md5sum = '7:d41d8cd98f00b204e9800998ecf8427e', orderexecuted = 2 where id = 'a' and author = 'b' and filename = 'c'", OracleDatabase.class);
-        assertCorrect("update [databasechangelog] set [dateexecuted] = getdate(), [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'", SybaseDatabase.class);
-        assertCorrect("update [databasechangelog] set [dateexecuted] = current year to fraction(5), [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'", InformixDatabase.class);
-        assertCorrect("update [databasechangelog] set [dateexecuted] = current timestamp, [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'", DB2Database.class);
-        assertCorrect("update [databasechangelog] set [dateexecuted] = current_timestamp, [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'", FirebirdDatabase.class, DerbyDatabase.class);
-        assertCorrect("update [databasechangelog] set [dateexecuted] = NOW(), [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'", SybaseASADatabase.class);
-        assertCorrect("update [databasechangelog] set [dateexecuted] = NOW(), [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'", MySQLDatabase.class,HsqlDatabase.class,PostgresDatabase.class,H2Database.class);
-        assertCorrectOnRest("update [databasechangelog] set [dateexecuted] = NOW(), [exectype] = 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'");
+        this.statementUnderTest = new MarkChangeSetRanStatement(new ChangeSet("a", "b", false, false, "c", "e", "f",
+                null), ChangeSet.ExecType.RERAN);
+        assertCorrect("update [databasechangelog] set [dateexecuted] = getdate(), [deployment_id] = null, [exectype] " +
+                        "= 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e', [orderexecuted] = 2 where [id] =" +
+                        " 'a' and" +
+                        " [author] = 'b' and [filename] = 'c'",
+                MSSQLDatabase.class);
+        assertCorrect("update databasechangelog set dateexecuted = systimestamp, deployment_id = null, exectype = " +
+                        "'reran', md5sum = '7:d41d8cd98f00b204e9800998ecf8427e', orderexecuted = 2 where id = 'a' and" +
+                        " author " +
+                        "= 'b' and filename = 'c'",
+                OracleDatabase.class);
+        assertCorrect("update [databasechangelog] set [dateexecuted] = getdate(), [deployment_id] = null, [exectype] " +
+                "= 'reran', [md5sum] = '7:d41d8cd98f00b204e9800998ecf8427e', [orderexecuted] = 2 where [id] = 'a' and" +
+                " [author] = 'b' and [filename] = 'c'", SybaseDatabase.class);
+        assertCorrect("update [databasechangelog] set [dateexecuted] = current year to fraction(5), deployment_id = " +
+                "null, exectype = 'reran', md5sum = '7:d41d8cd98f00b204e9800998ecf8427e', orderexecuted = 2 where id " +
+                "= 'a' and author = 'b' and filename = 'c'", InformixDatabase.class);
+        assertCorrect("update [databasechangelog] set [dateexecuted] = current timestamp, deployment_id = null, " +
+                        "exectype = 'reran', md5sum = '7:d41d8cd98f00b204e9800998ecf8427e', orderexecuted = 2 where " +
+                        "id = 'a' and author = 'b' and filename = 'c'",
+                DB2Database.class);
+        assertCorrect("update [databasechangelog] set [dateexecuted] = current_timestamp, deployment_id = null, " +
+                        "exectype = 'reran', md5sum = '7:d41d8cd98f00b204e9800998ecf8427e', orderexecuted = 2 where " +
+                        "id = 'a' and author = 'b' and filename = 'c'",
+                FirebirdDatabase.class,
+                DerbyDatabase.class);
+        assertCorrect("update [databasechangelog] set [dateexecuted] = NOW(), deployment_id = null, exectype = " +
+                        "'reran', md5sum = '7:d41d8cd98f00b204e9800998ecf8427e', orderexecuted = 2 where id = 'a' and" +
+                        " author = 'b' and filename = 'c'",
+                SybaseASADatabase.class);
+        assertCorrect("update [databasechangelog] set [dateexecuted] = NOW(), deployment_id = null, exectype = " +
+                        "'reran', md5sum = '7:d41d8cd98f00b204e9800998ecf8427e', orderexecuted = 2 where id = 'a' and" +
+                        " author = 'b' and filename = 'c'",
+                MySQLDatabase.class, MariaDBDatabase.class, HsqlDatabase.class, PostgresDatabase.class, H2Database
+                        .class);
+        assertCorrectOnRest("update [databasechangelog] set [dateexecuted] = NOW(), [exectype] = 'reran', [md5sum] = " +
+                "'7:d41d8cd98f00b204e9800998ecf8427e' where id='a' and author='b' and filename='c'");
     }
 }
