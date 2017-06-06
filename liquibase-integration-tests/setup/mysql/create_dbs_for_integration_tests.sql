0fba5ff3747c815988e25ff00bb9e5397267377a
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-06-06 18:31:14 +0200

    Travis CI: Workaround for old MySQL version

--- liquibase-integration-tests/setup/mysql/create_dbs_for_integration_tests.sql
+++ liquibase-integration-tests/setup/mysql/create_dbs_for_integration_tests.sql
@@ -13,7 +13,7 @@ DROP DATABASE IF EXISTS lbcat2;
 CREATE DATABASE lbcat2
   DEFAULT CHARACTER SET utf8;
 
-DROP USER IF EXISTS 'lbuser'@'localhost';
+/* DROP USER IF EXISTS 'lbuser'@'localhost'; */
 
 CREATE USER 'lbuser'@'localhost' IDENTIFIED BY 'lbuser';
 GRANT ALL PRIVILEGES ON lbcat.* TO 'lbuser'@'localhost';
