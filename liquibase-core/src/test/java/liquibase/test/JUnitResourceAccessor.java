466c0c9925d69cae5127dc1de71d92d2170da5bd
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-06-09 22:50:56 +0200

    Code quality: Removed dead code

--- liquibase-core/src/test/java/liquibase/test/JUnitResourceAccessor.java
+++ liquibase-core/src/test/java/liquibase/test/JUnitResourceAccessor.java
@@ -9,21 +9,7 @@ import java.net.URLClassLoader;
 public class JUnitResourceAccessor extends ClassLoaderResourceAccessor {
 
     public JUnitResourceAccessor() throws Exception {
-//        File srcDir = new File(TestContext.getInstance().findCoreProjectRoot(), "src");
-//        File integrationClassesDir = new File(TestContext.getInstance().findIntegrationTestProjectRoot(), "target/classes");
-//        File integrationTestClassesDir = new File(TestContext.getInstance().findIntegrationTestProjectRoot(), "target/test-classes");
-//         classLoader = new URLClassLoader(new URL[]{
-//                //integrationClassesDir.toURL(),
-//                 //integrationTestClassesDir.toURL(),
-//                //new File(srcDir, "test/java").toURL(),
-//                 new File(TestContext.getInstance().findIntegrationTestProjectRoot(), "src/test/resources/packaged-changelog.jar").toURL(),
-//                new File(System.getProperty("java.io.tmpdir")).toURL(),
-//        });
-
         super(new URLClassLoader(new URL[]{
-                //integrationClassesDir.toURL(),
-                //integrationTestClassesDir.toURL(),
-                //new File(srcDir, "test/java").toURL(),
                 new File(TestContext.getInstance().findIntegrationTestProjectRoot(), "src/test/resources/packaged-changelog.jar").toURI().toURL(),
                 new File(System.getProperty("java.io.tmpdir")).toURI().toURL(),
         }));
