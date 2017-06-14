beb03b3f80d9f8f90792a58f87cf9f81660ac6cb
Andreas Buschka <andreas.buschka@dbmanul.org>
2017-06-14 10:09:45 +0200

    Added test for AddNotNullConstraint.getInverse()

--- liquibase-core/src/test/groovy/liquibase/change/core/AddNotNullConstraintChangeTest.groovy
+++ liquibase-core/src/test/groovy/liquibase/change/core/AddNotNullConstraintChangeTest.groovy
@@ -27,6 +27,5 @@ public class AddNotNullConstraintChangeTest extends StandardChangeTest {
         reverses.length == 1
         reverses[0].getTableName() == "TABLE_NAME"
         reverses[0].getColumnName() == "COL_HERE"
-
     }
 }
