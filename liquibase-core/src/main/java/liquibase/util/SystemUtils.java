15f62228df6798a58923053f7a4e1431b0008be0
Vicente Rossello <vrossello@travelcompositor.com>
2017-07-11 16:34:48 -0500

    Compatibility with java 9 where version can be just "9". I just copied StringUtils.substring(str, start, end) from apache commons lang3 to prevent the IndexOutofBoundsException thrown when attempting to parse jdk 9 version
    (cherry picked from commit dc31937)

--- liquibase-core/src/main/java/liquibase/util/SystemUtils.java
+++ liquibase-core/src/main/java/liquibase/util/SystemUtils.java
@@ -1151,7 +1151,7 @@ public class SystemUtils {
         if (JAVA_VERSION_TRIMMED == null) {
             return 0f;
         }
-        String str = JAVA_VERSION_TRIMMED.substring(0, 3);
+        String str = StringUtils.substring(JAVA_VERSION_TRIMMED, 0, 3);
         if (JAVA_VERSION_TRIMMED.length() >= 5) {
             str = str + JAVA_VERSION_TRIMMED.substring(4, 5);
         }
