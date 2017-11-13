0185372626ff2a0e9988dd64b03712ca54991cec
Oleg Demkovych <odemk@softserveinc.com>
2017-11-13 15:54:13 +0200

    Fixed DB2 luw semicolon causes An unexpected token "DECLARE" was found following " DECLARE v_reccount INTEGER"

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGeneratorDB2.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/InsertOrUpdateGeneratorDB2.java
@@ -1,15 +1,36 @@
 package liquibase.sqlgenerator.core;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import liquibase.database.Database;
 import liquibase.database.core.AbstractDb2Database;
 import liquibase.database.core.Db2zDatabase;
+import liquibase.sql.Sql;
+import liquibase.sql.UnparsedSql;
+import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.statement.core.InsertOrUpdateStatement;
 
 public class InsertOrUpdateGeneratorDB2 extends InsertOrUpdateGenerator {
 
+	private static final String DB2_Z_INSERT_OR_UPDATE_PROCEDURE = "insertOrUpdateDb2Z";
+
 	@Override
 	protected String getElse(Database database) {
         return "\tELSEIF v_reccount = 1 THEN\n";
+	}
+
+	@Override
+	public Sql[] generateSql(InsertOrUpdateStatement insertOrUpdateStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+		Sql[] sqls = super.generateSql(insertOrUpdateStatement, database, sqlGeneratorChain);
+		if (database instanceof Db2zDatabase) {
+			List<Sql> list = new ArrayList<Sql>(Arrays.asList(sqls));
+			list.add(new UnparsedSql("CALL " + DB2_Z_INSERT_OR_UPDATE_PROCEDURE + "()"));
+			list.add(new UnparsedSql("DROP PROCEDURE " + DB2_Z_INSERT_OR_UPDATE_PROCEDURE));
+			sqls = list.toArray(new Sql[list.size()]);
+		}
+		return sqls;
 	}
 
 	@Override
@@ -19,7 +40,7 @@ public class InsertOrUpdateGeneratorDB2 extends InsertOrUpdateGenerator {
         StringBuffer recordCheckSql = new StringBuffer();
 
 		if (database instanceof Db2zDatabase) {
-			recordCheckSql.append("CREATE PROCEDURE insertOrUpdateDb2Z()\n");
+			recordCheckSql.append("CREATE PROCEDURE ").append(DB2_Z_INSERT_OR_UPDATE_PROCEDURE).append("()\n");
 			recordCheckSql.append("BEGIN\n");
 		} else {
 			recordCheckSql.append("BEGIN ATOMIC\n");
@@ -44,12 +65,8 @@ public class InsertOrUpdateGeneratorDB2 extends InsertOrUpdateGenerator {
 	protected String getPostUpdateStatements(Database database) {
         StringBuffer endStatements = new StringBuffer();
         endStatements.append("END IF;\n");
-        endStatements.append("END;\n");
-		if (database instanceof Db2zDatabase) {
-			endStatements.append("CALL insertOrUpdateDb2Z();\n");
-			endStatements.append("DROP PROCEDURE insertOrUpdateDb2Z;\n");
-		}
-        return endStatements.toString();
+		endStatements.append("END\n");
+		return endStatements.toString();
 	}
 
 }
