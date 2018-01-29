8160df33232da5518fc7f743b1dbf2a3288d6c74
Nathan Voxland <nathan@voxland.net>
2018-01-28 22:55:07 -0600

    Fixing test

--- liquibase-integration-tests/src/test/java/liquibase/dbtest/mssql/MssqlIntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/mssql/MssqlIntegrationTest.java
@@ -54,7 +54,7 @@ public class MssqlIntegrationTest extends AbstractMssqlIntegrationTest {
                             Calendar calendar = Calendar.getInstance();
                             calendar.setTime(((Date) defaultValue));
                             assertEquals(1, calendar.get(Calendar.DAY_OF_MONTH));
-                            assertEquals(1, calendar.get(Calendar.MONTH));
+                            assertEquals(0, calendar.get(Calendar.MONTH));
                             assertEquals(2000, calendar.get(Calendar.YEAR));
                         }
                     } else if (column.getName().toLowerCase().contains("char_")) {
