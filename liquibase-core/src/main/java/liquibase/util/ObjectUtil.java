195a9f4b8756ba0e5144922111839c6786ca2f73
Nathan Voxland <nathan@voxland.net>
2018-02-06 00:07:06 -0600

    Brought over Scope object from liquibase4 codebase

--- liquibase-core/src/main/java/liquibase/util/ObjectUtil.java
+++ liquibase-core/src/main/java/liquibase/util/ObjectUtil.java
@@ -9,9 +9,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Various methods that make it easier to read and write object properties using the propertyName, instead of having
@@ -238,4 +236,135 @@ public class ObjectUtil {
         return methods;
     }
 
+      /**
+     * Converts the given object to the targetClass
+     */
+    public static <T> T convert(Object object, Class<T> targetClass) throws IllegalArgumentException {
+        if (object == null) {
+            return null;
+        }
+        if (targetClass.isAssignableFrom(object.getClass())) {
+            return (T) object;
+        }
+
+        try {
+            if (Enum.class.isAssignableFrom(targetClass)) {
+                return (T) Enum.valueOf((Class<Enum>) targetClass, object.toString());
+            } else if (Number.class.isAssignableFrom(targetClass)) {
+                if (object instanceof Number) {
+                    Number number = (Number) object;
+                    String numberAsString = number.toString();
+                    numberAsString = numberAsString.replaceFirst("\\.0+$", ""); //remove zero decimal so int/long/etc. can parse it correctly.
+
+                    if (targetClass.equals(Byte.class)) {
+                        long value = Long.valueOf(numberAsString);
+                        if (value < Byte.MIN_VALUE || value > Byte.MAX_VALUE) {
+                            raiseOverflowException(number, targetClass);
+                        }
+                        return (T) (Byte) number.byteValue();
+                    } else if (targetClass.equals(Short.class)) {
+                        long value = Long.valueOf(numberAsString);
+                        if (value < Short.MIN_VALUE || value > Short.MAX_VALUE) {
+                            raiseOverflowException(number, targetClass);
+                        }
+                        return (T) (Short) number.shortValue();
+                    } else if (targetClass.equals(Integer.class)) {
+                        long value = Long.valueOf(numberAsString);
+                        if (value < Integer.MIN_VALUE || value > Integer.MAX_VALUE) {
+                            raiseOverflowException(number, targetClass);
+                        }
+                        return (T) (Integer) number.intValue();
+                    } else if (targetClass.equals(Long.class)) {
+                        return (T) Long.valueOf(numberAsString);
+                    } else if (targetClass.equals(Float.class)) {
+                        return (T) (Float) number.floatValue();
+                    } else if (targetClass.equals(Double.class)) {
+                        return (T) (Double) number.doubleValue();
+                    } else if (targetClass.equals(BigInteger.class)) {
+                        return (T) new BigInteger(numberAsString);
+                    } else if (targetClass.equals(BigDecimal.class)) {
+                        // using BigDecimal(String) here, to avoid unpredictability of BigDecimal(double)
+                        // (see BigDecimal javadoc for details)
+                        return (T) new BigDecimal(numberAsString);
+                    } else {
+                        return raiseUnknownConversionException(object, targetClass);
+                    }
+                } else if (object instanceof String) {
+                    String string = (String) object;
+                    if (string.contains(".")) {
+                        string = string.replaceFirst("\\.0+$", "");
+                    }
+                    if (string.equals("")) {
+                        string = "0";
+                    }
+                    if (targetClass.equals(Byte.class)) {
+                        return (T) Byte.decode(string);
+                    } else if (targetClass.equals(Short.class)) {
+                        return (T) Short.decode(string);
+                    } else if (targetClass.equals(Integer.class)) {
+                        return (T) Integer.decode(string);
+                    } else if (targetClass.equals(Long.class)) {
+                        return (T) Long.decode(string);
+                    } else if (targetClass.equals(Float.class)) {
+                        return (T) Float.valueOf(string);
+                    } else if (targetClass.equals(Double.class)) {
+                        return (T) Double.valueOf(string);
+                    } else if (targetClass.equals(BigInteger.class)) {
+                        return (T) new BigInteger(string);
+                    } else if (targetClass.equals(BigDecimal.class)) {
+                        return (T) new BigDecimal(string);
+                    } else {
+                        return raiseUnknownConversionException(object, targetClass);
+                    }
+                } else {
+                    return raiseUnknownConversionException(object, targetClass);
+                }
+            } else if (targetClass.isAssignableFrom(Boolean.class)) {
+                String lowerCase = object.toString().toLowerCase();
+                return (T) (Boolean) (lowerCase.equals("true") || lowerCase.equals("t") || lowerCase.equals("1") || lowerCase.equals("1.0") || lowerCase.equals("yes"));
+            } else if (targetClass.isAssignableFrom(String.class)) {
+                return (T) object.toString();
+            } else if (targetClass.isAssignableFrom(List.class)) {
+                if (object instanceof List) {
+                    return (T) object;
+                } else if (object instanceof Collection) {
+                    return (T) new ArrayList((Collection) object);
+                } else if (object instanceof Object[]) {
+                    return (T) new ArrayList(Arrays.asList((Object[]) object));
+                } else {
+                    return (T) object;
+                }
+            } else if (targetClass.isAssignableFrom(StringClauses.class)) {
+                return (T) new StringClauses().append(object.toString());
+            } else if (targetClass.isAssignableFrom(Class.class)) {
+                try {
+                    return (T) Class.forName(object.toString());
+                } catch (ClassNotFoundException e) {
+                    throw new IllegalArgumentException(e);
+                }
+            }
+            return (T) object;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static <T> T raiseUnknownConversionException(Object object, Class<T> targetClass) {
+        throw new IllegalArgumentException("Could not convert '" + object + "' of type " + object.getClass().getName() + " to unknown target class " + targetClass.getName());
+    }
+
+    private static void raiseOverflowException(Number number, Class targetClass) {
+        throw new IllegalArgumentException("Could not convert '" + number + "' of type " + number.getClass().getName() + " to target class " + targetClass.getName() + ": overflow");
+    }
+
+    /**
+     * Return the defaultValue if the passed value is null. Otherwise, return the original value.
+     */
+    public static <T> T defaultIfNull(T value, T defaultValue) {
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return value;
+        }
+    }
 }
