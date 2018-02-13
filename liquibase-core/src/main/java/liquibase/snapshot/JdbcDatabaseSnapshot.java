1f297df226508d2d351dd3a8c236e5b7dde96995
Nathan Voxland <nathan@voxland.net>
2018-02-13 00:31:48 -0600

    Merge branch 'CORE-2940--do-not-warn-about--DBA_RECYCLEBIN' of https://github.com/iracic/liquibase into iracic-CORE-2940--do-not-warn-about--DBA_RECYCLEBIN
    # Conflicts:
    #	liquibase-core/src/main/java/liquibase/snapshot/JdbcDatabaseSnapshot.java

--- liquibase-core/src/main/java/liquibase/snapshot/JdbcDatabaseSnapshot.java
+++ liquibase-core/src/main/java/liquibase/snapshot/JdbcDatabaseSnapshot.java
@@ -28,7 +28,7 @@ import liquibase.util.JdbcUtils;
 public class JdbcDatabaseSnapshot extends DatabaseSnapshot {
 
     private boolean warnedAboutDbaRecycleBin;
-    private static final boolean bIgnoreWarning_RecycleBin = Boolean.getBoolean( "liquibase.oracle.ignoreRecycleBin" );
+    private static final boolean ignoreWarnAboutDbaRecycleBin = Boolean.getBoolean( "liquibase.ignoreRecycleBinWarning" );
 
     private CachingDatabaseMetaData cachingDatabaseMetaData;
 
@@ -503,7 +503,7 @@ public class JdbcDatabaseSnapshot extends DatabaseSnapshot {
         }
 
         protected void warnAboutDbaRecycleBin() {
-            if (!bIgnoreWarning_RecycleBin && !warnedAboutDbaRecycleBin && !(((OracleDatabase) database).canAccessDbaRecycleBin())) {
+            if (!ignoreWarnAboutDbaRecycleBin && !warnedAboutDbaRecycleBin && !(((OracleDatabase) database).canAccessDbaRecycleBin())) {
                 LogService.getLog(getClass()).warning(LogType.LOG, ((OracleDatabase) database).getDbaRecycleBinWarning());
                 warnedAboutDbaRecycleBin = true;
             }
