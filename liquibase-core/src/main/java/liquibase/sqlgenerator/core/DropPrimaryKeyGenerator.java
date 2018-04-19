c2a6b136caac889aca06789dc212960318463f66
Gertjan van Oosten <gertjan@west.nl>
2018-04-19 12:42:54 +0200

    Fix dropping PostgreSQL primary key constraint
    Properly quote schema name and table name when dropping PostgreSQL primary key constraint;
    fixes https://liquibase.jira.com/browse/CORE-3129

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/DropPrimaryKeyGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/DropPrimaryKeyGenerator.java
@@ -61,9 +61,10 @@ public class DropPrimaryKeyGenerator extends AbstractSqlGenerator<DropPrimaryKey
 						+ "    FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc\n"
 						+ "    WHERE CONSTRAINT_TYPE = 'PRIMARY KEY'\n"
 						+ "      AND TABLE_NAME = '%2$s' AND TABLE_SCHEMA = '%1$s';\n"
-						+ "    EXECUTE 'alter table %1$s.%2$s drop constraint ' || constraint_name;\n"
+						+ "    EXECUTE 'alter table %3$s.%4$s drop constraint ' || constraint_name;\n"
 						+ "END $$;"
-						, schemaName, tableName);
+						, schemaName, tableName
+						, database.escapeObjectName(schemaName, Schema.class), database.escapeObjectName(tableName, Table.class));
 			} else {
 				sql = "ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " DROP CONSTRAINT " + database.escapeConstraintName(statement.getConstraintName());
 			}
