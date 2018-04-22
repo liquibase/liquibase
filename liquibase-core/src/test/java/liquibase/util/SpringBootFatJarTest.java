4b451e2508f1ea4a18dbf2607b285dd1f0746dbd
devnied <mxjulien@gmail.com>
2018-04-22 22:36:43 +0200

    Fix CORE-3213 includeAll will not find child changelogs in multi-modules Spring Boot's executable JAR Remove duplicate URL to improve speed of liquibase Avoid reuse path variable

--- liquibase-core/src/test/java/liquibase/util/SpringBootFatJarTest.java
+++ liquibase-core/src/test/java/liquibase/util/SpringBootFatJarTest.java
@@ -8,12 +8,30 @@ public class SpringBootFatJarTest {
     @Test
     public void testGetPathForResourceWithFatJarPath() {
         String result = SpringBootFatJar.getPathForResource("some/path!/that/has!/two/bangs");
-        assertEquals(result, "that/has/two/bangs");
+        assertEquals("that/has/two/bangs", result);
+    }
+
+    @Test
+    public void testGetPathForNestedJarResourceWithFatJarPath() {
+        String result = SpringBootFatJar.getPathForResource("some/path!/that/has/jar.jar!/one/bangs");
+        assertEquals("that/has/jar.jar", result);
     }
 
     @Test
     public void testGetPathForResourceWithSimplePath() {
         String result = SpringBootFatJar.getPathForResource("some/path!/that/has/one/bang");
-        assertEquals(result, "some/path!/that/has/one/bang");
+        assertEquals("some/path!/that/has/one/bang", result);
+    }
+
+    @Test
+    public void testGetSimplePathForResourceWithFatJarPath() {
+        String result = SpringBootFatJar.getSimplePathForResources("/that/has/two/bangs/entryname","some/path!/that/has!/two/bangs");
+        assertEquals("two/bangs/entryname", result);
+    }
+
+    @Test
+    public void testGetSimplePathForResourceWithSimplePath() {
+        String result = SpringBootFatJar.getSimplePathForResources("/that/has/one/bang","some/path!/that/has/one/bang");
+        assertEquals("/that/has/one/bang", result);
     }
 }
