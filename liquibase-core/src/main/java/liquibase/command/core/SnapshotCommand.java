2146e6671a1cbc306daf00a6614fdca47da6a464
Wesley Willard <wwillard@datical.com>
2018-05-23 13:54:45 -0500

    DAT-233 WIP

--- liquibase-core/src/main/java/liquibase/command/core/SnapshotCommand.java
+++ liquibase-core/src/main/java/liquibase/command/core/SnapshotCommand.java
@@ -137,5 +137,9 @@ public class SnapshotCommand extends AbstractCommand<SnapshotCommand.SnapshotCom
 
             return SnapshotSerializerFactory.getInstance().getSerializer(format).serialize(snapshot, true);
         }
+
+        public void merge(SnapshotCommandResult resultToMerge) {
+            this.snapshot.merge(resultToMerge.snapshot);
+        }
     }
 }
