e2d44d9c909c81c72ac5a23deaaa1988590e65fb
rmakhmutov <rmakhmutov@i-novus.ru>
2018-06-07 16:09:58 +0300

    bf fixed fat jar resource path for unittesting CORE-3192, CORE-2863 related

--- liquibase-core/src/test/java/liquibase/util/SpringBootFatJarTest.java
+++ liquibase-core/src/test/java/liquibase/util/SpringBootFatJarTest.java
@@ -6,14 +6,20 @@ import static org.junit.Assert.*;
 public class SpringBootFatJarTest {
 
     @Test
-    public void testGetPathForResourceWithFatJarPath() {
+    public void testGetPathForResourceWithTwoBangs() {
         String result = SpringBootFatJar.getPathForResource("some/path!/that/has!/two/bangs");
         assertEquals(result, "that/has/two/bangs");
     }
 
     @Test
-    public void testGetPathForResourceWithSimplePath() {
+    public void testGetPathForResourceWithOneBang() {
         String result = SpringBootFatJar.getPathForResource("some/path!/that/has/one/bang");
-        assertEquals(result, "some/path!/that/has/one/bang");
+        assertEquals(result, "that/has/one/bang");
+    }
+
+    @Test
+    public void testGetPathForResourceWithSimplePath() {
+        String result = SpringBootFatJar.getPathForResource("some/path/that/has/no/bangs");
+        assertEquals(result, "some/path/that/has/no/bangs");
     }
 }
