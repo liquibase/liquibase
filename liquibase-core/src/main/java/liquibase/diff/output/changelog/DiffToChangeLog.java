2e43f3de2544abe229b759a02957147955a3a772
Roman Iakivets <iakivets@gmail.com>
2019-06-11 17:33:58 +0300

    DAT-2155 Added fix for regexp

--- liquibase-core/src/main/java/liquibase/diff/output/changelog/DiffToChangeLog.java
+++ liquibase-core/src/main/java/liquibase/diff/output/changelog/DiffToChangeLog.java
@@ -552,7 +552,8 @@ public class DiffToChangeLog {
 
             for (Map<String, ?> row : queryForListResult) {
                 String afterValue = getNameWithoutArg(row.get("OBJECT_NAME").toString());
-                String referencingNames = row.get("REFERENCING_NAMES").toString().replaceAll("\\{|\\}", "").replace("\"", "");
+                String referencingNames = row.get("REFERENCING_NAMES").toString().replaceAll("\\s*\\([^\\)]*\\)\\s*", "").
+                        replaceAll("\\{|\\}", "").replace("\"", "");
                 final List<String> listReferencingNames= new ArrayList<String>(Arrays.asList(referencingNames.split(",")));
                 for (String firstValueReferencingName: listReferencingNames) {
                     if (schemas.contains(firstValueReferencingName)) continue; // -- don't add schema
