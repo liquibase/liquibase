0f6458454aa9b3327f218cc54a9eee08651c8731
Roman Iakivets <iakivets@gmail.com>
2019-06-11 11:38:21 +0300

    DAT-2472 Added implementation to sort db object using dependency graph

--- liquibase-core/src/main/java/liquibase/diff/output/changelog/DiffToChangeLog.java
+++ liquibase-core/src/main/java/liquibase/diff/output/changelog/DiffToChangeLog.java
@@ -7,19 +7,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.RandomAccessFile;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 import javax.xml.parsers.ParserConfigurationException;
 
 import liquibase.change.Change;
@@ -30,11 +18,7 @@ import liquibase.configuration.LiquibaseConfiguration;
 import liquibase.database.Database;
 import liquibase.database.ObjectQuotingStrategy;
 import liquibase.database.OfflineConnection;
-import liquibase.database.core.AbstractDb2Database;
-import liquibase.database.core.DB2Database;
-import liquibase.database.core.Db2zDatabase;
-import liquibase.database.core.MSSQLDatabase;
-import liquibase.database.core.OracleDatabase;
+import liquibase.database.core.*;
 import liquibase.diff.DiffResult;
 import liquibase.diff.ObjectDifferences;
 import liquibase.diff.compare.CompareControl;
@@ -300,7 +284,7 @@ public class DiffToChangeLog {
                     }
                 };
 
-                DependencyUtil.DependencyGraph graph = new DependencyUtil.DependencyGraph(nameListener);
+                DependencyUtil.DependencyGraph<String> graph = new DependencyUtil.DependencyGraph<String>(nameListener);
                 addDependencies(graph, schemas, objects, database);
                 graph.computeDependencies();
 
@@ -407,7 +391,8 @@ public class DiffToChangeLog {
      * Used by {@link #sortMissingObjects(Collection, Database)} to determine whether to go into the sorting logic.
      */
     protected boolean supportsSortingObjects(Database database) {
-        return database instanceof AbstractDb2Database || database instanceof MSSQLDatabase || database instanceof OracleDatabase;
+        return database instanceof AbstractDb2Database || database instanceof MSSQLDatabase || database instanceof OracleDatabase
+                || database instanceof PostgresDatabase;
     }
 
     /**
@@ -560,7 +545,108 @@ public class DiffToChangeLog {
                     }
                 }
             }
+        } else if (database instanceof PostgresDatabase) {
+            final String sql = generateSqlToGetPostgresDependencies(schemas);
+            final Executor executor = ExecutorService.getInstance().getExecutor(database);
+            final List<Map<String, ?>> queryForListResult = executor.queryForList(new RawSqlStatement(sql));
+
+            for (Map<String, ?> row : queryForListResult) {
+                String afterValue = getNameWithoutArg(row.get("OBJECT_NAME").toString());
+                String referencingNames = row.get("REFERENCING_NAMES").toString().replaceAll("\\{|\\}|\"}", "");
+                final List<String> listReferencingNames= new ArrayList<String>(Arrays.asList(referencingNames.split(",")));
+                for (String firstValueReferencingName: listReferencingNames) {
+                    if (schemas.contains(firstValueReferencingName)) continue; // -- don't add schema
+                    if (afterValue.equals(firstValueReferencingName)) continue; // -- don't add itself
+
+                    graph.add(getNameWithoutArg(firstValueReferencingName), afterValue);
+                }
+            }
         }
+    }
+
+    private String getNameWithoutArg(String objectName) {
+        String result = objectName;
+        if (result.contains("(")) {
+            result = result.substring(0, result.indexOf("("));
+        }
+        return result;
+    }
+
+    private String generateSqlToGetPostgresDependencies(List<String> schemas){
+        return "WITH RECURSIVE preference AS (\n" +
+                "  SELECT 10 AS max_depth  -- The deeper the recursion goes, the slower it performs.\n" +
+                "    , 16384 AS min_oid -- user objects only\n" +
+                "    , '^(londiste|pgq|pg_toast)'::text AS schema_exclusion\n" +
+                "    , '^pg_(conversion|language|ts_(dict|template))'::text AS class_exclusion\n" +
+                "    , '{\"SCHEMA\":\"00\", \"TABLE\":\"01\", \"CONSTRAINT\":\"02\", \"DEFAULT\":\"03\",\n" +
+                "        \"INDEX\":\"05\", \"SEQUENCE\":\"06\", \"TRIGGER\":\"07\", \"FUNCTION\":\"08\",\n" +
+                "        \"VIEW\":\"10\", \"MVIEW\":\"11\", \"FOREIGN\":\"12\"}'::json AS type_ranks), \n" +
+                "\t\tdependency_pair AS (\n" +
+                "    WITH relation_object AS ( SELECT oid, oid::regclass::text AS object_name  FROM pg_class )\n" +
+                "    SELECT objid,       \n" +
+                "        CASE classid\n" +
+                "            WHEN 'pg_attrdef'::regclass THEN (SELECT attname FROM pg_attrdef d JOIN pg_attribute c ON (c.attrelid,c.attnum)=(d.adrelid,d.adnum) WHERE d.oid = objid)\n" +
+                "            WHEN 'pg_cast'::regclass THEN (SELECT concat(castsource::regtype::text, ' AS ', casttarget::regtype::text,' WITH ', castfunc::regprocedure::text) FROM pg_cast WHERE oid = objid)\n" +
+                "            WHEN 'pg_class'::regclass THEN rel.object_name\n" +
+                "            WHEN 'pg_constraint'::regclass THEN (SELECT conname FROM pg_constraint WHERE oid = objid)\n" +
+                "            WHEN 'pg_extension'::regclass THEN (SELECT extname FROM pg_extension WHERE oid = objid)\n" +
+                "            WHEN 'pg_namespace'::regclass THEN (SELECT nspname FROM pg_namespace WHERE oid = objid)\n" +
+                "            WHEN 'pg_opclass'::regclass THEN (SELECT opcname FROM pg_opclass WHERE oid = objid)\n" +
+                "            WHEN 'pg_operator'::regclass THEN (SELECT oprname FROM pg_operator WHERE oid = objid)\n" +
+                "            WHEN 'pg_opfamily'::regclass THEN (SELECT opfname FROM pg_opfamily WHERE oid = objid)\n" +
+                "            WHEN 'pg_proc'::regclass THEN objid::regprocedure::text\n" +
+                "            WHEN 'pg_rewrite'::regclass THEN (SELECT ev_class::regclass::text FROM pg_rewrite WHERE oid = objid)\n" +
+                "            WHEN 'pg_trigger'::regclass THEN (SELECT tgname FROM pg_trigger WHERE oid = objid)\n" +
+                "            WHEN 'pg_type'::regclass THEN objid::regtype::text\n" +
+                "            ELSE objid::text\n" +
+                "        END AS object_name,\n" +
+                "        array_agg(objsubid ORDER BY objsubid) AS objsubids,\n" +
+                "        refobjid,\n" +
+                "        CASE refclassid\n" +
+                "            WHEN 'pg_namespace'::regclass THEN (SELECT nspname FROM pg_namespace WHERE oid = refobjid)\n" +
+                "            WHEN 'pg_class'::regclass THEN rrel.object_name\n" +
+                "            WHEN 'pg_opfamily'::regclass THEN (SELECT opfname FROM pg_opfamily WHERE oid = refobjid)\n" +
+                "            WHEN 'pg_proc'::regclass THEN refobjid::regprocedure::text\n" +
+                "            WHEN 'pg_type'::regclass THEN refobjid::regtype::text\n" +
+                "            ELSE refobjid::text\n" +
+                "        END AS refobj_name,\n" +
+                "        array_agg(refobjsubid ORDER BY refobjsubid) AS refobjsubids\n" +
+                "    FROM pg_depend dep\n" +
+                "    LEFT JOIN relation_object rel ON rel.oid = dep.objid\n" +
+                "    LEFT JOIN relation_object rrel ON rrel.oid = dep.refobjid, preference\n" +
+                "      WHERE deptype = ANY('{n,a}')\n" +
+                "      AND objid >= preference.min_oid\n" +
+                "      AND (refobjid >= preference.min_oid OR refobjid = 2200) -- need public schema as root node\n" +
+                "      AND classid::regclass::text !~ preference.class_exclusion\n" +
+                "      AND refclassid::regclass::text !~ preference.class_exclusion\n" +
+                "      AND COALESCE(SUBSTRING(objid::regclass::text, E'^(\\\\w+)\\\\.'),'') !~ preference.schema_exclusion\n" +
+                "      AND COALESCE(SUBSTRING(refobjid::regclass::text, E'^(\\\\w+)\\\\.'),'') !~ preference.schema_exclusion\n" +
+                "    GROUP BY classid, objid, refclassid, refobjid, deptype, rel.object_name, rrel.object_name\n" +
+                "), \n" +
+                "dependency_hierarchy AS ( SELECT DISTINCT 0 AS level, refobjid AS objid, refobj_name AS object_name,\n" +
+                "        ARRAY[refobjid] AS dependency_chain, ARRAY[refobj_name] AS dependency_name_chain\n" +
+                "    FROM dependency_pair root, preference\n" +
+                "    WHERE NOT EXISTS (SELECT 'x' FROM dependency_pair branch WHERE branch.objid = root.refobjid)\n" +
+                "    AND refobj_name !~ preference.schema_exclusion\n" +
+                "       UNION ALL\n" +
+                "    SELECT level + 1 AS level, child.objid, child.object_name, parent.dependency_chain || child.objid,\n" +
+                "        parent.dependency_name_chain || concat(preference.type_ranks->>' ',child.object_name)\n" +
+                "    FROM dependency_pair child\n" +
+                "    JOIN dependency_hierarchy parent ON (parent.objid = child.refobjid), preference\n" +
+                "    WHERE level < preference.max_depth\n" +
+                "    AND child.object_name !~ preference.schema_exclusion\n" +
+                "    AND child.refobj_name !~ preference.schema_exclusion\n" +
+                "    AND NOT (child.objid = ANY(parent.dependency_chain)) -- prevent circular referencing\n" +
+                ")\n" +
+                "SELECT object_name, cast(dependency_name_chain as text) as referencing_names  FROM dependency_hierarchy" +
+                " where " +
+                StringUtils.join(schemas, " OR ", new StringUtils.StringUtilsFormatter<String>() {
+                    @Override
+                    public String toString(String obj) {
+                        return " object_name like '" + obj + ".%' ";
+                    }
+                }) +
+                " ORDER BY dependency_chain";
     }
 
     protected List<Class<? extends DatabaseObject>> getOrderedOutputTypes(Class<? extends ChangeGenerator> generatorType) {
