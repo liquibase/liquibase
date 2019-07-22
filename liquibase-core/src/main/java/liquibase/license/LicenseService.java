3e8b99fdb23f0060905d5f52ecd6536201f8f31a
Wesley Willard <wwillard@datical.com>
2019-07-22 13:11:33 -0500

    Modified isValidLicense method to return simple boolean

--- liquibase-core/src/main/java/liquibase/license/LicenseService.java
+++ liquibase-core/src/main/java/liquibase/license/LicenseService.java
@@ -1,7 +1,5 @@
 package liquibase.license;
 
-import liquibase.license.LiquibaseProLicense;
-
 public interface LicenseService {
-  LiquibaseProLicense isValidLicense(String licenseType);
+  boolean isValidLicense(String licenseType);
 }
