5c47e4cf61f2083dca79ea16e942c3de4a6f7eb4
Steve Donie <sdonie@datical.com>
2019-08-05 16:18:59 -0500

    add fallback method of getting build version from manifest

--- liquibase-core/src/main/java/liquibase/util/LiquibaseUtil.java
+++ liquibase-core/src/main/java/liquibase/util/LiquibaseUtil.java
@@ -1,12 +1,18 @@
 package liquibase.util;
 
+import liquibase.exception.UnexpectedLiquibaseException;
+import liquibase.integration.commandline.CommandLineUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Properties;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 public class LiquibaseUtil {
+
     public static String getBuildVersion() {
         String buildVersion = "UNKNOWN";
         Properties buildInfo = new Properties();
@@ -16,8 +22,8 @@ public class LiquibaseUtil {
         InputStream in = null;
         try {
             if (buildInfoFile != null) {
-            	URLConnection connection = buildInfoFile.openConnection();
-            	connection.setUseCaches(false);
+                URLConnection connection = buildInfoFile.openConnection();
+                connection.setUseCaches(false);
                 in = connection.getInputStream();
                 buildInfo.load(in);
                 String o = (String) buildInfo.get("build.version");
@@ -25,7 +31,24 @@ public class LiquibaseUtil {
                 if (o != null) {
                     buildVersion = o;
                 }
+            } else {
+                Class clazz = LiquibaseUtil.class;
+                String className = clazz.getSimpleName() + ".class";
+                String classPath = clazz.getResource(className).toString();
+                if (classPath.startsWith("jar")) {
+                    String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) +
+                            "/META-INF/MANIFEST.MF";
+                    Manifest manifest = null;
+                    try {
+                        manifest = new Manifest(new URL(manifestPath).openStream());
+                    } catch (IOException e) {
+                        throw new UnexpectedLiquibaseException("Cannot open a URL to the manifest of our own JAR file.");
+                    }
+                    Attributes attr = manifest.getMainAttributes();
+                    buildVersion = attr.getValue("Bundle-Version");
+                }
             }
+
         } catch (IOException e) {
             // This is not a fatal exception.
             // Build info will be returned as 'UNKNOWN'        }
