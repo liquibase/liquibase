1b9d635c2bf21130d9b466471418af9f7f0b7327
Wesley Willard <wwillard@datical.com>
2019-08-05 14:25:32 -0500

    DAT-2700 Modified license check method so that message is correct

--- liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
+++ liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
@@ -13,7 +13,7 @@ import liquibase.changelog.ChangeSet;
 public class LicenseServiceUtils {
     private static Logger LOG = LogService.getLog(LicenseServiceUtils.class);
 
-    public static ValidationErrors checkForValidLicense(String licenseType, ChangeSet changeSet) {
+    public static ValidationErrors checkForValidLicense(String licenseType, ChangeSet changeSet, String changeType) {
       LicenseService licenseService = LicenseServiceFactory.getInstance().getLicenseService();
       if (licenseService == null) {
         return new ValidationErrors();
@@ -26,7 +26,7 @@ public class LicenseServiceUtils {
       }
       ValidationErrors validationErrors = new ValidationErrors();
       String message = "Change Set ID: " + changeSet.getId() + " Change Set Author: " + changeSet.getAuthor() + "\n";
-      message += "Change Type 'pro:createFunction' is not allowed without a valid Liquibase Pro License.\n";
+      message += "Change Type 'pro:" + changeType + "' is not allowed without a valid Liquibase Pro License.\n";
       message += "To purchase or renew a Liquibase Pro license key please contact lbprosupport@datical.com or\n" +
                 "go to https://download.liquibase.org/download/pricing";
       validationErrors.addError(message);
