c390a430aad086f85ebb0fc1171db881b2e23fb3
Wesley Willard <wwillard@datical.com>
2019-08-05 21:53:14 -0500

    Simplified interface

--- liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
+++ liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
@@ -3,6 +3,8 @@ package liquibase.license;
 import liquibase.exception.ValidationErrors;
 import liquibase.logging.LogService;
 import liquibase.logging.Logger;
+import liquibase.change.Change;
+import liquibase.change.ChangeFactory;
 import liquibase.changelog.ChangeSet;
 
 /**
@@ -13,17 +15,19 @@ import liquibase.changelog.ChangeSet;
 public class LicenseServiceUtils {
     private static Logger LOG = LogService.getLog(LicenseServiceUtils.class);
 
-    public static ValidationErrors checkForValidLicense(String licenseType, ChangeSet changeSet, String changeType) {
+    public static ValidationErrors checkForValidLicense(String licenseType, Change change) {
       LicenseService licenseService = LicenseServiceFactory.getInstance().getLicenseService();
       if (licenseService == null) {
         return new ValidationErrors();
       }
-
       if (licenseService.licenseIsValid(licenseType)) {
         String message = String.format("Found valid license with subject '%s'",licenseType);
         LOG.info(message);
         return new ValidationErrors();
       }
+
+      ChangeSet changeSet = change.getChangeSet();
+      String changeType = ChangeFactory.getInstance().getChangeMetaData(change).getName();
       ValidationErrors validationErrors = new ValidationErrors();
       String message = "Change Set ID: " + changeSet.getId() + " Change Set Author: " + changeSet.getAuthor() + "\n";
       message += "Change Type 'pro:" + changeType + "' is not allowed without a valid Liquibase Pro License.\n";
