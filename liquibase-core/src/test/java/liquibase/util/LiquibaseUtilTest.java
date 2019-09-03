89f8ba328ff23ed75e1d6dfb1805234d9c575396
Maksym Gendin <maksym.gendin@sunhill-technologies.com>
2019-09-03 14:35:43 +0200

    Added more unit tests

--- liquibase-core/src/test/java/liquibase/util/LiquibaseUtilTest.java
+++ liquibase-core/src/test/java/liquibase/util/LiquibaseUtilTest.java
@@ -7,6 +7,8 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.anyString;
 import static org.powermock.api.mockito.PowerMockito.spy;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -34,5 +36,38 @@ public class LiquibaseUtilTest {
         when(LiquibaseUtil.readFromProperties(anyString())).thenReturn(null);
 
         assertEquals("UNKNOWN", LiquibaseUtil.getBuildVersion());
+    }
+
+    @Test
+    public void testShouldReturnNotNullBuildTime() {
+        final String buildTime = LiquibaseUtil.getBuildTime();
+        assertNotNull(buildTime);
+        assertNotEquals("UNKNOWN", buildTime);
+    }
+
+    @Test
+    public void testShouldReturnNotNullBuildVersion() {
+        final String buildVersion = LiquibaseUtil.getBuildVersion();
+        assertNotNull(buildVersion);
+        assertNotEquals("UNKNOWN", buildVersion);
+    }
+
+    @Test
+    public void testShouldReturnNotNullBuildTimeWhenReadingFromManifestReturnsNull() {
+        when(LiquibaseUtil.readFromManifest(anyString())).thenReturn(null);
+
+        final String buildTime = LiquibaseUtil.getBuildTime();
+
+        assertNotNull(buildTime);
+        assertNotEquals("UNKNOWN", buildTime);
+    }
+
+    @Test
+    public void testShouldReturnNotNullBuildVersionWhenReadingFromManifestReturnsNull() {
+        when(LiquibaseUtil.readFromManifest(anyString())).thenReturn(null);
+
+        final String buildVersion = LiquibaseUtil.getBuildVersion();
+        assertNotNull(buildVersion);
+        assertNotEquals("UNKNOWN", buildVersion);
     }
 }
