978e8ffc7deef09843e15d7114c581d2d557b3b4
achehrynets <achehrynets@datical.com>
2019-09-17 09:38:14 -0500

    DAT-1999/DAT-3109: modified fix which fix issue with 'constraint' section in generated changelog

--- liquibase-core/src/main/java/liquibase/diff/output/changelog/core/MissingTableChangeGenerator.java
+++ liquibase-core/src/main/java/liquibase/diff/output/changelog/core/MissingTableChangeGenerator.java
@@ -156,7 +156,7 @@ public class MissingTableChangeGenerator extends AbstractChangeGenerator impleme
 
             ConstraintsConfig constraintsConfig = null;
             // In MySQL, the primary key must be specified at creation for an autoincrement column
-            if (column.isAutoIncrement() && (pkColumnList != null) && primaryKeyOrderMatchesTableOrder &&  pkColumnList.contains(column.getName())) {
+            if ((pkColumnList != null) && primaryKeyOrderMatchesTableOrder &&  pkColumnList.contains(column.getName())) {
                 if ((referenceDatabase instanceof MSSQLDatabase) && (primaryKey.getBackingIndex() != null) &&
                         (primaryKey.getBackingIndex().getClustered() != null) && !primaryKey.getBackingIndex()
                         .getClustered()) {
@@ -183,8 +183,12 @@ public class MissingTableChangeGenerator extends AbstractChangeGenerator impleme
                         constraintsConfig.setNullable(false);
                     }
                 }
-            } else if ((column.isNullable() != null) && !column.isNullable()) {
-                constraintsConfig = new ConstraintsConfig();
+            }
+
+            if ((column.isNullable() != null) && !column.isNullable()) {
+                if (constraintsConfig == null) {
+                    constraintsConfig = new ConstraintsConfig();
+                }
                 constraintsConfig.setNullable(false);
                 if (!column.shouldValidateNullable()) {
                     constraintsConfig.setShouldValidateNullable(false);
