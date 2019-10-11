14266523440db5f0f312d49ea5e22b8511834a8b
Steve Donie <steve@donie.us>
2019-10-11 14:10:25 -0500

    add some debug logging of arguments at startup

--- liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
+++ liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
@@ -158,6 +158,11 @@ public class Main {
         main.reconfigureLogging();
 
         Logger log = LogService.getLog(Main.class);
+        log.debug("There were " + args.length + " args passed to Liquibase. They are:");
+        for (String arg : args) {
+            log.debug("  " + arg);
+        }
+        boolean outputLoggingEnabled = false;
 
         try {
             GlobalConfiguration globalConfiguration = LiquibaseConfiguration.getInstance().getConfiguration
