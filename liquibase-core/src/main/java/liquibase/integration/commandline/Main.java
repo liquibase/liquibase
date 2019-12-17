e467d2049b586ecfd2cd3e2278a6556096ea1e5e
Wesley Willard <wwillard@datical.com>
2019-12-17 09:08:17 -0600

    DAT-3464 Check for invalid characters before running command

--- liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
+++ liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
@@ -125,11 +125,17 @@ public class Main {
     protected String outputSchemasAs;
     protected String referenceSchemas;
     protected String schemas;
-    protected String snapshotFormat;
     protected String liquibaseProLicenseKey;
     private Boolean managingLogConfig = null;
     private boolean outputsLogMessages = false;
-    private String sqlFile;
+
+    private static int[] unhandledCodePoints = {160, 225, 226, 227, 228, 229, 230, 198, 200, 201, 202, 203,
+                                                204, 205, 206, 207, 209, 210, 211, 212, 213, 214, 217, 218, 219,
+                                                220, 222, 223, 232, 233, 234, 235, 236, 237, 238, 239, 241,
+                                                249, 250, 251, 252, 255, 284, 332, 333, 334, 335, 336, 337, 359,
+                                                360, 361, 362, 363, 364, 365, 366, 367, 377, 399,
+                                                8192, 8193, 8194, 8196, 8197, 8199, 8200, 8201, 8202, 8203, 8211, 8287
+                                               };
 
     /**
      * Entry point. This is what gets executes when starting this program from the command line. This is actually
@@ -189,6 +195,20 @@ public class Main {
                         String.format(coreBundle.getString("version.number"), LiquibaseUtil.getBuildVersion() +
                                 StreamUtil.getLineSeparator()));
                 return 0;
+            }
+
+            //
+            // Look for characters which cannot be handled
+            //
+            for (int i=0; i < args.length; i++) {
+                int index = checkArg(args[i]);
+                if (index >= 0) {
+                    String message =
+                        "Invalid, non-printable character detected in command line at position " +
+                            (index+1) + ".\nIf problems occur, please remove the character and try again.";
+                    LOG.warning(message);
+                    System.err.println(message);
+                }
             }
 
             try {
@@ -914,6 +934,26 @@ public class Main {
         stream.println(CommandLineUtils.getBanner());
         String helpText = commandLineHelpBundle.getString("commandline-helptext");
         stream.println(helpText);
+    }
+
+    /**
+     *
+     * Check the string for known characters which cannot be handled
+     *
+     * @param   arg             Input parameter to check
+     * @return  int             The index of the found character or -1 if none found
+     *
+     */
+    private static int checkArg(String arg) {
+        char[] chars = arg.toCharArray();
+        for (int i=0; i < chars.length; i++) {
+            for (int j=0; j < unhandledCodePoints.length; j++) {
+                if (unhandledCodePoints[j] == chars[i]) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     /**
