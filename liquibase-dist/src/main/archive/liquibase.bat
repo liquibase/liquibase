37e895a5696cfccf9da90d819cce62e956f2548f
Nathan Voxland <nathan@voxland.net>
2020-01-09 10:50:28 -0600

    Updated 3.8.5 release notes

--- liquibase-dist/src/main/archive/liquibase.bat
+++ liquibase-dist/src/main/archive/liquibase.bat
@@ -9,8 +9,16 @@ set LIQUIBASE_HOME="%~dp0"
 set CP=.
 for /R %LIQUIBASE_HOME% %%f in (liquibase*.jar) do set CP=!CP!;%%f
 for /R %LIQUIBASE_HOME%\lib %%f in (*.jar) do set CP=!CP!;%%f
+
 rem remove quotes around LIQUIBASE_HOME
 set LIQUIBASE_HOME=%LIQUIBASE_HOME:"=%
+
+rem set JAVA_HOME to local jre dir if not set
+if exist "%LIQUIBASE_HOME%\jre" if "%JAVA_HOME%"=="" (
+    set JAVA_HOME=%LIQUIBASE_HOME%\jre
+)
+
+
 rem add the lib directory itself to the classpath
 set CP=!CP!;!LIQUIBASE_HOME!lib
 
@@ -19,4 +27,7 @@ setlocal DISABLEDELAYEDEXPANSION
 
 IF NOT DEFINED JAVA_OPTS set JAVA_OPTS=
 
-java -cp "%CP%" %JAVA_OPTS% liquibase.integration.commandline.Main %*
+set JAVA_PATH=""
+if NOT "%JAVA_HOME%" == "" set JAVA_PATH=%JAVA_HOME%\bin\
+
+"%JAVA_PATH%\java" -cp "%CP%" %JAVA_OPTS% liquibase.integration.commandline.Main %*
