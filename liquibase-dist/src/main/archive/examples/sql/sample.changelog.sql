1e3038477453d8b772c130c2c62c2bdeed3568f2
Nathan Voxland <nathan@voxland.net>
2020-02-04 14:20:35 -0600

    - Removed commented sample changesets - fixed typo in sample.changelog.sql DAT-3849

--- liquibase-dist/src/main/archive/examples/sql/sample.changelog.sql
+++ liquibase-dist/src/main/archive/examples/sql/sample.changelog.sql
@@ -18,14 +18,6 @@ create table company (
     city varchar(30)
 )
 
---changest other.dev:2
+--changeset other.dev:2
 alter table person add column country varchar(2)
 
-/* Uncomment the following code snippet to add the changeSet */
-/*
---changeset your.name:3
-ALTER TABLE PUBLIC.person ADD worksfor_company_id INT;
-
---changeset your.name:4
-ALTER TABLE person ADD FOREIGN KEY (fk_person_worksfor) REFERENCES company(id) ;
-*/
