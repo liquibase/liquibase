118e5a88cea7a76fcf23a2b54b3e08b4cb1a28b0
Wesley Willard <wwillard@datical.com>
2020-03-18 16:01:07 -0500

    Check for the dataOutputDirectory earlier to handle generateChangeLog without diffTypes

--- liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
+++ liquibase-core/src/main/java/liquibase/integration/commandline/Main.java
@@ -1341,6 +1341,12 @@ public class Main {
                                 OPTIONS.EXCLUDE_OBJECTS, OPTIONS.INCLUDE_OBJECTS));
             }
 
+            //
+            // Set the global configuration option based on presence of the dataOutputDirectory
+            //
+            boolean b = dataOutputDirectory != null;
+            LiquibaseConfiguration.getInstance().getConfiguration(GlobalConfiguration.class).setShouldSnapshotData(b);
+
             ObjectChangeFilter objectChangeFilter = null;
             CompareControl.ComputedSchemas computedSchemas = CompareControl.computeSchemas(
                     schemas,
@@ -1384,11 +1390,6 @@ public class Main {
                 );
                 return;
             } else if (COMMANDS.GENERATE_CHANGELOG.equalsIgnoreCase(command)) {
-                //
-                // Set the global configuration option based on presence of the dataOutputDirectory
-                //
-                boolean b = dataOutputDirectory != null;
-                LiquibaseConfiguration.getInstance().getConfiguration(GlobalConfiguration.class).setShouldSnapshotData(b);
                 String currentChangeLogFile = this.changeLogFile;
                 if (currentChangeLogFile == null) {
                     //will output to stdout:
@@ -1886,8 +1887,6 @@ public class Main {
                 defSchemaName = value;
             } else if (OPTIONS.DATA_OUTPUT_DIRECTORY.equalsIgnoreCase(attributeName)) {
                 dataOutputDirectory = value;
-                boolean b = dataOutputDirectory != null;
-                LiquibaseConfiguration.getInstance().getConfiguration(GlobalConfiguration.class).setShouldSnapshotData(b);
             }
         }
 
