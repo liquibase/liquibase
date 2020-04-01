d48348df567aed040bc8e0cb26e09ad78ad9de88
Michael Hausl <michael@hausl.com>
2020-04-01 20:40:50 +0200

    #1066 addNotNullConstraint / dropNotNullConstraint not supported on Firebird
    (cherry picked from commit a6f271bc39a26903a27365582b2f9e9001923cd1)

--- liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetNullableGenerator.java
+++ liquibase-core/src/main/java/liquibase/sqlgenerator/core/SetNullableGenerator.java
@@ -75,6 +75,8 @@ public class SetNullableGenerator extends AbstractSqlGenerator<SetNullableStatem
             }
             sql = "ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " MODIFY (" + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()) + " " + DataTypeFactory.getInstance().fromDescription(statement.getColumnDataType(), database).toDatabaseDataType(database) + nullableString + ")";
         } else if (database instanceof FirebirdDatabase && !(database instanceof Firebird3Database)) {
+            // For Firebird database prior to Firebird 3 the ALTER TABLE syntax is not working
+            // As a workaround we can modify the system table entry directly (see http://www.firebirdfaq.org/faq103/)
             sql = "UPDATE RDB$RELATION_FIELDS SET RDB$NULL_FLAG = " + (statement.isNullable() ? "1" : "NULL") + " WHERE RDB$RELATION_NAME = " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " AND RDB$FIELD_NAME = " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName());
         } else {
             sql = "ALTER TABLE " + database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName()) + " ALTER COLUMN  " + database.escapeColumnName(statement.getCatalogName(), statement.getSchemaName(), statement.getTableName(), statement.getColumnName()) + (statement.isNullable() ? " DROP NOT NULL" : " SET NOT NULL");
