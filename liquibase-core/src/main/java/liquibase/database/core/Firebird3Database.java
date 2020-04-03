2a651ce6ba26a7dab9d9cd14872c203c3023fa5d
Michael Hausl <michael@hausl.com>
2020-04-03 23:38:33 +0200

    - give Firebird3Database its own short name firebird3 - Distinguish between major versions <3 and >3

--- liquibase-core/src/main/java/liquibase/database/core/Firebird3Database.java
+++ liquibase-core/src/main/java/liquibase/database/core/Firebird3Database.java
@@ -1,5 +1,17 @@
 package liquibase.database.core;
 
+import liquibase.database.DatabaseConnection;
+import liquibase.exception.DatabaseException;
+
 public class Firebird3Database extends FirebirdDatabase {
 
+    @Override
+    public boolean isCorrectDatabaseImplementation(DatabaseConnection conn) throws DatabaseException {
+        return conn.getDatabaseProductName().startsWith("Firebird") && conn.getDatabaseMajorVersion() >= 3;
+    }
+
+    @Override
+    public String getShortName() {
+        return "firebird3";
+    }
 }
