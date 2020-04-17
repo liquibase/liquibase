49026d943b8a449e2d0d0075b93cd478eeb0f40c
Jose D. Hernandez Moreno <jdhmoreno@gmail.com>
2020-04-18 01:31:49 +0200

    Added a unit test for 'usePreparedStatements' when it is false

--- liquibase-core/src/test/groovy/liquibase/change/core/LoadDataChangeTest.groovy
+++ liquibase-core/src/test/groovy/liquibase/change/core/LoadDataChangeTest.groovy
@@ -415,4 +415,38 @@ public class LoadDataChangeTest extends StandardChangeTest {
         then:
         assert md5sum1.equals(md5sum2)
     }
+
+    def "usePreparedStatements set to false produces InsertSetStatement"() throws Exception {
+        when:
+        LoadDataChange loadDataChange = new LoadDataChange();
+        loadDataChange.setSchemaName("SCHEMA_NAME");
+        loadDataChange.setTableName("TABLE_NAME");
+        loadDataChange.setUsePreparedStatements(Boolean.FALSE);
+        loadDataChange.setFile("liquibase/change/core/sample.data1.csv");
+
+        SqlStatement[] sqlStatement = loadDataChange.generateStatements(new MSSQLDatabase());
+
+        then:
+        sqlStatement.length == 1
+        assert sqlStatement[0] instanceof InsertSetStatement
+
+        when:
+        SqlStatement[] sqlStatements = ((InsertSetStatement) sqlStatement[0]).getStatementsArray();
+
+        then:
+        sqlStatements.length == 2
+        assert sqlStatements[0] instanceof InsertStatement
+        assert sqlStatements[1] instanceof InsertStatement
+
+        "SCHEMA_NAME" == ((InsertStatement) sqlStatements[0]).getSchemaName()
+        "TABLE_NAME" == ((InsertStatement) sqlStatements[0]).getTableName()
+        "Bob Johnson" == ((InsertStatement) sqlStatements[0]).getColumnValue("name")
+        "bjohnson" == ((InsertStatement) sqlStatements[0]).getColumnValue("username")
+
+        "SCHEMA_NAME" == ((InsertStatement) sqlStatements[1]).getSchemaName()
+        "TABLE_NAME" == ((InsertStatement) sqlStatements[1]).getTableName()
+        "John Doe" == ((InsertStatement) sqlStatements[1]).getColumnValue("name")
+        "jdoe" == ((InsertStatement) sqlStatements[1]).getColumnValue("username")
+    }
+
 }
