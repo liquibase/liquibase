b108bd08ab544136f1c70dd8078b86e5ca8065fc
Stanimir Stamenkov <stanio@yahoo.com>
2020-05-02 19:34:13 +0300

    CORE-3540: Sample unit tests

--- liquibase-core/src/test/java/liquibase/database/core/DerbyDatabaseTest.java
+++ liquibase-core/src/test/java/liquibase/database/core/DerbyDatabaseTest.java
@@ -1,7 +1,12 @@
 package liquibase.database.core;
 
+import java.sql.Connection;
+import javax.sql.DataSource;
+import org.apache.derby.jdbc.BasicEmbeddedDataSource40;
+
 import junit.framework.TestCase;
 import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
 
 public class DerbyDatabaseTest extends TestCase {
     public void testGetDefaultDriver() {
@@ -17,6 +22,38 @@ public class DerbyDatabaseTest extends TestCase {
         assertEquals("TIMESTAMP('2008-01-25 13:57:41.300000')", new DerbyDatabase().getDateLiteral("2008-01-25 13:57:41.3"));
         assertEquals("TIMESTAMP('2008-01-25 13:57:41.340000')", new DerbyDatabase().getDateLiteral("2008-01-25 13:57:41.34"));
         assertEquals("TIMESTAMP('2008-01-25 13:57:41.347000')", new DerbyDatabase().getDateLiteral("2008-01-25 13:57:41.347"));
+    }
+
+    public void testCloseShutsEmbeddedDerbyDown() throws Exception {
+        DataSource ds = newDataSource();
+        try (Connection pooled = ds.getConnection()) {
+
+            Database database = new DerbyDatabase();
+            database.setConnection(new JdbcConnection(ds.getConnection()));
+            database.close();
+
+            assertEquals("connection.closed", true, pooled.isClosed());
+        }
+    }
+
+    public void testCloseDoesNotShutEmbeddedDerbyDown() throws Exception {
+        DataSource ds = newDataSource();
+        try (Connection connection = ds.getConnection()) {
+
+            DerbyDatabase database = new DerbyDatabase();
+            database.setShutdownEmbeddedDerby(false);
+            database.setConnection(new JdbcConnection(ds.getConnection()));
+            database.close();
+
+            assertEquals("connection.closed", false, connection.isClosed());
+        }
+    }
+
+    private BasicEmbeddedDataSource40 newDataSource() {
+        BasicEmbeddedDataSource40 ds = new BasicEmbeddedDataSource40();
+        ds.setDatabaseName("memory:Foo");
+        ds.setCreateDatabase("create");
+        return ds;
     }
 
 }
