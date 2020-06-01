ea8006b43caf903feadd993565b7e3dd3ce4c24f
Wesley Willard <wwillard@datical.com>
2020-06-01 07:45:58 -0500

    Added more commands and options DAT-4408

--- liquibase-dist/src/main/archive/lib/liquibase_autocomplete.sh
+++ liquibase-dist/src/main/archive/lib/liquibase_autocomplete.sh
@@ -7,58 +7,103 @@ _liquibase()
 
     # Liquibase options, has to be improved to be more context aware
     opts="
-update
-updateSQL
-updateCount 
-updateCountSQL
-rollback
-rollbackSQL
-rollbackToDate
-rollbackToDateSQL
-rollbackCount
-rollbackCountSQL
-futureRollbackSQL
-futureRollbackSQL
-updateTestingRollback
-generateChangeLog
-diff
-diffChangeLog
-dbDoc
-tag
-status
-status --verbose
-unexpectedChangeSets
-unexpectedChangeSets --verbose
-validate
-calculateCheckSum
-clearCheckSums
-changelogSync
-changelogSyncSQL
-markNextChangeSetRan
-markNextChangeSetRanSQL
-listLocks
-releaseLocks
-dropAll
---changeLogFile=
---username=
---password
---url
---classpath=
---driver=
---databaseClass=
---defaultSchemaName=
---contexts=
---defaultsFile=
---driverPropertiesFile=
---includeSystemClasspath=
---promptForNonLocalDatabase=
---logLevel=
---logFile=
---currentDateTimeFunction=
---help
---version
-
-"
+  help 
+  update 
+  updateSQL 
+  updateCount 
+  updateCountSQL 
+  updateToTag 
+  updateToTagSQL 
+  'status --verbose' 
+  rollback 
+  rollbackSQL 
+  'rollbackOneChangeSet --changeSetAuthor=<author> --changeSetId=<id> --changeSetPath=<changelogFile> --force' 
+  'rollbackOneChangeSetSQL --changeSetAuthor=<author> --changeSetId=<id> --changeSetPath=<changelogFile>' 
+  'rollbackOneUpdate --deploymentId=<deploymentId> --force' 
+  'rollbackOneUpdateSQL --deploymentId=<deploymentId>' 
+  rollbackToDate 
+  rollbackToDateSQL 
+  rollbackCount 
+  rollbackCountSQL 
+  futureRollbackSQL 
+  futureRollbackFromTagSQL 
+  updateTestingRollback 
+  generateChangeLog 
+  snapshot 
+  snapshotReference 
+  diff 
+  diffChangeLog 
+  dbDoc 
+  history 
+  tag 
+  tagExists 
+  status 
+  unexpectedChangeSets 
+  validate 
+  calculateCheckSum 
+  clearCheckSums 
+  changelogSync 
+  changelogSyncSQL 
+  markNextChangeSetRan 
+  markNextChangeSetRanSQL 
+  listLocks 
+  releaseLocks 
+  dropAll 
+  --changeLogFile 
+  --username 
+  --password 
+  --url 
+  --classpath 
+  --driver 
+  --databaseClass 
+  --propertyProviderClass 
+  --defaultSchemaName 
+  --contexts 
+  --labels 
+  --defaultsFile 
+  --delimiter 
+  --driverPropertiesFile 
+  --changeExecListenerClass 
+  --changeExecListenerPropertiesFile 
+  --liquibaseCatalogName 
+  --liquibaseSchemaName 
+  --databaseChangeLogTableName 
+  --databaseChangeLogLockTableName 
+  --databaseChangeLogTablespaceName 
+  --liquibaseSchemaName 
+  --includeSystemClasspath 
+  --overwriteOutputFile 
+  --promptForNonLocalDatabase 
+  --logLevel 
+  --logFile 
+  --currentDateTimeFunction 
+  --outputDefaultSchema 
+  --outputDefaultCatalog 
+  --outputFile 
+  --rollbackScript 
+  --excludeObjects 
+  --includeObjects 
+  --help 
+  --version 
+  --snapshotFormat 
+  --referenceUsername 
+  --referencePassword 
+  --referenceUrl 
+  --defaultCatalogName 
+  --defaultSchemaName 
+  --referenceDefaultCatalogName 
+  --referenceDefaultSchemaName 
+  --schemas 
+  --referenceSchemas 
+  --outputSchemaAs 
+  --includeCatalog 
+  --includeSchema 
+  --includeTablespace 
+  --referenceDriver 
+  --dataOutputDirectory 
+  --diffTypes 
+  --diffTypes=<catalog,tables,functions,views,columns,indexes,foreignkeys,primarykeys,uniqueconstraints,data,storedprocedure,triggers,sequences> -D<property.name>=<property.value> 
+  --liquibaseProLicenseKey"
     # Handle --xxxxxx=
     if [[ ${prev} == "--"* && ${cur} == "=" ]] ; then
         COMPREPLY=(*)
