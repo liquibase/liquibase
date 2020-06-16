00fc3a11ed153028c28990ba55761dfd37836aef
Wesley Willard <wwillard@datical.com>
2020-06-16 13:23:36 -0500

    Reduce logging to debug() in JdbcExecutor

--- liquibase-core/src/main/java/liquibase/executor/jvm/JdbcExecutor.java
+++ liquibase-core/src/main/java/liquibase/executor/jvm/JdbcExecutor.java
@@ -74,7 +74,7 @@ public class JdbcExecutor extends AbstractExecutor {
     }
 
     public Object execute(StatementCallback action, List<SqlVisitor> sqlVisitors) throws DatabaseException {
-        log.info("Executing with the '" + getName() + "' executor");
+        log.debug("Executing with the '" + getName() + "' executor");
         DatabaseConnection con = database.getConnection();
         Statement stmt = null;
         try {
