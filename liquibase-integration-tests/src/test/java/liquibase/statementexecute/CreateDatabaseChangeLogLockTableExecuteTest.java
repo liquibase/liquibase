e9827241fb43db592b89b8d05c988281fe154650
Bernhard Streit <Bernhard.Streit@gmail.com>
2020-06-17 22:52:43 +0200

    Fixed db generaiton test

--- liquibase-integration-tests/src/test/java/liquibase/statementexecute/CreateDatabaseChangeLogLockTableExecuteTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/statementexecute/CreateDatabaseChangeLogLockTableExecuteTest.java
@@ -1,12 +1,24 @@
 package liquibase.statementexecute;
 
-import liquibase.database.Database;
-import liquibase.database.core.*;
-import liquibase.statement.SqlStatement;
-import liquibase.statement.core.CreateDatabaseChangeLogLockTableStatement;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import liquibase.database.Database;
+import liquibase.database.core.DB2Database;
+import liquibase.database.core.Db2zDatabase;
+import liquibase.database.core.InformixDatabase;
+import liquibase.database.core.MSSQLDatabase;
+import liquibase.database.core.MariaDBDatabase;
+import liquibase.database.core.MySQLDatabase;
+import liquibase.database.core.OracleDatabase;
+import liquibase.database.core.PostgresDatabase;
+import liquibase.database.core.SQLiteDatabase;
+import liquibase.database.core.SybaseASADatabase;
+import liquibase.database.core.SybaseDatabase;
+import liquibase.statement.SqlStatement;
+import liquibase.statement.core.CreateDatabaseChangeLogLockTableStatement;
 
 public class CreateDatabaseChangeLogLockTableExecuteTest extends AbstractExecuteTest {
     @Override
@@ -14,94 +26,179 @@ public class CreateDatabaseChangeLogLockTableExecuteTest extends AbstractExecute
         return null;
     }
 
-    @Test
-    public void generate() throws Exception {
+    @Before
+    public void init() {
         this.statementUnderTest = new CreateDatabaseChangeLogLockTableStatement();
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] boolean not null, " +
-                "[lockgranted] datetime, " +
-                "[lockedby] text, " +
-                "constraint [pk_databasechangeloglock] primary key ([id]))"}, SQLiteDatabase.class);
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] bit not null, " +
-                "[lockgranted] datetime null, " +
-                "[lockedby] varchar(255) null, " +
-                "constraint [pk_databasechangeloglock] primary key ([id]))"}, SybaseDatabase.class);
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] bit not null, " +
-                "[lockgranted] datetime null, " +
-                "[lockedby] varchar(255) null, " +
-                "constraint [pk_databasechangeloglock] primary key ([id]))"}, SybaseASADatabase.class);
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] boolean not null, " +
-                "[lockgranted] datetime, " +
-                "[lockedby] varchar(255), " +
-                "primary key (id))"}, InformixDatabase.class);
-    
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] [int] not null, " +
-                "[locked] [bit] not null, " +
-                "[lockgranted] [datetime2](3), " +
-                "[lockedby] [nvarchar](255), " +
-                "constraint [pk_databasechangeloglock] primary key ([id]))"}, MSSQLDatabase.class);
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] integer not null, " +
-                "[locked] smallint not null, " +
-                "[lockgranted] timestamp, " +
-                "[lockedby] varchar(255), " +
-                "constraint [pk_dbchgloglock] primary key ([id]))"}, DB2Database.class);
-    
-        assertCorrect(new String[]{"create table databasechangeloglock (" +
-                "id integer not null, " +
-                "locked number(1) not null, " +
-                "lockgranted timestamp, " +
-                "lockedby varchar2(255), " +
-                "constraint pk_databasechangeloglock primary key (id))"}, OracleDatabase.class);
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] boolean not null, " +
-                "[lockgranted] datetime null, " +
-                "[lockedby] varchar(255) null, " +
-                "constraint [pk_databasechangeloglock] primary key ([id]))"}, MySQLDatabase.class);
-    
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] boolean not null, " +
-                "[lockgranted] datetime null, " +
-                "[lockedby] varchar(255) null, " +
-                "constraint [pk_databasechangeloglock] primary key ([id]))"}, MariaDBDatabase.class);
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] boolean not null, " +
-                "[lockgranted] datetime, " +
-                "[lockedby] varchar(255), " +
-                "constraint [databasechangeloglock_pkey] primary key ([id]))"}, PostgresDatabase.class);
-
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] boolean not null, " +
-                "[lockgranted] datetime, " +
-                "[lockedby] varchar(255), " +
-                "constraint [pk_dbchgloglock] primary key ([id]))"}, Db2zDatabase.class);
-
-        // all other RDBMS
-        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
-                "[id] int not null, " +
-                "[locked] boolean not null, " +
-                "[lockgranted] datetime, " +
-                "[lockedby] varchar(255), " +
-                "constraint [pk_databasechangeloglock] primary key ([id]))"});
-
     }
+
+    @Test
+    public void generateSQLite() throws Exception {
+
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] boolean not null, " +
+            "[lockgranted] datetime, " +
+            "[lockedby] text, " +
+            "constraint [pk_databasechangeloglock] primary key ([id]))"}, SQLiteDatabase.class);
+    }
+
+
+    @Test
+    public void generateSybase() throws Exception {
+
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] bit not null, " +
+            "[lockgranted] datetime null, " +
+            "[lockexpires] datetime null, " +
+            "[lockedby] varchar(255) null, " +
+            "[lockedbyid] varchar(36) null, " +
+            "constraint [pk_databasechangeloglock] primary key ([id]))"}, SybaseDatabase.class);
+    }
+
+
+    @Test
+    public void generateSybaseASADatabase() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] bit not null, " +
+            "[lockgranted] datetime null, " +
+            "[lockexpires] datetime null, " +
+            "[lockedby] varchar(255) null, " +
+            "[lockedbyid] varchar(36) null, " +
+            "constraint [pk_databasechangeloglock] primary key ([id]))"}, SybaseASADatabase.class);
+    }
+
+
+    @Test
+    public void generateInformixDatabase() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] boolean not null, " +
+            "[lockgranted] datetime, " +
+            "[lockexpires] datetime, " +
+            "[lockedby] varchar(255), " +
+            "[lockedbyid] varchar(36), " +
+            "primary key (id))"}, InformixDatabase.class);
+    }
+
+
+    @Test
+    public void generateMSSQLDatabase() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] [int] not null, " +
+            "[locked] [bit] not null, " +
+            "[lockgranted] [datetime2](3), " +
+            "[lockexpires] [datetime2](3), " +
+            "[lockedby] [nvarchar](255), " +
+            "[lockedbyid] [nvarchar](36), " +
+            "constraint [pk_databasechangeloglock] primary key ([id]))"}, MSSQLDatabase.class);
+    }
+
+
+    @Test
+    public void generateDB2Database() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] integer not null, " +
+            "[locked] smallint not null, " +
+            "[lockgranted] timestamp, " +
+            "[lockexpires] datetime, " +
+            "[lockedby] varchar(255), " +
+            "[lockedbyid] varchar(36), " +
+            "constraint [pk_dbchgloglock] primary key ([id]))"}, DB2Database.class);
+    }
+
+
+    @Test
+    public void generateOracleDatabase() throws Exception {
+        assertCorrect(new String[]{"create table databasechangeloglock (" +
+            "id integer not null, " +
+            "locked number(1) not null, " +
+            "lockgranted timestamp, " +
+            "[lockexpires] timestamp, " +
+            "[lockedby] varchar2(255), " +
+            "[lockedbyid] varchar2(36), " +
+            "constraint pk_databasechangeloglock primary key (id))"}, OracleDatabase.class);
+    }
+
+
+    @Test
+    public void generateMySQLDatabase() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] boolean not null, " +
+            "[lockgranted] datetime null, " +
+            "[lockexpires] datetime null, " +
+            "[lockedby] varchar(255) null, " +
+            "[lockedbyid] varchar(36) null, " +
+            "constraint [pk_databasechangeloglock] primary key ([id]))"}, MySQLDatabase.class);
+    }
+
+
+    @Test
+    public void generateMariaDBDatabase() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] boolean not null, " +
+            "[lockgranted] datetime null, " +
+            "[lockexpires] datetime null, " +
+            "[lockedby] varchar(255) null, " +
+            "[lockedbyid] varchar(36) null, " +
+            "constraint [pk_databasechangeloglock] primary key ([id]))"}, MariaDBDatabase.class);
+    }
+
+
+    @Test
+    public void generatePostgresDatabase() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] boolean not null, " +
+            "[lockgranted] datetime, " +
+            "[lockexpires] timestamp without time zone, " +
+            "[lockedby] varchar(255), " +
+            "[lockedbyid] varchar(36), " +
+            "constraint [databasechangeloglock_pkey] primary key ([id]))"}, PostgresDatabase.class);
+    }
+
+
+    @Test
+    public void generateDb2zDatabase() throws Exception {
+        assertCorrect(new String[]{"create table [databasechangeloglock] (" +
+            "[id] int not null, " +
+            "[locked] boolean not null, " +
+            "[lockgranted] datetime, " +
+            "[lockexpires] timestamp, " +
+            "[lockedby] varchar(255), " +
+            "[lockedbyid] varchar(36), " +
+            "constraint [pk_dbchgloglock] primary key ([id]))"}, Db2zDatabase.class);
+    }
+
+
+    @Test
+    public void generateOtherDatabases() throws Exception {
+        // all other RDBMS
+        testOnAllExcept("create table [databasechangeloglock] (" +
+                "[id] int not null, " +
+                "[locked] boolean not null, " +
+                "[lockgranted] datetime, " +
+                "[lockexpires] datetime, " +
+                "[lockedby] varchar(255), " +
+                "[lockedbyid] varchar(36), " +
+                "constraint [pk_databasechangeloglock] primary key ([id]))",
+            SQLiteDatabase.class,
+            SybaseDatabase.class,
+            SybaseASADatabase.class,
+            InformixDatabase.class,
+            MSSQLDatabase.class,
+            DB2Database.class,
+            OracleDatabase.class,
+            MySQLDatabase.class,
+            MariaDBDatabase.class,
+            PostgresDatabase.class,
+            Db2zDatabase.class);
+    }
+
 }
+
+
+
