d4acab73b367b124e422e118e613f080d9bd8598
Thomas Heyer <thomas.heyer@otto.de>
2020-08-27 11:42:40 +0200

    reactivate logging of the SQL-Error in case of an erroneous change set

--- liquibase-core/src/main/java/liquibase/changelog/ChangeSet.java
+++ liquibase-core/src/main/java/liquibase/changelog/ChangeSet.java
@@ -664,6 +664,8 @@ public class ChangeSet implements Conditional, ChangeLogChild {
                 log.fine("Failure Stacktrace", e);
                 execType = ExecType.FAILED;
             } else {
+                // just log the message, dont log the stacktrace by appending exception. Its logged anyway to stdout
+                log.severe(LogType.LOG, "Change Set " + toString(false) + " failed.  Error: " + e.getMessage());
                 if (e instanceof MigrationFailedException) {
                     throw ((MigrationFailedException) e);
                 } else {
