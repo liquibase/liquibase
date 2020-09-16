c61d6729e82d9b9b66c8d69e98fbca0ec0e48dbd
Wesley Willard <wwillard@datical.com>
2020-09-16 14:11:35 -0500

    Remove duplicate code DAT-5468

--- liquibase-core/src/main/java/liquibase/hub/core/OnlineHubService.java
+++ liquibase-core/src/main/java/liquibase/hub/core/OnlineHubService.java
@@ -84,8 +84,7 @@ public class OnlineHubService implements HubService {
                     this.available = false;
                 }
             }
-            HubConfiguration hubConfiguration = LiquibaseConfiguration.getInstance().getConfiguration(HubConfiguration.class);
-            String apiKey = StringUtil.trimToNull(hubConfiguration.getLiquibaseHubApiKey());
+            String apiKey = getApiKey();
             if (!this.available && apiKey != null) {
               String message = "Hub communication failure: " + hubServiceFactory.getOfflineReason() + ".\n" +
                       "The data for your operations will not be recorded in your Liquibase Hub project";
