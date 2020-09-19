6d1c1800c54356384f990060c15fb514895399ca
mattbertolini <mattbertolini@users.noreply.github.com>
2020-09-19 13:17:46 -0400

    Add some unit tests for the DatabaseFactory openConnection method.
    This is not an exhaustive list of tests but it's better than nothing and a decent start. As the class is refactored tests can be improved.
    
    (cherry picked from commit 226acd2e61fb7e08321b9e6292062fac8ac4ac3b)

--- liquibase-core/src/test/java/liquibase/database/DatabaseFactoryTest.java
+++ liquibase-core/src/test/java/liquibase/database/DatabaseFactoryTest.java
@@ -1,13 +1,110 @@
 package liquibase.database;
 
+import liquibase.exception.DatabaseException;
+import liquibase.resource.ResourceAccessor;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Properties;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class DatabaseFactoryTest {
-    
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    private ResourceAccessor resourceAccessor;
+    private DatabaseFactory databaseFactory;
+
+    @Before
+    public void setUp() {
+        resourceAccessor = mock(ResourceAccessor.class);
+        databaseFactory = DatabaseFactory.getInstance();
+    }
+
+    @After
+    public void tearDown() {
+        temporaryFolder.delete();
+    }
+
     @Test
     public void getInstance() {
         assertNotNull(DatabaseFactory.getInstance());
+    }
+
+    // openConnection() method tests
+    @Test
+    public void openConnectionReturnsOfflineConnectionWhenUrlPrefixMatches() throws Exception {
+        String username = "sa";
+        DatabaseConnection dbConnection = databaseFactory.openConnection("offline:h2?param1=value1&aparam2=value2", username, "", null, resourceAccessor);
+        assertThat(dbConnection, notNullValue());
+        assertThat(dbConnection, instanceOf(OfflineConnection.class));
+        assertThat(dbConnection.getConnectionUserName(), equalTo(username));
+    }
+
+    @Test
+    public void openConnectionLoadsDriverPropertiesFromGivenFile() throws Exception {
+        File propsFile = temporaryFolder.newFile("db-factory-test-connection-props.properties");
+        Properties expectedProps = new Properties();
+        expectedProps.setProperty("param1", "value1");
+        expectedProps.setProperty("param2", "value2");
+        try (FileWriter writer = new FileWriter(propsFile)) {
+            expectedProps.store(writer, "connection properties");
+        }
+        String propsFilePath = propsFile.getAbsolutePath();
+
+        DatabaseConnection dbConnection = databaseFactory.openConnection("jdbc:h2:mem:DatabaseFactoryTest", "sa", "", null, null, propsFilePath, null, resourceAccessor);
+        assertThat(dbConnection, notNullValue());
+        // TODO: Figure out how to assert the properties are loaded
+    }
+
+    @Test
+    public void openConnectionCreatesCustomPropertyProviderClassWhenGiven() throws Exception {
+        DatabaseConnection databaseConnection = databaseFactory.openConnection("jdbc:h2:mem:DatabaseFactoryTest", "sa", "", "liquibase.database.DatabaseFactoryTest$CustomProperties", resourceAccessor);
+        assertThat(databaseConnection, notNullValue());
+    }
+
+    @Test
+    public void openConnectionThrowsRuntimeExceptionWhenDriverPropertiesFileNotFound() throws Exception {
+        expectedException.expect(instanceOf(DatabaseException.class));
+        expectedException.expectCause(instanceOf(RuntimeException.class));
+        expectedException.expectMessage(containsString("Can't open JDBC Driver specific properties from the file"));
+
+        databaseFactory.openConnection("jdbc:h2:mem:DatabaseFactoryTest", "sa", "", null, null, "unknown file", null, resourceAccessor);
+    }
+
+    // Misc
+    // TODO: Delete this test
+    @Test
+    public void name() throws Exception {
+        DatabaseConnection databaseConnection = databaseFactory.openConnection("jdbc:h2:mem:DatabaseFactoryTest", "sa", "", null, resourceAccessor);
+        assertNotNull(databaseConnection);
+    }
+
+    /**
+     * Simple type to test the propertyProviderClass
+     */
+    @SuppressWarnings({"unused", "RedundantSuppression"})
+    private static class CustomProperties extends Properties {
+        // Default constructor needed for reflective construction
+        public CustomProperties() {
+            super();
+        }
     }
 }
