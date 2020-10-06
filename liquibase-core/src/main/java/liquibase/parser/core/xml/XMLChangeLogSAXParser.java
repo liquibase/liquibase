331b5859cdb0e0fc5a05c935f1ad2e822c74503a
Nathan Voxland <nathan@voxland.net>
2020-10-06 10:47:17 -0500

    Fixed schema version number for 3.10

--- liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXParser.java
+++ liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXParser.java
@@ -22,7 +22,7 @@ import java.io.InputStream;
 
 public class XMLChangeLogSAXParser extends AbstractChangeLogParser {
     
-    public static final String LIQUIBASE_SCHEMA_VERSION = "3.9";
+    public static final String LIQUIBASE_SCHEMA_VERSION = "3.10";
     private static final boolean PREFER_INTERNAL_XSD = Boolean.getBoolean("liquibase.prefer.internal.xsd");
     private static final String XSD_FILE = "dbchangelog-" + LIQUIBASE_SCHEMA_VERSION + ".xsd";
     private SAXParserFactory saxParserFactory;
