13f2b0e506d16a9f7a4c6b1af55360d7265e93cc
Yuri Orlov <yorlov@stiltsoft.com>
2020-10-23 23:32:13 +0300

    Remove unnecessary 'null' checks

--- liquibase-core/src/main/java/liquibase/change/core/LoadDataChange.java
+++ liquibase-core/src/main/java/liquibase/change/core/LoadDataChange.java
@@ -222,7 +222,7 @@ public class LoadDataChange extends AbstractChange implements ChangeWithColumns<
     }
 
     public void setSeparator(String separator) {
-        if ((separator != null) && "\\t".equals(separator)) {
+        if ("\\t".equals(separator)) {
             separator = "\t";
         }
         this.separator = separator;
