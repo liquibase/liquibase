89cc09d8510111308ffc0cbf1a4288a90687754f
Nathan Voxland <nathan@voxland.net>
2020-12-17 16:08:30 -0600

    MariaDB :: MySQL-specific grants messages displayed when a MariaDB user does not have access to objects
    DAT-6225

--- liquibase-core/src/main/java/liquibase/snapshot/jvm/ViewSnapshotGenerator.java
+++ liquibase-core/src/main/java/liquibase/snapshot/jvm/ViewSnapshotGenerator.java
@@ -5,6 +5,7 @@ import liquibase.Scope;
 import liquibase.database.AbstractJdbcDatabase;
 import liquibase.database.Database;
 import liquibase.database.core.InformixDatabase;
+import liquibase.database.core.MariaDBDatabase;
 import liquibase.database.core.MySQLDatabase;
 import liquibase.database.core.OracleDatabase;
 import liquibase.exception.DatabaseException;
@@ -103,12 +104,21 @@ public class ViewSnapshotGenerator extends JdbcSnapshotGenerator {
                     definition = StringUtil.trimToNull(definition);
                     if (definition == null) {
                         definition = "[CANNOT READ VIEW DEFINITION]";
-                        if (database instanceof MySQLDatabase) {
-                            String warningMessage =
+                        String warningMessage = null;
+                        if (database instanceof MariaDBDatabase) {
+                            warningMessage =
+                                    "\nThe current MariaDB user does not have permissions to access view definitions needed for this Liquibase command.\n" +
+                                            "Please search the changelog for '[CANNOT READ VIEW DEFINITION]' to locate inaccessible objects. " +
+                                            "Learn more about altering permissions with suggested MariaDB GRANTs at https://docs.liquibase.com/workflows/liquibase-pro/mariadbgrants.html\n";
+
+                        } else if (database instanceof MySQLDatabase) {
+                            warningMessage =
                                     "\nThe current MySQL user does not have permissions to access view definitions needed for this Liquibase command.\n" +
                                     "Please search the changelog for '[CANNOT READ VIEW DEFINITION]' to locate inaccessible objects. This is\n" +
                                     "potentially due to a known MySQL bug https://bugs.mysql.com/bug.php?id=22763. Learn more about altering\n" +
                                     "permissions with suggested MySQL GRANTs at https://docs.liquibase.com/workflows/liquibase-pro/mysqlgrants.html\n";
+                        }
+                        if (warningMessage != null) {
                             Scope.getCurrentScope().getUI().sendMessage("WARNING: " + warningMessage);
                             Scope.getCurrentScope().getLog(getClass()).warning(warningMessage);
                         }
