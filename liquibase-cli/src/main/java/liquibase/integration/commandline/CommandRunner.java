bc1b099a8771d9aa4b8bb1e4f9eb4e1d42e71130
Nathan Voxland <nathan@voxland.net>
2021-04-20 15:38:32 -0500

    New CLI work - Fixed --version
    LB-1250

--- liquibase-cli/src/main/java/liquibase/integration/commandline/CommandRunner.java
+++ liquibase-cli/src/main/java/liquibase/integration/commandline/CommandRunner.java
@@ -1,31 +1,20 @@
 package liquibase.integration.commandline;
 
-import liquibase.Scope;
+import liquibase.command.CommandResults;
 import liquibase.command.CommandScope;
-import liquibase.configuration.ConfigurationDefinition;
-import liquibase.configuration.LiquibaseConfiguration;
-import liquibase.configuration.core.DefaultsFileValueProvider;
-import liquibase.exception.CommandExecutionException;
-import liquibase.exception.UnexpectedLiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
-import liquibase.resource.CompositeResourceAccessor;
-import liquibase.resource.FileSystemResourceAccessor;
 import liquibase.util.StringUtil;
-import liquibase.util.SystemUtil;
 import picocli.CommandLine;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.Callable;
 
-class CommandRunner implements Runnable {
+class CommandRunner implements Callable<CommandResults> {
 
     private CommandLine.Model.CommandSpec spec;
 
     @Override
-    public void run() {
+    public CommandResults call() throws Exception {
         List<String> command = new ArrayList<>();
         command.add(spec.commandLine().getCommandName());
 
@@ -36,89 +25,24 @@ class CommandRunner implements Runnable {
         }
         final CommandLine rootCommand = parentCommand;
 
-        String classpath = getBootstrapSetting(parentCommand, CommandLineConfiguration.CLASSPATH);
-        if (StringUtil.trimToNull(classpath) == null) {
-            classpath = ".";
+
+        String[] commandName = new String[command.size()];
+        for (int i = 0; i < command.size(); i++) {
+            commandName[i] = StringUtil.toCamelCase(command.get(i));
         }
-        final String bootstrapClasspath = classpath;
+        final CommandScope commandScope = new CommandScope(commandName);
 
-        final CompositeResourceAccessor createResourceAccessor = createResourceAccessor(bootstrapClasspath);
-
-        try {
-            Scope.child(Scope.Attr.resourceAccessor, createResourceAccessor, () -> {
-                String defaultsFile = getBootstrapSetting(rootCommand, CommandLineConfiguration.DEFAULTS_FILE);
-
-                if (defaultsFile != null) {
-                    Scope.getCurrentScope().getLog(getClass()).fine("Using defaults file " + defaultsFile);
-                    Scope.getCurrentScope().getSingleton(LiquibaseConfiguration.class).registerProvider(new DefaultsFileValueProvider(defaultsFile));
-                }
-
-                Map<String, Object> finalScopeValues = new HashMap<>();
-                //check if classpath changed from defaultsFile
-                String newClasspath = CommandLineConfiguration.CLASSPATH.getCurrentValue();
-                if (newClasspath != null && !newClasspath.equals(bootstrapClasspath)) {
-                    finalScopeValues.put(Scope.Attr.resourceAccessor.name(), createResourceAccessor(newClasspath));
-                }
-
-                Scope.child(finalScopeValues, () -> {
-                    try {
-                        String[] commandName = new String[command.size()];
-                        for (int i=0; i<command.size(); i++) {
-                            commandName[i] = StringUtil.toCamelCase(command.get(i));
-                        }
-                        final CommandScope commandScope = new CommandScope(commandName);
-
-                        for (CommandLine.Model.OptionSpec option : spec.commandLine().getParseResult().matchedOptions()) {
-                            commandScope.addArgumentValue(toCommandArgumentDefinition(option), option.getValue());
-                        }
-
-                        for (CommandLine.Model.OptionSpec option : rootCommand.getParseResult().matchedOptions()) {
-                            commandScope.addArgumentValue(toCommandArgumentDefinition(option), option.getValue());
-                        }
-
-                        commandScope.execute();
-                    } catch (CommandExecutionException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            });
-        } catch (Exception e) {
-            throw new UnexpectedLiquibaseException(e);
+        for (CommandLine.Model.OptionSpec option : spec.commandLine().getParseResult().matchedOptions()) {
+            commandScope.addArgumentValue(toCommandArgumentDefinition(option), option.getValue());
         }
 
-
-//        parseResult.matchedOptions()
-    }
-
-    private CompositeResourceAccessor createResourceAccessor(String classpath) {
-        List<File> classpathFiles = splitClasspath(classpath);
-        return new CompositeResourceAccessor(new FileSystemResourceAccessor(classpathFiles.toArray(new File[0])), new ClassLoaderResourceAccessor());
-    }
-
-    private List<File> splitClasspath(String bootstrapClasspath) {
-        List<File> classpathFiles = new ArrayList<>();
-        if (StringUtil.trimToNull(bootstrapClasspath) != null) {
-            String[] splitClasspath;
-            if (SystemUtil.isWindows()) {
-                splitClasspath = bootstrapClasspath.split(";");
-            } else {
-                splitClasspath = bootstrapClasspath.split(":");
-            }
-            for (String entry : splitClasspath) {
-                classpathFiles.add(new File(".", entry));
-            }
-        }
-        return classpathFiles;
-    }
-
-    private <T> T getBootstrapSetting(CommandLine rootCommand, ConfigurationDefinition<T> config) {
-        final CommandLine.Model.OptionSpec matchedOption = rootCommand.getParseResult().matchedOption(StringUtil.toKabobCase(config.getKey().replace(".", "-")));
-        if (matchedOption != null) {
-            return matchedOption.getValue();
+        for (CommandLine.Model.OptionSpec option : rootCommand.getParseResult().matchedOptions()) {
+            commandScope.addArgumentValue(toCommandArgumentDefinition(option), option.getValue());
         }
 
-        return config.getCurrentValue();
+        return commandScope.execute();
     }
+
 
     private String toCommandArgumentDefinition(CommandLine.Model.OptionSpec option) {
         if (option.names().length > 1) {
--- liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
+++ liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
@@ -6,10 +6,18 @@ import liquibase.command.CommandDefinition;
 import liquibase.command.CommandFactory;
 import liquibase.configuration.ConfigurationDefinition;
 import liquibase.configuration.LiquibaseConfiguration;
+import liquibase.configuration.core.DefaultsFileValueProvider;
+import liquibase.license.LicenseServiceFactory;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.CompositeResourceAccessor;
+import liquibase.resource.FileSystemResourceAccessor;
+import liquibase.util.LiquibaseUtil;
 import liquibase.util.StringUtil;
+import liquibase.util.SystemUtil;
 import picocli.CommandLine;
 
-import java.util.SortedSet;
+import java.io.File;
+import java.util.*;
 
 
 public class LiquibaseCommandLine {
@@ -18,28 +26,115 @@ public class LiquibaseCommandLine {
 
     public static void main(String[] args) {
         final LiquibaseCommandLine cli = new LiquibaseCommandLine();
-        cli.execute(args);
+
+        try {
+            cli.execute(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public LiquibaseCommandLine() {
+        this.commandLine = buildPicoCommandLine();
+    }
 
+    private CommandLine buildPicoCommandLine() {
         final CommandLine.Model.CommandSpec rootCommandSpec = CommandLine.Model.CommandSpec.create();
         rootCommandSpec.name("liquibase");
         configureHelp(rootCommandSpec);
 
-        commandLine = new CommandLine(rootCommandSpec);
+        CommandLine commandLine = new CommandLine(rootCommandSpec);
 
-        addGlobalArguments();
+        addGlobalArguments(commandLine);
 
         for (CommandDefinition commandDefinition : getCommands()) {
-            addSubcommand(commandDefinition);
+            addSubcommand(commandDefinition, commandLine);
         }
+
+        return commandLine;
     }
 
-    public void execute(String[] args) {
-        commandLine.execute(args);
+    public void execute(String[] args) throws Exception {
+        final CommandLine.ParseResult parseResult = commandLine.parseArgs(args);
+
+        String classpath = getBootstrapSetting(parseResult, CommandLineConfiguration.CLASSPATH);
+        if (StringUtil.trimToNull(classpath) == null) {
+            classpath = ".";
+        }
+        final String bootstrapClasspath = classpath;
+
+        final CompositeResourceAccessor createResourceAccessor = createResourceAccessor(bootstrapClasspath);
+
+        Scope.child(Scope.Attr.resourceAccessor, createResourceAccessor, () -> {
+            String defaultsFile = getBootstrapSetting(parseResult, CommandLineConfiguration.DEFAULTS_FILE);
+
+            if (defaultsFile != null) {
+                Scope.getCurrentScope().getLog(getClass()).fine("Using defaults file " + defaultsFile);
+                Scope.getCurrentScope().getSingleton(LiquibaseConfiguration.class).registerProvider(new DefaultsFileValueProvider(defaultsFile));
+            }
+
+            Map<String, Object> finalScopeValues = new HashMap<>();
+            //check if classpath changed from defaultsFile
+            String newClasspath = CommandLineConfiguration.CLASSPATH.getCurrentValue();
+            if (newClasspath != null && !newClasspath.equals(bootstrapClasspath)) {
+                finalScopeValues.put(Scope.Attr.resourceAccessor.name(), createResourceAccessor(newClasspath));
+            }
+
+            Scope.child(finalScopeValues, () -> {
+                getRootCommand().getCommandSpec().version(
+                        CommandLineUtils.getBanner(),
+                        String.format("Running Java under %s (Version %s)",
+                                System.getProperties().getProperty("java.home"),
+                                System.getProperty("java.version")
+                        ),
+                        "",
+                        "Liquibase Version: " + LiquibaseUtil.getBuildVersion(),
+                        Scope.getCurrentScope().getSingleton(LicenseServiceFactory.class).getLicenseService().getLicenseInfo()
+                );
+
+                commandLine.execute(args);
+            });
+        });
     }
 
+    private CommandLine getRootCommand() {
+        CommandLine commandLine = this.commandLine;
+        while (commandLine.getParent() != null) {
+            commandLine = commandLine.getParent();
+        }
+        return commandLine;
+    }
+
+    private <T> T getBootstrapSetting(CommandLine.ParseResult parseResult, ConfigurationDefinition<T> config) {
+        final CommandLine.Model.OptionSpec matchedOption = parseResult.matchedOption(StringUtil.toKabobCase(config.getKey().replace(".", "-")));
+        if (matchedOption != null) {
+            return matchedOption.getValue();
+        }
+
+        return config.getCurrentValue();
+    }
+
+
+    private CompositeResourceAccessor createResourceAccessor(String classpath) {
+        List<File> classpathFiles = splitClasspath(classpath);
+        return new CompositeResourceAccessor(new FileSystemResourceAccessor(classpathFiles.toArray(new File[0])), new ClassLoaderResourceAccessor());
+    }
+
+    private List<File> splitClasspath(String bootstrapClasspath) {
+        List<File> classpathFiles = new ArrayList<>();
+        if (StringUtil.trimToNull(bootstrapClasspath) != null) {
+            String[] splitClasspath;
+            if (SystemUtil.isWindows()) {
+                splitClasspath = bootstrapClasspath.split(";");
+            } else {
+                splitClasspath = bootstrapClasspath.split(":");
+            }
+            for (String entry : splitClasspath) {
+                classpathFiles.add(new File(".", entry));
+            }
+        }
+        return classpathFiles;
+    }
 
 //
 //        Map<String, String> passedArgs = new HashMap<>();
@@ -78,7 +173,7 @@ public class LiquibaseCommandLine {
 //        }
 
 
-    private void addSubcommand(CommandDefinition commandDefinition) {
+    private void addSubcommand(CommandDefinition commandDefinition, CommandLine commandLine) {
         final CommandRunner commandRunner = new CommandRunner();
         final CommandLine.Model.CommandSpec subCommandSpec = CommandLine.Model.CommandSpec.wrapWithoutInspection(commandRunner);
         commandRunner.setSpec(subCommandSpec);
@@ -109,7 +204,7 @@ public class LiquibaseCommandLine {
         return commandFactory.getCommands();
     }
 
-    private void addGlobalArguments() {
+    private void addGlobalArguments(CommandLine commandLine) {
         final CommandLine.Model.CommandSpec rootCommandSpec = commandLine.getCommandSpec();
 
         final SortedSet<ConfigurationDefinition<?>> globalConfigurations = Scope.getCurrentScope().getSingleton(LiquibaseConfiguration.class).getRegisteredDefinitions();
