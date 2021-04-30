ebe3f92f7b3d102f3b2f8f70f848788cbb7b6395
Wesley Willard <wwillard@datical.com>
2021-04-30 12:38:19 -0500

    Use internal commands for Maven History and SyncHub
    DAT-6573

--- liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseHistoryMojo.java
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseHistoryMojo.java
@@ -14,7 +14,7 @@ public class LiquibaseHistoryMojo extends AbstractLiquibaseMojo {
 
     @Override
     protected void performLiquibaseTask(Liquibase liquibase) throws LiquibaseException {
-      CommandScope historyCommand = new CommandScope("history");
+      CommandScope historyCommand = new CommandScope(InternalHistoryCommandStep.COMMAND_NAME);
 
       historyCommand.addArgumentValue(InternalHistoryCommandStep.DATABASE_ARG, getLiquibase().getDatabase());
 
