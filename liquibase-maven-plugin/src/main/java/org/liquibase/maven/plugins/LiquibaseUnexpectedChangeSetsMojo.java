8bdf3ad0800468b23ee030122bfdbffcb624426f
Surya Aki <saki@datical.com>
2021-06-09 15:14:47 -0500

    Add mvn command support unexpectedChangesets LB-766
    LB-766

---
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseUnexpectedChangeSetsMojo.java
@@ -0,0 +1,18 @@
+package org.liquibase.maven.plugins;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.exception.LiquibaseException;
+
+/**
+ * produces a list of changesets that were run in the database but do not exist in the current changelog.
+ */
+public class LiquibaseUnexpectedChangeSetsMojo extends AbstractLiquibaseChangeLogMojo {
+
+    @Override
+    protected void performLiquibaseTask(Liquibase liquibase) throws LiquibaseException {
+        super.performLiquibaseTask(liquibase);
+        liquibase.listUnexpectedChangeSets(new Contexts(contexts), new LabelExpression(labels));
+    }
+}
