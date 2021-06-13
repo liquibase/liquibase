bd01bb9f2521967209425c2c7858696b4c6d1cff
Christian Grubert <ch.grubert@gmail.com>
2021-06-13 11:48:15 +0200

    removed unused test code from InsertExecutablePreparedStatementTest

--- liquibase-core/src/test/java/liquibase/statement/InsertExecutablePreparedStatementTest.java
+++ liquibase-core/src/test/java/liquibase/statement/InsertExecutablePreparedStatementTest.java
@@ -1,5 +1,7 @@
 package liquibase.statement;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import liquibase.change.ColumnConfig;
 import liquibase.changelog.ChangeSet;
 import liquibase.database.Database;
@@ -7,17 +9,13 @@ import liquibase.database.PreparedStatementFactory;
 import liquibase.database.core.MSSQLDatabase;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ResourceAccessor;
-import liquibase.structure.core.Column;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.*;
 
-import java.sql.PreparedStatement;
-import java.util.ArrayList;
-
 import static java.util.Arrays.asList;
-import static org.mockito.MockitoAnnotations.initMocks;
 import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class InsertExecutablePreparedStatementTest {
     @Mock
@@ -34,7 +32,7 @@ public class InsertExecutablePreparedStatementTest {
 
     private Database database;
 
-    private PreparedStatementFactory preparedStatementFactory;	
+    private PreparedStatementFactory preparedStatementFactory;
 
     @Before
     public void setUp() throws Exception {
@@ -43,7 +41,7 @@ public class InsertExecutablePreparedStatementTest {
         database = new MSSQLDatabase();
         preparedStatementFactory = new PreparedStatementFactory(connection);
     }
-	
+
 	@Test
 	public void testExecuteWithClobAndComputedValue() throws Exception {
         // given
@@ -71,75 +69,4 @@ public class InsertExecutablePreparedStatementTest {
 		verify(ps, never()).setNull(eq(2), anyInt());
 	}
 
-//    @Test
-//    public void testExecuteWithParamPlaceholders() throws Exception {
-//        // given
-//        UpdateExecutablePreparedStatement statement = new UpdateExecutablePreparedStatement(
-//                        database,
-//                        null,
-//                        null,
-//                        "DATABASECHANGELOG",
-//                        new ArrayList<ColumnConfig>(asList(
-//                                new ColumnConfig()
-//                                        .setName("MD5SUM")
-//                                        .setValue("7:e27bf9c0c2313160ef960a15d44ced47"))),
-//                        changeSet,
-//                        resourceAccessor)
-//                .setWhereClause(
-//                        database.escapeObjectName("ID", Column.class) + " = ? " +
-//                        "AND " + database.escapeObjectName("AUTHOR", Column.class) + " = ? " +
-//                        "AND " + database.escapeObjectName("FILENAME", Column.class) + " = ?")
-//                .addWhereParameters(
-//                        "SYPA: AUTO_START tüüp INT -> TEXT, vaartus 0 00 17 * * ?",
-//                        "martin",
-//                        "db/changelog.xml");
-//
-//        // when
-//        statement.execute(preparedStatementFactory);
-//
-//        // then
-//        verify(connection).prepareStatement(
-//            "UPDATE DATABASECHANGELOG " +
-//                "SET MD5SUM = ? " +
-//                "WHERE ID = N'SYPA: AUTO_START tüüp INT -> TEXT, vaartus 0 00 17 * * ?' " +
-//                "AND AUTHOR = 'martin' " +
-//                "AND FILENAME = 'db/changelog.xml'");
-//        verify(ps).setString(1, "7:e27bf9c0c2313160ef960a15d44ced47");
-//    }
-
-//    @Test
-//    public void testExecuteWithNameValuePlaceholderPairs() throws Exception {
-//        // given
-//        UpdateExecutablePreparedStatement statement = new UpdateExecutablePreparedStatement(
-//                        database,
-//                        null,
-//                        null,
-//                        "DATABASECHANGELOG",
-//                        new ArrayList<ColumnConfig>(asList(
-//                                new ColumnConfig()
-//                                        .setName("MD5SUM")
-//                                        .setValue("7:e27bf9c0c2313160ef960a15d44ced47"))),
-//                        changeSet,
-//                        resourceAccessor)
-//                .setWhereClause(":name = :value AND :name = :value AND :name = :value")
-//                .addWhereColumnName("ID")
-//                .addWhereColumnName("AUTHOR")
-//                .addWhereColumnName("FILENAME")
-//                .addWhereParameters(
-//                        "SYPA: AUTO_START tüüp INT -> TEXT, vaartus 0 00 17 * * ?",
-//                        "martin",
-//                        "db/changelog.xml");
-//
-//        // when
-//        statement.execute(preparedStatementFactory);
-//
-//        // then
-//        verify(connection).prepareStatement(
-//            "UPDATE DATABASECHANGELOG " +
-//                "SET MD5SUM = ? " +
-//                "WHERE ID = N'SYPA: AUTO_START tüüp INT -> TEXT, vaartus 0 00 17 * * ?' " +
-//                "AND AUTHOR = 'martin' " +
-//                "AND FILENAME = 'db/changelog.xml'");
-//        verify(ps).setString(1, "7:e27bf9c0c2313160ef960a15d44ced47");
-//    }
 }
