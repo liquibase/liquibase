1d4db5a237cc2c4828889a0bd165ae5e09106bc2
Steven Massaro <steven.massaro.web@gmail.com>
2021-08-30 13:02:12 -0500

    display output from customized rules in the checks show output

--- liquibase-core/src/main/java/liquibase/util/TableOutput.java
+++ liquibase-core/src/main/java/liquibase/util/TableOutput.java
@@ -9,6 +9,19 @@ import java.io.Writer;
 import java.util.*;
 
 public class TableOutput {
+    /**
+     *
+     * This method outputs the input data in a tabular format with wrapping of lines
+     *
+     * @param table                    2-dimensional array of data
+     * @param maxWidths                Maximum widths of each column to control wrapping
+     * @param leftJustifiedRows        If true then add "-" to format string
+     * @param writer                   Writer to use for output
+     *
+     */
+    public static void formatOutput(List<List<String>> table, int[] maxWidths, boolean leftJustifiedRows, Writer writer) throws LiquibaseException {
+        formatOutput(table.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new), maxWidths, leftJustifiedRows, writer);
+    }
 
     /**
      *
