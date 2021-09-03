1b0f077f4e0f68b71d6dd5d2e383e04c9fd6fdaf
Nicolas Humblot <contact@nicolashumblot.fr>
2021-09-03 14:49:43 +0200

    #1054 Add test for varchar default value

--- liquibase-core/src/test/groovy/liquibase/change/core/AddNotNullConstraintChangeTest.groovy
+++ liquibase-core/src/test/groovy/liquibase/change/core/AddNotNullConstraintChangeTest.groovy
@@ -1,6 +1,9 @@
 package liquibase.change.core
 
 import liquibase.change.StandardChangeTest
+import liquibase.database.core.MySQLDatabase
+import liquibase.statement.core.SetNullableStatement
+import liquibase.statement.core.UpdateStatement
 
 public class AddNotNullConstraintChangeTest extends StandardChangeTest {
 
@@ -28,4 +31,30 @@ public class AddNotNullConstraintChangeTest extends StandardChangeTest {
         reverses[0].getTableName() == "TABLE_NAME"
         reverses[0].getColumnName() == "COL_HERE"
     }
+
+    def should_generateStatements_add_update_statement_before_not_null_constraint() {
+        given:
+        def change = new AddNotNullConstraintChange()
+        change.setTableName("table_name")
+        change.setColumnName("column_name")
+        change.setColumnDataType("varchar(20)")
+        change.setDefaultNullValue("Hello World!")
+
+        def database = new MySQLDatabase()
+
+        when:
+        def output = change.generateStatements(database)
+
+        then:
+        output.length == 2
+        output[0] instanceof UpdateStatement
+        def update = (UpdateStatement) output[0]
+        update.getTableName() == "table_name"
+        update.getNewColumnValues().size() == 1
+        update.getNewColumnValues().get("column_name") == "Hello World!"
+        update.getWhereClause() == "column_name IS NULL"
+
+        output[1] instanceof SetNullableStatement
+    }
+
 }
