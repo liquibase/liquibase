949cbbe9524cb1dc3636419bcf9fba01de74f2fa
Nathan Voxland <nathan@voxland.net>
2021-10-06 11:59:54 -0500

    Set up unit tests

---
+++ src/test/groovy/liquibase/ext/bigquery/database/BigqueryDatabaseTest.groovy
@@ -0,0 +1,14 @@
+package liquibase.ext.bigquery.database
+
+import spock.lang.Specification
+
+class BigqueryDatabaseTest extends Specification {
+
+    def checkSetup() {
+        when:
+        def db = new BigqueryDatabase()
+
+        then:
+        db.getShortName() == "bigquery"
+    }
+}
