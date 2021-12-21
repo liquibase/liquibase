83535ee57b671773d485ea50ffe3dba1b19ac7dd
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2021-12-21 16:17:13 -0600

    Recognize jdbc:edb urls as a distinct EnterpriseDB dialect (#2244)
    * added new EnterpriseDB dialect support
    * Removed old/unused Postgresql DbTypes enum
    * Updated code to use standard "instanceof EnterpriseDBDatabase" style vs. getDbType() logic
    
    Co-authored-by: Nathan Voxland <nathan@voxland.net>

---
+++ liquibase-core/src/main/java/liquibase/database/core/EnterpriseDBDatabase.java
@@ -0,0 +1,39 @@
+package liquibase.database.core;
+
+import liquibase.database.DatabaseConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.statement.SqlStatement;
+import liquibase.statement.core.RawSqlStatement;
+
+public class EnterpriseDBDatabase extends PostgresDatabase {
+
+    @Override
+    public boolean isCorrectDatabaseImplementation(DatabaseConnection conn) throws DatabaseException {
+        final String url = conn.getURL();
+        return url.contains("edb") || (url.contains("postgres") && url.contains(":5444"));
+    }
+
+    @Override
+    public String getShortName() {
+        return "edb";
+    }
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_DATABASE + 5;
+    }
+
+
+    @Override
+    public String getDefaultDriver(String url) {
+        if (url.startsWith("jdbc:edb:")) {
+            return "com.edb.Driver";
+        }
+        return null;
+    }
+
+    @Override
+    public Integer getDefaultPort() {
+        return 5444;
+    }
+}
