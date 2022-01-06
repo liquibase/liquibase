ad37648f9f9abce0c93a4b907d99e6dace21706a
Steven Massaro <steven.massaro.web@gmail.com>
2022-01-06 08:46:05 -0600

    update HubService to allow POSTing operation for an organization (DAT-8776) (#2336)

--- liquibase-core/src/main/java/liquibase/hub/HubServiceFactory.java
+++ liquibase-core/src/main/java/liquibase/hub/HubServiceFactory.java
@@ -148,6 +148,11 @@ public class HubServiceFactory extends AbstractPluginFactory<HubService> {
         }
 
         @Override
+        public Operation createOperationInOrganization(String operationType, String operationCommand, UUID organizationId) throws LiquibaseHubException {
+            return null;
+        }
+
+        @Override
         public OperationEvent sendOperationEvent(Operation operation, OperationEvent operationEvent) throws LiquibaseException {
             return null;
         }
