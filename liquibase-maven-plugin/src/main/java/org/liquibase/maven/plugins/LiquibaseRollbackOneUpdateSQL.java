99cd4483596db263ce877f7d4981aa43484b754e
Steven Massaro <steven.massaro.web@gmail.com>
2022-01-11 07:57:37 -0600

    change phrasing of rollbackOneUpdateSQL error message

--- liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseRollbackOneUpdateSQL.java
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseRollbackOneUpdateSQL.java
@@ -67,8 +67,7 @@ public class LiquibaseRollbackOneUpdateSQL extends AbstractLiquibaseChangeLogMoj
         boolean hasProLicense = MavenUtils.checkProLicense(liquibaseProLicenseKey, commandName, getLog());
         if (!hasProLicense) {
             throw new LiquibaseException(
-                    "The command 'rollbackOneUpdateSQL' requires a Liquibase Pro License, available at http://www.liquibase.org/download or sales@liquibase.com." +
-                            "Add liquibase.pro.licenseKey as a Maven property or add liquibase.pro.licenseKey=<yourKey> into your defaults file.");
+                    "The 'rollbackOneUpdateSQL' command requires a valid Liquibase Pro license key. Get a free Pro license key from https://liquibase.com/protrial");
         }
         Database database = liquibase.getDatabase();
         CommandScope liquibaseCommand = new CommandScope("internalRollbackOneUpdateSQL");
