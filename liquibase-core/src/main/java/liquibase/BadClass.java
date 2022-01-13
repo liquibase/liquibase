a1783b7285cdf9a057c2a75b8d9080ded70c1f8d
Nathan Voxland <nathan@voxland.net>
2022-01-13 01:09:14 -0600

    Testing bad code

---
+++ liquibase-core/src/main/java/liquibase/BadClass.java
@@ -0,0 +1,12 @@
+package liquibase;
+
+public class BadClass {
+    public static void main(String[] args) {
+        //TODO: Remove this testing thing
+        Integer test = null;
+        System.out.println(test.toString());
+
+        Integer other = null;
+        System.out.println(other.toString());
+    }
+}
