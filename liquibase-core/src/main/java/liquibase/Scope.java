3579f56a7962306bae493f37af513e1271e777f7
protocol6 <hello@protocol6.com>
2022-02-09 13:56:57 -0500

    ap-liquibase fork test2

--- liquibase-core/src/main/java/liquibase/Scope.java
+++ liquibase-core/src/main/java/liquibase/Scope.java
@@ -60,6 +60,7 @@ public class Scope {
     }
 
     private static ScopeManager scopeManager;
+    private String andreFork2;
 
     private Scope parent;
     private SmartMap values = new SmartMap();
