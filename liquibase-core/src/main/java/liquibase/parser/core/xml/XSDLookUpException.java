3e9dbdd4922f4ae270544bd5d7538f9aafbd6823
Kavya Shastri <kavya.shastri@sap.com>
2022-02-18 17:48:14 +0530

    [XSDLookUp] Updated entity resolver to not fallback to network lookup when xsd is not found

---
+++ liquibase-core/src/main/java/liquibase/parser/core/xml/XSDLookUpException.java
@@ -0,0 +1,9 @@
+package liquibase.parser.core.xml;
+
+public class XSDLookUpException extends RuntimeException{
+
+    public XSDLookUpException(String message) {
+        super(message);
+    }
+
+}
