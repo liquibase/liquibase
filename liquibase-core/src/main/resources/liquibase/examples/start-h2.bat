2cc3920c99a617226384e2a762fc3d63a86cec15
Nathan Voxland <nathan@voxland.net>
2022-03-03 10:07:54 -0600

    Refactored startH2 command so the logic is contained in StartH2CommandStep and the old start-h2 standalone script is just a call to `liquibase init start-h2`

--- liquibase-core/src/main/resources/liquibase/examples/start-h2.bat
+++ liquibase-core/src/main/resources/liquibase/examples/start-h2.bat
@@ -17,18 +17,4 @@ if "%LIQUIBASE_HOME%"=="" (
     exit /B 1
 )
 
-if "%JAVA_HOME%"=="" (
-
-    rem check for jre dir in liquibase_home
-    if NOT "%LIQUIBASE_HOME%"=="" if exist "%LIQUIBASE_HOME%\jre" (
-        set JAVA_HOME=%LIQUIBASE_HOME%\jre
-    )
-)
-
-if "%JAVA_HOME%"=="" (
-    set JAVA_PATH=java
-) else (
-    set JAVA_PATH=%JAVA_HOME%\bin\java
-)
-
-"%JAVA_PATH%" -cp "%LIQUIBASE_HOME%\lib\h2-2.1.210.jar;%LIQUIBASE_HOME%\liquibase.jar" liquibase.example.StartH2Main
+"%LIQUIBASE_HOME%\liquibase.bat" init start-h2
---
+++ liquibase-integration-tests/src/test/resources/liquibase/extension/testing/command/initstartH2.test.groovy
@@ -0,0 +1,25 @@
+package liquibase.extension.testing.command
+
+CommandTests.define {
+
+    command = ["init", "startH2"]
+
+    signature = """
+Short Description: Launches H2, an included open source in-memory database. This Java application is shipped with Liquibase, and is useful in the Getting Started experience and for testing out Liquibase commands.
+Long Description: NOT SET
+Required Args:
+  NONE
+Optional Args:
+  bindAddress (String) Network address to bind to
+    Default: 127.0.0.1
+  dbPort (Integer) Port to run h2 database on
+    Default: 9090
+  password (String) Password to use for created h2 user
+    Default: letmein
+    OBFUSCATED
+  username (String) Username to create in h2
+    Default: dbuser
+  webPort (Integer) Port to run h2's web interface on
+    Default: 8080
+"""
+}
