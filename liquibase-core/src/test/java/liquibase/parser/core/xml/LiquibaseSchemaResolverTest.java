e0c574062586c40c1cde5d687b194a9c16e4951e
atytarchuk <atytarchuk@liquibase.com>
2022-03-08 15:23:43 +0100

    [DAT-8303] WIP stable build and tests - with 14 FIXMEs

--- liquibase-core/src/test/java/liquibase/parser/core/xml/LiquibaseSchemaResolverTest.java
+++ liquibase-core/src/test/java/liquibase/parser/core/xml/LiquibaseSchemaResolverTest.java
@@ -10,11 +10,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.internal.util.reflection.Whitebox;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 import org.xml.sax.InputSource;
 
 import liquibase.parser.LiquibaseParser;
