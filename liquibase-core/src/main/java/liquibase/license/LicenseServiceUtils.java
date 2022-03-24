e73febd76e59fe61431bd1a25d45361780a37c6f
Steven Massaro <steven.massaro.web@gmail.com>
2022-03-24 10:49:53 -0500

    move LicenseTier into pro code

--- liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
+++ liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
@@ -13,28 +13,6 @@ import liquibase.exception.ValidationErrors;
  */
 public class LicenseServiceUtils {
 
-    public static ValidationErrors checkForValidLicense(LicenseTier licenseType, Change change) {
-      LicenseService licenseService = Scope.getCurrentScope().getSingleton(LicenseServiceFactory.class).getLicenseService();
-      if (licenseService == null) {
-        return new ValidationErrors();
-      }
-      if (licenseService.licenseIsValid(licenseType.getSubject())) {
-        String message = String.format("Found valid license with subject '%s' for '%s'",licenseType.getSubject(), change.getDescription());
-        Scope.getCurrentScope().getLog(LicenseService.class).fine(message);
-        return new ValidationErrors();
-      }
-
-      ChangeSet changeSet = change.getChangeSet();
-      String changeType = Scope.getCurrentScope().getSingleton(ChangeFactory.class).getChangeMetaData(change).getName();
-      ValidationErrors validationErrors = new ValidationErrors();
-      String message = "Change Set ID: " + changeSet.getId() + " Change Set Author: " + changeSet.getAuthor() + "\n";
-      message += "Change Type 'pro:" + changeType + "' is not allowed without a valid Liquibase Pro License.\n";
-      message += "To purchase or renew a Liquibase Pro license key, please contact sales@liquibase.com or\n" +
-                "go to https://www.liquibase.org/download";
-      validationErrors.addError(message);
-      return validationErrors;
-    }
-
   /**
    * Check for a Liquibase Pro License.
    * @return true if licensed, false if not
@@ -49,7 +27,7 @@ public class LicenseServiceUtils {
     if (licenseService == null) {
       return false;
     }
-    return licenseService.licenseIsValid(LicenseTier.PRO.getSubject());
+    return licenseService.licenseIsValid("Liquibase Pro");
   }
 
 }
