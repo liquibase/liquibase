50a87d229e9bc499d8d26bd7013216590b324b1f
TheDecker <TheDecker@gmx.net>
2022-04-21 09:45:24 +0200

    Exclude the "catalog" property from the AddColumnGeneratorTest since I'm not quite certain of the expected outcome.

--- liquibase-core/src/test/java/liquibase/sqlgenerator/core/AddColumnGeneratorTest.java
+++ liquibase-core/src/test/java/liquibase/sqlgenerator/core/AddColumnGeneratorTest.java
@@ -106,7 +106,6 @@ public class AddColumnGeneratorTest extends AbstractSqlGeneratorTest<AddColumnSt
         AddColumnChange change = new AddColumnChange();
         change.setTableName(TABLE_NAME);
         change.setSchemaName(SCHEMA_NAME);
-        change.setCatalogName(CATALOG_NAME);
 
         AddColumnConfig column = new AddColumnConfig();
         column.setName("column1");
@@ -127,10 +126,10 @@ public class AddColumnGeneratorTest extends AbstractSqlGeneratorTest<AddColumnSt
         Sql[] sql = instance.generateSql(statements, new MySQLDatabase());
 
         assertEquals(5, sql.length);
-        assertEquals("ALTER TABLE catalog_name.schema_name.table_name ADD column1 BIGINT NULL, ADD column2 BIT(1) NULL", sql[0].toSql());
-        assertEquals("UPDATE catalog_name.schema_name.table_name SET column1 = 0", sql[1].toSql());
-        assertEquals("UPDATE catalog_name.schema_name.table_name SET column2 = 1", sql[2].toSql());
-        assertEquals("ALTER TABLE catalog_name.schema_name.table_name MODIFY column1 BIGINT NOT NULL", sql[3].toSql());
-        assertEquals("ALTER TABLE catalog_name.schema_name.table_name MODIFY column2 BIT(1) NOT NULL", sql[4].toSql());
+        assertEquals("ALTER TABLE schema_name.table_name ADD column1 BIGINT NULL, ADD column2 BIT(1) NULL", sql[0].toSql());
+        assertEquals("UPDATE schema_name.table_name SET column1 = 0", sql[1].toSql());
+        assertEquals("UPDATE schema_name.table_name SET column2 = 1", sql[2].toSql());
+        assertEquals("ALTER TABLE schema_name.table_name MODIFY column1 BIGINT NOT NULL", sql[3].toSql());
+        assertEquals("ALTER TABLE schema_name.table_name MODIFY column2 BIT(1) NOT NULL", sql[4].toSql());
     }
 }
