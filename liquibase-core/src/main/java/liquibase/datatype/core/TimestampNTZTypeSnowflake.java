1760ca8b12bb6f5942c3619b5b7ba03441468293
KushnirykOleh <kushnirykoleh@gmail.com>
2022-05-10 17:42:27 +0300

    added Snowflake DB files, wip

---
+++ liquibase-core/src/main/java/liquibase/datatype/core/TimestampNTZTypeSnowflake.java
@@ -0,0 +1,46 @@
+package liquibase.datatype.core;
+
+import liquibase.change.core.LoadDataChange;
+import liquibase.database.Database;
+import liquibase.database.core.SnowflakeDatabase;
+import liquibase.datatype.DataTypeInfo;
+import liquibase.datatype.DatabaseDataType;
+import liquibase.datatype.LiquibaseDataType;
+
+@DataTypeInfo(name = "timestamp_ntz", aliases = {"java.sql.Types.DATETIME", "datetime"}, minParameters = 0, maxParameters = 0, priority = LiquibaseDataType.PRIORITY_DATABASE)
+public class TimestampNTZTypeSnowflake extends LiquibaseDataType {
+
+    @Override
+    public DatabaseDataType toDatabaseDataType(Database database) {
+
+        if (database instanceof SnowflakeDatabase) {
+            return new DatabaseDataType("TIMESTAMP_NTZ", getParameters());
+        }
+
+        return super.toDatabaseDataType(database);
+    }
+
+    @Override
+    public void finishInitialization(String originalDefinition) {
+        super.finishInitialization(originalDefinition);
+    }
+
+    @Override
+    public LoadDataChange.LOAD_DATA_TYPE getLoadTypeName() {
+        return LoadDataChange.LOAD_DATA_TYPE.DATE;
+    }
+
+    @Override
+    public boolean supports(Database database) {
+        if (database instanceof SnowflakeDatabase)
+            return true;
+        return false;
+    }
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_DATABASE;
+    }
+
+
+}
