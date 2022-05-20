ac555823dd7eaf629b4c263cb25e53cb46fe52fe
ap-liquibase <apierre@liquibase.com>
2022-05-20 10:11:36 -0400

    DAT-10364 split oss from pro jar in assembly plugin config

--- liquibase-dist/src/main/archive/liquibase.bat
+++ liquibase-dist/src/main/archive/liquibase.bat
@@ -6,6 +6,11 @@ setlocal enabledelayedexpansion
 rem %~dp0 is expanded pathname of the current script under NT
 set LIQUIBASE_HOME="%~dp0"
 
+set CP=.
+for /R %LIQUIBASE_HOME% %%f in (liquibase*.jar) do set CP=!CP!;%%f
+for /R liquibase_libs %%f in (*.jar) do set CP=!CP!;%%f
+for /R %LIQUIBASE_HOME%\lib %%f in (*.jar) do set CP=!CP!;%%f
+
 rem remove quotes around LIQUIBASE_HOME
 set LIQUIBASE_HOME=%LIQUIBASE_HOME:"=%
 
@@ -17,6 +22,10 @@ if exist "%LIQUIBASE_HOME%\jre" if "%JAVA_HOME%"=="" (
     set JAVA_HOME=%LIQUIBASE_HOME%\jre
 )
 
+
+rem add the lib directory itself to the classpath
+set CP=!CP!;!LIQUIBASE_HOME!lib
+
 rem special characters may be lost
 setlocal DISABLEDELAYEDEXPANSION
 
@@ -25,4 +34,4 @@ IF NOT DEFINED JAVA_OPTS set JAVA_OPTS=
 set JAVA_PATH=java
 if NOT "%JAVA_HOME%" == "" set JAVA_PATH=%JAVA_HOME%\bin\java
 
-"%JAVA_PATH%" %JAVA_OPTS% -jar "%LIQUIBASE_HOME%\liquibase.jar" %*
+"%JAVA_PATH%" -cp "%CP%" %JAVA_OPTS% liquibase.integration.commandline.LiquibaseCommandLine %*
