43c2f17a984e4411cabf0c88c885d223964e5ebc
Steven Massaro <steven.massaro.web@gmail.com>
2022-06-28 09:09:29 -0500

    add flow and flow.validate maven goals

---
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/AbstractLiquibaseFlowMojo.java
@@ -0,0 +1,50 @@
+package org.liquibase.maven.plugins;
+
+import liquibase.Liquibase;
+import liquibase.command.CommandScope;
+import liquibase.exception.CommandExecutionException;
+import liquibase.exception.LiquibaseException;
+import org.liquibase.maven.property.PropertyElement;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public abstract class AbstractLiquibaseFlowMojo extends AbstractLiquibaseMojo {
+    /**
+     * Specifies the <i>flowFile</i> to use. If not specified, the default
+     * checks will be used and no file will be created.
+     *
+     * @parameter property="liquibase.flowFile"
+     */
+    @PropertyElement
+    protected String flowFile;
+
+    /**
+     * @parameter property="liquibase.outputFile"
+     */
+    @PropertyElement
+    protected File outputFile;
+
+    @Override
+    public boolean databaseConnectionRequired() {
+        return false;
+    }
+
+    @Override
+    protected void performLiquibaseTask(Liquibase liquibase) throws LiquibaseException {
+        CommandScope liquibaseCommand = new CommandScope(getCommandName());
+        liquibaseCommand.addArgumentValue("flowFile", flowFile);
+        liquibaseCommand.addArgumentValue("flowIntegration", "maven");
+        if (outputFile != null) {
+            try {
+                liquibaseCommand.setOutput(new FileOutputStream(outputFile));
+            } catch (FileNotFoundException e) {
+                throw new CommandExecutionException(e);
+            }
+        }
+        liquibaseCommand.execute();
+    }
+
+    public abstract String[] getCommandName();
+}
