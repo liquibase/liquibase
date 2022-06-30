9e1ce2aa666db351b0f68dec034d0c194879829d
Nathan Voxland <nathan@voxland.net>
2022-06-30 00:49:48 -0500

    Fixed failing test

--- liquibase-core/src/test/groovy/liquibase/resource/FileSystemResourceAccessorTest.groovy
+++ liquibase-core/src/test/groovy/liquibase/resource/FileSystemResourceAccessorTest.groovy
@@ -112,7 +112,7 @@ class FileSystemResourceAccessorTest extends Specification {
 
         then:
         def e = thrown(IOException)
-        e.message.startsWith("Found 3 files matching 'com/example/everywhere/file-everywhere.txt':")
+        e.message.startsWith("Found 3 files with the path 'com/example/everywhere/file-everywhere.txt':")
         e.message.contains("file-everywhere.txt")
         e.message.contains("test-classes")
         e.message.contains("simple-files.jar")
