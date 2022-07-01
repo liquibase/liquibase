e91f5a691c08c7bbf6cd9962cb77d634e8bed19a
Alex Brackx <abrackx@liquibase.com>
2022-07-01 11:20:45 -0400

    [DAT-9854] Revert grammar update.

--- liquibase-core/src/main/java/liquibase/util/StringUtil.java
+++ liquibase-core/src/main/java/liquibase/util/StringUtil.java
@@ -163,7 +163,7 @@ public class StringUtil {
                         return false;
                     }
                 }
-                return piece.equalsIgnoreCase(endDelimiter.toLowerCase());
+                return piece.toLowerCase().equalsIgnoreCase(endDelimiter.toLowerCase());
             } else {
                 return piece.toLowerCase().matches(endDelimiter.toLowerCase()) || (previousPiece + piece).toLowerCase().matches("[\\s\n\r]*" + endDelimiter.toLowerCase());
             }
