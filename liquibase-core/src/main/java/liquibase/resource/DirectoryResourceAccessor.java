748f782160220bd2d41a2d185709da558460baca
Wesley Willard <wwillard@datical.com>
2022-08-23 12:32:38 -0500

    Remove unneeded test and debug code
    DAT-11052

--- liquibase-core/src/main/java/liquibase/resource/DirectoryResourceAccessor.java
+++ liquibase-core/src/main/java/liquibase/resource/DirectoryResourceAccessor.java
@@ -37,7 +37,7 @@ public class DirectoryResourceAccessor extends AbstractPathResourceAccessor {
         Scope.getCurrentScope().getLog(getClass()).fine("Creating resourceAccessor for directory " + directory);
         this.rootDirectory = directory;
         if (!Files.exists(directory)) {
-            throw new FileNotFoundException("Non-existent directory: " + directory.toAbsolutePath() + " for " + System.getProperty("os.name"));
+            throw new FileNotFoundException("Non-existent directory: " + directory.toAbsolutePath());
         }
         if (!Files.isDirectory(directory)) {
             throw new IllegalArgumentException("Not a directory: " + directory.toAbsolutePath());
