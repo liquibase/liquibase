a45daca710aed0bcd015315a4a32d153c6ff3d0c
Łukasz Olejniczak <lolejniczak@google.com>
2022-08-27 20:47:02 +0200

    renameTable

---
+++ src/test/resources/liquibase/harness/change/expectedSql/bigquery/dropTable.sql
@@ -0,0 +1,2 @@
+CREATE TABLE test_table (test_id INT, test_column STRING)
+DROP TABLE test_table
---
+++ src/test/resources/liquibase/harness/change/expectedSql/bigquery/renameTable.sql
@@ -0,0 +1,2 @@
+CREATE TABLE oldnametable (test_id INT, test_column STRING)
+ALTER TABLE oldnametable RENAME TO newnametable
