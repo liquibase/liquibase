f95442b58006921d59c93e7ca78f7f1a7bfbb4f4
Wesley Willard <wwillard@datical.com>
2022-09-06 14:20:25 -0500

    Revert previous test change
    DAT-11317

--- liquibase-extension-testing/src/main/groovy/liquibase/extension/testing/command/CommandTests.groovy
+++ liquibase-extension-testing/src/main/groovy/liquibase/extension/testing/command/CommandTests.groovy
@@ -284,33 +284,32 @@ Long Description: ${commandDefinition.getLongDescription() ?: "NOT SET"}
             }
         }
 
-        def resourceAccessor = null
+        def resourceAccessor = Scope.getCurrentScope().getResourceAccessor()
 
-        if (testDef.searchPath == null) {
-            testDef.searchPath = "."
+        if (testDef.searchPath != null) {
+            def config = Scope.getCurrentScope().getSingleton(LiquibaseConfiguration.class)
+
+            searchPathPropertiesProvider = new AbstractMapConfigurationValueProvider() {
+                @Override
+                protected Map<?, ?> getMap() {
+                    return Collections.singletonMap(GlobalConfiguration.SEARCH_PATH.getKey(), testDef.searchPath)
+                }
+
+                @Override
+                protected String getSourceDescription() {
+                    return "command tests search path override"
+                }
+
+                @Override
+                int getPrecedence() {
+                    return 1
+                }
+            }
+
+            config.registerProvider(searchPathPropertiesProvider)
+            resourceAccessor =
+                    new CompositeResourceAccessor(new SearchPathResourceAccessor(testDef.searchPath, null), Scope.getCurrentScope().getResourceAccessor())
         }
-        def config = Scope.getCurrentScope().getSingleton(LiquibaseConfiguration.class)
-
-        searchPathPropertiesProvider = new AbstractMapConfigurationValueProvider() {
-            @Override
-            protected Map<?, ?> getMap() {
-                return Collections.singletonMap(GlobalConfiguration.SEARCH_PATH.getKey(), testDef.searchPath)
-            }
-
-            @Override
-            protected String getSourceDescription() {
-                return "command tests search path override"
-            }
-
-            @Override
-            int getPrecedence() {
-                return 1
-            }
-        }
-
-        config.registerProvider(searchPathPropertiesProvider)
-        resourceAccessor =
-            new CompositeResourceAccessor(new SearchPathResourceAccessor(testDef.searchPath, null), Scope.getCurrentScope().getResourceAccessor())
 
         def scopeSettings = [
                 (LiquibaseCommandLineConfiguration.LOG_LEVEL.getKey()): Level.INFO,
