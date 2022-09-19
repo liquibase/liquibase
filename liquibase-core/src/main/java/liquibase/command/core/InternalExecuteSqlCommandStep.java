beeb415ed579b119ea0e59407a4698175a16cdc5
Steven Massaro <steven.massaro.web@gmail.com>
2022-09-19 12:28:17 -0500

    use proper file not found message for execute-sql

--- liquibase-core/src/main/java/liquibase/command/core/InternalExecuteSqlCommandStep.java
+++ liquibase-core/src/main/java/liquibase/command/core/InternalExecuteSqlCommandStep.java
@@ -9,6 +9,7 @@ import liquibase.executor.ExecutorService;
 import liquibase.resource.PathHandlerFactory;
 import liquibase.resource.Resource;
 import liquibase.statement.core.RawSqlStatement;
+import liquibase.util.FileUtil;
 import liquibase.util.StreamUtil;
 import liquibase.util.StringUtil;
 
@@ -61,7 +62,7 @@ public class InternalExecuteSqlCommandStep extends AbstractCommandStep {
             final PathHandlerFactory pathHandlerFactory = Scope.getCurrentScope().getSingleton(PathHandlerFactory.class);
             Resource resource = pathHandlerFactory.getResource(sqlFile, true);
             if (!resource.exists()){
-              throw new LiquibaseException(String.format("The file '%s' does not exist", sqlFile));
+                throw new LiquibaseException(FileUtil.getFileNotFoundMessage(sqlFile));
             }
             sqlText = StreamUtil.readStreamAsString(resource.openInputStream());
         }
