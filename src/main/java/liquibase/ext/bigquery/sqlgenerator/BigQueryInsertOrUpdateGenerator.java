398b58c748a7bbc4189b66d48422af4639e5ee8a
lolejniczak <olejniczak.lukasz@gmail.com>
2022-09-21 20:13:54 +0200

    Snapshot tests (#37)
    * invalidate trigger tests
    
    * remove non-modified files
    
    * remove non-modified files
    
    * invalidated create disable and drop trigger
    
    * invalidated create disable and drop trigger
    
    * invalidated rename trigger
    
    * modified expected sql files
    
    * modified expected sql files
    
    * modified expected sql files
    
    * modified expected sql files
    
    * modified expected sql files
    
    * modified expected sql files
    
    * added files for data tests
    
    * added files for data tests
    
    * added files for data tests
    
    * added files for data tests
    
    * added files for data tests
    
    * added files for data tests
    
    * added files for data tests
    
    * added files for data tests
    
    * added files for snapshot tests
    
    * modified technical names in InsertOrUpdateGenerator
    
    * modified technical names in InsertOrUpdateGenerator
    
    * modified technical names in InsertOrUpdateGenerator
    
    * modified loadUpdateData.json - added attributes
    
    Co-authored-by: lolejniczak <bisideu@gmail.com>

--- src/main/java/liquibase/ext/bigquery/sqlgenerator/BigQueryInsertOrUpdateGenerator.java
+++ src/main/java/liquibase/ext/bigquery/sqlgenerator/BigQueryInsertOrUpdateGenerator.java
@@ -18,37 +18,34 @@ public class BigQueryInsertOrUpdateGenerator extends InsertOrUpdateGenerator {
         return database instanceof BigqueryDatabase;
     }
 
+    @Override
     protected String getInsertStatement(InsertOrUpdateStatement insertOrUpdateStatement, Database database, SqlGeneratorChain sqlGeneratorChain) {
         StringBuilder columns = new StringBuilder();
         StringBuilder values = new StringBuilder();
-        Iterator var6 = insertOrUpdateStatement.getColumnValues().keySet().iterator();
 
-        while(var6.hasNext()) {
-            String columnKey = (String)var6.next();
+        for (String columnKey : insertOrUpdateStatement.getColumnValues().keySet()) {
             columns.append(",");
             columns.append(columnKey);
             values.append(",");
-            values.append(this.convertToString(insertOrUpdateStatement.getColumnValue(columnKey), database));
+            values.append(convertToString(insertOrUpdateStatement.getColumnValue(columnKey), database));
         }
-
         columns.deleteCharAt(0);
         values.deleteCharAt(0);
         return "INSERT (" + columns + ") VALUES (" + values + ")";
     }
 
+    @Override
     protected String getUpdateStatement(InsertOrUpdateStatement insertOrUpdateStatement, Database database, String whereClause, SqlGeneratorChain sqlGeneratorChain) {
-        StringBuilder sql = new StringBuilder("UPDATE SET ");
-        Iterator var6 = insertOrUpdateStatement.getColumnValues().keySet().iterator();
 
-        while(var6.hasNext()) {
-            String columnKey = (String)var6.next();
+        StringBuilder sql = new StringBuilder("UPDATE SET ");
+
+        for (String columnKey : insertOrUpdateStatement.getColumnValues().keySet()) {
             if (insertOrUpdateStatement.getAllowColumnUpdate(columnKey)) {
                 sql.append(columnKey).append(" = ");
-                sql.append(this.convertToString(insertOrUpdateStatement.getColumnValue(columnKey), database));
+                sql.append(convertToString(insertOrUpdateStatement.getColumnValue(columnKey), database));
                 sql.append(",");
             }
         }
-
         int lastComma = sql.lastIndexOf(",");
         if (lastComma > -1) {
             sql.deleteCharAt(lastComma);
@@ -57,34 +54,34 @@ public class BigQueryInsertOrUpdateGenerator extends InsertOrUpdateGenerator {
         return sql.toString();
     }
 
+    @Override
     protected String getRecordCheck(InsertOrUpdateStatement insertOrUpdateStatement, Database database, String whereClause) {
         return "MERGE INTO " + insertOrUpdateStatement.getTableName() + " USING (SELECT 1) ON " + whereClause + " WHEN NOT MATCHED THEN ";
     }
 
+    @Override
     protected String getElse(Database database) {
         return " WHEN MATCHED THEN ";
     }
 
+    // Copied from liquibase.sqlgenerator.core.InsertOrUpdateGeneratorHsql
     private String convertToString(Object newValue, Database database) {
         String sqlString;
-        if (newValue != null && !"".equals(newValue.toString()) && !"NULL".equalsIgnoreCase(newValue.toString())) {
-            if (newValue instanceof String && !this.looksLikeFunctionCall((String)newValue, database)) {
-                sqlString = "'" + database.escapeStringForDatabase(newValue.toString()) + "'";
-            } else if (newValue instanceof Date) {
-                sqlString = database.getDateLiteral((Date)newValue);
-            } else if (newValue instanceof Boolean) {
-                if (Boolean.TRUE.equals(newValue)) {
-                    sqlString = DataTypeFactory.getInstance().getTrueBooleanValue(database);
-                } else {
-                    sqlString = DataTypeFactory.getInstance().getFalseBooleanValue(database);
-                }
+        if ((newValue == null) || "".equals(newValue.toString()) || "NULL".equalsIgnoreCase(newValue.toString())) {
+            sqlString = "NULL";
+        } else if ((newValue instanceof String) && !looksLikeFunctionCall(((String) newValue), database)) {
+            sqlString = "'" + database.escapeStringForDatabase(newValue.toString()) + "'";
+        } else if (newValue instanceof Date) {
+            sqlString = database.getDateLiteral(((Date) newValue));
+        } else if (newValue instanceof Boolean) {
+            if (Boolean.TRUE.equals(newValue)) {
+                sqlString = DataTypeFactory.getInstance().getTrueBooleanValue(database);
             } else {
-                sqlString = newValue.toString();
+                sqlString = DataTypeFactory.getInstance().getFalseBooleanValue(database);
             }
         } else {
-            sqlString = "NULL";
+            sqlString = newValue.toString();
         }
-
         return sqlString;
     }
 }
