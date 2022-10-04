71dac11be23712a3f877560778a8ce1e23e8a899
Daniel Mallorga <dmallorga@liquibase.com>
2022-10-04 17:52:19 -0300

    getLocalHostName method logic updated to retrieved host name in case external address does not have a useful host name attached to it.

--- liquibase-core/src/main/java/liquibase/util/NetUtil.java
+++ liquibase-core/src/main/java/liquibase/util/NetUtil.java
@@ -11,6 +11,7 @@ import java.util.Enumeration;
 public class NetUtil {
 
     private static InetAddress localHost;
+    private static String hostName;
 
     /**
      * Smarter way to get localhost than InetAddress.getLocalHost.  See https://bugs.java.com/bugdatabase/view_bug.do?bug_id=4665037
@@ -19,7 +20,7 @@ public class NetUtil {
         if (localHost == null) {
             InetAddress foundHost = InetAddress.getLocalHost();
 
-            if (foundHost.isLoopbackAddress() || foundHost.isLinkLocalAddress()) {
+            if (foundHost == null || foundHost.isLoopbackAddress() || foundHost.isLinkLocalAddress()) {
                 //try to find something other than localhost
 
                 Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
@@ -56,15 +57,29 @@ public class NetUtil {
     }
 
     /**
-     * @return Machine's host name
+     * @return Machine's host name. This method can be better to call than getting it off {@link #getLocalHost()} because sometimes the external address returned by that function does not have a useful hostname attached to it.
+     * This function will make sure a good value is returned.
      */
     public static String getLocalHostName() throws UnknownHostException, SocketException {
-        try {
-            return getLocalHost().getHostName();
-        } catch (Exception e) {
-            Scope.getCurrentScope().getLog(NetUtil.class).fine("Error getting hostname", e);
-            return "unknown";
+        if (hostName == null ) {
+            try {
+                InetAddress localHost = getLocalHost();
+                hostName = localHost.getHostName();
+                if (hostName.equals(localHost.getHostAddress())) {
+                    //sometimes the external IP interface doesn't have a hostname associated with it but localhost always does
+                    InetAddress lHost = InetAddress.getLocalHost();
+                    if (lHost != null) {
+                        hostName =  lHost.getHostName();
+                    }
+                }
+            } catch (Exception e) {
+                Scope.getCurrentScope().getLog(NetUtil.class).fine("Error getting hostname", e);
+                if (hostName == null) {
+                    hostName = "unknown";
+                }
+            }
         }
+        return hostName;
     }
 
 
