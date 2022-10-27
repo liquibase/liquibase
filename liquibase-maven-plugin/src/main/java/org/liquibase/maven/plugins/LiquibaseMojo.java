458a340e2bfc2d75cf694aa045e186afbce47b9c
Wesley Willard <wwillard@datical.com>
2022-10-27 11:55:15 -0500

    WIP

--- liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseMojo.java
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseMojo.java
@@ -3,8 +3,13 @@ package org.liquibase.maven.plugins;
 import liquibase.Scope;
 import liquibase.command.CommandDefinition;
 import liquibase.command.CommandFactory;
+import liquibase.command.CommandResults;
 import liquibase.command.CommandScope;
+import liquibase.configuration.AbstractMapConfigurationValueProvider;
+import liquibase.configuration.LiquibaseConfiguration;
+import liquibase.exception.CommandExecutionException;
 import liquibase.integration.commandline.Main;
+import liquibase.util.StringUtil;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecution;
@@ -69,9 +74,14 @@ public class LiquibaseMojo extends AbstractMojo implements MapOrientedComponent 
         Main.runningFromNewCli = true;
 
         String goal = mojoExecution.getGoal();
+        LiquibaseConfiguration liquibaseConfiguration = Scope.getCurrentScope().getSingleton(LiquibaseConfiguration.class);
+        MojoConfigurationValueProvider mojoConfigurationValueProvider = null;
         try {
             CommandScope commandScope = new CommandScope(goal);
 
+            mojoConfigurationValueProvider = new MojoConfigurationValueProvider(configuration);
+            liquibaseConfiguration.registerProvider(mojoConfigurationValueProvider);
+            /*
             CommandFactory commandFactory = Scope.getCurrentScope().getSingleton(CommandFactory.class);
             CommandDefinition commandDef = commandFactory.getCommandDefinition(goal);
             for (String argName : commandDef.getArguments().keySet()) {
@@ -80,6 +90,7 @@ public class LiquibaseMojo extends AbstractMojo implements MapOrientedComponent 
                     commandScope.addArgumentValue(argName, value);
                 }
             }
+            */
 
             Map<String, Object> scopeValues = new HashMap<>();
             scopeValues.put(Scope.Attr.resourceAccessor.name(), new MavenResourceAccessor(project));
@@ -88,9 +99,35 @@ public class LiquibaseMojo extends AbstractMojo implements MapOrientedComponent 
             });
         } catch (Exception e) {
             throw new MojoExecutionException(e.getMessage(), e);
+        } finally {
+            if (mojoConfigurationValueProvider != null) {
+                liquibaseConfiguration.unregisterProvider(mojoConfigurationValueProvider);
+                mojoConfigurationValueProvider = null;
+            }
         }
     }
 
+    private static class MojoConfigurationValueProvider extends AbstractMapConfigurationValueProvider {
+        private final Map<String, Object> configurationMap;
+
+        MojoConfigurationValueProvider(Map<String, Object> configurationMap) {
+            this.configurationMap = configurationMap;
+        }
+        @Override
+        protected Map<?, ?> getMap() {
+            return configurationMap;
+        }
+
+        @Override
+        protected String getSourceDescription() {
+            return "Maven value provider";
+        }
+
+        @Override
+        public int getPrecedence() {
+            return 1;
+        }
+    }
 
 //    public static void setField(Object field) {
 //        System.out.println("setting");
