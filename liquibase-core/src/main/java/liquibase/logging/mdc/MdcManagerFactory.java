0adde6f5597224fcd87408dbdc34b4560af3529f
Steven Massaro <steven.massaro.web@gmail.com>
2022-12-13 06:42:11 -0600

    add basic MDC classes

---
+++ liquibase-core/src/main/java/liquibase/logging/mdc/MdcManagerFactory.java
@@ -0,0 +1,23 @@
+package liquibase.logging.mdc;
+
+import liquibase.plugin.AbstractPluginFactory;
+
+public class MdcManagerFactory extends AbstractPluginFactory<MdcManager> {
+    @Override
+    protected Class<MdcManager> getPluginClass() {
+        return MdcManager.class;
+    }
+
+    @Override
+    protected int getPriority(MdcManager obj, Object... args) {
+        return obj.getPriority();
+    }
+
+    public MdcManager getMdcManager() {
+        return getPlugin();
+    }
+
+    public void unregister(MdcManager manager) {
+        this.removeInstance(manager);
+    }
+}
