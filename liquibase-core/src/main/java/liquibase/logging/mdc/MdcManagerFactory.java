477cf9efd44eb9d21338d995607ead1ccf593d3a
Steven Massaro <steven.massaro.web@gmail.com>
2022-12-13 07:12:42 -0600

    add private constructor

--- liquibase-core/src/main/java/liquibase/logging/mdc/MdcManagerFactory.java
+++ liquibase-core/src/main/java/liquibase/logging/mdc/MdcManagerFactory.java
@@ -3,6 +3,9 @@ package liquibase.logging.mdc;
 import liquibase.plugin.AbstractPluginFactory;
 
 public class MdcManagerFactory extends AbstractPluginFactory<MdcManager> {
+    private MdcManagerFactory() {
+    }
+
     @Override
     protected Class<MdcManager> getPluginClass() {
         return MdcManager.class;
