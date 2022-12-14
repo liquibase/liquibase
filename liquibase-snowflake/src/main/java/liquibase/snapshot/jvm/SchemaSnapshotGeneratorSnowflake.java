4be18406cf5977ad90d1959ee00a3deeaf969eb8
Yogurt-lei <yogurt_lei@foxmail.com>
2022-12-14 11:27:18 +0800

    fix: snowflake has wrong priority
    Close: #3538

--- liquibase-snowflake/src/main/java/liquibase/snapshot/jvm/SchemaSnapshotGeneratorSnowflake.java
+++ liquibase-snowflake/src/main/java/liquibase/snapshot/jvm/SchemaSnapshotGeneratorSnowflake.java
@@ -15,11 +15,11 @@ import java.util.List;
 public class SchemaSnapshotGeneratorSnowflake extends SchemaSnapshotGenerator {
     @Override
     public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
-        int priority = super.getPriority(objectType, database);
         if (database instanceof SnowflakeDatabase) {
-            priority += PRIORITY_DATABASE;
+            return super.getPriority(objectType, database) + PRIORITY_DATABASE;
+        } else {
+            return PRIORITY_NONE;
         }
-        return priority;
     }
 
     @Override
