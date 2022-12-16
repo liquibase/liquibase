42bb18c9d52dff07ff3f21c2cf91bf03f5ec8f09
mike-seger <github@net128.com>
2022-12-16 19:53:54 +0100

    Allow loading gzipped data files (#3379)
    * allow loading gzipped changes
    
    * Adds integration tests for gzipped csv files.
    
    * Adds type checking to make sure that we don't try to handle an InputStream that was already converted.
    
    Co-authored-by: michael <michael@mac>
    Co-authored-by: filipe <flautert@liquibase.org>

--- liquibase-core/src/main/java/liquibase/change/core/LoadDataChange.java
+++ liquibase-core/src/main/java/liquibase/change/core/LoadDataChange.java
@@ -40,6 +40,7 @@ import java.io.InputStream;
 import java.io.Reader;
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
 import static java.util.ResourceBundle.getBundle;
 import static liquibase.change.ChangeParameterMetaData.ALL;
@@ -720,7 +721,13 @@ public class LoadDataChange extends AbstractTableChange implements ChangeWithCol
         if (!resource.exists()) {
             return null;
         }
-        Reader streamReader = StreamUtil.readStreamWithReader(resource.openInputStream(), getEncoding());
+
+        @SuppressWarnings("java:S2095") // SONAR want us to close the stream here, but it is only read by CSVReader outside this method.
+        InputStream stream = resource.openInputStream();
+        if (resource.getPath().toLowerCase().endsWith(".gz") && !(stream instanceof GZIPInputStream)) {
+            stream = new GZIPInputStream(stream);
+        }
+        Reader streamReader = StreamUtil.readStreamWithReader(stream, getEncoding());
 
         char quotchar;
         if (StringUtil.trimToEmpty(this.quotchar).isEmpty()) {
