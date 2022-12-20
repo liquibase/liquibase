2e610dbfe03bb9e30e0165a5efbf38f9c46b5d73
filipe <flautert@liquibase.org>
2022-12-20 10:42:49 -0300

    Fix imports.

--- liquibase-integration-tests/src/test/java/liquibase/dbtest/AbstractIntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/AbstractIntegrationTest.java
@@ -1,14 +1,6 @@
 package liquibase.dbtest;
 
 import groovy.lang.Tuple2;
-
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import liquibase.*;
 import liquibase.change.ColumnConfig;
 import liquibase.change.core.LoadDataChange;
