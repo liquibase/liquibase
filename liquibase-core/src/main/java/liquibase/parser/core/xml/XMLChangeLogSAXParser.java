fcc1833b29cdcfce9fec8ff2228bf20fa4b931de
Wesley Willard <wwillard@datical.com>
2023-01-13 14:57:52 -0600

    Handle full paths which might come from extensions
    DAT-11478

--- liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXParser.java
+++ liquibase-core/src/main/java/liquibase/parser/core/xml/XMLChangeLogSAXParser.java
@@ -151,13 +151,6 @@ public class XMLChangeLogSAXParser extends AbstractChangeLogParser {
             }
 
             throw new ChangeLogParseException("Invalid Migration File: " + reason, e);
-        } catch (InvalidPathException e) {
-            final String message = "ERROR: The file '" + e.getInput() + "' was not found. " +
-                    "The <changelog|snapshot> file property cannot be configured with " +
-                    "a fully qualified path, but must be a relative path on the property, " +
-                    "and any local or remote base of the path set on the '--search-path' property.";
-            Scope.getCurrentScope().getLog(getClass()).severe("ERROR: " + e.getMessage(), e);
-            throw new ChangeLogParseException(message);
         } catch (Exception e) {
             throw new ChangeLogParseException(e);
         }
