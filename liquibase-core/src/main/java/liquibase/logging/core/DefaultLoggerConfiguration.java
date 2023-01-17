0b336c4c0d739974d0c489ffec23a8f036ec45e2
Steven Massaro <steven.massaro.web@gmail.com>
2023-01-17 14:36:34 -0600

    deprecate DefaultLoggerConfiguration

--- liquibase-core/src/main/java/liquibase/logging/core/DefaultLoggerConfiguration.java
+++ liquibase-core/src/main/java/liquibase/logging/core/DefaultLoggerConfiguration.java
@@ -6,8 +6,10 @@ import liquibase.configuration.AutoloadedConfigurations;
 /**
  * Configuration container for {@link liquibase.logging.LogService} properties
  */
+@Deprecated
 public class DefaultLoggerConfiguration implements AutoloadedConfigurations {
 
+    @Deprecated
     public static ConfigurationDefinition<String> LOG_LEVEL;
 
     static {
@@ -15,6 +17,7 @@ public class DefaultLoggerConfiguration implements AutoloadedConfigurations {
         LOG_LEVEL = builder.define("level", String.class)
                 .setDescription("Logging level")
                 .setDefaultValue("INFO")
+                .setInternal(true)
                 .build();
     }
 }
