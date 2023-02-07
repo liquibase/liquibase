314800205a97d4292ff9d6e9d9e653b840e5fe76
Steven Massaro <steven.massaro.web@gmail.com>
2023-02-07 13:54:34 -0700

    add MDC for rollback-one-update (DAT-13025) (#3792)

--- liquibase-core/src/main/java/liquibase/logging/mdc/MdcKey.java
+++ liquibase-core/src/main/java/liquibase/logging/mdc/MdcKey.java
@@ -27,4 +27,5 @@ public class MdcKey {
     public static final String ROLLBACK_COUNT = "rollbackCount";
     public static final String CHANGESETS_ROLLED_BACK = "changesetsRolledback";
     public static final String ROLLBACK_ONE_CHANGESET_FORCE = "rollbackOneChangesetForce";
+    public static final String ROLLBACK_ONE_UPDATE_FORCE = "rollbackOneUpdateForce";
 }
