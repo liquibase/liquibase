7c11d3e7fcf9ba682afd712d4bc46e2e7ae907fd
Soner Sivri <sonersivri@gmail.com>
2023-02-15 12:05:40 +0300

    #3819 Liquibase classloader fix

--- liquibase-core/src/main/java/liquibase/servicelocator/StandardServiceLocator.java
+++ liquibase-core/src/main/java/liquibase/servicelocator/StandardServiceLocator.java
@@ -16,21 +16,40 @@ public class StandardServiceLocator implements ServiceLocator {
     @Override
     public <T> List<T> findInstances(Class<T> interfaceType) throws ServiceNotFoundException {
         List<T> allInstances = new ArrayList<>();
-
+        Set<String> classNameSet = new HashSet<>();
         final Logger log = Scope.getCurrentScope().getLog(getClass());
-        final Iterator<T> services = ServiceLoader.load(interfaceType, Scope.getCurrentScope().getClassLoader(true)).iterator();
+        ClassLoader classLoader = Scope.getCurrentScope().getClassLoader(true);
+        findInstances(interfaceType, allInstances, log, classLoader, classNameSet);
+
+        if (!classLoader.equals(interfaceType.getClassLoader())) {
+            findInstances(interfaceType, allInstances, log, interfaceType.getClassLoader(), classNameSet);
+        }
+
+        return Collections.unmodifiableList(allInstances);
+    }
+
+    private static <T> void findInstances(Class<T> interfaceType, List<T> allInstances, Logger log, ClassLoader classLoader,
+                                          Set<String> classNameSet) {
+        final Iterator<T> services = ServiceLoader.load(interfaceType, classLoader).iterator();
+
         while (services.hasNext()) {
+
             try {
                 final T service = services.next();
-                log.fine("Loaded "+interfaceType.getName()+" instance "+service.getClass().getName());
-                allInstances.add(service);
+                String className = service.getClass().getName();
+
+                if (!classNameSet.contains(className)) {
+                    log.fine("Loaded "+ interfaceType.getName()+" instance "+ className);
+                    classNameSet.add(className);
+                    allInstances.add(service);
+                }
+
             } catch (Throwable e) {
                 log.info("Cannot load service: "+e.getMessage());
                 log.fine(e.getMessage(), e);
             }
-        }
 
-        return Collections.unmodifiableList(allInstances);
+        }
 
     }
 }
