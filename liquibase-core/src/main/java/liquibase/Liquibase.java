06673b6e7bc9b54a0f196fcd5f67c2ea035edd41
Arturo Bernal <arturobernalg@gmail.com>
2023-02-22 21:00:16 +0100

    Simplify lambda expressions when possible (#3736)
    Use lambda expressions with code block bodies when expression-style bodies.
    
    Co-authored-by: Daniel Mallorga <dmallorga@liquibase.com>
    Co-authored-by: filipe <flautert@liquibase.org>

--- liquibase-core/src/main/java/liquibase/Liquibase.java
+++ liquibase-core/src/main/java/liquibase/Liquibase.java
@@ -837,12 +837,9 @@ public class Liquibase implements AutoCloseable {
                             new CountChangeSetFilter(changesToApply));
 
                     CompositeLogService compositeLogService = new CompositeLogService(true, bufferLog);
-                    Scope.child(Scope.Attr.logService.name(), compositeLogService, () -> {
-                        runChangeLogIterator.run(createUpdateVisitor(), new RuntimeEnvironment(database, contexts, labelExpression));
-                    });
+                    Scope.child(Scope.Attr.logService.name(), compositeLogService, () -> runChangeLogIterator.run(createUpdateVisitor(), new RuntimeEnvironment(database, contexts, labelExpression)));
 
                     showUpdateSummary(changeLog, statusVisitor);
-
                     hubUpdater.postUpdateHub(updateOperation, bufferLog);
                     logDeploymentOutcomeMdc(true);
                 }
@@ -976,12 +973,9 @@ public class Liquibase implements AutoCloseable {
                             new UpToTagChangeSetFilter(tag, ranChangeSetList));
 
                     CompositeLogService compositeLogService = new CompositeLogService(true, bufferLog);
-                    Scope.child(Scope.Attr.logService.name(), compositeLogService, () -> {
-                        runChangeLogIterator.run(createUpdateVisitor(), new RuntimeEnvironment(database, contexts, labelExpression));
-                    });
+                    Scope.child(Scope.Attr.logService.name(), compositeLogService, () -> runChangeLogIterator.run(createUpdateVisitor(), new RuntimeEnvironment(database, contexts, labelExpression)));
 
                     showUpdateSummary(changeLog, statusVisitor);
-
                     hubUpdater.postUpdateHub(updateOperation, bufferLog);
                     logDeploymentOutcomeMdc(true);
                 }
@@ -1261,7 +1255,6 @@ public class Liquibase implements AutoCloseable {
                             new DbmsChangeSetFilter(database),
                             new IgnoreChangeSetFilter(),
                             new CountChangeSetFilter(changesToRollback));
-
                     doRollback(bufferLog, rollbackScript, logIterator, contexts, labelExpression, hubUpdater, rollbackOperation);
                 }
                 catch (Throwable t) {
@@ -1546,7 +1539,6 @@ public class Liquibase implements AutoCloseable {
                             new LabelChangeSetFilter(labelExpression),
                             new IgnoreChangeSetFilter(),
                             new DbmsChangeSetFilter(database));
-
                     doRollback(bufferLog, rollbackScript, logIterator, contexts, labelExpression, hubUpdater, rollbackOperation);
                 }
                 catch (Throwable t) {
@@ -1696,7 +1688,6 @@ public class Liquibase implements AutoCloseable {
                             new LabelChangeSetFilter(labelExpression),
                             new IgnoreChangeSetFilter(),
                             new DbmsChangeSetFilter(database));
-
                     doRollback(bufferLog, rollbackScript, logIterator, contexts, labelExpression, hubUpdater, rollbackOperation);
                 }
                 catch (Throwable t) {
@@ -1844,10 +1835,8 @@ public class Liquibase implements AutoCloseable {
 
                     ChangeLogIterator runChangeLogIterator = buildChangeLogIterator(tag, changeLog, contexts, labelExpression);
                     CompositeLogService compositeLogService = new CompositeLogService(true, bufferLog);
-                    Scope.child(Scope.Attr.logService.name(), compositeLogService, () -> {
-                        runChangeLogIterator.run(new ChangeLogSyncVisitor(database, changeLogSyncListener),
-                                new RuntimeEnvironment(database, contexts, labelExpression));
-                    });
+                    Scope.child(Scope.Attr.logService.name(), compositeLogService, () -> runChangeLogIterator.run(new ChangeLogSyncVisitor(database, changeLogSyncListener),
+                            new RuntimeEnvironment(database, contexts, labelExpression)));
                     hubUpdater.postUpdateHub(changeLogSyncOperation, bufferLog);
                 }
                 catch (Exception e) {
