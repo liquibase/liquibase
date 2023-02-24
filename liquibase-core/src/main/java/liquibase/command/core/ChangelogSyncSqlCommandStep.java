3650926f5322921279f457ef24fc7b12f73e6d59
filipe <filipe@caelum>
2023-02-24 10:54:55 -0300

    Uses writer helper step instead of creating it again.

--- liquibase-core/src/main/java/liquibase/command/core/ChangelogSyncSqlCommandStep.java
+++ liquibase-core/src/main/java/liquibase/command/core/ChangelogSyncSqlCommandStep.java
@@ -1,16 +1,12 @@
 package liquibase.command.core;
 
-import liquibase.GlobalConfiguration;
-import liquibase.Scope;
 import liquibase.command.*;
 import liquibase.database.Database;
-import liquibase.exception.LiquibaseException;
-import liquibase.executor.Executor;
-import liquibase.executor.ExecutorService;
-import liquibase.executor.LoggingExecutor;
 import liquibase.util.LoggingExecutorTextUtil;
 
-import java.io.*;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
 
 public class ChangelogSyncSqlCommandStep extends ChangelogSyncCommandStep {
 
@@ -33,24 +29,19 @@ public class ChangelogSyncSqlCommandStep extends ChangelogSyncCommandStep {
     }
 
     @Override
+    public List<Class<?>> requiredDependencies() {
+        return Arrays.asList(Database.class, Writer.class);
+    }
+
+    @Override
     public void run(CommandResultsBuilder resultsBuilder) throws Exception {
         final CommandScope commandScope = resultsBuilder.getCommandScope();
         final Database database = (Database) commandScope.getDependency(Database.class);
-        final Writer output = getOutputWriter(resultsBuilder);
         final String changelogFile = commandScope.getArgumentValue(CHANGELOG_FILE_ARG);
-
-        Executor oldTemplate = getAndReplaceJdbcExecutor(output, database);
         LoggingExecutorTextUtil.outputHeader("SQL to add all changesets to database history table", database, changelogFile);
         super.run(resultsBuilder);
-        flushOutputWriter(output);
-        Scope.getCurrentScope().getSingleton(ExecutorService.class).setExecutor("jdbc", database, oldTemplate);
     }
 
-    private static Writer getOutputWriter(CommandResultsBuilder resultsBuilder) throws UnsupportedEncodingException {
-        PrintStream outputStream = new PrintStream(resultsBuilder.getOutputStream());
-        String charsetName = GlobalConfiguration.OUTPUT_FILE_ENCODING.getCurrentValue();
-        return new OutputStreamWriter(outputStream, charsetName);
-    }
 
     @Override
     public String[][] defineCommandNames() {
@@ -61,25 +52,5 @@ public class ChangelogSyncSqlCommandStep extends ChangelogSyncCommandStep {
     public void adjustCommandDefinition(CommandDefinition commandDefinition) {
         commandDefinition.setShortDescription("Output the raw SQL used by Liquibase when running changelogSync");
     }
-
-    private Executor getAndReplaceJdbcExecutor(Writer output, Database database) {
-        Executor oldTemplate = Scope.getCurrentScope().getSingleton(ExecutorService.class).getExecutor("jdbc", database);
-        final LoggingExecutor loggingExecutor = new LoggingExecutor(oldTemplate, output, database);
-        Scope.getCurrentScope().getSingleton(ExecutorService.class).setExecutor("logging", database, loggingExecutor);
-        Scope.getCurrentScope().getSingleton(ExecutorService.class).setExecutor("jdbc", database, loggingExecutor);
-        return oldTemplate;
-    }
-    private void flushOutputWriter(Writer output) throws LiquibaseException {
-        if (output == null) {
-            return;
-        }
-
-        try {
-            output.flush();
-        } catch (IOException e) {
-            throw new LiquibaseException(e);
-        }
-    }
-
 
 }
