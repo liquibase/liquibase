3e878824978ff5b411c4eac6bd47483ffaca3815
nwcm <111259588+nwcm@users.noreply.github.com>
2023-03-18 07:06:01 +1100

    Fixes #3734 MySQL ENUM and SET column type  (#3842)
    * fix MySQL ENUM and SET bug #3734
    
    * - ENUM integration test added for MySQL
    - CommandUtil class created with different command methods to keep them in a single place
    
    * - Generate changelog tests updated to make use of a command util class
    - Command util refactoring with latest command step framework udpates
    
    ---------
    
    Co-authored-by: Daniel Mallorga <dmallorga@liquibase.com>

--- liquibase-core/src/main/java/liquibase/snapshot/jvm/ColumnSnapshotGenerator.java
+++ liquibase-core/src/main/java/liquibase/snapshot/jvm/ColumnSnapshotGenerator.java
@@ -395,34 +395,18 @@ public class ColumnSnapshotGenerator extends JdbcSnapshotGenerator {
         if ((database instanceof MySQLDatabase) && ("ENUM".equalsIgnoreCase(columnTypeName) || "SET".equalsIgnoreCase
                 (columnTypeName))) {
             try {
-                String boilerLength;
-                if ("ENUM".equalsIgnoreCase(columnTypeName)) {
-                    boilerLength = "7";
-                } else {
-                    // SET
-                    boilerLength = "6";
-                }
 
-                List<String> enumValues = Scope.getCurrentScope().getSingleton(ExecutorService.class).getExecutor("jdbc", database).queryForList(
-                        new RawSqlStatement(
-                                "SELECT DISTINCT SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING(COLUMN_TYPE, " + boilerLength +
-                                        ", LENGTH(COLUMN_TYPE) - " + boilerLength +
-                                        " - 1 ), \"','\", 1 + units.i + tens.i * 10) , \"','\", -1)\n" +
-                                        "FROM INFORMATION_SCHEMA.COLUMNS\n" +
-                                        "CROSS JOIN (SELECT 0 AS i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 " +
-                                        "UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) units\n" +
-                                        "CROSS JOIN (SELECT 0 AS i UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 " +
-                                        "UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) tens\n" +
-                                        "WHERE TABLE_SCHEMA = '" + column.getSchema().getName() + "' \n" +
-                                        "AND TABLE_NAME = '" + column.getRelation().getName() + "' \n" +
-                                        "AND COLUMN_NAME = '" + column.getName() + "'\n" +
-                                        "ORDER BY tens.i, units.i"), String.class);
-                String enumClause = "";
-                for (String enumValue : enumValues) {
-                    enumClause += "'" + enumValue + "', ";
-                }
-                enumClause = enumClause.replaceFirst(", $", "");
-                return new DataType(columnTypeName + "(" + enumClause + ")");
+                String enumValue = Scope.getCurrentScope().getSingleton(ExecutorService.class)
+                        .getExecutor("jdbc", database)
+                        .queryForObject(new RawSqlStatement("SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS\n" +
+                                "WHERE TABLE_SCHEMA = '" + column.getSchema().getName() + "'\n" +
+                                "AND TABLE_NAME = '" + column.getRelation().getName() + "'\n" +
+                                "AND COLUMN_NAME = '" + column.getName() + "'"), String.class);
+
+                enumValue = enumValue.replace("enum(", "ENUM(");
+                enumValue = enumValue.replace("set(", "SET(");
+
+                return new DataType(enumValue);
             } catch (DatabaseException e) {
                 Scope.getCurrentScope().getLog(getClass()).warning("Error fetching enum values", e);
             }
