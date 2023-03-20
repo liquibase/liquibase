0403ea9e1ce45ef0228d8f2484e2a6af2f0e55c5
Wesley Willard <wwillard@datical.com>
2023-03-20 10:56:03 -0500

    Revert "Change Index.setTable method to take a Relation parameter DAT-13676 (#3907)"
    This reverts commit 6a08136f3893f62ea078c4ec6e6d02afee5c8f8d.

--- liquibase-core/src/test/groovy/liquibase/snapshot/JsonSnapshotParserTest.groovy
+++ liquibase-core/src/test/groovy/liquibase/snapshot/JsonSnapshotParserTest.groovy
@@ -1,14 +1,8 @@
 package liquibase.snapshot
 
-import liquibase.Scope
-import liquibase.parser.SnapshotParser
-import liquibase.parser.SnapshotParserFactory
 import liquibase.parser.core.yaml.YamlSnapshotParser
 import liquibase.resource.SearchPathResourceAccessor
-import liquibase.structure.core.Index
-import liquibase.structure.core.View
 import spock.lang.Specification
-import spock.lang.Unroll
 
 class JsonSnapshotParserTest extends Specification {
     def "Test parsing a large file"() {
@@ -19,20 +13,5 @@ class JsonSnapshotParserTest extends Specification {
         then:
         noExceptionThrown()
         snapshot != null
-    }
-
-    @Unroll
-    def "Correctly parse a snapshot that contains Index objects on Views"() {
-        def snapshotFile = "snapshot-with-index-views.json"
-        when:
-        SnapshotParser parser = SnapshotParserFactory.getInstance().getParser(snapshotFile, Scope.getCurrentScope().getResourceAccessor())
-        DatabaseSnapshot parsedSnapshot = parser.parse(snapshotFile, Scope.getCurrentScope().getResourceAccessor())
-
-        then:
-        parsedSnapshot != null
-        Set<Index> indexes = parsedSnapshot.get(Index.class)
-        indexes[110]
-        indexes[110].getTable() == null
-        indexes[110].getRelation() instanceof View
     }
 }
