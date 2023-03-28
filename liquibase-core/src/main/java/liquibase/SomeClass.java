0b5e450bf08e17429665f7918322c9c66d227c88
Alex Brackx <abrackx@liquibase.com>
2023-03-28 09:32:06 -0400

    [DAT-13930] Testing build changes

---
+++ liquibase-core/src/main/java/liquibase/SomeClass.java
@@ -0,0 +1,17 @@
+package liquibase;
+
+public class SomeClass {
+    public String someField;
+
+    public SomeClass(String someField) {
+        this.someField = someField;
+    }
+
+    public String getSomeField() {
+        return someField;
+    }
+
+    public void setSomeField(String someField) {
+        this.someField = someField;
+    }
+}
