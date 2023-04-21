5d989176ff822fbb15a08474fba580bf7a87efc2
Alex Brackx <abrackx@liquibase.com>
2023-04-21 08:07:58 -0400

    [DAT-13292] Fix maven dropAll.

--- liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseDropAll.java
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseDropAll.java
@@ -16,44 +16,34 @@ import java.util.List;
  * @goal dropAll
  * @since 2.0.2
  */
-public class LiquibaseDropAll extends AbstractLiquibaseChangeLogMojo {
+public class LiquibaseDropAll extends AbstractLiquibaseMojo {
 
-  	/**
-		 *
-  	 * The schemas to be dropped. Comma separated list.
-  	 *
-  	 * @parameter property="liquibase.schemas"
+    /**
+     * The schemas to be dropped. Comma separated list.
      *
-  	 */
-	@PropertyElement
-  	protected String schemas;
+     * @parameter property="liquibase.schemas"
+     */
+    @PropertyElement
+    protected String schemas;
 
     protected String catalog;
 
-  	@Override
-  	protected void performLiquibaseTask(Liquibase liquibase)
-			throws LiquibaseException {
-  		  super.performLiquibaseTask(liquibase);
-  		  try {
-				  	checkRequiredParametersAreSpecified();
-				}
-  		  catch (Exception e) {
-  		      throw new LiquibaseException(e);
-				}
-    		if (schemas != null) {
+    @Override
+    protected void performLiquibaseTask(Liquibase liquibase) throws LiquibaseException {
+        if (schemas != null) {
             List<CatalogAndSchema> schemaObjs = new ArrayList<>();
             for (String name : schemas.split(",")) {
                 schemaObjs.add(new CatalogAndSchema(catalog, name));
             }
-	      		liquibase.dropAll(schemaObjs.toArray(new CatalogAndSchema[0]));
-		    } else {
-			      liquibase.dropAll();
-		    }
-	  }
+            liquibase.dropAll(schemaObjs.toArray(new CatalogAndSchema[0]));
+        } else {
+            liquibase.dropAll();
+        }
+    }
 
-	  @Override
-	  protected void printSettings(String indent) {
-	    	super.printSettings(indent);
-		    getLog().info(indent + "schemas: " + schemas);
-	  }
+    @Override
+    protected void printSettings(String indent) {
+        super.printSettings(indent);
+        getLog().info(indent + "schemas: " + schemas);
+    }
 }
