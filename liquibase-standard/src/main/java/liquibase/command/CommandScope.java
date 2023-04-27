e88a2673b70d0ed3ac251f7305198d24425e6af8
Steven Massaro <steven.massaro.web@gmail.com>
2023-04-27 12:30:37 -0600

    command-specific user metadata DAT-12374 (#4181)

--- liquibase-standard/src/main/java/liquibase/command/CommandScope.java
+++ liquibase-standard/src/main/java/liquibase/command/CommandScope.java
@@ -287,11 +287,13 @@ public class CommandScope {
     }
 
     /**
-     * Returns a string of the entire defined command names, joined together with spaces
+     * Returns a string of the entire defined command names, joined together with the specified delimiter.
      * @param commandStep the command step to get the name of
-     * @return the full command step name definition delimited by spaces or an empty string if there are no defined command names
+     * @param delimiter the delimiter to use for joining the command names
+     * @return the full command step name definition delimited by the specified delimiter or an empty string if there are
+     * no defined command names
      */
-    private String getCommandStepName(CommandStep commandStep) {
+    public static String getCommandStepName(CommandStep commandStep, String delimiter) {
         StringBuilder commandStepName = new StringBuilder();
         String[][] commandDefinition = commandStep.defineCommandNames();
         if (commandDefinition != null) {
@@ -299,10 +301,18 @@ public class CommandScope {
                 if (commandStepName.length() != 0) {
                     commandStepName.append(" ");
                 }
-                commandStepName.append(String.join(" ", commandNames));
+                commandStepName.append(String.join(delimiter, commandNames));
             }
         }
         return commandStepName.toString();
+    }
+    /**
+     * Returns a string of the entire defined command names, joined together with spaces
+     * @param commandStep the command step to get the name of
+     * @return the full command step name definition delimited by spaces or an empty string if there are no defined command names
+     */
+    public static String getCommandStepName(CommandStep commandStep) {
+        return getCommandStepName(commandStep, " ");
     }
 
     /**
