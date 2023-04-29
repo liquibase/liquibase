ac55bf2f426b06feb78387e0ca4f5ba9815653bc
Mark Chesney <mches@users.noreply.github.com>
2023-04-29 15:14:59 -0700

    Establish baseline specs

--- liquibase-standard/src/test/groovy/liquibase/database/core/PostgresDatabaseSpec.groovy
+++ liquibase-standard/src/test/groovy/liquibase/database/core/PostgresDatabaseSpec.groovy
@@ -1,22 +1,18 @@
 package liquibase.database.core
 
-import liquibase.database.jvm.JdbcConnection
 import liquibase.structure.core.Column
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import java.sql.ResultSet
-import java.sql.Statement
-
 import static liquibase.database.ObjectQuotingStrategy.QUOTE_ALL_OBJECTS
 import static liquibase.database.ObjectQuotingStrategy.QUOTE_ONLY_RESERVED_WORDS
 
-class EnterpriseDBDatabaseTest extends Specification {
+class PostgresDatabaseSpec extends Specification {
 
     @Unroll("#featureName [#quotingStrategy], [#objectName, #objectType], [#expectedCorrect, #expectedEscape]")
     def "correctObjectName, escapeObjectName"() {
         given:
-        def database = new EnterpriseDBDatabase().tap {
+        def database = new PostgresDatabase().tap {
             if (quotingStrategy != null) {
                 it.objectQuotingStrategy = quotingStrategy
             }
@@ -34,36 +30,19 @@ class EnterpriseDBDatabaseTest extends Specification {
         null                      || 'COL1'           | Column     || 'col1'           | 'COL1'
         null                      || 'Col1'           | Column     || 'Col1'           | '"Col1"'
         null                      || 'col with space' | Column     || 'col with space' | '"col with space"'
+        null                      || 'COL WITH SPACE' | Column     || 'col with space' | '"COL WITH SPACE"'
+        null                      || 'Col with space' | Column     || 'Col with space' | '"Col with space"'
         QUOTE_ONLY_RESERVED_WORDS || 'col1'           | Column     || 'col1'           | 'col1'
         QUOTE_ONLY_RESERVED_WORDS || 'COL1'           | Column     || 'col1'           | 'COL1'
         QUOTE_ONLY_RESERVED_WORDS || 'Col1'           | Column     || 'col1'           | 'Col1'
         QUOTE_ONLY_RESERVED_WORDS || 'col with space' | Column     || 'col with space' | '"col with space"'
+        QUOTE_ONLY_RESERVED_WORDS || 'COL WITH SPACE' | Column     || 'col with space' | '"COL WITH SPACE"'
+        QUOTE_ONLY_RESERVED_WORDS || 'Col with space' | Column     || 'col with space' | '"Col with space"'
         QUOTE_ALL_OBJECTS         || 'col1'           | Column     || 'col1'           | '"col1"'
         QUOTE_ALL_OBJECTS         || 'COL1'           | Column     || 'COL1'           | '"COL1"'
         QUOTE_ALL_OBJECTS         || 'Col1'           | Column     || 'Col1'           | '"Col1"'
         QUOTE_ALL_OBJECTS         || 'col with space' | Column     || 'col with space' | '"col with space"'
-    }
-
-    @Unroll
-    def "isCorrectDatabaseImplementation"() {
-        when:
-        def conn = Mock(JdbcConnection)
-        def stmt = Mock(Statement)
-        def rs = Mock(ResultSet)
-
-        conn.getURL() >> url
-        conn.createStatement() >> stmt
-        stmt.executeQuery("select version()") >> rs
-        rs.next() >> true
-        rs.getObject(1) >> version
-
-        then:
-        new EnterpriseDBDatabase().isCorrectDatabaseImplementation(conn) == expected
-
-        where:
-        url                                   | version                                                                                                                                         | expected
-        "jdbc:edb://localhost:50000/db"       | "PostgreSQL 11.0 (EnterpriseDB Advanced Server 11.0.0) on x86_64-pc-linux-gnu, compiled by gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-11), 64-bit" | true
-        "jdbc:postgresql://localhost:5432/db" | "PostgreSQL 11.0 (EnterpriseDB Advanced Server 11.0.0) on x86_64-pc-linux-gnu, compiled by gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-11), 64-bit" | true
-        "jdbc:edb://localhost:50000/db"       | "PostgreSQL 9.3.10 on x86_64-unknown-linux-gnu, compiled by gcc (Ubuntu 4.8.2-19ubuntu1) 4.8.2, 64-bit"                                         | false
+        QUOTE_ALL_OBJECTS         || 'COL WITH SPACE' | Column     || 'COL WITH SPACE' | '"COL WITH SPACE"'
+        QUOTE_ALL_OBJECTS         || 'Col with space' | Column     || 'Col with space' | '"Col with space"'
     }
 }
