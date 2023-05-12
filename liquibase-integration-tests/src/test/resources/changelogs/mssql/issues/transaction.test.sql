52f4c2b0a6a117c4e5bcecefa96f026f1c60e1a8
Daniel Mallorga <dmallorga@liquibase.com>
2023-05-12 16:10:01 -0300

    Missing closing bracket added.

--- liquibase-integration-tests/src/test/resources/changelogs/mssql/issues/transaction.test.sql
+++ liquibase-integration-tests/src/test/resources/changelogs/mssql/issues/transaction.test.sql
@@ -1,12 +1,13 @@
 CREATE OR ALTER PROCEDURE dbo.Test as
 BEGIN TRAN
-CREATE TABLE test.dbo.person (
-                                 id int primary key not null,
-                                 name varchar(50) not null,
-                                 address1 varchar(50),
-                                 address2 varchar(50),
-                                 city varchar(30)
-
+CREATE TABLE test.dbo.person
+(
+    id       int primary key not null,
+    name     varchar(50)     not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city     varchar(30)
+);
 INSERT INTO test.dbo.person(id, name)
 VALUES(1, 'Name1');
 COMMIT
