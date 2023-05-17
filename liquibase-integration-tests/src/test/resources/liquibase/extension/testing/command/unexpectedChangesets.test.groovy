ef0aee92ec4a90fb388defc410a79ef797f99431
Steven Massaro <steven.massaro.web@gmail.com>
2023-05-17 12:20:49 -0500

    refactor unexpected changesets command

--- liquibase-integration-tests/src/test/resources/liquibase/extension/testing/command/unexpectedChangesets.test.groovy
+++ liquibase-integration-tests/src/test/resources/liquibase/extension/testing/command/unexpectedChangesets.test.groovy
@@ -8,11 +8,11 @@ CommandTests.define {
 Short Description: Generate a list of changesets that have been executed but are not in the current changelog
 Long Description: NOT SET
 Required Args:
-  changelogFile (String) The root changelog
+  changelogFile (String) The root changelog file
   url (String) The JDBC database connection URL
     OBFUSCATED
 Optional Args:
-  contexts (String) Changeset contexts to match
+  contexts (String) Context string to use for filtering
     Default: null
   defaultCatalogName (String) The default catalog name to use for the database connection
     Default: null
@@ -21,6 +21,8 @@ Optional Args:
   driver (String) The JDBC driver class
     Default: null
   driverPropertiesFile (String) The JDBC driver properties file
+    Default: null
+  labelFilter (String) Label expression to use for filtering
     Default: null
   password (String) Password to use to connect to the database
     Default: null
@@ -43,10 +45,6 @@ Optional Args:
         setup {
             syncChangelog "changelogs/h2/complete/rollback.tag.changelog.xml"
         }
-
-        expectedResults = [
-                statusCode   : 0
-        ]
     }
 
     run "Run without a URL should throw an exception",  {
