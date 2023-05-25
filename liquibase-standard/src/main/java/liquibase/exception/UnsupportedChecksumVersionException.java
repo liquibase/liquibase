d4e3a992ca16c1a56a012ed9b09842128ec850ae
filipe <flautert@liquibase.org>
2023-05-25 09:39:02 -0300

    Make error message simpler.

--- liquibase-standard/src/main/java/liquibase/exception/UnsupportedChecksumVersionException.java
+++ liquibase-standard/src/main/java/liquibase/exception/UnsupportedChecksumVersionException.java
@@ -7,9 +7,8 @@ public class UnsupportedChecksumVersionException extends RuntimeException {
     private static final long serialVersionUID = -229400973681987065L;
 
     public UnsupportedChecksumVersionException(int i) {
-        super(String.format("Liquibase detected an unknown checksum calculation version %s in the DatabaseChangelog table. " +
-                "This can be caused by a previous operation on this changelog with a newer version than your current %S release. " +
-                "Please update your current Liquibase, or clear checksums on changesets you are certain you need " +
-                "to deploy.", i, LiquibaseUtil.getBuildVersionInfo()));
+        super(String.format("Liquibase detected an unknown checksum calculation version %s. This can be caused by a previous " +
+                        "operation on this changelog with a newer version than your current %S release. ",
+                 i, LiquibaseUtil.getBuildVersionInfo()));
     }
 }
