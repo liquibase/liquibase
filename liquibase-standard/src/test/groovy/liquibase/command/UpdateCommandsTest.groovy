6141c67791e939890459b07b44d1b6bd318d91a6
Steven Massaro <steven.massaro.web@gmail.com>
2023-06-01 11:07:24 -0500

    change

--- liquibase-standard/src/test/groovy/liquibase/command/UpdateCommandsTest.groovy
+++ liquibase-standard/src/test/groovy/liquibase/command/UpdateCommandsTest.groovy
@@ -54,6 +54,7 @@ class UpdateCommandsTest extends Specification {
     }
 
     @Ignore
+
     def "run Update from CommandStep"() {
         when:
         def updateCommand = new CommandScope(UpdateCommandStep.COMMAND_NAME)
