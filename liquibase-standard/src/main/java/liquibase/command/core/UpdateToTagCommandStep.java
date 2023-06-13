18d85df258a7189bc17416e0ceafec702a3f195b
filipe <flautert@liquibase.org>
2023-06-13 10:36:50 -0300

    Fix update summary.

--- liquibase-standard/src/main/java/liquibase/command/core/UpdateToTagCommandStep.java
+++ liquibase-standard/src/main/java/liquibase/command/core/UpdateToTagCommandStep.java
@@ -90,6 +90,7 @@ public class UpdateToTagCommandStep extends AbstractUpdateCommandStep {
         List<RanChangeSet> ranChangeSetList = database.getRanChangeSetList();
         String tag = commandScope.getArgumentValue(TAG_ARG);
         return new StatusChangeLogIterator(changeLog, tag,
+                new ShouldRunChangeSetFilter(database),
                 new ContextChangeSetFilter(contexts),
                 new LabelChangeSetFilter(labelExpression),
                 new DbmsChangeSetFilter(database),
