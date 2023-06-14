3b68a6a8e43dc36299f7914b50d1574314aa0fca
jandroav <jandroav@icloud.com>
2023-06-14 10:58:18 +0200

    refactor(liquibase.sh): change shebang to use /bin/sh instead of /usr/bin/env bash feat(liquibase.sh): add LIQUIBASE_HOME to PATH to make liquibase executable available system-wide

--- liquibase-dist/src/main/archive/liquibase.sh
+++ liquibase-dist/src/main/archive/liquibase.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-# To be used by jdeb to create a debian package
-# and place it under /etc/profile.d/ to be sourced
+#!/bin/sh
+
 export LIQUIBASE_HOME=/opt/liquibase
+export PATH=$PATH:$LIQUIBASE_HOME
