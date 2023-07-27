5ff4b4c87413079503b60e658efd294233fc7c74
Steven Massaro <steven.massaro.web@gmail.com>
2023-07-27 10:28:07 -0500

    test build

--- liquibase-standard/src/main/java/liquibase/change/AbstractChange.java
+++ liquibase-standard/src/main/java/liquibase/change/AbstractChange.java
@@ -39,7 +39,9 @@ import static liquibase.statement.SqlStatement.EMPTY_SQL_STATEMENT;
  * Place the @DatabaseChangeProperty annotations on the read "get" methods to control property metadata.
  */
 public abstract class AbstractChange extends AbstractPlugin implements Change {
-
+/*
+build
+ */
     protected static final String NODENAME_COLUMN = "column";
 
     private ChangeSet changeSet;
