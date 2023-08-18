5cbd2db895f85e8c70ee6690fd2019edc1f83b48
Marcq <Julien.Marcq@Exxeta.com>
2023-08-18 10:22:25 +0200

    ISSUE-3814: Added possibility to pass single or multiple parameters to calculdate checksum command

--- liquibase-standard/src/main/java/liquibase/command/core/CalculateChecksumCommandStep.java
+++ liquibase-standard/src/main/java/liquibase/command/core/CalculateChecksumCommandStep.java
@@ -41,6 +41,10 @@ public class CalculateChecksumCommandStep extends AbstractCommandStep {
 
     public static final CommandArgumentDefinition<String> CHANGESET_IDENTIFIER_ARG;
 
+    private static final int CHANGESET_IDENTIFIER_PARTS_LENGTH = 3;
+    private static final int CHANGESET_IDENTIFIER_AUTHOR_PART = 2;
+    private static final int CHANGESET_IDENTIFIER_ID_PART = 1;
+    private static final int CHANGESET_IDENTIFIER_PATH_PART = 0;
 
     static {
         CommandBuilder builder = new CommandBuilder(COMMAND_NAME);
@@ -48,23 +52,19 @@ public class CalculateChecksumCommandStep extends AbstractCommandStep {
                                     .description("The root changelog file").build();
 
         CHANGESET_IDENTIFIER_ARG = builder.argument("changeSetIdentifier", String.class)
-                                    .required()
-                                    .description("Changeset ID identifier of form filepath::id::author")
+                                    .description("ChangeSet ID identifier of form filepath::id::author")
                                     .build();
 
         CHANGESET_PATH_ARG = builder.argument("changeSetPath", String.class)
-                                    .required()
-                                    .description("Changelog path in which the changeset is included")
+                                    .description("Changelog path in which the changeSet is included")
                                     .build();
 
         CHANGESET_ID_ARG = builder.argument("changesetId", String.class)
-                                  .required()
-                                  .description("Changeset ID attribute")
+                                  .description("ChangeSet ID attribute")
                                   .build();
 
         CHANGESET_AUTHOR_ARG = builder.argument("changeSetAuthor", String.class)
-                                      .required()
-                                      .description("Changeset Author attribute")
+                                      .description("ChangeSet Author attribute")
                                       .build();
 
         CHECKSUM_RESULT = builder.result("checksumResult", CheckSum.class).description("Calculated checksum").build();
@@ -83,10 +83,26 @@ public class CalculateChecksumCommandStep extends AbstractCommandStep {
     @Override
     public void run(CommandResultsBuilder resultsBuilder) throws Exception {
         CommandScope commandScope = resultsBuilder.getCommandScope();
-        final String changeSetPath = commandScope.getArgumentValue(CHANGESET_PATH_ARG);
-        final String changeSetId = commandScope.getArgumentValue(CHANGESET_ID_ARG);
-        final String changeSetAuthor = commandScope.getArgumentValue(CHANGESET_AUTHOR_ARG);
+
+        final String changeSetIdentifier = commandScope.getArgumentValue(CHANGESET_IDENTIFIER_ARG);
         final String changeLogFile = commandScope.getArgumentValue(CHANGELOG_FILE_ARG).replace('\\', '/');
+        String changeSetPath;
+        String changeSetId;
+        String changeSetAuthor;
+
+        Boolean isChangeSetIdentifierPassed = changeSetIdentifier != null;
+
+        if (isChangeSetIdentifierPassed) {
+            List<String> parts = validateAndExtractParts(changeSetIdentifier, changeLogFile);
+            changeSetPath = parts.get(CHANGESET_IDENTIFIER_PATH_PART);
+            changeSetId = parts.get(CHANGESET_IDENTIFIER_ID_PART);
+            changeSetAuthor = parts.get(CHANGESET_IDENTIFIER_AUTHOR_PART);
+        } else {
+            changeSetPath = commandScope.getArgumentValue(CHANGESET_PATH_ARG);
+            changeSetId = commandScope.getArgumentValue(CHANGESET_ID_ARG);
+            changeSetAuthor = commandScope.getArgumentValue(CHANGESET_AUTHOR_ARG);
+        }
+
         final Database database = (Database) commandScope.getDependency(Database.class);
 
         Scope.getCurrentScope().getLog(getClass()).info(String.format("Calculating checksum for changeset identified by changeset id: %s, author: %s, path: %s",
@@ -118,6 +134,24 @@ public class CalculateChecksumCommandStep extends AbstractCommandStep {
         );
     }
 
+    private List<String> validateAndExtractParts(String changeSetIdentifier, String changeLogFile) throws LiquibaseException {
+        if (StringUtil.isEmpty(changeSetIdentifier)) {
+            throw new LiquibaseException(new IllegalArgumentException(CHANGESET_IDENTIFIER_ARG.getName()));
+        }
+
+        if (StringUtil.isEmpty(changeLogFile)) {
+            throw new LiquibaseException(new IllegalArgumentException(CHANGELOG_FILE_ARG.getName()));
+        }
+
+        final List<String> parts = StringUtil.splitAndTrim(changeSetIdentifier, "::");
+        if ((parts == null) || (parts.size() < CHANGESET_IDENTIFIER_PARTS_LENGTH)) {
+            throw new LiquibaseException(
+                    new IllegalArgumentException("Invalid changeSet identifier: " + changeSetIdentifier)
+            );
+        }
+        return parts;
+    }
+
     private static void sendMessages(CommandResultsBuilder resultsBuilder, CheckSum checkSum) {
         resultsBuilder.addResult(CHECKSUM_RESULT, checkSum);
         Scope.getCurrentScope().getUI().sendMessage(checkSum.toString());
@@ -128,5 +162,14 @@ public class CalculateChecksumCommandStep extends AbstractCommandStep {
         commandDefinition.setShortDescription("Calculates and prints a checksum for the changeset");
         commandDefinition.setLongDescription(
                 "Calculates and prints a checksum for the changeset with the given id in the format filepath::id::author");
+        commandDefinition.setHelpFooter("Calculate checksum provides two ways to identify a changeset.\n\n" +
+                                        "1. Composite changeset identifier\n\n" +
+                                        "The composite changeset identifier must be passed in the following pattern myPath::myId::myAuthor\n" +
+                                        "liquibase calculateCheckSum --changeSetIdentifier myFile::myId::myAuthor\n\n" +
+                                        "2. Individual changeset parameters\n\n" +
+                                        "The second option requires all three parameters to be defined\n" +
+                                        "This variant offers some more flexibility in naming conventions for path, id and author\n"+
+                                        "liquibase calculateCheckSum --changesetId myId --changesetAuthor myAuthor --changesetPath myPath\n"
+        );
     }
 }
