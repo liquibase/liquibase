b31ab38c9188f7a95582eac0cfa1b94dd1535887
Daniel Mallorga <dmallorga@liquibase.com>
2023-08-18 16:41:27 -0300

    - Fixed calcula-checksum command test. - Minor text updates changing changeset to changeSet.

--- liquibase-integration-tests/src/test/resources/liquibase/extension/testing/command/calculateChecksum.test.groovy
+++ liquibase-integration-tests/src/test/resources/liquibase/extension/testing/command/calculateChecksum.test.groovy
@@ -11,10 +11,17 @@ Short Description: Calculates and prints a checksum for the changeset
 Long Description: Calculates and prints a checksum for the changeset with the given id in the format filepath::id::author
 Required Args:
   changelogFile (String) The root changelog file
-  changesetIdentifier (String) Changeset ID identifier of form filepath::id::author
   url (String) The JDBC database connection URL
     OBFUSCATED
 Optional Args:
+  changeSetAuthor (String) ChangeSet Author attribute
+    Default: null
+  changeSetId (String) ChangeSet ID attribute
+    Default: null
+  changeSetIdentifier (String) ChangeSet identifier of form filepath::id::author
+    Default: null
+  changeSetPath (String) Changelog path in which the changeSet is included
+    Default: null
   defaultCatalogName (String) The default catalog name to use for the database connection
     Default: null
   defaultSchemaName (String) The default schema name to use for the database connection
@@ -30,12 +37,28 @@ Optional Args:
     Default: null
 """
 
-    run "Happy path", {
+    run "Happy path using changeSetIdentifier", {
         arguments = [
                 url              : { it.altUrl },
                 username         : { it.altUsername },
                 password         : { it.altPassword },
-                changesetIdentifier: "changelogs/h2/complete/rollback.tag.changelog.xml::1::nvoxland",
+                changeSetIdentifier: "changelogs/h2/complete/rollback.tag.changelog.xml::1::nvoxland",
+                changelogFile    : "changelogs/h2/complete/rollback.tag.changelog.xml"
+        ]
+
+        expectedResults = [
+                checksumResult   : "9:10de8cd690aed1d88d837cbe555d1684"
+        ]
+    }
+
+    run "Happy path using changeSetPath, ChangeSetId and ChangeSetPath", {
+        arguments = [
+                url              : { it.altUrl },
+                username         : { it.altUsername },
+                password         : { it.altPassword },
+                changeSetPath    : "changelogs/h2/complete/rollback.tag.changelog.xml",
+                changeSetId      : "1",
+                changeSetAuthor  : "nvoxland",
                 changelogFile    : "changelogs/h2/complete/rollback.tag.changelog.xml"
         ]
 
@@ -46,27 +69,18 @@ Optional Args:
 
     run "Run without changelogFile should throw an exception",  {
         arguments = [
-                changesetIdentifier: "changelogs/h2/complete/rollback.tag.changelog.xml::1::nvoxland",
+                changeSetIdentifier: "changelogs/h2/complete/rollback.tag.changelog.xml::1::nvoxland",
         ]
 
         expectedException = CommandValidationException.class
         expectedExceptionMessage = 'Invalid argument \'changelogFile\': missing required argument'
     }
 
-    run "Run without changesetIdentifier should throw an exception",  {
-        arguments = [
-                changelogFile    : "changelogs/h2/complete/rollback.tag.changelog.xml"
-        ]
-
-        expectedException = CommandValidationException.class
-        expectedExceptionMessage = "Invalid argument \'changesetIdentifier\': missing required argument"
-    }
-
     run "Run without URL should throw an exception",  {
         arguments = [
                 url: "",
                 changelogFile    : "changelogs/h2/complete/rollback.tag.changelog.xml",
-                changesetIdentifier: "changelogs/h2/complete/rollback.tag.changelog.xml::1::nvoxland",
+                changeSetIdentifier: "changelogs/h2/complete/rollback.tag.changelog.xml::1::nvoxland",
         ]
 
         expectedException = CommandValidationException.class
