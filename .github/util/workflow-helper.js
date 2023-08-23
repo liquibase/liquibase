4cb163463a55815ff9230507c8869abdbe81bbd2
jandroav <jandroav@icloud.com>
2023-08-23 13:29:26 +0200

    chore(workflow-helper.js): fix console log message to display the correct debug information

--- .github/util/workflow-helper.js
+++ .github/util/workflow-helper.js
@@ -48,8 +48,8 @@ module.exports = ({github, context}) => {
                 return this.cleanBranchRef(context.payload.after);
             } else {
                 // Assuming context.payload.ref is the current branch SHA when context.payload.after is undefined
-                console.log("debug context.payload.ref:" + context.payload)
-                return this.cleanBranchRef(context.payload.ref);
+                console.log("debug context.payload.sha:" + context.payload.sha)
+                return context.payload.sha;
             }
         },
 
