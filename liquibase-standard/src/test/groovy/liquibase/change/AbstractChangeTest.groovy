2fef5803969a0fdbc45b2aa7d98d11bf46a9f9c9
Mark Chesney <mches@users.noreply.github.com>
2023-08-25 06:25:53 -0700

    Fix H2 database case-sensitive identifier issues + upgrades JUnit tests cases to version 5 (#4052)
    * Utilize JUnit 5 BOM
    
    * Fix whitespace errors
    
    * Convert JUnit 4 database tests to JUnit 5
    
    * Upgrade to Groovy 2.5
    
    * Execute tests ending "Spec" with Maven
    
    * Establish baseline specs
    
    * Use former reserved words list on H2 1.x
    
    * Fold or preserve identifier case based on quoting strategy on H2
    
    * Fix unread local variable
    
    * Manage Groovy, Spock transitive dependency versions

--- liquibase-standard/src/test/groovy/liquibase/change/AbstractChangeTest.groovy
+++ liquibase-standard/src/test/groovy/liquibase/change/AbstractChangeTest.groovy
@@ -78,7 +78,7 @@ class AbstractChangeTest extends Specification {
 
     def createChangeMetaData() {
         when:
-        ExampleAbstractChange change = new ExampleAbstractChange()
+        def change = new ExampleAbstractChange()
         ChangeMetaData changeMetaData = change.createChangeMetaData()
         Map<String, ChangeParameterMetaData> parameters = changeMetaData.getParameters()
 
@@ -120,7 +120,7 @@ class AbstractChangeTest extends Specification {
 
     def createChangeMetaData_noParams() {
         when:
-        ExampleParamlessAbstractChange change = new ExampleParamlessAbstractChange()
+        def change = new ExampleParamlessAbstractChange()
         ChangeMetaData changeMetaData = change.createChangeMetaData()
         Map<String, ChangeParameterMetaData> parameters = changeMetaData.getParameters()
 
@@ -144,7 +144,7 @@ class AbstractChangeTest extends Specification {
 
     def getChangeSet() {
         when:
-        ExampleAbstractChange change = new ExampleAbstractChange()
+        def change = new ExampleAbstractChange()
         ChangeSet changeSet = new ChangeSet(new DatabaseChangeLog())
         change.setChangeSet(changeSet)
 
@@ -249,7 +249,7 @@ class AbstractChangeTest extends Specification {
 
     def validate_missingRequiredValue() throws Exception {
         when:
-        ExampleAbstractChange change = new ExampleAbstractChange()
+        def change = new ExampleAbstractChange()
         ValidationErrors errors = change.validate(new MSSQLDatabase())
         then:
 
@@ -259,7 +259,7 @@ class AbstractChangeTest extends Specification {
 
     def validate_noErrors() throws Exception {
         when:
-        ExampleAbstractChange change = new ExampleAbstractChange()
+        def change = new ExampleAbstractChange()
         change.setParamTwo(3)
 
         then:
