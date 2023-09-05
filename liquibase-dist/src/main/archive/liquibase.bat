4a8e22330f3d51a0e573ace55f133236285584b0
filipe <flautert@liquibase.org>
2023-09-05 16:37:14 -0300

    Fix liquibase.bat java home handling under Windows (#4845)
    * Revert "Fix liquibase 4.23.1 won't work on windows if there is no JAVA_HOME system variable set (#4803)"
    
    This reverts commit 7e709fe1c54906e31fb39ff80d46d2e8b9d184ae.
    
    * Revert "Fixes #3625 JAVA_HOME Path when parenthesis are present (#4306)"
    
    This reverts commit 202c0a87f084de2f646656c054df151d94e94174.

--- liquibase-dist/src/main/archive/liquibase.bat
+++ liquibase-dist/src/main/archive/liquibase.bat
@@ -9,13 +9,13 @@ set LIQUIBASE_HOME="%~dp0"
 rem remove quotes around LIQUIBASE_HOME
 set LIQUIBASE_HOME=%LIQUIBASE_HOME:"=%
 
-rem set JAVA_HOME to local jre dir if not set
-if exist "%LIQUIBASE_HOME%\jre" if "%JAVA_HOME%"=="" (
-    set JAVA_HOME="%LIQUIBASE_HOME%\jre"
-)
-
 rem remove quotes around JAVA_HOME if set
 if NOT "%JAVA_HOME%" == "" set JAVA_HOME=%JAVA_HOME:"=%
+
+rem set JAVA_HOME to local jre dir if not set
+if exist "%LIQUIBASE_HOME%\jre" if "%JAVA_HOME%"=="" (
+    set JAVA_HOME=%LIQUIBASE_HOME%\jre
+)
 
 if NOT "%JAVA_HOME%" == "" if not exist "%JAVA_HOME%" (
   echo ERROR: The JAVA_HOME environment variable is not defined correctly, so Liquibase cannot be started. JAVA_HOME is set to "%JAVA_HOME%" and it does not exist. >&2
