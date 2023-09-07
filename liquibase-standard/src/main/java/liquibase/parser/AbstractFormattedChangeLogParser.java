0a548cba806d5481ccbd3c9de7601b0afe0eac1f
Wesley Willard <wwillard@datical.com>
2023-09-07 17:18:34 -0500

    DAT-15865
    Fix regression in recognizing formatted SQL file

--- liquibase-standard/src/main/java/liquibase/parser/AbstractFormattedChangeLogParser.java
+++ liquibase-standard/src/main/java/liquibase/parser/AbstractFormattedChangeLogParser.java
@@ -220,16 +220,16 @@ public abstract class AbstractFormattedChangeLogParser implements ChangeLogParse
 
                 String firstLine = reader.readLine();
 
+                while (firstLine != null && firstLine.trim().isEmpty() && reader.ready()) {
+                    firstLine = reader.readLine();
+                }
+
                 //
                 // Handle empty files with a WARNING message
                 //
                 if (StringUtil.isEmpty(firstLine)) {
                     Scope.getCurrentScope().getLog(getClass()).warning(String.format("Skipping empty file '%s'", changeLogFile));
                     return false;
-                }
-
-                while (firstLine.trim().isEmpty() && reader.ready()) {
-                    firstLine = reader.readLine();
                 }
                 return FIRST_LINE_PATTERN.matcher(firstLine).matches();
             } else {
