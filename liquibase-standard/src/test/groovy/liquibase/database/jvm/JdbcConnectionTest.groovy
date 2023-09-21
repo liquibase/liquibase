05069a12d7b6904185e9848629287dea950d4043
Wesley Willard <wwillard@datical.com>
2023-09-21 13:03:19 -0500

    Filter additional credential-like strings in the URL sanitization process
    DAT-9973

--- liquibase-standard/src/test/groovy/liquibase/database/jvm/JdbcConnectionTest.groovy
+++ liquibase-standard/src/test/groovy/liquibase/database/jvm/JdbcConnectionTest.groovy
@@ -72,6 +72,7 @@ class JdbcConnectionTest extends Specification {
         "jdbc:jtds:sqlserver://localhost:1433/proCatalog;user=my_user;"                      | "jdbc:jtds:sqlserver://localhost:1433/proCatalog;user=*****;"
         "jdbc:oracle:thin:user/password@host:1521/db"                                        | "jdbc:oracle:thin:*****/*****@host:1521/db"
         "jdbc:oracle:thin:@host:1521/db"                                                     | "jdbc:oracle:thin:@host:1521/db"
+        "cosmosdb://maincosmosliquibase.documents.azure.com:t27yJICDSFdR1HN==@maincosmosliquibase.documents.azure.com:443/testdb1" | "cosmosdb://maincosmosliquibase.documents.azure.com:*****@maincosmosliquibase.documents.azure.com:443/testdb1"
         null                                                                                 | null
     }
 }
