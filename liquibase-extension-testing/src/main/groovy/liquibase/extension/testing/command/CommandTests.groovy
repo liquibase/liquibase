8ffd4051dd62621d0521c7734615ede7ae083997
Steven Massaro <steven.massaro.web@gmail.com>
2023-09-22 09:18:25 -0600

    command tests improvements (DAT-15555) (#4921)
    initial add of checks create command, still needs to work on comma separated packages
    
    Co-authored-by: obovsunivskyii <baqaua@gmail.com>

--- liquibase-extension-testing/src/main/groovy/liquibase/extension/testing/command/CommandTests.groovy
+++ liquibase-extension-testing/src/main/groovy/liquibase/extension/testing/command/CommandTests.groovy
@@ -688,7 +688,7 @@ Long Description: ${commandDefinition.getLongDescription() ?: "NOT SET"}
                 throw new RuntimeException("${message}.\n\n!!------------- TEST EXECUTION FAILURE -------------!!\n" +
                         "\nIf you are running CommandTests directly through your IDE, make sure you are including the module with your 'test.groovy' files in your classpath.\n" +
                         "\nNOTE: For example, if you are running these tests in liquibase-core, use the liquibase-integration-tests module as the classpath in your run configuration.\n" +
-                        "\n!!--------------------------------------------------!!")
+                        "\n!!--------------------------------------------------!!", e)
 
             }
         }
