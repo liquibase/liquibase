47519d8bff69a68ba8b6c8d23bb6d7e525ea084d
filipe <flautert@liquibase.com>
2023-10-12 10:26:08 -0300

    Code refactoring splitting huge main method.

--- liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseLauncher.java
+++ liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseLauncher.java
@@ -3,18 +3,17 @@ package liquibase.integration.commandline;
 import liquibase.Scope;
 import liquibase.util.StringUtil;
 
-import static liquibase.integration.commandline.LiquibaseLauncherSettings.LiquibaseLauncherSetting.LIQUIBASE_HOME;
-import static liquibase.integration.commandline.LiquibaseLauncherSettings.LiquibaseLauncherSetting.LIQUIBASE_LAUNCHER_DEBUG;
-import static liquibase.integration.commandline.LiquibaseLauncherSettings.LiquibaseLauncherSetting.LIQUIBASE_LAUNCHER_PARENT_CLASSLOADER;
-import static liquibase.integration.commandline.LiquibaseLauncherSettings.getSetting;
-
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static liquibase.integration.commandline.LiquibaseLauncherSettings.LiquibaseLauncherSetting.*;
+import static liquibase.integration.commandline.LiquibaseLauncherSettings.getSetting;
 
 /**
  * Launcher which builds up the classpath needed to run Liquibase, then calls {@link LiquibaseCommandLine#main(String[])}.
@@ -90,6 +89,92 @@ public class LiquibaseLauncher {
         }
         File liquibaseHome = new File(liquibaseHomeEnv);
 
+        List<URL> libUrls = getLibUrls(liquibaseHome);
+        checkForDuplicatedJars(libUrls);
+
+        if (debug) {
+            debug("Final Classpath:");
+            for (URL url : libUrls) {
+                debug("  " + url.toString());
+            }
+        }
+
+        ClassLoader parentLoader = getClassLoader(parentLoaderSetting);
+
+        final URLClassLoader classloader = new URLClassLoader(libUrls.toArray(new URL[0]), parentLoader);
+        Thread.currentThread().setContextClassLoader(classloader);
+
+        Class<?> cli = null;
+        try {
+            cli = classloader.loadClass(LiquibaseCommandLine.class.getName());
+        } catch (ClassNotFoundException classNotFoundException) {
+            throw new RuntimeException(
+                String.format("Unable to find Liquibase classes in the configured home: '%s'.", liquibaseHome)
+            );
+        }
+
+        cli.getMethod("main", String[].class).invoke(null, new Object[]{args});
+    }
+
+    private static ClassLoader getClassLoader(String parentLoaderSetting) {
+        if (parentLoaderSetting.equalsIgnoreCase("system")) {
+            //loading with the regular system classloader includes liquibase.jar in the parent.
+            //That causes the parent classloader to load LiquibaseCommandLine which makes it not able to access files in the child classloader
+            //The system classloader's parent is the boot classloader, which keeps the only classloader with liquibase-core.jar the same as the rest of the classes it needs to access.
+            return ClassLoader.getSystemClassLoader().getParent();
+
+        } else if (parentLoaderSetting.equalsIgnoreCase("thread")) {
+            return Thread.currentThread().getContextClassLoader();
+        } else {
+            throw new RuntimeException("Unknown liquibase launcher parent classloader value: "+ parentLoaderSetting);
+        }
+    }
+
+    /**
+     * Check for duplicate core and commercial JAR files
+     */
+    private static void checkForDuplicatedJars(List<URL> urls) {
+        List<String> duplicateCore =
+                urls
+                  .stream()
+                  .map(URL::getFile)
+                  .filter(file -> file.matches(LIQUIBASE_CORE_JAR_PATTERN))
+                  .collect(Collectors.toList());
+        List<String> duplicateCommercial =
+                urls
+                  .stream()
+                  .map(URL::getFile)
+                  .filter(file -> file.matches(LIQUIBASE_COMMERCIAL_JAR_PATTERN))
+                  .collect(Collectors.toList());
+        if (duplicateCore.size() > 1) {
+            buildDupsMessage(duplicateCore, LIQUIBASE_CORE_MESSAGE);
+        }
+        if (duplicateCommercial.size() > 1) {
+            buildDupsMessage(duplicateCommercial, LIQUIBASE_COMMERCIAL_MESSAGE);
+        }
+        Pattern versionedJarPattern = Pattern.compile(DEPENDENCY_JAR_PATTERN);
+        Map<String, List<String>> duplicates = new LinkedHashMap<>();
+        urls
+          .forEach(url -> {
+              Matcher m = versionedJarPattern.matcher(new File(url.getFile()).getName());
+              if (m.find()) {
+                  String key = m.group(1);
+                  List<String> dupEntries = duplicates.get(key);
+                  if (dupEntries == null) {
+                      dupEntries = new ArrayList<>();
+                  }
+                  dupEntries.add(url.getFile());
+                  duplicates.put(key, dupEntries);
+              }
+          });
+        duplicates.forEach((key, value) -> {
+            if (value.size() > 1) {
+                buildDupsMessage(value, key);
+            }
+        });
+    }
+
+    private static List<URL> getLibUrls(File liquibaseHome) throws MalformedURLException {
         List<URL> urls = new ArrayList<>();
         urls.add(new File(liquibaseHome, "internal/lib/liquibase-core.jar").toURI().toURL()); //make sure liquibase-core.jar is first in the list
 
@@ -131,83 +216,7 @@ public class LiquibaseLauncher {
                 debug("Error adding " + libDirFile.getAbsolutePath() + ":" + e.getMessage(), e);
             }
         }
-
-        //
-        // Check for duplicate core and commercial JAR files
-        //
-
-        List<String> duplicateCore =
-                urls
-                  .stream()
-                  .map(URL::getFile)
-                  .filter(file -> file.matches(LIQUIBASE_CORE_JAR_PATTERN))
-                  .collect(Collectors.toList());
-        List<String> duplicateCommercial =
-                urls
-                  .stream()
-                  .map(URL::getFile)
-                  .filter(file -> file.matches(LIQUIBASE_COMMERCIAL_JAR_PATTERN))
-                  .collect(Collectors.toList());
-        if (duplicateCore.size() > 1) {
-            buildDupsMessage(duplicateCore, LIQUIBASE_CORE_MESSAGE);
-        }
-        if (duplicateCommercial.size() > 1) {
-            buildDupsMessage(duplicateCommercial, LIQUIBASE_COMMERCIAL_MESSAGE);
-        }
-        Pattern versionedJarPattern = Pattern.compile(DEPENDENCY_JAR_PATTERN);
-        Map<String, List<String>> duplicates = new LinkedHashMap<>();
-        urls
-          .forEach(url -> {
-              Matcher m = versionedJarPattern.matcher(new File(url.getFile()).getName());
-              if (m.find()) {
-                  String key = m.group(1);
-                  List<String> dupEntries = duplicates.get(key);
-                  if (dupEntries == null) {
-                      dupEntries = new ArrayList<>();
-                  }
-                  dupEntries.add(url.getFile());
-                  duplicates.put(key, dupEntries);
-              }
-          });
-        duplicates.forEach((key, value) -> {
-            if (value.size() > 1) {
-                buildDupsMessage(value, key);
-            }
-        });
-        if (debug) {
-            debug("Final Classpath:");
-            for (URL url : urls) {
-                debug("  " + url.toString());
-            }
-        }
-
-        ClassLoader parentLoader;
-        if (parentLoaderSetting.equalsIgnoreCase("system")) {
-            //loading with the regular system classloader includes liquibase.jar in the parent.
-            //That causes the parent classloader to load LiquibaseCommandLine which makes it not able to access files in the child classloader
-            //The system classloader's parent is the boot classloader, which keeps the only classloader with liquibase-core.jar the same as the rest of the classes it needs to access.
-            parentLoader = ClassLoader.getSystemClassLoader().getParent();
-
-        } else if (parentLoaderSetting.equalsIgnoreCase("thread")) {
-            parentLoader = Thread.currentThread().getContextClassLoader();
-        } else {
-            throw new RuntimeException("Unknown liquibase launcher parent classloader value: "+parentLoaderSetting);
-        }
-
-        final URLClassLoader classloader = new URLClassLoader(urls.toArray(new URL[0]), parentLoader);
-        Thread.currentThread().setContextClassLoader(classloader);
-
-        Class<?> cli = null;
-
-        try {
-            cli = classloader.loadClass(LiquibaseCommandLine.class.getName());
-        } catch (ClassNotFoundException classNotFoundException) {
-            throw new RuntimeException(
-                String.format("Unable to find Liquibase classes in the configured home: '%s'.", liquibaseHome)
-            );
-        }
-
-        cli.getMethod("main", String[].class).invoke(null, new Object[]{args});
+        return urls;
     }
 
     private static void buildDupsMessage(List<String> duplicates, String title) {
