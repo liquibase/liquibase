75bc03ea4fca5c155262a96d2ed46f0d319473b3
filipe <flautert@liquibase.com>
2023-10-16 14:51:51 -0300

    Try using test containers

--- liquibase-extension-testing/src/main/java/liquibase/extension/testing/testsystem/core/DB2TestSystem.java
+++ liquibase-extension-testing/src/main/java/liquibase/extension/testing/testsystem/core/DB2TestSystem.java
@@ -7,9 +7,8 @@ import org.jetbrains.annotations.NotNull;
 import org.testcontainers.containers.Db2Container;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.Collections;
-
 public class DB2TestSystem extends DatabaseTestSystem {
+
 
     public DB2TestSystem() {
         super("db2");
@@ -18,18 +17,39 @@ public class DB2TestSystem extends DatabaseTestSystem {
     public DB2TestSystem(Definition definition) {
         super(definition);
     }
+//
+//    public String getUrl() {
+//        return "jdbc:db2://localhost:"+db2.getMappedPort(50000)+"/testdb";
+//    }
+
 
     @SuppressWarnings("java:S2095")
     @Override
     protected @NotNull DatabaseWrapper createContainerWrapper() {
+
+//         new GenericContainer<>("ibmcom/db2")
+//                .withEnv("LICENSE", "accept")
+//                .withEnv("DB2INST1_PASSWORD", getPassword())
+//                .withEnv("DBNAME", "testdb")
+////            .withNetwork(network)
+//                .withNetworkAliases(db2NetworkAlias)
+//                .withExposedPorts(50000, 55000)
+//                //.withFileSystemBind(System.getProperty("java.io.tmpdir"), "/database")
+//                .waitingFor(new LogMessageWaitStrategy().withRegEx(".*Setup has completed.*").withStartupTimeout(Duration.ofMinutes(10)))
+//                .withCommand("db2start")
+//                .withPrivilegedMode(true)
+//                .withReuse(true); // It is necessary to have this set to true, because the stop method will stop the containers if necessary.
+
+
         return new DockerDatabaseWrapper(
-                new Db2Container(DockerImageName.parse(getImageName()).withTag("latest"))
+                new Db2Container(DockerImageName.parse(getImageName()).withTag(getVersion()))
                         .withUsername(getUsername())
                         .withPassword(getPassword())
                         .withDatabaseName(getCatalog())
-                        .withTmpFs(Collections.singletonMap("/tmp", "rw"))
-                        .withUrlParam("retrieveMessagesFromServerOnGetMessage", "true"),
-                this
+                        .acceptLicense()
+                        .withPrivilegedMode(true)
+                        .withReuse(true)
+                , this
         ) {
             @Override
             public Runnable requireLicense() {
