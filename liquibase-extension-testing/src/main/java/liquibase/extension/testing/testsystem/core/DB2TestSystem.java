63c7173211fdb71a92a5378d5842a13d716957fd
filipe <flautert@liquibase.com>
2023-10-17 15:30:16 -0300

    Rollback overhead changes.

--- liquibase-extension-testing/src/main/java/liquibase/extension/testing/testsystem/core/DB2TestSystem.java
+++ liquibase-extension-testing/src/main/java/liquibase/extension/testing/testsystem/core/DB2TestSystem.java
@@ -9,13 +9,6 @@ import org.testcontainers.utility.DockerImageName;
 
 public class DB2TestSystem extends DatabaseTestSystem {
 
-    private Db2Container db2 = new Db2Container(DockerImageName.parse(getImageName())
-            .withTag(getVersion()))
-            .withUsername(getUsername())
-            .withPassword(getPassword())
-            .withDatabaseName(getCatalog())
-            .acceptLicense();
-
     public DB2TestSystem() {
         super("db2");
     }
@@ -24,22 +17,18 @@ public class DB2TestSystem extends DatabaseTestSystem {
         super(definition);
     }
 
-    @Override
-    public void start() throws Exception {
-        if (db2.isRunning()) {
-            return;
-        }
-        // overriding db2 container start here, as using start from parent command calls exec on the docker containers
-        // and for some unknown reason db2 does not start the tcpip listener. It causes tests to fail with error:
-        // Reply.fill() - insufficient data (-1).  Message: Insuficient data. ERRORCODE=-4499, SQLSTATE=08001
-        db2.start();
-        super.start();
-    }
-
     @SuppressWarnings("java:S2095")
     @Override
     protected @NotNull DatabaseWrapper createContainerWrapper() {
-        return new DockerDatabaseWrapper(db2, this);
+        return new DockerDatabaseWrapper(
+                new Db2Container(DockerImageName.parse(getImageName()).withTag(getVersion()))
+                        .withUsername(getUsername())
+                        .withPassword(getPassword())
+                        .withDatabaseName(getCatalog())
+                        .acceptLicense()
+                        .withUrlParam("retrieveMessagesFromServerOnGetMessage", "true"),
+                this
+        );
     }
 
     @Override
