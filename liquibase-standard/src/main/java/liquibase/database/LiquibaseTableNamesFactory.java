b230593c84de1aab034d0c0594b3adec105c774f
Steven Massaro <steven.massaro.web@gmail.com>
2023-10-27 11:10:57 -0500

    test hardcode of table names in factory

--- liquibase-standard/src/main/java/liquibase/database/LiquibaseTableNamesFactory.java
+++ liquibase-standard/src/main/java/liquibase/database/LiquibaseTableNamesFactory.java
@@ -4,6 +4,7 @@ import liquibase.Scope;
 import liquibase.SingletonObject;
 import liquibase.servicelocator.ServiceLocator;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +18,7 @@ public class LiquibaseTableNamesFactory implements SingletonObject {
     }
 
     public List<String> getLiquibaseTableNames(Database database) {
-        return generators.stream().flatMap(f -> f.getLiquibaseGeneratedTableNames(database).stream()).collect(Collectors.toList());
+//        return generators.stream().flatMap(f -> f.getLiquibaseGeneratedTableNames(database).stream()).collect(Collectors.toList());
+        return Arrays.asList("DATABASECHANGELOG", "DATABASECHANGELOGLOCK", "DATABASECHANGELOGHISTORY");
     }
 }
