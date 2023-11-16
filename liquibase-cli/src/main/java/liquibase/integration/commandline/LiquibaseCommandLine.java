cf763763df3eb56bea45c500837cb055be00efb8
rberezen <ruslan.berezenskyi@gmail.com>
2023-11-15 17:05:21 -0700

    Update LiquibaseCommandLine.java
    Signed-off-by: rberezen <ruslan.berezenskyi@gmail.com>

--- liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
+++ liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
@@ -84,6 +84,7 @@ public class LiquibaseCommandLine {
     private final ResourceBundle coreBundle = getBundle("liquibase/i18n/liquibase-core");
 
     /**
+     * 
      * Pico's defaultFactory does a lot of reflection, checking for classes we know we don't have.
      * That is slow on older JVMs and impact initial startup time, so do our own factory for performance reasons.
      * It is easy to configure pico to it's default factory, when profiling check for `CommandLine$DefaultFactory` usage
