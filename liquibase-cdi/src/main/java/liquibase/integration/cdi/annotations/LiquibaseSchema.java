40ea202c1796abebe8e875a4cc614e61013a0225
Sayali Mohadikar <76010603+sayaliM0412@users.noreply.github.com>
2023-11-30 08:34:31 -0800

    Update LiquibaseSchema.java
    Signed-off-by: Sayali Mohadikar <76010603+sayaliM0412@users.noreply.github.com>

--- liquibase-cdi/src/main/java/liquibase/integration/cdi/annotations/LiquibaseSchema.java
+++ liquibase-cdi/src/main/java/liquibase/integration/cdi/annotations/LiquibaseSchema.java
@@ -1,5 +1,8 @@
 package liquibase.integration.cdi.annotations;
 
+
+
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
