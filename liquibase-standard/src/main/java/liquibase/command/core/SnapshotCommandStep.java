73b96ef37b51dacd9e377576900b9ffa64ff28cd
Wesley Willard <wwillard@datical.com>
2023-12-13 08:42:51 -0600

    Refactor to make snapshot command extensible
    DAT-16054

--- liquibase-standard/src/main/java/liquibase/command/core/SnapshotCommandStep.java
+++ liquibase-standard/src/main/java/liquibase/command/core/SnapshotCommandStep.java
@@ -2,20 +2,16 @@ package liquibase.command.core;
 
 import liquibase.CatalogAndSchema;
 import liquibase.GlobalConfiguration;
-import liquibase.Scope;
 import liquibase.command.*;
 import liquibase.database.Database;
 import liquibase.database.ObjectQuotingStrategy;
-import liquibase.diff.ObjectDifferences;
-import liquibase.diff.output.ObjectChangeFilter;
-import liquibase.diff.output.StandardObjectChangeFilter;
 import liquibase.serializer.SnapshotSerializerFactory;
 import liquibase.snapshot.DatabaseSnapshot;
 import liquibase.snapshot.SnapshotControl;
 import liquibase.snapshot.SnapshotGeneratorFactory;
 import liquibase.structure.DatabaseObject;
-import liquibase.structure.core.DatabaseObjectFactory;
 import liquibase.util.StringUtil;
+import lombok.Getter;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -23,6 +19,7 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.*;
 
+@Getter
 public class SnapshotCommandStep extends AbstractCommandStep {
 
     public static final String[] COMMAND_NAME = {"snapshot"};
@@ -30,7 +27,6 @@ public class SnapshotCommandStep extends AbstractCommandStep {
     public static final CommandArgumentDefinition<String> SCHEMAS_ARG;
     public static final CommandArgumentDefinition<String> SNAPSHOT_FORMAT_ARG;
     public static final CommandArgumentDefinition<SnapshotControl> SNAPSHOT_CONTROL_ARG;
-    public static final CommandArgumentDefinition<String> SNAPSHOT_TYPES_ARG;
 
     static {
         CommandBuilder builder = new CommandBuilder(COMMAND_NAME);
@@ -38,27 +34,9 @@ public class SnapshotCommandStep extends AbstractCommandStep {
         SNAPSHOT_FORMAT_ARG = builder.argument("snapshotFormat", String.class)
                 .description("Output format to use (JSON, YAML, or TXT)").build();
         SNAPSHOT_CONTROL_ARG = builder.argument("snapshotControl", SnapshotControl.class).hidden().build();
-        SNAPSHOT_TYPES_ARG = builder.argument("snapshotTypes", String.class)
-                .description("Types of objects to snapshot: " + getStandardTypes()).build();
     }
 
     private Map<String, Object> snapshotMetadata;
-
-    private static String getStandardTypes() {
-        Set<Class<? extends DatabaseObject>> standardTypes = DatabaseObjectFactory.getInstance().getStandardTypes();
-        StringBuilder builder = new StringBuilder();
-        standardTypes.forEach(type -> {
-            String[] parts= type.getName().split("\\.");
-            String name = parts[parts.length-1];
-            if (builder.length() == 0) {
-                builder.append(name);
-            } else {
-                builder.append(", ");
-                builder.append(name);
-            }
-        });
-        return builder.toString();
-    }
 
     @Override
     public List<Class<?>> requiredDependencies() {
@@ -89,14 +67,6 @@ public class SnapshotCommandStep extends AbstractCommandStep {
         return finalList.toArray(new CatalogAndSchema[0]);
     }
 
-    public Map<String, Object> getSnapshotMetadata() {
-        return snapshotMetadata;
-    }
-
-    public void setSnapshotMetadata(Map<String, Object> snapshotMetadata) {
-        this.snapshotMetadata = snapshotMetadata;
-    }
-
     @Override
     public void run(CommandResultsBuilder resultsBuilder) throws Exception {
         CommandScope commandScope = resultsBuilder.getCommandScope();
@@ -107,17 +77,7 @@ public class SnapshotCommandStep extends AbstractCommandStep {
 
         InternalSnapshotCommandStep.logUnsupportedDatabase(database, this.getClass());
         SnapshotControl snapshotControl;
-        if (commandScope.getArgumentValue(SNAPSHOT_CONTROL_ARG) == null) {
-            String typesString = commandScope.getArgumentValue(SNAPSHOT_TYPES_ARG);
-            if (typesString != null) {
-                Class<? extends DatabaseObject>[] types = DiffCommandStep.parseSnapshotTypes(typesString);
-                snapshotControl = new SnapshotControl(database, types);
-            } else {
-                snapshotControl = new SnapshotControl(database);
-            }
-        } else {
-            snapshotControl = commandScope.getArgumentValue(SnapshotCommandStep.SNAPSHOT_CONTROL_ARG);
-        }
+        snapshotControl = createSnapshotControl(commandScope, database);
 
         if (schemas == null) {
             schemas = new CatalogAndSchema[]{database.getDefaultSchema()};
@@ -148,6 +108,16 @@ public class SnapshotCommandStep extends AbstractCommandStep {
             //
             database.setObjectQuotingStrategy(originalQuotingStrategy);
         }
+    }
+
+    protected SnapshotControl createSnapshotControl(CommandScope commandScope, Database database) {
+        SnapshotControl snapshotControl;
+        if (commandScope.getArgumentValue(SNAPSHOT_CONTROL_ARG) == null) {
+            snapshotControl = new SnapshotControl(database);
+        } else {
+            snapshotControl = commandScope.getArgumentValue(SnapshotCommandStep.SNAPSHOT_CONTROL_ARG);
+        }
+        return snapshotControl;
     }
 
     private Writer getOutputWriter(final OutputStream outputStream) throws IOException {
