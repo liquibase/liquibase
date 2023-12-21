1cea6bfa49752fb47da464a89cf28ddd60869069
Steven Massaro <steven.massaro.web@gmail.com>
2023-12-21 13:57:29 -0600

    move method to util class

--- liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
+++ liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
@@ -417,7 +417,7 @@ public class LiquibaseCommandLine {
                         liquibaseConfiguration.unregisterProvider(provider);
                     }
 
-                    setPersistedMdcKeysToEmptyString();
+                    LogUtil.setPersistedMdcKeysToEmptyString();
                 }
             });
         } catch (Throwable e) {
@@ -428,20 +428,6 @@ public class LiquibaseCommandLine {
         }
     }
 
-    /**
-     * In {@link LiquibaseCommandLine#addEmptyMdcValues()}, baseline values are added to the MDC with empty strings.
-     * It is desired that {@link MdcKey#CHANGESET_ID}, {@link MdcKey#CHANGESET_AUTHOR} and {@link MdcKey#LIQUIBASE_SYSTEM_USER}
-     * are not ever cleared from the scope, except when the command finishes executing (or they are replaced with a new
-     * value). When the command finishes executing, these keys must be set back to an empty string so that incorrect
-     * values do not persist outside of command execution (like would occur during flow). {@link MdcKey#LIQUIBASE_SYSTEM_USER}
-     * is purposefully excluded from this method because it does not need to be reset when command execution finishes
-     * because it should be constant throughout the entire Liquibase execution.
-     */
-    public static void setPersistedMdcKeysToEmptyString() {
-        Scope.getCurrentScope().addMdcValue(MdcKey.CHANGESET_ID, "", false);
-        Scope.getCurrentScope().addMdcValue(MdcKey.CHANGESET_AUTHOR, "", false);
-    }
-
     private void addEmptyMdcValues() {
         if (LiquibaseCommandLineConfiguration.ADD_EMPTY_MDC_VALUES.getCurrentValue()) {
             Scope.getCurrentScope().addMdcValue(MdcKey.DEPLOYMENT_ID, "");
@@ -449,7 +435,7 @@ public class LiquibaseCommandLine {
             Scope.getCurrentScope().addMdcValue(MdcKey.DEPLOYMENT_OUTCOME_COUNT, "0");
             Scope.getCurrentScope().addMdcValue(MdcKey.ROWS_AFFECTED, "0");
             Scope.getCurrentScope().addMdcValue(MdcKey.CHANGELOG_FILE, "");
-            setPersistedMdcKeysToEmptyString();
+            LogUtil.setPersistedMdcKeysToEmptyString();
             Scope.getCurrentScope().addMdcValue(MdcKey.CHANGESET_OUTCOME, "NOOP");
             Scope.getCurrentScope().addMdcValue(MdcKey.CHANGESETS_UPDATED, new ChangesetsUpdated());
             Scope.getCurrentScope().addMdcValue(MdcKey.OPERATION_START_TIME, "");
