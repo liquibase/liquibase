2f537bd98dce6a410df46d1b266782cb51ccf4a9
filipe <flautert@liquibase.com>
2024-01-25 14:10:16 -0300

    New tests

--- liquibase-standard/src/test/groovy/liquibase/util/grammar/SimpleSqlGrammarTest.groovy
+++ liquibase-standard/src/test/groovy/liquibase/util/grammar/SimpleSqlGrammarTest.groovy
@@ -48,5 +48,8 @@ class SimpleSqlGrammarTest extends Specification {
         "'''' sometimesEquals '\\''"                           | ["''''", " ", "sometimesEquals", " ", "'\\''"]
         "'\\'' sometimesEquals \"'\""                          | ["'\\''", " ", "sometimesEquals", " ", "\"'\""]
         "\"'\" sometimesEquals \"\\'\""                        | ["\"'\"", " ", "sometimesEquals", " ", "\"\\'\""]
+        "This has a symbol ≤ (u2264) but no backslash"         | ["This", " ", "has", " ", "a", " ", "symbol", " ", "≤", " ", "(", "u2264", ")", " ", "but", " ", "no", " ", "backslash"]
+        "This has a \\ and symbol ≤ (u2264)"                   | ["This", " ", "has", " ", "a", " ", "\\", " ", "and", " ", "symbol", " ", "≤", " ", "(", "u2264", ")"]
+        "This ≤ (u2264) is before the \\"                      | ["This", " ", "≤", " ", "(", "u2264", ")", " ", "is", " ", "before", " ", "the", " ", "\\"]
     }
 }
