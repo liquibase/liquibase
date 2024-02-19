9c34f0e537ff3b66e17e5173796e45c2b429a8bd
Steven Massaro <steven.massaro.web@gmail.com>
2024-02-19 09:27:29 -0600

    WIP ignore index test (not actually ignored)

--- liquibase-integration-tests/src/test/java/liquibase/dbtest/snowflake/SnowflakeIntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/snowflake/SnowflakeIntegrationTest.java
@@ -2,10 +2,18 @@ package liquibase.dbtest.snowflake;
 
 import liquibase.database.DatabaseFactory;
 import liquibase.dbtest.AbstractIntegrationTest;
+import liquibase.exception.DatabaseException;
+import org.junit.Ignore;
 
 public class SnowflakeIntegrationTest extends AbstractIntegrationTest {
 
     public SnowflakeIntegrationTest() throws Exception {
         super("snowflake", DatabaseFactory.getInstance().getDatabase("snowflake"));
+    }
+
+    @Ignore(value = "Snowflake does not support indexes.")
+    @Override
+    public void verifyIndexIsCreatedWhenAssociatedWithPropertyIsSetAsNone() throws DatabaseException {
+        super.verifyIndexIsCreatedWhenAssociatedWithPropertyIsSetAsNone();
     }
 }
