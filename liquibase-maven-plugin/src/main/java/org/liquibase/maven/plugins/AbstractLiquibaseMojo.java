7a1e556023e1e39bbe02641b8fd70623ea95aa43
Steven Massaro <steven.massaro.web@gmail.com>
2024-02-26 13:11:46 -0700

    fix dbclHistoryEnabled property from liquibase.properties file in maven (DAT-16881) (#5609)
    fix dbclHistoryEnabled property from liquibase.properties file in maven

--- liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/AbstractLiquibaseMojo.java
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/AbstractLiquibaseMojo.java
@@ -672,7 +672,7 @@ public abstract class AbstractLiquibaseMojo extends AbstractMojo {
      *
      * @parameter property="liquibase.dbclHistoryEnabled"
      */
-    @PropertyElement
+    @PropertyElement(key = "liquibase.dbclHistory.enabled")
     protected Boolean dbclHistoryEnabled;
 
     /**
@@ -682,7 +682,7 @@ public abstract class AbstractLiquibaseMojo extends AbstractMojo {
      *
      * @parameter property="liquibase.databaseChangelogHistoryEnabled"
      */
-    @PropertyElement
+    @PropertyElement(key = "liquibase.databaseChangelogHistory.enabled")
     protected Boolean databaseChangelogHistoryEnabled;
 
     protected String commandName;
@@ -806,8 +806,6 @@ public abstract class AbstractLiquibaseMojo extends AbstractMojo {
                 IntegrationDetails integrationDetails = new IntegrationDetails();
                 integrationDetails.setName("maven");
 
-                scopeValues.put("liquibase.dbclhistory.enabled", isDbclHistoryEnabled());
-
                 final PluginDescriptor pluginDescriptor = (PluginDescriptor) getPluginContext().get("pluginDescriptor");
                 for (MojoDescriptor descriptor : pluginDescriptor.getMojos()) {
                     if (!descriptor.getImplementationClass().equals(this.getClass())) {
@@ -928,6 +926,7 @@ public abstract class AbstractLiquibaseMojo extends AbstractMojo {
                         //
                         Map<String, Object> innerScopeValues = new HashMap<>();
                         innerScopeValues.put(key, preserveSchemaCase);
+                        innerScopeValues.put("liquibase.dbclhistory.enabled", isDbclHistoryEnabled());
                         Scope.child(innerScopeValues, () -> performLiquibaseTask(liquibase));
                     } catch (LiquibaseException e) {
                         cleanup(database);
