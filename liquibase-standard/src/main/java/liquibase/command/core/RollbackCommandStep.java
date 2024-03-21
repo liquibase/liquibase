7b4ac210edd798a3d34b8dd554abbe2fc81db6c0
Alex <abrackx@gmail.com>
2024-03-21 16:03:43 -0400

    DAT-16798: Add new parameters to rollback report (#5630)
    * [DAT-16798] Add new parameters to rollback report. Simplify some database info setup with shared method.
    
    * [DAT-16798] Include correct failed count.
    
    * [DAT-16922] Rework ExceptionDetails to encapsulate building of error object. Update commandscope to reflect ExceptionDetails changes. Use lombok getters on DefaultChangeExecListener. Add pending rollbacks, exception details and failed changeset name to rollback parameters.
    
    * [DAT-16922] Set size for pending rollbacks.
    
    * [DAT-16922] Rework ExceptionDetails to encapsulate building of error object. Update commandscope to reflect ExceptionDetails changes. Use lombok getters on DefaultChangeExecListener. Add pending rollbacks, exception details and failed changeset name to rollback parameters.
    
    * [DAT-16922] Set size for pending rollbacks.
    
    * [DAT-16568] Cleanup update report parameters imports.
    
    [DAT-17041] Add deployment id and targeted changeset details to rollback report parameters.
    
    * [DAT-16798] Simplify setting rollback report parameters. Actually report error when no tag is found.
    
    ---------
    
    Co-authored-by: obovsunivskyii <baqaua@gmail.com>

--- liquibase-standard/src/main/java/liquibase/command/core/RollbackCommandStep.java
+++ liquibase-standard/src/main/java/liquibase/command/core/RollbackCommandStep.java
@@ -7,6 +7,7 @@ import liquibase.changelog.filter.AfterTagChangeSetFilter;
 import liquibase.command.*;
 import liquibase.database.Database;
 import liquibase.logging.mdc.MdcKey;
+import liquibase.logging.mdc.customobjects.ExceptionDetails;
 import liquibase.report.RollbackReportParameters;
 import liquibase.util.StringUtil;
 
@@ -51,13 +52,21 @@ public class RollbackCommandStep extends AbstractRollbackCommandStep {
         Scope.getCurrentScope().addMdcValue(MdcKey.ROLLBACK_TO_TAG, tagToRollBackTo);
 
         Database database = (Database) commandScope.getDependency(Database.class);
-        rollbackReportParameters.getDatabaseInfo().setDatabaseType(database.getDatabaseProductName());
-        rollbackReportParameters.getDatabaseInfo().setVersion(database.getDatabaseProductVersion());
-        rollbackReportParameters.setJdbcUrl(database.getConnection().getURL());
+        rollbackReportParameters.setupDatabaseInfo(database);
+        rollbackReportParameters.setRollbackTag(tagToRollBackTo);
 
         List<RanChangeSet> ranChangeSetList = database.getRanChangeSetList();
         TagVersionEnum tagVersion = TagVersionEnum.valueOf(commandScope.getArgumentValue(TAG_VERSION_ARG));
-        this.doRollback(resultsBuilder, ranChangeSetList, new AfterTagChangeSetFilter(tagToRollBackTo, ranChangeSetList, tagVersion), rollbackReportParameters);
+        try {
+            AfterTagChangeSetFilter afterTagChangeSetFilter = new AfterTagChangeSetFilter(tagToRollBackTo, ranChangeSetList, tagVersion); // This can throw an exception
+            this.doRollback(resultsBuilder, ranChangeSetList, afterTagChangeSetFilter, rollbackReportParameters);
+        } catch (Exception exception) {
+            rollbackReportParameters.setSuccess(false);
+            String source = ExceptionDetails.findSource(database);
+            ExceptionDetails exceptionDetails = new ExceptionDetails(exception, source);
+            rollbackReportParameters.setRollbackException(exceptionDetails);
+            throw exception;
+        }
     }
 
     @Override
