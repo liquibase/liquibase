3ec31383e1a38f420c4908b3c9c445b49c93a60f
Steven Massaro <steven.massaro.web@gmail.com>
2024-03-28 09:56:37 -0500

    Revert "add foreign key integration test"
    This reverts commit b7cbaf09a8e20c8b9ab0885f7444be7e6828b126.

--- liquibase-integration-tests/src/test/groovy/liquibase/command/UpdateSnowflakeIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/UpdateSnowflakeIntegrationTest.groovy
@@ -2,18 +2,11 @@ package liquibase.command
 
 import liquibase.Scope
 import liquibase.command.util.CommandUtil
-import liquibase.database.core.InformixDatabase
-import liquibase.database.jvm.JdbcConnection
-import liquibase.executor.jvm.ColumnMapRowMapper
-import liquibase.executor.jvm.RowMapperResultSetExtractor
 import liquibase.extension.testing.testsystem.DatabaseTestSystem
 import liquibase.extension.testing.testsystem.TestSystemFactory
 import liquibase.extension.testing.testsystem.spock.LiquibaseIntegrationTest
 import spock.lang.Shared
 import spock.lang.Specification
-
-import java.sql.ResultSet
-import java.sql.SQLException
 
 @LiquibaseIntegrationTest
 class UpdateSnowflakeIntegrationTest extends Specification{
@@ -27,38 +20,5 @@ class UpdateSnowflakeIntegrationTest extends Specification{
 
         then:
         noExceptionThrown()
-    }
-
-    def "another"() {
-        when:
-        snowflake.executeSql("""
-CREATE TABLE table2 (
-    col1 INTEGER NOT NULL,
-    col2 INTEGER NOT NULL,
-    CONSTRAINT pkey_1 PRIMARY KEY (col1, col2)
-    );""")
-        snowflake.executeSql("""
-CREATE TABLE table3 (
-    col_a INTEGER NOT NULL,
-    col_b INTEGER NOT NULL,
-    CONSTRAINT fkey_1 FOREIGN KEY (col_a, col_b) REFERENCES table2 (col1, col2)
-    );
-""")
-
-        then:
-        def keys = (snowflake.getDatabaseFromFactory().getConnection() as JdbcConnection).getUnderlyingConnection().getMetaData().getImportedKeys(
-                snowflake.getDatabaseFromFactory().getDefaultCatalogName(),
-                snowflake.getDatabaseFromFactory().getDefaultSchemaName(),
-                "table3"
-        )
-        keys != null
-
-        List<Map> result = (List<Map>) new RowMapperResultSetExtractor(new ColumnMapRowMapper(true)).extractData(keys)
-
-        result != null
-
-        cleanup:
-        snowflake.executeSql("drop table table3")
-        snowflake.executeSql("drop table table2")
     }
 }
