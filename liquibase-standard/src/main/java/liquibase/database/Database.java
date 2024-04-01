02a6af084feaf368c46c588949bb7a1f4533c2e2
Steven Massaro <steven.massaro.web@gmail.com>
2024-04-01 10:54:42 -0600

    make "connect" success message generation more generic by moving into Database interface (DAT-16602) (#5750)

--- liquibase-standard/src/main/java/liquibase/database/Database.java
+++ liquibase-standard/src/main/java/liquibase/database/Database.java
@@ -629,5 +629,16 @@ public interface Database extends PrioritizedService, AutoCloseable {
     default boolean supportsDatabaseChangeLogHistory() {
         return false;
     }
+
+    /**
+     * Returns a custom message to be displayed upon successful execution of the connect command.
+     * This method can be overridden by a database implementation to provide a specific message.
+     * If not overridden, it returns null by default.
+     *
+     * @return A custom success message for the connect command, or null if not provided.
+     */
+    default String generateConnectCommandSuccessMessage() {
+        return null;
+    }
 }
 
