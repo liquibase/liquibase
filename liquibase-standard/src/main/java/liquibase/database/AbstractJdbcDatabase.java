0d54467d9386fcbec41be8fd351b4cce69b53eb6
Ahmed Ashour <asashour@yahoo.com>
2024-04-06 14:16:11 +0200

    Remove unboxing

--- liquibase-standard/src/main/java/liquibase/database/AbstractJdbcDatabase.java
+++ liquibase-standard/src/main/java/liquibase/database/AbstractJdbcDatabase.java
@@ -731,7 +731,7 @@ public abstract class AbstractJdbcDatabase implements Database {
         if (caseSensitive == null) {
             return false;
         } else {
-            return caseSensitive.booleanValue();
+            return caseSensitive;
         }
     }
 
