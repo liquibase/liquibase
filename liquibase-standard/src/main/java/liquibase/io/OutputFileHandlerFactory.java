eea54342a219cfd3eb31c8419c44eed85830cabc
Steven Massaro <steven.massaro.web@gmail.com>
2024-04-12 10:30:20 -0500

    fix SingletonObjectTest

--- liquibase-standard/src/main/java/liquibase/io/OutputFileHandlerFactory.java
+++ liquibase-standard/src/main/java/liquibase/io/OutputFileHandlerFactory.java
@@ -1,7 +1,10 @@
 package liquibase.io;
 
 import liquibase.plugin.AbstractPluginFactory;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class OutputFileHandlerFactory extends AbstractPluginFactory<OutputFileHandler> {
     @Override
     protected Class<OutputFileHandler> getPluginClass() {
