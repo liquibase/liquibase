1d2ab278344850d82b062a70e85ec8428e00c7af
filipe <flautert@liquibase.org>
2024-05-01 11:13:39 -0300

    fix: Bigquery getpriority not working after merging to core

--- liquibase-bigquery/src/main/java/liquibase/snapshot/jvm/BigQuerySequenceSnapshotGenerator.java
+++ liquibase-bigquery/src/main/java/liquibase/snapshot/jvm/BigQuerySequenceSnapshotGenerator.java
@@ -17,14 +17,11 @@ public class BigQuerySequenceSnapshotGenerator extends SequenceSnapshotGenerator
 
         @Override
         public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
-            if (!(database instanceof BigqueryDatabase)) {
+            if (database instanceof BigqueryDatabase) {
+                return super.getPriority(objectType, database) + PRIORITY_DATABASE;
+            } else {
                 return PRIORITY_NONE;
             }
-            int priority = super.getPriority(objectType, database);
-            if (priority > PRIORITY_NONE && database instanceof BigqueryDatabase) {
-                priority += PRIORITY_DATABASE;
-            }
-            return priority;
         }
 
         @Override
