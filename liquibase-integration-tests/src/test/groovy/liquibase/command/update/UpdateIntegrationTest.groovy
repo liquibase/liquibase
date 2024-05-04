836bd19a5ff53b906b2c9b15afd693907d35ac9c
Jason Lyle <jason.lyle@oracle.com>
2024-05-04 06:43:32 -0400

    Propper integration test, convert to h2 database for easier testing

--- liquibase-integration-tests/src/test/groovy/liquibase/command/update/UpdateIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/update/UpdateIntegrationTest.groovy
@@ -15,28 +15,37 @@ import spock.lang.Specification
 @LiquibaseIntegrationTest
 class UpdateIntegrationTest extends Specification {
     @Shared
-    private DatabaseTestSystem postgres = (DatabaseTestSystem) Scope.getCurrentScope().getSingleton(TestSystemFactory.class).getTestSystem("postgresql")
+    private DatabaseTestSystem testDatabase = (DatabaseTestSystem) Scope.getCurrentScope().getSingleton(TestSystemFactory.class).getTestSystem("h2")
 
     def "check execution parameters are correctly replaced" () {
         given:
-        CommandUtil.runUpdate(postgres,"src/test/resources/changelogs/pgsql/update/execution-parameter.xml")
+        CommandUtil.runUpdate(testDatabase,"src/test/resources/changelogs/h2/update/execution-parameter.xml")
         String sql = "select * from parameter_value_tests order by id"
 
         when:
         CommandScope executeSql = new CommandScope(ExecuteSqlCommandStep.COMMAND_NAME[0])
         executeSql.addArgumentValue(ExecuteSqlCommandStep.SQL_ARG, sql)
-        executeSql.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, postgres.getConnectionUrl())
-        executeSql.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, postgres.getUsername())
-        executeSql.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, postgres.getPassword())
+        executeSql.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, testDatabase.getConnectionUrl())
+        executeSql.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, testDatabase.getUsername())
+        executeSql.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, testDatabase.getPassword())
 
-        then:
         CommandResults results = executeSql.execute()
         String output = results.getResult("output") as String
-        System.out.println("JML TEST: '" + output + "'")
-        assert 1 == 1
+
+        then:
+        assert output == """Output of select * from parameter_value_tests order by id:
+ID | CHANGELOG_FILE | CHANGESET_ID | CHANGESET_AUTHOR |
+1 | src/test/resources/changelogs/h2/update/execution-parameter/1.xml | 1.1 | jlyle | 
+2 | src/test/resources/changelogs/h2/update/execution-parameter/1.xml | 1.2 | not_jlyle | 
+3 | src/test/resources/changelogs/h2/update/execution-parameter/1.xml | 1.3 | jlyle | 
+4 | src/test/resources/changelogs/h2/update/execution-parameter/2.xml | 2.1 | jlyle | 
+5 | src/test/resources/changelogs/h2/update/execution-parameter/2.xml | 2.2 | not_jlyle | 
+6 | src/test/resources/changelogs/h2/update/execution-parameter/2.xml | 2.3 | jlyle | 
+
+"""
 
         cleanup:
-        postgres.getConnection().close()
-        CommandUtil.runDropAll(postgres)
+        testDatabase.getConnection().close()
+        CommandUtil.runDropAll(testDatabase)
     }
 }
