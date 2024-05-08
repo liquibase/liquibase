a218222e121208d3f953089bdf73025584fcccd8
Daniel Mallorga <75833793+MalloD12@users.noreply.github.com>
2024-05-08 10:32:10 -0300

    Fix (MSSQL): default end delimiter applied when there is an endDelimiter applied for a given chanset (#5781)
    * - Added logic to only add default sql visitor when endDelimiter has not been specified.
    
    - Test added.
    
    * EndDelimiter check updated to avoid casting issue when dealing with a not AbstractSqlChange.
    
    ---------
    
    Co-authored-by: rberezen <ruslan.berezenskyi@gmail.com>

--- liquibase-integration-tests/src/test/groovy/liquibase/command/core/MssqlMergeIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/core/MssqlMergeIntegrationTest.groovy
@@ -5,6 +5,8 @@ import liquibase.command.util.CommandUtil
 import liquibase.extension.testing.testsystem.DatabaseTestSystem
 import liquibase.extension.testing.testsystem.TestSystemFactory
 import liquibase.extension.testing.testsystem.spock.LiquibaseIntegrationTest
+import liquibase.util.FileUtil
+import static org.junit.Assert.fail
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -22,5 +24,30 @@ class MssqlMergeIntegrationTest extends Specification {
         noExceptionThrown()
         cleanup:
         CommandUtil.runDropAll(mssql)
+    }
+
+    def "verify store procedure does not get default endDelimiter added when a given delimiter is specified"() {
+        given:
+        CommandUtil.runUpdate(mssql,'src/test/resources/changelogs/mssql/issues/create.procedure.changelog.sql')
+
+        def sProcedureName = "TestProcedure"
+        def getStoreProcedureContentQuery = String.format("""
+        SELECT ISNULL(smsp.definition, ssmsp.definition) AS [SPBody]
+        FROM sys.all_objects AS sp LEFT OUTER JOIN sys.sql_modules AS smsp ON smsp.object_id = sp.object_id
+        LEFT OUTER JOIN sys.system_sql_modules AS ssmsp ON ssmsp.object_id = sp.object_id
+        WHERE (sp.type = N'P' OR sp.type = N'RF' OR sp.type='PC')and(sp.name=N'%s' and SCHEMA_NAME(sp.schema_id)=N'dbo')
+        """, sProcedureName)
+
+        when:
+        def resultSet = mssql.getConnection().prepareStatement(getStoreProcedureContentQuery).executeQuery()
+
+        then:
+        def changelogContent = FileUtil.getContents(new File('src/test/resources/changelogs/mssql/issues/create.procedure.changelog.sql'))
+        if(resultSet.next()) {
+            def deployedSPContent = resultSet.getString("SPBody")
+            changelogContent.contains(deployedSPContent.split(String.format("procedure dbo.%s", sProcedureName))[1])
+        } else {
+            fail(String.format("There is not procedure stored in the DB with name %s", sProcedureName))
+        }
     }
 }
