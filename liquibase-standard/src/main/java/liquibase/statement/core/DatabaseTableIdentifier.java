91938641601f0c630994e13e7ca5aa7ad8ce8b0d
tschoemaker <tschoemaker@uni-osnabrueck.de>
2024-05-10 20:17:48 +0200

    replaced traditional getters and setters by lombok

--- liquibase-standard/src/main/java/liquibase/statement/core/DatabaseTableIdentifier.java
+++ liquibase-standard/src/main/java/liquibase/statement/core/DatabaseTableIdentifier.java
@@ -1,39 +1,16 @@
 package liquibase.statement.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class DatabaseTableIdentifier{
     private String catalogName;
-
-    public String getCatalogName(){
-        return catalogName;
-    }
-
-    public void setCatalogName(String catalogName){
-        this.catalogName=catalogName;
-    }
-
     private String schemaName;
-
-    public String getSchemaName(){
-        return schemaName;
-    }
-
-    public void setSchemaName(String schemaName){
-        this.schemaName=schemaName;
-    }
-
     private String tableName;
 
-    public String getTableName(){
-        return tableName;
-    }
-
-    public void setTableName(String tableName){
-        this.tableName=tableName;
-    }
-
-    public DatabaseTableIdentifier(String catalogName,String schemaName,String tableName){
-        this.catalogName=catalogName;
-        this.schemaName=schemaName;
-        this.tableName=tableName;
-    }
 }
 
