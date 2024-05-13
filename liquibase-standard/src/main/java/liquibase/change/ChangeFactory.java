9fa290ed5a4024c8e4b63b6ff3b1a8865861146b
filipe <flautert@liquibase.org>
2024-05-13 17:47:46 -0300

    chore: formatting

--- liquibase-standard/src/main/java/liquibase/change/ChangeFactory.java
+++ liquibase-standard/src/main/java/liquibase/change/ChangeFactory.java
@@ -144,8 +144,7 @@ public class ChangeFactory extends AbstractPluginFactory<Change>{
         //we only verify supports method if this is not part of the default liquibase changes
         for (Change plugin : plugins) {
             String packageName = plugin.getClass().getPackage().getName();
-            if (packageName.startsWith("liquibase.change") ||
-                packageName.startsWith("com.datical.liquibase.ext")) {
+            if (packageName.startsWith("liquibase.change") || packageName.startsWith("com.datical.liquibase.ext")) {
                 continue;
             }
 
