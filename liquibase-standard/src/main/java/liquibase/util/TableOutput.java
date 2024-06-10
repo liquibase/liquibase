f56909f1a088ee3373b25332e5107aa8971b3675
Steven Massaro <steven.massaro.web@gmail.com>
2024-06-10 12:48:12 -0500

    don't write extra unnecessary blank lines

--- liquibase-standard/src/main/java/liquibase/util/TableOutput.java
+++ liquibase-standard/src/main/java/liquibase/util/TableOutput.java
@@ -302,11 +302,14 @@ public class TableOutput {
             runningWidth = runningWidth % maxWidth;
         }
         int spaceWidth = runningWidth > 0 ? 1 : 0;
-        // If runningWidth is 0, then no need to add a space before the part because it is the first part of the line.
-        if (runningWidth + (part.length() + spaceWidth) > maxWidth && runningWidth > 0) {
+
+        boolean lineNotFilled = runningWidth != maxWidth;
+        if (runningWidth + (part.length() + spaceWidth) > maxWidth
+                && runningWidth > 0 // If runningWidth is 0, then no need to add a space before the part because it is the first part of the line.
+                && lineNotFilled) { // If runningWidth is not equal to maxWidth, then no need to add a space before the part because the next part will be written to the next line.
             runningWidth = fillLineWithSpaces(runningWidth, maxWidth, result);
         }
-        if (runningWidth > 0) {
+        if (runningWidth > 0 && lineNotFilled) {
             result.append(" ");
             runningWidth++;
         }
