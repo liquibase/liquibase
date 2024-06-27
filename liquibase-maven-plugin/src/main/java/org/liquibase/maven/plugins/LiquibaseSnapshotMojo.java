3890daec661cbaaaef84406ba99f8a206b2cc984
Timur Malikin <timur.malikin@gmail.com>
2024-06-26 22:48:45 -0400

    review fixes, clean unused code

--- liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseSnapshotMojo.java
+++ liquibase-maven-plugin/src/main/java/org/liquibase/maven/plugins/LiquibaseSnapshotMojo.java
@@ -1,18 +1,17 @@
 package org.liquibase.maven.plugins;
 
 import liquibase.Liquibase;
-import liquibase.command.CommandExecutionException;
-import liquibase.command.CommandFactory;
 import liquibase.command.CommandScope;
-import liquibase.command.core.SnapshotCommand;
 import liquibase.command.core.SnapshotCommandStep;
 import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep;
 import liquibase.database.Database;
 import liquibase.exception.LiquibaseException;
-import liquibase.util.StringUtil;
-import org.apache.maven.plugin.MojoExecutionException;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * <p>The snapshot command captures the current state of the url database, which is the target database.<p/>
@@ -36,11 +35,6 @@ public class LiquibaseSnapshotMojo extends AbstractLiquibaseChangeLogMojo {
      */
     protected File outputFile;
 
-    /**
-     * The writer for writing the snapshot file.
-     */
-    private Writer outputWriter;
-
     @Override
     protected void performLiquibaseTask(Liquibase liquibase) throws LiquibaseException {
         Database db = liquibase.getDatabase();
@@ -62,42 +56,6 @@ public class LiquibaseSnapshotMojo extends AbstractLiquibaseChangeLogMojo {
         } catch (IOException cee) {
             throw new LiquibaseException(cee);
         }
-    }
-
-    @Override
-    protected Liquibase createLiquibase(Database db)
-            throws MojoExecutionException {
-        Liquibase liquibase = super.createLiquibase(db);
-
-        if (outputFile != null) {
-            // Setup the output file writer
-            try {
-                if (!outputFile.exists()) {
-                    // Ensure the parent directories exist
-                    outputFile.getParentFile().mkdirs();
-                    // Create the actual file
-                    if (!outputFile.createNewFile()) {
-                        throw new MojoExecutionException(
-                                "Cannot create the output snapshot file; "
-                                        + outputFile.getAbsolutePath());
-                    }
-                }
-
-                outputWriter = getOutputWriter(outputFile);
-            } catch (IOException e) {
-                getLog().error(e);
-                throw new MojoExecutionException(
-                        "Failed to create the output writer", e);
-            }
-            getLog().info(
-                    "Output snapshot file: "
-                            + outputFile.getAbsolutePath());
-        } else {
-            getLog().info("Output snapshot goes to STDOUT");
-            outputWriter = new OutputStreamWriter(System.out);
-        }
-
-        return liquibase;
     }
 
     @Override
