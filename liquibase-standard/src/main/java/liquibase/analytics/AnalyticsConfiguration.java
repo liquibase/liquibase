3bc9810aafc9ca717d1dffa086824a12e6364227
Steven Massaro <steven.massaro.web@gmail.com>
2024-08-20 10:18:00 -0500

    initial add of Segment analytics integration

--- liquibase-standard/src/main/java/liquibase/analytics/AnalyticsConfiguration.java
+++ liquibase-standard/src/main/java/liquibase/analytics/AnalyticsConfiguration.java
@@ -7,6 +7,7 @@ public class AnalyticsConfiguration implements AutoloadedConfigurations {
 
     public static final ConfigurationDefinition<AnalyticsOutputDestination> OUTPUT_DESTINATION;
     public static final ConfigurationDefinition<String> FILENAME;
+    public static final ConfigurationDefinition<String> WRITE_KEY;
 
     static {
         ConfigurationDefinition.Builder builder = new ConfigurationDefinition.Builder("liquibase.analytics");
@@ -17,6 +18,9 @@ public class AnalyticsConfiguration implements AutoloadedConfigurations {
 
         FILENAME = builder.define("filename", String.class)
                 .setDefaultValue("./analytics.csv")
+                .build();
+
+        WRITE_KEY = builder.define("writeKey", String.class)
                 .build();
     }
 }
---
+++ liquibase-standard/src/main/java/liquibase/analytics/SegmentTrackEvent.java
@@ -0,0 +1,56 @@
+package liquibase.analytics;
+
+import liquibase.Scope;
+import liquibase.license.LicenseService;
+import liquibase.license.LicenseServiceFactory;
+import lombok.Data;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+public class SegmentTrackEvent {
+    private final String type = "track";
+    /**
+     * Unique identifier for the user in your database.
+     * A userId or an anonymousId is required. See the Identities docs for more details.
+     * https://segment.com/docs/connections/spec/identify#identities
+     */
+    private final String userId;
+    /**
+     * A pseudo-unique substitute for a User ID, for cases when you don’t have an absolutely unique identifier.
+     * A userId or an anonymousId is required.
+     * See the Identities docs for more details: https://segment.com/docs/connections/spec/identify#identities
+     */
+    private final String anonymousId;
+    /**
+     * Name of the action that a user has performed. See the Event field docs for more details.
+     * https://segment.com/docs/connections/spec/track#event
+     */
+    private final String event = "liquibase-command-executed";
+    /**
+     * Free-form dictionary of properties of the event, like revenue.
+     * See the Properties docs for a list of reserved property names.
+     * https://segment.com/docs/connections/spec/track#properties
+     */
+    private final Map<String, ?> properties;
+    /**
+     * Dictionary of extra information that provides useful context about a message,
+     * but is not directly related to the API call like ip address or locale See the Context field docs for more details.
+     * https://segment.com/docs/connections/spec/common#context
+     */
+    private final Map<String, ?> context;
+    private final String messageId = UUID.randomUUID().toString();
+
+    public static SegmentTrackEvent fromLiquibaseEvent(Event event) {
+        LicenseService licenseService = Scope.getCurrentScope().getSingleton(LicenseServiceFactory.class).getLicenseService();
+        String userId = licenseService.getLicenseInfoObject().getIssuedTo();
+        SegmentTrackEvent segmentTrackEvent = new SegmentTrackEvent(
+                userId,
+                UUID.randomUUID().toString(), // todo this should be more sophisticated in the future
+                event.getPropertiesAsMap(),
+                null
+        );
+        return segmentTrackEvent;
+    }
+}
