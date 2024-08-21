af37f1b3f2cb8aff788af13583ef18caea4918ad
filipe <flautert@liquibase.org>
2024-08-21 09:16:13 -0300

    chore: switching to h2 for performance improvement; changed tests order (!?); fxied exception message

--- liquibase-integration-tests/src/test/groovy/liquibase/command/core/UpdateToTagIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/core/UpdateToTagIntegrationTest.groovy
@@ -18,25 +18,6 @@ class UpdateToTagIntegrationTest extends Specification {
     @Shared
     private DatabaseTestSystem h2 = (DatabaseTestSystem) Scope.getCurrentScope().getSingleton(TestSystemFactory.class).getTestSystem("h2")
 
-    def "validate updateToTag is successfully executed when there is not TagDatabaseChange, but tag argument is specified "() {
-        when:
-        def outputStream =  new ByteArrayOutputStream()
-        def updateToTagCommand = new CommandScope(UpdateToTagCommandStep.COMMAND_NAME)
-        updateToTagCommand.addArgumentValue(UpdateToTagCommandStep.TAG_ARG, "testTag")
-        updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, h2.getConnectionUrl())
-        updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, h2.getUsername())
-        updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, h2.getPassword())
-        updateToTagCommand.addArgumentValue(UpdateSqlCommandStep.CHANGELOG_FILE_ARG, "liquibase/update-tests.yml")
-        updateToTagCommand.setOutput(outputStream)
-        def result = updateToTagCommand.execute().getResults()
-
-        then:
-        def a = ((DefaultChangeExecListener)result.get("defaultChangeExecListener"))
-        a.getDeployedChangeSets().get(0).getId() == "1"
-        outputStream.toString().contains("Run:                          1")
-        outputStream.toString().contains("Filtered out:                 0")
-    }
-
     def "validate exception is thrown if STRICT global argument is set as true and there is not TagDatabaseChange, but tag argument is specified "() {
         when:
         def scopeSettings = [
@@ -55,6 +36,45 @@ class UpdateToTagIntegrationTest extends Specification {
         then:
         final CommandExecutionException exception = thrown()
         exception.message.contains("Change 'TagDatabaseChange' not found or supported")
+    }
+
+    def "validate exception is thrown if STRICT global argument is set as true, there is a TagDatabaseChange, but tag argument specified does not match with TagDatabaseChange's tag"() {
+        when:
+        def scopeSettings = [
+                (GlobalConfiguration.STRICT.getKey()): Boolean.TRUE
+        ]
+        Scope.child(scopeSettings, {
+            def updateToTagCommand = new CommandScope(UpdateToTagCommandStep.COMMAND_NAME)
+            updateToTagCommand.addArgumentValue(UpdateToTagCommandStep.TAG_ARG, "testTag")
+            updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, h2.getConnectionUrl())
+            updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, h2.getUsername())
+            updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, h2.getPassword())
+            updateToTagCommand.addArgumentValue(UpdateSqlCommandStep.CHANGELOG_FILE_ARG, "liquibase/update-to-tag-changelog.xml")
+            updateToTagCommand.execute()
+        } as Scope.ScopedRunner)
+
+        then:
+        final CommandExecutionException exception = thrown()
+        exception.message.contains("liquibase.exception.LiquibaseException: Command execution tag testTag does not match with any changeSet tag")
+    }
+
+    def "validate updateToTag is successfully executed when there is not TagDatabaseChange, but tag argument is specified "() {
+        when:
+        def outputStream =  new ByteArrayOutputStream()
+        def updateToTagCommand = new CommandScope(UpdateToTagCommandStep.COMMAND_NAME)
+        updateToTagCommand.addArgumentValue(UpdateToTagCommandStep.TAG_ARG, "testTag")
+        updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, h2.getConnectionUrl())
+        updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, h2.getUsername())
+        updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, h2.getPassword())
+        updateToTagCommand.addArgumentValue(UpdateSqlCommandStep.CHANGELOG_FILE_ARG, "liquibase/update-tests.yml")
+        updateToTagCommand.setOutput(outputStream)
+        def result = updateToTagCommand.execute().getResults()
+
+        then:
+        def a = ((DefaultChangeExecListener)result.get("defaultChangeExecListener"))
+        a.getDeployedChangeSets().get(0).getId() == "1"
+        outputStream.toString().contains("Run:                          1")
+        outputStream.toString().contains("Filtered out:                 0")
     }
 
     def "validate updateToTag is successfully executed when there is TagDatabaseChange, but tag argument specified does not match with TagDatabaseChange's tag"() {
@@ -77,26 +97,5 @@ class UpdateToTagIntegrationTest extends Specification {
         outputStream.toString().contains("Run:                          3")
         outputStream.toString().contains("Filtered out:                 0")
     }
-
-    def "validate exception is thrown if STRICT global argument is set as true, there is a TagDatabaseChange, but tag argument specified does not match with TagDatabaseChange's tag"() {
-        when:
-        def scopeSettings = [
-                (GlobalConfiguration.STRICT.getKey()): Boolean.TRUE
-        ]
-        Scope.child(scopeSettings, {
-            def updateToTagCommand = new CommandScope(UpdateToTagCommandStep.COMMAND_NAME)
-            updateToTagCommand.addArgumentValue(UpdateToTagCommandStep.TAG_ARG, "testTag")
-            updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, h2.getConnectionUrl())
-            updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, h2.getUsername())
-            updateToTagCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, h2.getPassword())
-            updateToTagCommand.addArgumentValue(UpdateSqlCommandStep.CHANGELOG_FILE_ARG, "liquibase/update-to-tag-changelog.xml")
-            updateToTagCommand.execute()
-        } as Scope.ScopedRunner)
-
-        then:
-        final CommandExecutionException exception = thrown()
-        exception.message.contains("Command execution tag testTag does not match with changeSet tag aDifferentTag")
-    }
-
 
 }
