cf79fee59dceb02d3c2442f412fb329e473befbe
Carlo Dapor <carlo.dapor@mimacom.com>
2024-10-01 11:21:31 +0200

    Added test cases for empty and non-empty tables.

--- liquibase-integration-tests/src/test/groovy/liquibase/command/core/GenerateChangeLogEmptyIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/core/GenerateChangeLogEmptyIntegrationTest.groovy
@@ -1,42 +1,99 @@
 package liquibase.command.core
 
 import liquibase.Scope
+import liquibase.command.CommandScope
+import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep
+import liquibase.command.core.helpers.DiffOutputControlCommandStep
+import liquibase.command.core.helpers.PreCompareCommandStep
 import liquibase.command.util.CommandUtil
 import liquibase.extension.testing.testsystem.DatabaseTestSystem
 import liquibase.extension.testing.testsystem.TestSystemFactory
 import liquibase.extension.testing.testsystem.spock.LiquibaseIntegrationTest
 import liquibase.util.FileUtil
-import spock.lang.Ignore
+
 import spock.lang.Shared
 import spock.lang.Specification
 
-@LiquibaseIntegrationTest
-@Ignore
-class GenerateChangeLogEmptyIntegrationTest extends Specification {
-    @Shared
-    private DatabaseTestSystem database = (DatabaseTestSystem) Scope.getCurrentScope().getSingleton(TestSystemFactory.class).getTestSystem("postgresql")
+import java.nio.file.Files
+import java.nio.file.Path
 
-    def "Should generate view comments and be able to use the generated sql changelog"() {
+@LiquibaseIntegrationTest
+class GenerateChangeLogEmptyIntegrationTest extends Specification {
+
+    @Shared
+    private DatabaseTestSystem db = (DatabaseTestSystem) Scope.getCurrentScope().getSingleton(TestSystemFactory.class).getTestSystem("postgresql")
+
+    def "Should not generate changelog file with empty table"() {
         given:
-        CommandUtil.runUpdate(database,'changelogs/db2/issues/view.comments.sql')
+        db.executeSql("create table \"TEST\" (b VARCHAR(10));")
 
         when:
-        CommandUtil.runGenerateChangelog(database,'output.db2.sql')
+        def outputFileName = 'test/test-classes/output.postgresql.sql'
+        CommandScope commandScope = new CommandScope(GenerateChangelogCommandStep.COMMAND_NAME)
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, db.getConnectionUrl())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, db.getUsername())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, "data")
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
+        OutputStream outputStream = new ByteArrayOutputStream()
+        commandScope.setOutput(outputStream)
+        commandScope.execute()
 
         then:
-        def outputFile = new File('output.db2.sql')
-        def contents = FileUtil.getContents(outputFile)
-        contents.contains("COMMENT ON TABLE SOME_VIEW IS 'THIS IS A COMMENT ON SOME_VIEW VIEW. THIS VIEW COMMENT SHOULD BE CAPTURED BY GenerateChangeLog.'")
+        !Files.exists(Path.of(outputFileName))
 
         when:
-        CommandUtil.runDropAll(database)
-        CommandUtil.runUpdate(database,'output.mssql.sql')
+        CommandUtil.runDropAll(db)
 
         then:
         noExceptionThrown()
 
         cleanup:
-        CommandUtil.runDropAll(database)
+        CommandUtil.runDropAll(db)
+    }
+
+    def "Should generate changelog file with non-empty table"() {
+        given:
+        db.executeSql("""
+create table "TEST" (
+  b VARCHAR(10)
+);
+INSERT INTO "TEST" (b) VALUES ('Geronimo!');
+COMMIT;
+""")
+
+        when:
+        def outputFileName = 'test/test-classes/output.postgresql.sql'
+        CommandScope commandScope = new CommandScope(GenerateChangelogCommandStep.COMMAND_NAME)
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, db.getConnectionUrl())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, db.getUsername())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, "data")
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
+        OutputStream outputStream = new ByteArrayOutputStream()
+        commandScope.setOutput(outputStream)
+        commandScope.execute()
+
+        then:
+        def outputFile = new File(outputFileName)
+        Files.exists(Path.of(outputFileName))
+        def contents = FileUtil.getContents(outputFile)
+        contents.contains("""
+INSERT INTO "public"."TEST" ("b") VALUES ('Geronimo!');
+""")
+
+        when:
+        CommandUtil.runDropAll(db)
+
+        then:
+        noExceptionThrown()
+
+        cleanup:
+        CommandUtil.runDropAll(db)
         outputFile.delete()
     }
 }
