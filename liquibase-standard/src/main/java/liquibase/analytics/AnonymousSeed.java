a4e467549a7148783483c958c5a2025e10152b57
Steven Massaro <steven.massaro.web@gmail.com>
2024-10-03 14:42:31 -0500

    add javadoc

--- liquibase-standard/src/main/java/liquibase/analytics/AnonymousSeed.java
+++ liquibase-standard/src/main/java/liquibase/analytics/AnonymousSeed.java
@@ -14,11 +14,22 @@ import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+/**
+ * This class generates an anonymous seed based on various system properties
+ * such as the username, MAC addresses, and machine ID. It uses this data
+ * to generate a UUID that can serve as an identifier.
+ */
 public class AnonymousSeed {
     private String username;
     private String macAddresses;
     private String machineId;
 
+    /**
+     * Constructs an AnonymousSeed object. It attempts to retrieve the current
+     * username, the machine ID (on Linux systems), and the MAC addresses
+     * of network interfaces.
+     * If the machine ID is available, it is preferred over MAC addresses.
+     */
     public AnonymousSeed() {
         username = SystemProperties.getUserName();
 
@@ -40,6 +51,13 @@ public class AnonymousSeed {
         }
     }
 
+    /**
+     * Retrieves the MAC addresses of all network interfaces on the machine.
+     * This method sorts the MAC addresses to ensure consistent ordering.
+     *
+     * @return a concatenated string of MAC addresses
+     * @throws SocketException if there is an error accessing network interfaces
+     */
     private static String getMacAddresses() throws SocketException {
         Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
         List<String> macAddresses = new ArrayList<>();
@@ -50,13 +68,19 @@ public class AnonymousSeed {
                 macAddresses.add(StringUtils.toEncodedString(macArray, StandardCharsets.UTF_8));
             }
         }
-        // Sort the list in case the mac addresses are returned in a different order at different times
+        // Sort the list in case the MAC addresses are returned in a different order at different times.
         Collections.sort(macAddresses);
         return StringUtils.join(macAddresses);
     }
 
+    /**
+     * Generates a UUID based on the collected system data (username, MAC addresses, machine ID).
+     * If neither the machine ID nor MAC addresses are available, a random UUID is generated instead.
+     *
+     * @return a UUID generated from the system data, or a random UUID if insufficient data is available
+     */
     public UUID generateId() {
-        // If we only have a username, then we don't have robust enough seed data so use a random ID.
+        // If we only have a username, then we don't have robust enough seed data, so use a random ID.
         if (StringUtils.isEmpty(machineId) && StringUtils.isEmpty(macAddresses)) {
             return UUID.randomUUID();
         }
