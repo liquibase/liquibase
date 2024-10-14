a3a5956e5fcbb9b8fb0de5ee32da863c8d1c0267
Carlo Dapor <carlo.dapor@mimacom.com>
2024-10-14 22:53:51 +0200

    Faulty filters that result in no matching columns lead to a CommandExecutionException.  Added logib to support case-sensitive matching.

--- liquibase-integration-tests/src/test/groovy/liquibase/command/core/GenerateChangeLogPostgresqlIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/core/GenerateChangeLogPostgresqlIntegrationTest.groovy
@@ -6,6 +6,7 @@ import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep
 import liquibase.command.core.helpers.DiffOutputControlCommandStep
 import liquibase.command.core.helpers.PreCompareCommandStep
 import liquibase.command.util.CommandUtil
+import liquibase.exception.CommandExecutionException
 import liquibase.extension.testing.testsystem.DatabaseTestSystem
 import liquibase.extension.testing.testsystem.TestSystemFactory
 import liquibase.extension.testing.testsystem.spock.LiquibaseIntegrationTest
@@ -16,9 +17,57 @@ import spock.lang.Specification
 
 @LiquibaseIntegrationTest
 class GenerateChangeLogPostgresqlIntegrationTest extends Specification {
+    // Postgresql has case-insensitive table / column names: 'TEST.foo' == 'tEsT.FoO' == 'test.FOO'
+
     @Shared
     private DatabaseTestSystem db =
             (DatabaseTestSystem) Scope.getCurrentScope().getSingleton(TestSystemFactory.class).getTestSystem("postgresql")
+
+    def "Should export full database table TEST when neither excludeObjects nor includeObjects are used"() {
+        given:
+        db.executeSql("""
+CREATE TABLE "TEST" (
+    AFOO VARCHAR(4),
+    BFOO VARCHAR(4),
+    FOO  VARCHAR(4),
+    FOOL VARCHAR(4)
+);
+INSERT INTO "TEST" (AFOO, BFOO, FOO, FOOL) VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
+COMMIT;
+""")
+
+        when:
+        def outputFileName = 'test/test-classes/output.postgresql.sql'
+        CommandScope commandScope = new CommandScope(GenerateChangelogCommandStep.COMMAND_NAME)
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, db.getConnectionUrl())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, db.getUsername())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
+        OutputStream outputStream = new ByteArrayOutputStream()
+        commandScope.setOutput(outputStream)
+        commandScope.execute()
+
+        then:
+        def outputFile = new File(outputFileName)
+        def contents = FileUtil.getContents(outputFile)
+        contents.contains("""
+INSERT INTO "public"."TEST" ("afoo", "bfoo", "foo", "fool") VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
+"""
+        )
+
+        when:
+        CommandUtil.runDropAll(db)
+
+        then:
+        noExceptionThrown()
+
+        cleanup:
+        CommandUtil.runDropAll(db)
+        outputFile.delete()
+    }
 
     def "Should export database table TEST by applying includeObjects filter"() {
         given:
@@ -54,7 +103,7 @@ COMMIT;
         contents.contains("""
 INSERT INTO "public"."TEST" ("afoo", "bfoo", "fool") VALUES ('AFOO', 'BFOO', 'FOOL');
 """
-)
+        )
 
         when:
         CommandUtil.runDropAll(db)
@@ -64,10 +113,9 @@ INSERT INTO "public"."TEST" ("afoo", "bfoo", "fool") VALUES ('AFOO', 'BFOO', 'FO
 
         cleanup:
         CommandUtil.runDropAll(db)
-        outputFile.delete()
     }
 
-    def "Should export database table TEST by applying excludeObjects filter"() {
+    def "Should export database table TEST by applying excludeObjects filter out some columns"() {
         given:
         db.executeSql("""
 CREATE TABLE "TEST" (
@@ -90,7 +138,7 @@ COMMIT;
         commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
         commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
         commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
-        commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:.foo$') // skip 'afoo' and 'bfoo'
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:^.foo$') // skip 'afoo' and 'bfoo'
         OutputStream outputStream = new ByteArrayOutputStream()
         commandScope.setOutput(outputStream)
         commandScope.execute()
@@ -114,7 +162,7 @@ INSERT INTO "public"."TEST" ("foo", "fool") VALUES ('FOO', 'FOOL');
         outputFile.delete()
     }
 
-    def "Should export full database table TEST excludeObjects filter produces NO columns"() {
+    def "Should export table TEST when excludeObjects filters case-insensitively"() {
         given:
         db.executeSql("""
 CREATE TABLE "TEST" (
@@ -137,7 +185,7 @@ COMMIT;
         commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
         commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
         commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
-        commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:.*') // ALL columns are excluded, this is a mistake => ALL columns are taken
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:^.FoO$') // skip 'afoo' and 'bfoo'
         OutputStream outputStream = new ByteArrayOutputStream()
         commandScope.setOutput(outputStream)
         commandScope.execute()
@@ -146,7 +194,7 @@ COMMIT;
         def outputFile = new File(outputFileName)
         def contents = FileUtil.getContents(outputFile)
         contents.contains("""
-INSERT INTO "public"."TEST" ("afoo", "bfoo", "foo", "fool") VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
+INSERT INTO "public"."TEST" ("foo", "fool") VALUES ('FOO', 'FOOL');
 """
         )
 
@@ -161,8 +209,55 @@ INSERT INTO "public"."TEST" ("afoo", "bfoo", "foo", "fool") VALUES ('AFOO', 'BFO
         outputFile.delete()
     }
 
+    def "Should export table TEST when excludeObjects filters case-sensitively"() {
+        // Quoted columns is a form of case-sensitive columns.
+        given:
+        db.executeSql("""
+CREATE TABLE "TEST" (
+    "AFOO" VARCHAR(4),
+    "BFOO" VARCHAR(4),
+    "FOO"  VARCHAR(4),
+    "FOOL" VARCHAR(4)
+);
+INSERT INTO "TEST" ("AFOO", "BFOO", "FOO", "FOOL") VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
+COMMIT;
+""")
 
-    def "Should export full database table TEST when neither excludeObjects not includeObjects are used"() {
+        when:
+        def outputFileName = 'test/test-classes/output.postgresql.sql'
+        CommandScope commandScope = new CommandScope(GenerateChangelogCommandStep.COMMAND_NAME)
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, db.getConnectionUrl())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, db.getUsername())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:^.FoO$') // skip 'afoo' and 'bfoo'
+        OutputStream outputStream = new ByteArrayOutputStream()
+        commandScope.setOutput(outputStream)
+        commandScope.execute()
+
+        then:
+        def outputFile = new File(outputFileName)
+        def contents = FileUtil.getContents(outputFile)
+        contents.contains("""
+INSERT INTO "public"."TEST" ("FOO", "FOOL") VALUES ('FOO', 'FOOL');
+"""
+        )
+
+        when:
+        CommandUtil.runDropAll(db)
+
+        then:
+        noExceptionThrown()
+
+        cleanup:
+        CommandUtil.runDropAll(db)
+        //outputFile.delete()
+    }
+
+    def "Should throw an Exception when excludeObjects filter produces NO columns"() {
         given:
         db.executeSql("""
 CREATE TABLE "TEST" (
@@ -185,17 +280,14 @@ COMMIT;
         commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
         commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
         commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:^.*') // ALL columns are excluded, this is a mistake
         OutputStream outputStream = new ByteArrayOutputStream()
         commandScope.setOutput(outputStream)
         commandScope.execute()
 
         then:
-        def outputFile = new File(outputFileName)
-        def contents = FileUtil.getContents(outputFile)
-        contents.contains("""
-INSERT INTO "public"."TEST" ("afoo", "bfoo", "foo", "fool") VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
-"""
-        )
+        def e = thrown(CommandExecutionException)
+        e.message.contains("No columns matched with excludeObjects 'column:^.*' / includeObjects 'null'")
 
         when:
         CommandUtil.runDropAll(db)
@@ -205,6 +297,5 @@ INSERT INTO "public"."TEST" ("afoo", "bfoo", "foo", "fool") VALUES ('AFOO', 'BFO
 
         cleanup:
         CommandUtil.runDropAll(db)
-        outputFile.delete()
     }
 }
