1655e6829bcbeedc3edca95af7d2057907c145e1
Carlo Dapor <carlo.dapor@mimacom.com>
2024-10-14 19:28:42 +0200

    Incorporated previous logic to minimise query impact.

--- liquibase-integration-tests/src/test/groovy/liquibase/command/core/GenerateChangeLogPostgresqlIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/core/GenerateChangeLogPostgresqlIntegrationTest.groovy
@@ -41,7 +41,7 @@ COMMIT;
         commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
         commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
         commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
-        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, "data")
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
         commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
         commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_OBJECTS, "table:TEST, column:(?!foo\$).*") // skip only 'foo'
         OutputStream outputStream = new ByteArrayOutputStream()
@@ -88,7 +88,7 @@ COMMIT;
         commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
         commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
         commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
-        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, "data")
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
         commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
         commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:.foo$') // skip 'afoo' and 'bfoo'
         OutputStream outputStream = new ByteArrayOutputStream()
@@ -100,6 +100,100 @@ COMMIT;
         def contents = FileUtil.getContents(outputFile)
         contents.contains("""
 INSERT INTO "public"."TEST" ("foo", "fool") VALUES ('FOO', 'FOOL');
+"""
+        )
+
+        when:
+        CommandUtil.runDropAll(db)
+
+        then:
+        noExceptionThrown()
+
+        cleanup:
+        CommandUtil.runDropAll(db)
+        outputFile.delete()
+    }
+
+    def "Should export full database table TEST excludeObjects filter produces NO columns"() {
+        given:
+        db.executeSql("""
+CREATE TABLE "TEST" (
+    AFOO VARCHAR(4),
+    BFOO VARCHAR(4),
+    FOO  VARCHAR(4),
+    FOOL VARCHAR(4)
+);
+INSERT INTO "TEST" (AFOO, BFOO, FOO, FOOL) VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
+COMMIT;
+""")
+
+        when:
+        def outputFileName = 'test/test-classes/output.postgresql.sql'
+        CommandScope commandScope = new CommandScope(GenerateChangelogCommandStep.COMMAND_NAME)
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, db.getConnectionUrl())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, db.getUsername())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.EXCLUDE_OBJECTS, 'column:.*') // ALL columns are excluded, this is a mistake => ALL columns are taken
+        OutputStream outputStream = new ByteArrayOutputStream()
+        commandScope.setOutput(outputStream)
+        commandScope.execute()
+
+        then:
+        def outputFile = new File(outputFileName)
+        def contents = FileUtil.getContents(outputFile)
+        contents.contains("""
+INSERT INTO "public"."TEST" ("afoo", "bfoo", "foo", "fool") VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
+"""
+        )
+
+        when:
+        CommandUtil.runDropAll(db)
+
+        then:
+        noExceptionThrown()
+
+        cleanup:
+        CommandUtil.runDropAll(db)
+        outputFile.delete()
+    }
+
+
+    def "Should export full database table TEST when neither excludeObjects not includeObjects are used"() {
+        given:
+        db.executeSql("""
+CREATE TABLE "TEST" (
+    AFOO VARCHAR(4),
+    BFOO VARCHAR(4),
+    FOO  VARCHAR(4),
+    FOOL VARCHAR(4)
+);
+INSERT INTO "TEST" (AFOO, BFOO, FOO, FOOL) VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
+COMMIT;
+""")
+
+        when:
+        def outputFileName = 'test/test-classes/output.postgresql.sql'
+        CommandScope commandScope = new CommandScope(GenerateChangelogCommandStep.COMMAND_NAME)
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, db.getConnectionUrl())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, db.getUsername())
+        commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, db.getPassword())
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.OVERWRITE_OUTPUT_FILE_ARG, true)
+        commandScope.addArgumentValue(GenerateChangelogCommandStep.CHANGELOG_FILE_ARG, outputFileName)
+        commandScope.addArgumentValue(PreCompareCommandStep.DIFF_TYPES_ARG, 'data')
+        commandScope.addArgumentValue(DiffOutputControlCommandStep.INCLUDE_SCHEMA_ARG, true)
+        OutputStream outputStream = new ByteArrayOutputStream()
+        commandScope.setOutput(outputStream)
+        commandScope.execute()
+
+        then:
+        def outputFile = new File(outputFileName)
+        def contents = FileUtil.getContents(outputFile)
+        contents.contains("""
+INSERT INTO "public"."TEST" ("afoo", "bfoo", "foo", "fool") VALUES ('AFOO', 'BFOO', 'FOO', 'FOOL');
 """
         )
 
--- liquibase-standard/src/main/java/liquibase/diff/output/changelog/core/MissingDataChangeGenerator.java
+++ liquibase-standard/src/main/java/liquibase/diff/output/changelog/core/MissingDataChangeGenerator.java
@@ -1,5 +1,7 @@
 package liquibase.diff.output.changelog.core;
 
+import liquibase.Scope;
+import liquibase.ScopeManager;
 import liquibase.change.Change;
 import liquibase.change.ColumnConfig;
 import liquibase.change.core.InsertDataChange;
@@ -14,13 +16,20 @@ import liquibase.diff.output.changelog.MissingObjectChangeGenerator;
 import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.statement.DatabaseFunction;
 import liquibase.structure.DatabaseObject;
-import liquibase.structure.core.*;
+import liquibase.structure.core.Data;
+import liquibase.structure.core.ForeignKey;
+import liquibase.structure.core.Index;
+import liquibase.structure.core.PrimaryKey;
+import liquibase.structure.core.Table;
 import liquibase.util.JdbcUtil;
+
+import org.apache.commons.lang3.StringUtils;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -62,24 +71,46 @@ public class MissingDataChangeGenerator extends AbstractChangeGenerator implemen
                 return null;
             }
 
+            String excludeObjects = outputControl.getExcludeObjects();
+            String includeObjects = outputControl.getIncludeObjects();
+            List<String> columnNames = Collections.emptyList();
             JdbcConnection connection = (JdbcConnection) referenceDatabase.getConnection();
-            // first identify all column names dynamically w/ "SELECT * FROM table WHERE 1=0"
-            String sql = "SELECT * FROM " + referenceDatabase.escapeTableName(table.getSchema().getCatalogName(), table.getSchema().getName(), table.getName()) + " WHERE 1=0";
-            stmt = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setFetchSize(1);
-            rs = stmt.executeQuery(sql);
+            String sql = "SELECT * FROM " + referenceDatabase.escapeTableName(table.getSchema().getCatalogName(), table.getSchema().getName(), table.getName());
 
-            List<String> columnNames = identifyColumnNames(rs, outputControl);
-            // replace "*" in SELECT statement with list of filtered column names, also remove WHERE-clause
-            sql = sql.replace("*", String.join(",", columnNames));
+            if (StringUtils.isNotEmpty(excludeObjects) || StringUtils.isNotEmpty(includeObjects)) {
+                String queryToRetrieveTableMetaDataOnly = sql + " WHERE 1=0";
+                stmt = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+                stmt.setFetchSize(1);
+                rs = stmt.executeQuery(queryToRetrieveTableMetaDataOnly);
+                // filter which column names to use for export below
+                columnNames = identifyColumnNames(rs, excludeObjects, includeObjects);
 
-            // remove WHERE-clause
-            sql = sql.replace(" WHERE 1=0", "");
+                // the filtering _may_ have
+                //   a) excluded ALL columns
+                //   b) included NO columns
+                if (columnNames.isEmpty()) {
+                    Scope.getCurrentScope().getLog(MissingObjectChangeGenerator.class).warning("Filtering w/ excludeObjects or includeObjects had no effect, thus ALL columns are considered.");
+                }
+                else {
+                    // replace "*" in original SELECT statement with list of filtered column names
+                    sql = sql.replace("*", String.join(",", columnNames));
+                }
+            }
+
             stmt = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
             stmt.setFetchSize(1000);
             rs = stmt.executeQuery(sql);
 
+            // identify all column names
+            if (columnNames.isEmpty()) {
+                columnNames = new ArrayList<>();
+                for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                    columnNames.add(rs.getMetaData().getColumnName(i + 1));
+                }
+            }
+
             List<Change> changes = new ArrayList<>();
+
             while (rs.next()) {
                 InsertDataChange change = new InsertDataChange();
                 if (outputControl.getIncludeCatalog()) {
@@ -139,28 +170,27 @@ public class MissingDataChangeGenerator extends AbstractChangeGenerator implemen
         }
     }
 
-    private List<String> identifyColumnNames (final ResultSet rs, final DiffOutputControl outputControl) throws SQLException {
+    private List<String> identifyColumnNames (final ResultSet rs, String excludeObjects, String includeObjects) throws SQLException {
         List<String> columnNames = new ArrayList<>();
-        String includeObjects = extractColumnInfo(outputControl.getIncludeObjects());
-        String excludeObjects = extractColumnInfo(outputControl.getExcludeObjects());
-        boolean withIncludeAll = Objects.isNull(excludeObjects) && Objects.isNull(includeObjects);
-        boolean withIncludeOnly = Objects.nonNull(includeObjects);
+        excludeObjects = extractColumnInfo(excludeObjects);
+        includeObjects = extractColumnInfo(includeObjects);
         boolean withExcludeOnly = Objects.nonNull(excludeObjects);
+        boolean withIncludeOnly = Objects.nonNull(includeObjects);
         Pattern pattern = null;
-
-        if (withIncludeOnly) {
-            pattern = Pattern.compile(includeObjects);
-        }
 
         if (withExcludeOnly) {
             pattern = Pattern.compile(excludeObjects);
         }
 
-        // filter by "exportObjects" pattern
+        if (withIncludeOnly) {
+            pattern = Pattern.compile(includeObjects);
+        }
+
         for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) {
             String columnName = rs.getMetaData().getColumnName(i);
-            if (withIncludeAll ||
-                (withIncludeOnly && pattern.matcher(columnName).matches()) ||
+
+            // only collect columns which are either explicitly "included" or "not excluded"
+            if ((withIncludeOnly && pattern.matcher(columnName).matches()) ||
                 (withExcludeOnly && !pattern.matcher(columnName).matches()))
             {
                 columnNames.add(columnName);
