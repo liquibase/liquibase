10915f27d6b00149ba991bda64796c3fd5e98f3e
filipe <flautert@liquibase.org>
2024-10-18 17:38:29 -0300

    chore: improve error message trying to guess checksum algorithm by size.

--- liquibase-standard/src/main/java/liquibase/checksums/ChecksumAlgorithm.java
+++ liquibase-standard/src/main/java/liquibase/checksums/ChecksumAlgorithm.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ChecksumAlgorithm {
-    MD5("MD5"),
-    SHA1("SHA-1"),
-    SHA256("SHA-256");
+    MD5("MD5", 32),
+    SHA1("SHA-1", 40),
+    SHA256("SHA-256", 64);
 
     private final String algorithm;
+
+    private final int size;
 
 }
