93b5d9ee39c8398cd64f77b7054f06bd1ef0ce3c
Alex <abrackx@gmail.com>
2024-10-23 13:02:52 -0600

    Allow for scopemanager to be shared between threads created by liquibase (DAT-18871) (#6446)
    [DAT-18871] Use InhertiableThreadLocal to allow for scopemanager to be carried over between threads created by liquibase.

--- liquibase-standard/src/main/java/liquibase/Scope.java
+++ liquibase-standard/src/main/java/liquibase/Scope.java
@@ -78,7 +78,7 @@ public class Scope {
 
     public static final String JAVA_PROPERTIES = "javaProperties";
 
-    private static final ThreadLocal<ScopeManager> scopeManager = new ThreadLocal<>();
+    private static final InheritableThreadLocal<ScopeManager> scopeManager = new InheritableThreadLocal<>();
 
     private final Scope parent;
     private final SmartMap values = new SmartMap();
