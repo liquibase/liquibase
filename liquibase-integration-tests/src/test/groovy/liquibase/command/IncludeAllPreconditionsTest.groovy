a16b2337ff68ffa0496cba075b06d7ffee080cb3
Daniel Mallorga <dmallorga@liquibase.com>
2024-11-28 14:39:55 -0300

    IncludeAll integration tests added for onFail CONTINUE and HALT values.

--- liquibase-integration-tests/src/test/groovy/liquibase/command/IncludeAllPreconditionsTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/IncludeAllPreconditionsTest.groovy
@@ -6,6 +6,8 @@ import liquibase.command.core.UpdateCommandStep
 import liquibase.command.core.helpers.DatabaseChangelogCommandStep
 import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep
 import liquibase.command.util.CommandUtil
+import liquibase.exception.ChangeLogParseException
+import liquibase.exception.PreconditionFailedException
 import liquibase.extension.testing.testsystem.DatabaseTestSystem
 import liquibase.extension.testing.testsystem.TestSystemFactory
 import liquibase.extension.testing.testsystem.spock.LiquibaseIntegrationTest
@@ -26,7 +28,7 @@ class IncludeAllPreconditionsTest extends Specification {
 
     def "run include with preconditions fail option WARN"() {
         when:
-        String changelogFile = "changelogs/h2/includeAll/master.xml"
+        String changelogFile = "changelogs/h2/includeAll/master-warn.xml"
         def changelog =
                 DatabaseChangelogCommandStep.getDatabaseChangeLog(changelogFile, new ChangeLogParameters(), h2.getDatabaseFromFactory())
         def resourceAccessor = new SearchPathResourceAccessor("target/test-classes")
@@ -54,6 +56,61 @@ class IncludeAllPreconditionsTest extends Specification {
         includedFiles.size() == 3
         includedFiles.count("included") == 1
         includedFiles.count("test_precondition") == 1
-        includedFiles.count("changelogs/h2/includeAll/master.xml") == 1
+        includedFiles.count("changelogs/h2/includeAll/master-warn.xml") == 1
+    }
+
+    def "run include with preconditions fail option HALT"() {
+        when:
+        String changelogFile = "changelogs/h2/includeAll/master-halt.xml"
+        def changelog =
+                DatabaseChangelogCommandStep.getDatabaseChangeLog(changelogFile, new ChangeLogParameters(), h2.getDatabaseFromFactory())
+        def resourceAccessor = new SearchPathResourceAccessor("target/test-classes")
+        def scopeSettings = [
+                (Scope.Attr.resourceAccessor.name()) : resourceAccessor
+        ]
+
+        Scope.child(scopeSettings, {
+            def updateCommand = new CommandScope(UpdateCommandStep.COMMAND_NAME)
+            updateCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.DATABASE_ARG, h2.getDatabaseFromFactory())
+            updateCommand.addArgumentValue(UpdateCommandStep.CHANGELOG_ARG, changelog)
+            updateCommand.execute()
+        } as Scope.ScopedRunner)
+        then:
+        def changeLogParseException = thrown(ChangeLogParseException)
+        def preconditionException = changeLogParseException.getCause().getCause();
+        preconditionException.class == PreconditionFailedException.class
+        preconditionException.getMessage() == "Preconditions Failed"
+    }
+
+    def "run include with preconditions fail option CONTINUE"() {
+        when:
+        String changelogFile = "changelogs/h2/includeAll/master-continue.xml"
+        def changelog =
+                DatabaseChangelogCommandStep.getDatabaseChangeLog(changelogFile, new ChangeLogParameters(), h2.getDatabaseFromFactory())
+        def resourceAccessor = new SearchPathResourceAccessor("target/test-classes")
+        def scopeSettings = [
+                (Scope.Attr.resourceAccessor.name()) : resourceAccessor
+        ]
+
+        Scope.child(scopeSettings, {
+            def updateCommand = new CommandScope(UpdateCommandStep.COMMAND_NAME)
+            updateCommand.addArgumentValue(DbUrlConnectionArgumentsCommandStep.DATABASE_ARG, h2.getDatabaseFromFactory())
+            updateCommand.addArgumentValue(UpdateCommandStep.CHANGELOG_ARG, changelog)
+            updateCommand.execute()
+        } as Scope.ScopedRunner)
+        def changelogResultSet = h2.getConnection().createStatement().executeQuery("select * from databasechangelog")
+        h2.getConnection().createStatement().executeQuery("select * from INCLUDED_TABLE")
+        h2.getConnection().createStatement().executeQuery("select * from DEFAULT_TABLE")
+
+        then:
+        noExceptionThrown()
+        List<String> includedFiles = new ArrayList<>(2)
+        while (changelogResultSet.next()) {
+            def filename = changelogResultSet.getString("filename")
+            includedFiles.add(filename)
+        }
+        includedFiles.size() == 2
+        includedFiles.count("included") == 1
+        includedFiles.count("changelogs/h2/includeAll/master-continue.xml") == 1
     }
 }
