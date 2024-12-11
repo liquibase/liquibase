a4da538d7d9970deae977ff4fd01bcbf1b254836
Alex Brackx <abrackx@gmail.com>
2024-12-11 09:01:42 -0700

    [DAT-18890] Copy files using copyLarge. Format file.

--- liquibase-standard/src/main/java/liquibase/command/copy/FileSystemProjectCopier.java
+++ liquibase-standard/src/main/java/liquibase/command/copy/FileSystemProjectCopier.java
@@ -1,9 +1,11 @@
 package liquibase.command.copy;
 
 import liquibase.exception.UnexpectedLiquibaseException;
-import liquibase.util.FileUtil;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileSystemProjectCopier implements ProjectCopier {
     @Override
@@ -28,41 +30,37 @@ public class FileSystemProjectCopier implements ProjectCopier {
     }
 
     /**
-     *
      * Create the project directory if it does not exist
      * For the local file system implementation of ProjectCopier this will not
      * be a temporary directory, so the keepTempFiles flag is ignored
      *
-     * @param   projectDir                         The project directory
-     * @param   keepTempFiles                      *** IGNORED ***
-     * @return  File
-     *
+     * @param projectDir    The project directory
+     * @param keepTempFiles *** IGNORED ***
+     * @return File
      */
     @Override
     public File createWorkingStorage(String projectDir, boolean keepTempFiles) {
         File projectDirFile = new File(projectDir);
         boolean b = projectDirFile.mkdirs();
-        if (! b && ! projectDirFile.exists()) {
+        if (!b && !projectDirFile.exists()) {
             throw new UnexpectedLiquibaseException("Unable to create project directory '" + projectDir + "'");
         }
         return projectDirFile;
     }
 
     /**
-     *
      * Copy files from the source to the remote location
      * This is a no-op currently for this implementation of ProjectCopier
      *
-     * @param source             The source directory
-     * @param target             The target directory
-     * @param recursive          Flag for copying recursively
-     *
+     * @param source    The source directory
+     * @param target    The target directory
+     * @param recursive Flag for copying recursively
      */
     @Override
     public void copy(String source, String target, boolean recursive) {
-        try {
-            String contents = FileUtil.getContents(new File(source));
-            FileUtil.write(contents, new File(target));
+        try (InputStream input = Files.newInputStream(Paths.get(source));
+             OutputStream output = Files.newOutputStream(Paths.get(target))) {
+            IOUtils.copyLarge(input, output);
         } catch (Exception e) {
             throw new UnexpectedLiquibaseException("Unable to copy file(s)! Make sure you are targeting a valid path for the new file.", e);
         }
