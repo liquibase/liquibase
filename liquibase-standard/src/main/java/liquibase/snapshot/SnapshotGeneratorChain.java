ec623c67825614abac410ca0e6b1b2ad41c776f2
filipe <flautert@liquibase.org>
2025-01-22 21:42:04 -0300

    chore: remove first generator logic

--- liquibase-standard/src/main/java/liquibase/snapshot/SnapshotGeneratorChain.java
+++ liquibase-standard/src/main/java/liquibase/snapshot/SnapshotGeneratorChain.java
@@ -32,13 +32,8 @@ public class SnapshotGeneratorChain {
 
     /**
      * This calls all the non-replaced {@link SnapshotGenerator} in the chain, by comparison order
-     * Only the first generator in the chain is allowed to create a new instance of T
-     * Subsequent generators must modify the instance or call the chain if the provided object is not handled,
-     * otherwise a {@link DatabaseException} is thrown
      *
      * @return snapshot object
-     * @throws DatabaseException if any of the subsequent generators return an instance different from the first generator's
-     *                           invocation result
      * @see SnapshotGenerator#replaces() to skip generators that do not comply to the above requireemnts
      */
     public <T extends DatabaseObject> T snapshot(T example, DatabaseSnapshot snapshot)
@@ -59,39 +54,18 @@ public class SnapshotGeneratorChain {
             return null;
         }
 
-        boolean firstRoundDone = false;
-        SnapshotGenerator lastGenerator = null;
-        T lastObject = example;
+        T objectToSnapshot = example;
         while (snapshotGenerators.hasNext()) {
             SnapshotGenerator generator = snapshotGenerators.next();
             if (replacedGenerators.contains(generator.getClass())) {
                 continue;
             }
-            T object = generator.snapshot(lastObject, snapshot, this);
+            T object = generator.snapshot(objectToSnapshot, snapshot, this);
             if ((object != null) && (object.getSnapshotId() == null)) {
                 object.setSnapshotId(snapshotIdService.generateId());
             }
-            // only first generator in the chain is allowed to create new instances - subsequent ones are not
-            if (firstRoundDone && object != lastObject) {
-                throw new DatabaseException(String.format("Snapshot generator %s has returned a different reference from the previous generator %s.\n" +
-                                "\tSnapshot object was: %s, it is now: %s.\n" +
-                                "\tConsider declaring %1$s as being replaced by one the generator in the chain via liquibase.snapshot.SnapshotGenerator#replaces.",
-                        generator.getClass().getName(),
-                        lastGenerator.getClass().getName(),
-                        identity(lastObject),
-                        identity(object)));
-            }
-            lastObject = object;
-            lastGenerator = generator;
-            firstRoundDone = true;
+            objectToSnapshot = object;
         }
-        return lastObject;
-    }
-
-    private static String identity(Object object) {
-        if (object == null) {
-            return "null";
-        }
-        return String.format("%s@%s", object.getClass(), System.identityHashCode(object));
+        return objectToSnapshot;
     }
 }
