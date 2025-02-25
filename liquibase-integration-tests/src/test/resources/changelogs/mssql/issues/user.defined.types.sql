8cd4a8c7fde4feaa6dbc0932879d7f5ef606c682
Alex Brackx <abrackx@gmail.com>
2025-02-25 08:26:26 -0700

    [DAT-19150] Fix test

--- liquibase-integration-tests/src/test/resources/changelogs/mssql/issues/user.defined.types.sql
+++ liquibase-integration-tests/src/test/resources/changelogs/mssql/issues/user.defined.types.sql
@@ -1,7 +1,8 @@
+--liquibase formatted sql
 --changeset user:add-type
-CREATE TYPE dbo.Flag FROM bit NOT NULL
+CREATE TYPE dbo.Flag FROM bit NOT NULL;
 
 --changeset user:use-type
 CREATE TABLE dbo.test(
     flag Flag not null
-)
+);
