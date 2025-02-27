9755b534a9c388e0bde9165323f45e05fc6040d4
filipe <flautert@liquibase.org>
2025-02-27 16:15:03 -0300

    feat: springboot test module with 1 test

---
+++ liquibase-spring/src/main/java/org/liquibase/SpringBootTestProject.java
@@ -0,0 +1,11 @@
+package org.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestProject {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestProject.class, args);
+    }
+}
