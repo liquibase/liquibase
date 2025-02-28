fc4cf6512d72dfbebc7f67a330a895ea32f1e361
Daniel Mallorga <dmallorga@liquibase.com>
2025-02-28 16:18:42 -0300

    Undo lower function call from getConnectionSchemaNameCallStatement.

--- liquibase-standard/src/main/java/liquibase/database/core/PostgresDatabase.java
+++ liquibase-standard/src/main/java/liquibase/database/core/PostgresDatabase.java
@@ -345,7 +345,7 @@ public class PostgresDatabase extends AbstractJdbcDatabase {
 
     @Override
     protected SqlStatement getConnectionSchemaNameCallStatement() {
-        return new RawCallStatement("select lower(current_schema())");
+        return new RawCallStatement("select current_schema()");
     }
 
 //    @Override
