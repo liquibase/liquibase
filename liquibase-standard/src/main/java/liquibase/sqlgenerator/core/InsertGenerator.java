970e73875635680af2773df9c7503b7f84a9b5af
Steven Massaro <steven.massaro.web@gmail.com>
2025-03-05 11:51:22 -0600

    refactor InsertGenerator so that it can be overridden more easily (DAT-19678) (#6775)

--- liquibase-standard/src/main/java/liquibase/sqlgenerator/core/InsertGenerator.java
+++ liquibase-standard/src/main/java/liquibase/sqlgenerator/core/InsertGenerator.java
@@ -67,24 +67,7 @@ public class InsertGenerator extends AbstractSqlGenerator<InsertStatement> {
 
         for (String column : statement.getColumnValues().keySet()) {
             Object newValue = statement.getColumnValues().get(column);
-            if ((newValue == null) || "NULL".equalsIgnoreCase(newValue.toString())) {
-                sql.append("NULL");
-            } else if ((newValue instanceof String) && !looksLikeFunctionCall(((String) newValue), database)) {
-                sql.append(DataTypeFactory.getInstance().fromObject(newValue, database).objectToSql(newValue, database));
-            } else if (newValue instanceof Date) {
-                sql.append(database.getDateLiteral(((Date) newValue)));
-            } else if (newValue instanceof Boolean) {
-                if (((Boolean) newValue)) {
-                    sql.append(DataTypeFactory.getInstance().getTrueBooleanValue(database));
-                } else {
-                    sql.append(DataTypeFactory.getInstance().getFalseBooleanValue(database));
-                }
-            } else if (newValue instanceof DatabaseFunction) {
-                sql.append(database.generateDatabaseFunctionValue((DatabaseFunction) newValue));
-            }
-            else {
-                sql.append(newValue);
-            }
+            appendValue(sql, database, newValue);
             sql.append(", ");
         }
 
@@ -95,6 +78,27 @@ public class InsertGenerator extends AbstractSqlGenerator<InsertStatement> {
         }
 
         sql.append(")");
+    }
+
+    public void appendValue(StringBuilder sql, Database database, Object newValue) {
+        if ((newValue == null) || "NULL".equalsIgnoreCase(newValue.toString())) {
+            sql.append("NULL");
+        } else if ((newValue instanceof String) && !looksLikeFunctionCall(((String) newValue), database)) {
+            sql.append(DataTypeFactory.getInstance().fromObject(newValue, database).objectToSql(newValue, database));
+        } else if (newValue instanceof Date) {
+            sql.append(database.getDateLiteral(((Date) newValue)));
+        } else if (newValue instanceof Boolean) {
+            if (((Boolean) newValue)) {
+                sql.append(DataTypeFactory.getInstance().getTrueBooleanValue(database));
+            } else {
+                sql.append(DataTypeFactory.getInstance().getFalseBooleanValue(database));
+            }
+        } else if (newValue instanceof DatabaseFunction) {
+            sql.append(database.generateDatabaseFunctionValue((DatabaseFunction) newValue));
+        }
+        else {
+            sql.append(newValue);
+        }
     }
 
 
