b4ce4e44ff59e8e44b7aef338446fd3fbcf5509e
Daniel Mallorga <dmallorga@liquibase.com>
2025-03-06 13:30:54 -0300

    Un-ignore ChangeLogIncludeAllTest and update its tests.

--- liquibase-standard/src/test/groovy/liquibase/changelog/ChangeLogIncludeAllTest.groovy
+++ liquibase-standard/src/test/groovy/liquibase/changelog/ChangeLogIncludeAllTest.groovy
@@ -1,17 +1,25 @@
 package liquibase.changelog
 
 import liquibase.include.ChangeLogIncludeAll
+import liquibase.include.StandardIncludeService
+import liquibase.parser.core.ParsedNode
+import liquibase.sdk.resource.MockResourceAccessor
 import spock.lang.Ignore
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
-@Ignore
 class ChangeLogIncludeAllTest extends Specification {
+
+    @Shared
+    def includeAll = new StandardIncludeService()
+            .createChangelogIncludeAll(new ParsedNode(null, "includeAll").addChildren([path: "testPath"]),
+                    new MockResourceAccessor(), null, new HashMap<String, Object>())
 
     @Unroll
     def getSerializableFields() {
         when:
-        def changeLogIncludeAll = new ChangeLogIncludeAll()
+        def changeLogIncludeAll = includeAll
         
         then:
         changeLogIncludeAll.getSerializableFields() != null
@@ -20,14 +28,14 @@ class ChangeLogIncludeAllTest extends Specification {
     @Unroll
     def getSerializedObjectName() {
         when:
-        def changeLogIncludeAll = new ChangeLogIncludeAll()
+        def changeLogIncludeAll = includeAll
         then:
         changeLogIncludeAll.getSerializedObjectName() != null
     }
 
     def getSerializedObjectNamespace() {
         when:
-        def changeLogIncludeAll = new ChangeLogIncludeAll()
+        def changeLogIncludeAll = includeAll
         then:
         changeLogIncludeAll.getSerializedObjectNamespace() != null
     }
