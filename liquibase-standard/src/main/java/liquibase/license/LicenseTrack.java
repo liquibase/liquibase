49c0078e2e291657156b87b3671232419579c165
Steven Massaro <steven.massaro.web@gmail.com>
2025-03-10 08:35:47 -0500

    fix issue associated with not having a license key

--- liquibase-standard/src/main/java/liquibase/license/LicenseTrack.java
+++ liquibase-standard/src/main/java/liquibase/license/LicenseTrack.java
@@ -1,18 +1,27 @@
 package liquibase.license;
 
 import liquibase.Scope;
+import liquibase.configuration.ConfiguredValue;
 import lombok.Data;
 
 @Data
 public class LicenseTrack {
-    private String licenseKey;
+    private String licenseKey = null;
     private String jdbcUrl;
     private String schema;
     private String catalog;
 
     public LicenseTrack(String jdbcUrl, String schema, String catalog) {
-        LicenseServiceFactory license = Scope.getCurrentScope().getSingleton(LicenseServiceFactory.class);
-        this.licenseKey = license.getLicenseService().getLicenseKey().getValue();
+        LicenseServiceFactory licenseServiceFactory = Scope.getCurrentScope().getSingleton(LicenseServiceFactory.class);
+        if (licenseServiceFactory != null) {
+            LicenseService licenseService = licenseServiceFactory.getLicenseService();
+            if (licenseService != null) {
+                ConfiguredValue<String> configuredValue = licenseService.getLicenseKey();
+                if (configuredValue != null && configuredValue.found()) {
+                    this.licenseKey = configuredValue.getValue();
+                }
+            }
+        }
         this.jdbcUrl = jdbcUrl;
         this.schema = schema;
         this.catalog = catalog;
