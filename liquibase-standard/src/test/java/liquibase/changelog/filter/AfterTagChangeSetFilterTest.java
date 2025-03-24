6243595d2a56c1be131e9bb590f0fd60fa53a003
filipe <flautert@liquibase.org>
2025-03-24 17:12:59 -0300

    feat: Keep tags during changelog rollback operations
    Implement functionality to preserve tags when performing rollback operations
    in Liquibase. This ensures tag information is maintained in the database
    change history during rollback scenarios, providing better traceability
    and consistency for database version control.

--- liquibase-standard/src/test/java/liquibase/changelog/filter/AfterTagChangeSetFilterTest.java
+++ liquibase-standard/src/test/java/liquibase/changelog/filter/AfterTagChangeSetFilterTest.java
@@ -2,9 +2,10 @@ package liquibase.changelog.filter;
 
 import liquibase.TagVersionEnum;
 import liquibase.change.CheckSum;
+import liquibase.change.core.TagDatabaseChange;
 import liquibase.changelog.ChangeSet;
+import liquibase.changelog.DatabaseChangeLog;
 import liquibase.changelog.RanChangeSet;
-import liquibase.command.core.RollbackCommandStep;
 import liquibase.exception.RollbackFailedException;
 import org.junit.Test;
 
@@ -35,6 +36,34 @@ public class AfterTagChangeSetFilterTest {
 
         assertFalse(filter.accepts(new ChangeSet("1", "testAuthor", false, false, "path/changelog", null, null, null)).isAccepted());
         assertFalse(filter.accepts(new ChangeSet("2", "testAuthor", false, false, "path/changelog", null, null, null)).isAccepted());
+        assertTrue(filter.accepts(new ChangeSet("3", "testAuthor", false, false, "path/changelog", null, null, null)).isAccepted());
+
+    }
+
+    @Test
+    public void keepTag() throws Exception {
+        ArrayList<RanChangeSet> ranChanges = new ArrayList<RanChangeSet>();
+        ranChanges.add(new RanChangeSet("path/changelog", "1", "testAuthor", CheckSum.parse("12345"), new Date(), null, null, null, null, null, null, null));
+        ranChanges.add(new RanChangeSet("path/changelog", "2", "testAuthor", CheckSum.parse("12345"), new Date(), "tag1", null, "tagDatabase", null, null, null, null));
+        ranChanges.add(new RanChangeSet("path/changelog", "3", "testAuthor", CheckSum.parse("12345"), new Date(), null, null, null, null, null, null, null));
+
+        DatabaseChangeLog changeLog = new DatabaseChangeLog();
+        ranChanges.forEach(change -> {
+            ChangeSet changeSet = new ChangeSet(change.getId(), change.getAuthor(), false, false, change.getChangeLog(), null, null, changeLog );
+            if (change.getTag() != null) {
+                TagDatabaseChange tagChange = new TagDatabaseChange();
+                tagChange.setTag(change.getTag());
+                tagChange.setKeepTagOnRollback(true);
+                changeSet.addChange(tagChange);
+            }
+            changeLog.addChangeSet(changeSet);
+        });
+
+        AfterTagChangeSetFilter filter = new AfterTagChangeSetFilter("tag1", ranChanges, TagVersionEnum.OLDEST, changeLog);
+
+
+        assertFalse(filter.accepts(new ChangeSet("1", "testAuthor", false, false, "path/changelog", null, null, null)).isAccepted());
+        assertTrue(filter.accepts(new ChangeSet("2", "testAuthor", false, false, "path/changelog", null, null, null)).isAccepted());
         assertTrue(filter.accepts(new ChangeSet("3", "testAuthor", false, false, "path/changelog", null, null, null)).isAccepted());
 
     }
