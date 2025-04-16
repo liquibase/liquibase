c70497c9792382ca669771eb50fe8571691e1d1e
tatibeans <tatiana.f.ganz@gmail.com>
2025-04-16 13:46:05 -0300

    Optimise code
    .

--- liquibase-spring/src/test/java/org/liquibase/spring/LiquibaseSpringTest.java
+++ liquibase-spring/src/test/java/org/liquibase/spring/LiquibaseSpringTest.java
@@ -1,117 +1,68 @@
 package org.liquibase.spring;
 
-import javax.sql.DataSource;
 import jakarta.transaction.Transactional;
-import liquibase.Scope;
-import liquibase.command.CommandScope;
-import liquibase.command.core.DropAllCommandStep;
-import liquibase.command.core.UpdateCommandStep;
-import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep;
-import liquibase.database.Database;
-import liquibase.database.DatabaseConnection;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
-import liquibase.executor.ExecutorService;
 import liquibase.integration.spring.SpringLiquibase;
-import liquibase.listener.SqlListener;
-import liquibase.statement.core.RawParameterizedSqlStatement;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-/*import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;*/
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.sql.*;
+import java.util.*;
 
 @DataJpaTest
 @Testcontainers
 public class LiquibaseSpringTest {
     static final class DatasourceInitializer {
 
-        private static final Logger LOGGER = LoggerFactory.getLogger(DatasourceInitializer.class);
-        //
-        public static final Logger CONTAINER_LOGGER = LoggerFactory.getLogger("Containerlogger");
+        private static final Logger LOGGER = LoggerFactory.getLogger(LiquibaseSpringTest.class);
 
-        public static void initialize(DynamicPropertyRegistry registry, JdbcDatabaseContainer container) {
-            registry.add("spring.datasource.url", container::getJdbcUrl);
-            registry.add("spring.datasource.username", container::getUsername);
-            registry.add("spring.datasource.password", container::getPassword);
-//            logDatasourceInfo(container);
-        }
-
-        private static void logDatasourceInfo(JdbcDatabaseContainer container) {
-            LOGGER.info("################################################");
-            LOGGER.info("Using datasource config for tests: ");
-            LOGGER.info("URL     : {}", container.getJdbcUrl());
-            LOGGER.info("User    : {}", container.getUsername());
-            LOGGER.info("Password: {}", container.getPassword());
-            LOGGER.info("################################################");
+        @Container
+        protected static final PostgreSQLContainer TEST_DB_POSTGRES = new PostgreSQLContainer<>("postgres:16").withDatabaseName("lbcat");
+        public static void initialize(DynamicPropertyRegistry registry) {
+            registry.add("spring.datasource.url", TEST_DB_POSTGRES::getJdbcUrl);
+            registry.add("spring.datasource.username", TEST_DB_POSTGRES::getUsername);
+            registry.add("spring.datasource.password", TEST_DB_POSTGRES::getPassword);
         }
     }
 
     @Autowired
     private SpringLiquibase liquibase;
 
-/*    @Autowired
-    private DataSource dataSource;*/
-
-    @Container
-    protected static final PostgreSQLContainer MY_TEST_DB = new PostgreSQLContainer<>("postgres:16")
-            // .withLogConsumer(new Slf4jLogConsumer(DatasourceInitializer.CONTAINER_LOGGER))
-            .withDatabaseName("lbcat");
-
-    @DynamicPropertySource
-    static void jdbcProperties(DynamicPropertyRegistry registry) {
-        DatasourceInitializer.initialize(registry, MY_TEST_DB);
+    @BeforeEach
+    void runLiquibaseMigrations() throws LiquibaseException {
+        liquibase.afterPropertiesSet();
     }
+
 
     @Test
     @Transactional(Transactional.TxType.NEVER)
-    void migrationAddsVweNummer() throws LiquibaseException, SQLException {
-        liquibase.afterPropertiesSet();
+    @DisplayName("Should create expected tables and constraints, and verify column details")
+    void shouldVerifyLiquibaseSchemaChanges() throws LiquibaseException, SQLException {
+
+        try(Connection conn = liquibase.getDataSource().getConnection(); Statement stmt = conn.createStatement()) {
+
+        } catch (Exception x) {
+
+        }
 
         ResultSet resultSet = liquibase.getDataSource().getConnection().createStatement().executeQuery("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';");
         ResultSetMetaData md = resultSet.getMetaData();
 
         int numCols = md.getColumnCount();
 
-        List<String> colNames = IntStream.range(0, numCols)
-                .mapToObj(i -> {
-                    try {
-                        return md.getColumnName(i + 1);
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                        return "?";
-                    }
-                })
-                .collect(Collectors.toList());
-
-
         String[] tableNames = {"databasechangelog","databasechangeloglock","city","person","department"};
         for (int i = 0; resultSet.next(); i++){
             Assert.assertEquals(tableNames[i],resultSet.getObject("tablename"));
-            //LoggerFactory.getLogger(DatasourceInitializer.class).info(String.valueOf(i));
             switch (i) {
                 case 0:
                     ResultSet getRowCount = liquibase.getDataSource().getConnection().createStatement().executeQuery("SELECT COUNT(*) FROM databasechangelog;");
@@ -177,33 +128,80 @@ public class LiquibaseSpringTest {
                         Assert.assertEquals(indexNames[j],getIndexes.getString("index_name"));
                         Assert.assertEquals(involvedColumns[j],getIndexes.getString("column_names"));
                     }
-
+                    getIndexes.close();
                     break;
             }
         }
-
-        /*JSONArray result = new JSONArray();
-        while (resultSet.next()) {
-            JSONObject row = new JSONObject();
-            colNames.forEach(cn -> {
-                try {
-                    row.put(cn, resultSet.getObject(cn));
-                } catch (JSONException | SQLException e) {
-                    e.printStackTrace();
-                }
-            });
-            result.put(row);
-        }*/
-
-        /*List<Map<String, ?>> queryResult = Scope.getCurrentScope().getSingleton(ExecutorService.class).getExecutor("jdbc", (Database) liquibase.getDataSource())
-                .queryForList(new RawParameterizedSqlStatement(String.format("select * from %s", tableName)));*/
-
-//        LoggerFactory.getLogger(DatasourceInitializer.class).info(String.valueOf(result));
-/*        Assert.assertEquals("Rollbacking for " + insertedValue, 2, queryResult.size());
-        Assert.assertEquals(insertedValue.toString(), queryResult.get(1).get(colName));*/
     }
 
-    @Test
+
+    /*
+    *
+    *
+    *
+    *       UTILITY METHODS
+    *
+    *
+    *
+    * */
+
+    private void assertRowCount(Statement stmt, String table, int expectedCount) {
+        try (ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM " + table)) {
+            Assert.assertTrue(rs.next());
+            Assert.assertEquals(expectedCount,rs.getInt(1));
+        } catch (SQLException e) {
+            Assert.fail("Failed to assert row count for table " + table + ": " + e.getMessage());
+        }
+    }
+
+    private void assertConstraintExists(Statement stmt, String table, String constraintName) {
+        try (ResultSet rs = stmt.executeQuery("SELECT conname FROM pg_catalog.pg_constraint WHERE conrelid::regclass::text = '" + table + "';")) {
+            Set<String> constraints = new HashSet<>();
+            while (rs.next()) {
+                constraints.add(rs.getString("conname"));
+            }
+            Assert.assertTrue(constraints.contains(constraintName));
+        } catch (SQLException e) {
+            Assert.fail("Failed to assert constraint for table " + table + ": " + e.getMessage());
+        }
+    }
+
+    private void assertForeignKeyExists(Statement stmt, String table, String fkName) {
+        try (ResultSet rs = stmt.executeQuery("SELECT conname FROM pg_catalog.pg_constraint WHERE conrelid::regclass::text = '" + table + "' AND contype = 'f';")) {
+            List<String> fks = new ArrayList<>();
+            while (rs.next()) {
+                fks.add(rs.getString("conname"));
+            }
+            Assert.assertTrue(fks.contains(fkName));
+        } catch (SQLException e) {
+            Assert.fail("Failed to assert foreign key for table " + table + ": " + e.getMessage());
+        }
+    }
+
+    private void assertColumnDefaults(Statement stmt, String tableName, List<String> colNames, List<String> expectedDefaults) throws SQLException {
+        if (colNames.size() != expectedDefaults.size()) Assert.fail("Number of columns does not match number of expected default values.");
+
+        try (ResultSet rs = stmt.executeQuery("SELECT column_name, column_default FROM information_schema.columns WHERE table_name = '" + tableName + "' ORDER BY column_name;")) {
+            Map<String,String> expDefaultsMap = new HashMap<>();
+            for (int i = 0; i < colNames.size(); i++){
+                expDefaultsMap.put(colNames.get(i),expectedDefaults.get(i));
+            }
+
+            Map<String, String> actualDefaults = new HashMap<>();
+            while (rs.next()) {
+                String name = rs.getString("column_name");
+                String def = rs.getString("column_default");
+                if (expDefaultsMap.containsKey(name)) {
+                    actualDefaults.put(name, def);
+                }
+            }
+            Assert.assertTrue(actualDefaults.entrySet().containsAll(expDefaultsMap.entrySet()));
+        } catch (Exception e) {
+            Assert.fail("Failed to assert default values for table " + tableName + ": " + e.getMessage());
+        }
+    }
+
+    /*@Test
     @Ignore
     public void testObjectCreationAndDeletion_basic(){
         DataSource dataSource = liquibase.getDataSource();
@@ -232,7 +230,7 @@ public class LiquibaseSpringTest {
 
 
 
-    }
+    }*/
 
 
 }
