d0ee80ab881c12c53bda14078c95bd7efe88276f
filipe <flautert@liquibase.org>
2025-04-16 13:47:31 -0300

    chore: re-add method that is used by some extensions

--- liquibase-standard/src/main/java/liquibase/util/HelpUtil.java
+++ liquibase-standard/src/main/java/liquibase/util/HelpUtil.java
@@ -1,7 +1,20 @@
 package liquibase.util;
 
+import liquibase.command.CommandDefinition;
+
 public class HelpUtil {
 
     public static final String HISTORY_URL = "https://docs.liquibase.com/DATABASECHANGELOGHISTORY";
+    /**
+     * Hides the command name when running liquibase --help
+     * @param commandDefinition the command definition to adjust
+     * @deprecated instead of using this method, use the {@link liquibase.command.CommandDefinition#setInternal(boolean)} method in your commands
+     */
+    @Deprecated
+    public static void hideCommandNameInHelpView(CommandDefinition commandDefinition) {
+        if (commandDefinition.getPipeline().size() == 1) {
+            commandDefinition.setInternal(true);
+        }
+    }
 
 }
