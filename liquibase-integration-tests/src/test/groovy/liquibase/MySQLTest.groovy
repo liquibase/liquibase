05897c965df1a002794d909d6e596eb4d1190bad
Wesley Willard <wwillard@liquibase.com>
2025-06-02 13:45:23 -0500

    Added more database tests

--- liquibase-integration-tests/src/test/groovy/liquibase/MySQLTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/MySQLTest.groovy
@@ -1,4 +1,6 @@
-import liquibase.Scope
+package liquibase
+
+
 import liquibase.command.CommandScope
 import liquibase.command.core.UpdateCommandStep
 import liquibase.command.core.UpdateCountCommandStep
@@ -34,4 +36,24 @@ class MySQLTest extends Specification {
         then:
         noExceptionThrown()
     }
+
+    def "verify Unique constraint is not created again when precondition fails because it already exists"() {
+        when:
+        def changeLogFile = "changelogs/uniqueConstraint-mysql.xml"
+        def scopeSettings = [
+                (Scope.Attr.resourceAccessor.name()): new SearchPathResourceAccessor(".,target/test-classes")
+        ]
+        Scope.child(scopeSettings, {
+            CommandScope commandScope = new CommandScope(UpdateCommandStep.COMMAND_NAME)
+            commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, mysql.getConnectionUrl())
+            commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, mysql.getUsername())
+            commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, mysql.getPassword())
+            commandScope.addArgumentValue(UpdateCountCommandStep.CHANGELOG_FILE_ARG, changeLogFile)
+            commandScope.execute()
+        } as Scope.ScopedRunnerWithReturn<Void>)
+
+        then:
+        noExceptionThrown()
+    }
+
 }
