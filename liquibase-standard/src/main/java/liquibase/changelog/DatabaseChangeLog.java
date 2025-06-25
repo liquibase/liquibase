6111fa2fdca13fcd924d9029ee0f4ddd99ffc7ed
Tim Eichinger <privat.timeichinger@gmail.com>
2025-06-25 22:15:55 +0200

    revert use lombok setter in DatabaseChangeLog

--- liquibase-standard/src/main/java/liquibase/changelog/DatabaseChangeLog.java
+++ liquibase-standard/src/main/java/liquibase/changelog/DatabaseChangeLog.java
@@ -36,7 +36,6 @@ import liquibase.util.ExceptionUtil;
 import liquibase.util.FileUtil;
 import liquibase.util.StringUtil;
 import lombok.Getter;
-import lombok.Setter;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -92,13 +91,10 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
 
     private final PreconditionContainer preconditionContainer = new GlobalPreconditionContainer();
 
-    @Setter
     @Getter
     private String physicalFilePath;
-    @Setter
     private String logicalFilePath;
 
-    @Setter
     @Getter
     private ObjectQuotingStrategy objectQuotingStrategy;
 
@@ -116,33 +112,26 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
     @Getter
     private final List<ChangeSet> skippedBecauseOfPreconditionsChangeSets = new ArrayList<>();
 
-    @Setter
     @Getter
     private ChangeLogParameters changeLogParameters;
 
-    @Setter
     @Getter
     private RuntimeEnvironment runtimeEnvironment;
 
-    @Setter
     private DatabaseChangeLog rootChangeLog = ROOT_CHANGE_LOG.get();
 
-    @Setter
     @Getter
     private DatabaseChangeLog parentChangeLog = PARENT_CHANGE_LOG.get();
 
-    @Setter
     @Getter
     private ContextExpression contextFilter;
 
-    @Setter
     @Getter
     private ContextExpression includeContextFilter;
 
     @Getter
     private Labels includeLabels;
 
-    @Setter
     @Getter
     private boolean includeIgnore;
 
@@ -156,8 +145,20 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
         this.physicalFilePath = physicalFilePath;
     }
 
+    public void setRootChangeLog(DatabaseChangeLog rootChangeLog) {
+        this.rootChangeLog = rootChangeLog;
+    }
+
     public DatabaseChangeLog getRootChangeLog() {
         return (rootChangeLog != null) ? rootChangeLog : this;
+    }
+
+    public void setParentChangeLog(DatabaseChangeLog parentChangeLog) {
+        this.parentChangeLog = parentChangeLog;
+    }
+
+    public void setRuntimeEnvironment(RuntimeEnvironment runtimeEnvironment) {
+        this.runtimeEnvironment = runtimeEnvironment;
     }
 
     @Override
@@ -168,6 +169,15 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
     @Override
     public void setPreconditions(PreconditionContainer precondition) {
         this.preconditionContainer.addNestedPrecondition(precondition);
+    }
+
+
+    public void setChangeLogParameters(ChangeLogParameters changeLogParameters) {
+        this.changeLogParameters = changeLogParameters;
+    }
+
+    public void setPhysicalFilePath(String physicalFilePath) {
+        this.physicalFilePath = physicalFilePath;
     }
 
     public String getRawLogicalFilePath() {
@@ -186,12 +196,20 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
         return SLASH_PATTERN.matcher(path).replaceFirst("");
     }
 
+    public void setLogicalFilePath(String logicalFilePath) {
+        this.logicalFilePath = logicalFilePath;
+    }
+
     public String getFilePath() {
         if (logicalFilePath == null) {
             return physicalFilePath;
         } else {
             return getLogicalFilePath();
         }
+    }
+
+    public void setObjectQuotingStrategy(ObjectQuotingStrategy objectQuotingStrategy) {
+        this.objectQuotingStrategy = objectQuotingStrategy;
     }
 
     /**
@@ -210,6 +228,10 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
         setContextFilter(contexts);
     }
 
+    public void setContextFilter(ContextExpression contextFilter) {
+        this.contextFilter = contextFilter;
+    }
+
     /**
      * @deprecated Correct version is {@link #setIncludeLabels(Labels)}. Kept for backwards compatibility.
      */
@@ -224,12 +246,20 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
         this.includeLabels = labels;
     }
 
+    public void setIncludeIgnore(boolean ignore) {
+        this.includeIgnore = ignore;
+    }
+
     /**
      * @deprecated use {@link #setIncludeContextFilter(ContextExpression)}
      */
     @Deprecated
     public void setIncludeContexts(ContextExpression includeContexts) {
         setIncludeContextFilter(includeContexts);
+    }
+
+    public void setIncludeContextFilter(ContextExpression includeContextFilter) {
+        this.includeContextFilter = includeContextFilter;
     }
 
     @Override
@@ -1159,10 +1189,10 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
      */
     private boolean ranChangeSetExists(ChangeSet changeSet, List<RanChangeSet> ranChangeSets) {
         Optional<RanChangeSet> ranChangeSet =
-                ranChangeSets.stream().filter(rc ->
-                        rc.getId().equals(changeSet.getId()) &&
-                                rc.getAuthor().equals(changeSet.getAuthor()) &&
-                                rc.getStoredChangeLog().equals(changeSet.getFilePath())).findFirst();
+            ranChangeSets.stream().filter( rc ->
+                rc.getId().equals(changeSet.getId()) &&
+                rc.getAuthor().equals(changeSet.getAuthor()) &&
+                rc.getStoredChangeLog().equals(changeSet.getFilePath())).findFirst();
         return ranChangeSet.isPresent();
     }
 
