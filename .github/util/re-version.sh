b4f9901bfb656138130d8dcea38657061edca69c
filipe <flautert@liquibase.org>
2025-07-14 17:43:45 -0300

    chore: remove minimal zip

--- .github/util/re-version.sh
+++ .github/util/re-version.sh
@@ -177,23 +177,11 @@ if [ -z "$extension_name" ]; then
   mkdir $workdir/tgz-repackage 
   tar -xzf $workdir/liquibase-$MODIFIED_BRANCH_NAME-SNAPSHOT.tar.gz -C $workdir/tgz-repackage
 
-  mkdir $workdir/tgz-repackage-minimal
-  tar -xzf $workdir/liquibase-minimal-$MODIFIED_BRANCH_NAME-SNAPSHOT.tar.gz -C $workdir/tgz-repackage-minimal
-
   cp $workdir/internal/lib/liquibase-core.jar $workdir/tgz-repackage/internal/lib/liquibase-core.jar
-
-  # copy core jar to minimal
-  cp $workdir/internal/lib/liquibase-core.jar $workdir/tgz-repackage-minimal/internal/lib/liquibase-core.jar
 
   # replace the versions in all the text files
   find $workdir/tgz-repackage -name "*.txt" -exec sed -i -e "s/\(0\|release\|master\)-SNAPSHOT/$version/" {} \;
 
   (cd $workdir/tgz-repackage && tar -czf $outdir/liquibase-$version.tar.gz *)
   (cd $workdir/tgz-repackage && zip -qr $outdir/liquibase-$version.zip *)
-
-  # replace the versions in all the text files in minimal
-  find $workdir/tgz-repackage-minimal -name "*.txt" -exec sed -i -e "s/\(0\|release\|master\)-SNAPSHOT/$version/" {} \;
-
-  (cd $workdir/tgz-repackage-minimal && tar -czf $outdir/liquibase-minimal-$version.tar.gz *)
-  (cd $workdir/tgz-repackage-minimal && zip -qr $outdir/liquibase-minimal-$version.zip *)
 fi
