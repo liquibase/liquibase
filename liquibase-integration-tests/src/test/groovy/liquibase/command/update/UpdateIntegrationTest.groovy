2ec99ed19105f2cdd7fa450eff910dc45f1367b9
Wesley Willard <wwillard@liquibase.com>
2025-07-26 14:36:12 -0500

    Another test

--- liquibase-integration-tests/src/test/groovy/liquibase/command/update/UpdateIntegrationTest.groovy
+++ liquibase-integration-tests/src/test/groovy/liquibase/command/update/UpdateIntegrationTest.groovy
@@ -209,4 +209,56 @@ ID | DEPLOYMENT_ID | CHANGELOG_FILE | CHANGESET_ID | CHANGESET_AUTHOR |
         assert outputBuffer.contains("target/test-classes/changelogs/multiple-sql-changes-one-ran.yaml")
         assert outputBuffer.contains("nvoxland")
     }
+
+    def "Changeset should be marked as ran if all SQL changes with DBMS run"() {
+        def changelogFile = "target/test-classes/changelogs/multiple-sql-changes-all-ran.yaml"
+        when:
+
+        Map<String, Object> scopeValues = new HashMap<>()
+        def resourceAccessor = new SearchPathResourceAccessor(".,target/test-classes", Scope.getCurrentScope().getResourceAccessor())
+        scopeValues.put(Scope.Attr.resourceAccessor.name(), resourceAccessor)
+        OutputStream os = new ByteArrayOutputStream()
+        Scope.child(scopeValues, new Scope.ScopedRunner() {
+            @Override
+            void run() throws Exception {
+                CommandScope commandScope = new CommandScope(UpdateCommandStep.COMMAND_NAME)
+                commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, testDatabase.getConnectionUrl())
+                commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, testDatabase.getUsername())
+                commandScope.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, testDatabase.getPassword())
+                commandScope.addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, changelogFile)
+                commandScope.addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY, UpdateSummaryEnum.VERBOSE)
+                commandScope.addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY_OUTPUT, UpdateSummaryOutputEnum.CONSOLE)
+                commandScope.setOutput(os)
+                try {
+                    commandScope.execute()
+                } catch (Exception ignored) {
+
+                }
+            }
+        })
+
+        then:
+        def console = os.toString()
+        console.contains("UPDATE SUMMARY")
+        console.contains("Run:                          1")
+        console.contains("Previously run:               0")
+        console.contains("Filtered out:                 0")
+        console.contains("-------------------------------")
+        console.contains("Total change sets:            1")
+
+        when:
+        def outputStream = new ByteArrayOutputStream()
+        CommandScope history = new CommandScope(HistoryCommandStep.COMMAND_NAME[0])
+        history.addArgumentValue(DbUrlConnectionArgumentsCommandStep.URL_ARG, testDatabase.getConnectionUrl())
+        history.addArgumentValue(DbUrlConnectionArgumentsCommandStep.USERNAME_ARG, testDatabase.getUsername())
+        history.addArgumentValue(DbUrlConnectionArgumentsCommandStep.PASSWORD_ARG, testDatabase.getPassword())
+        history.setOutput(outputStream)
+        history.execute()
+        String outputBuffer = outputStream.toString()
+
+        then:
+        outputBuffer != null
+        assert outputBuffer.contains("target/test-classes/changelogs/multiple-sql-changes-all-ran.yaml")
+        assert outputBuffer.contains("nvoxland")
+    }
 }
