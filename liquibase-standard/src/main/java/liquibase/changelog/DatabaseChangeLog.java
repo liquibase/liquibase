a97c6dddd67086c5af7514ce69fd61b7f4a37de5
George Gastaldi <gegastaldi@gmail.com>
2025-08-28 20:49:27 -0300

    Comparator resoluition should work in non-flat classloaders

--- liquibase-standard/src/main/java/liquibase/changelog/DatabaseChangeLog.java
+++ liquibase-standard/src/main/java/liquibase/changelog/DatabaseChangeLog.java
@@ -605,7 +605,7 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
         IncludeAllFilter resourceFilter = null;
         if (resourceFilterDef != null) {
             try {
-                resourceFilter = (IncludeAllFilter) Class.forName(resourceFilterDef).getConstructor().newInstance();
+                resourceFilter = (IncludeAllFilter) Class.forName(resourceFilterDef, true, Thread.currentThread().getContextClassLoader()).getConstructor().newInstance();
             } catch (ReflectiveOperationException e) {
                 throw new SetupException(e);
             }
@@ -695,7 +695,7 @@ public class DatabaseChangeLog implements Comparable<DatabaseChangeLog>, Conditi
             resourceComparator = getStandardChangeLogComparator();
         } else {
             try {
-                resourceComparator = (Comparator<String>) Class.forName(resourceComparatorDef).getConstructor().newInstance();
+                resourceComparator = (Comparator<String>) Class.forName(resourceComparatorDef, true, Thread.currentThread().getContextClassLoader()).getConstructor().newInstance();
             } catch (ReflectiveOperationException e) {
                 //take default comparator
                 Scope.getCurrentScope().getLog(getClass()).info("no resourceComparator defined - taking default " +
