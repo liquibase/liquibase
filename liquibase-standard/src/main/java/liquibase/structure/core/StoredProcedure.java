a38712af05be2627c16eb39989a647a2ad42bd0f
HorbatenkoYehor <nelfan95@gmail.com>
2025-09-12 17:30:52 +0300

    INT-1392: add get/setArguments and get/setProcedureName methods into StoredProcedure and update generateSql logic in DropProcedureGeneratorSnowflake

--- liquibase-standard/src/main/java/liquibase/structure/core/StoredProcedure.java
+++ liquibase-standard/src/main/java/liquibase/structure/core/StoredProcedure.java
@@ -1,5 +1,8 @@
 package liquibase.structure.core;
 
+import liquibase.structure.DatabaseObject;
+import org.apache.commons.lang3.StringUtils;
+
 public class StoredProcedure extends StoredDatabaseLogic<StoredProcedure> {
 
     public StoredProcedure() {
@@ -8,6 +11,35 @@ public class StoredProcedure extends StoredDatabaseLogic<StoredProcedure> {
     public StoredProcedure(String catalogName, String schemaName, String procedureName) {
         this.setSchema(new Schema(catalogName, schemaName));
         setName(procedureName);
+    }
+
+    public String getArguments() {
+        return getAttribute("arg", String.class);
+    }
+
+    public StoredProcedure setArguments(String arg) {
+        setAttribute("arg", arg);
+        return this;
+    }
+
+    public String getProcedureName() {
+        return getAttribute("procedureName", String.class);
+    }
+
+    public StoredProcedure setProcedureName(String procedureName) {
+        setAttribute("procedureName", procedureName);
+        return this;
+    }
+
+    /**
+     * For databases (like Snowflake) that support procedure overloading {@link #getName()} will contain id (name with input arguments)
+     * so actual name should be fetched from {@link #getProcedureName()}
+     */
+    public static String getProcedureName(DatabaseObject dbObject) {
+        if (dbObject instanceof StoredProcedure procedure) {
+            return StringUtils.isNotEmpty(procedure.getProcedureName()) ? procedure.getProcedureName() : procedure.getName();
+        }
+        throw new IllegalArgumentException("Expected Procedure, got: " + (dbObject == null ? "null" : dbObject.getClass().getName()));
     }
 
 }
