61340677840b9c07a408eef246a257def1e2526c
Harsh Mehta <harshmehta010102@gmail.com>
2025-11-25 18:55:31 +0530

    Resolved bot comments.
    Signed-off-by: Harsh Mehta <harshmehta010102@gmail.com>

--- liquibase-standard/src/test/groovy/liquibase/changelog/ChangeLogIncludeTest.groovy
+++ liquibase-standard/src/test/groovy/liquibase/changelog/ChangeLogIncludeTest.groovy
@@ -4,9 +4,19 @@ package liquibase.changelog
 import spock.lang.Specification
 import spock.lang.Unroll
 
+/**
+ * Unit tests for {@link ChangeLogInclude} class.
+ * <p>
+ * These tests verify the functionality of the ChangeLogInclude class,
+ * particularly focusing on the logicalFilePath attribute added in version 4.30.
+ * </p>
+ */
 class ChangeLogIncludeTest extends Specification {
 
-    @Unroll
+    /**
+     * Tests that the logicalFilePath field is included in the set of serializable fields.
+     * This ensures the field will be properly serialized and deserialized.
+     */
     def "getSerializableFields includes logicalFilePath"() {
         when:
         def changeLogInclude = new ChangeLogInclude()
@@ -16,6 +26,10 @@ class ChangeLogIncludeTest extends Specification {
         changeLogInclude.getSerializableFields().contains("logicalFilePath")
     }
 
+    /**
+     * Tests that the logicalFilePath property can be set and retrieved correctly.
+     * Verifies the getter and setter methods work as expected.
+     */
     def "logicalFilePath can be set and retrieved"() {
         when:
         def changeLogInclude = new ChangeLogInclude()
@@ -25,7 +39,10 @@ class ChangeLogIncludeTest extends Specification {
         changeLogInclude.getLogicalFilePath() == "my/logical/path"
     }
 
-    @Unroll
+    /**
+     * Tests that the serialized object name is correctly set to "include".
+     * This is used when serializing the object to XML or other formats.
+     */
     def "getSerializedObjectName returns include"() {
         when:
         def changeLogInclude = new ChangeLogInclude()
@@ -33,6 +50,10 @@ class ChangeLogIncludeTest extends Specification {
         changeLogInclude.getSerializedObjectName() == "include"
     }
 
+    /**
+     * Tests that the serialized object namespace is not null.
+     * The namespace is used for XML serialization.
+     */
     def "getSerializedObjectNamespace returns standard namespace"() {
         when:
         def changeLogInclude = new ChangeLogInclude()
