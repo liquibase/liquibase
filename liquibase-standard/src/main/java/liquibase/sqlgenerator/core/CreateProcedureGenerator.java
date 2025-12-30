02e23fd55c65d6db5fb25a12733b91b7e028a040
Daniel Mallorga <dmallorga@liquibase.com>
2025-12-30 17:56:40 -0300

    - Fixed CreateProcedureGenerator logic to set the correct search path for the current transaction. - Tests updated.

--- liquibase-standard/src/main/java/liquibase/sqlgenerator/core/CreateProcedureGenerator.java
+++ liquibase-standard/src/main/java/liquibase/sqlgenerator/core/CreateProcedureGenerator.java
@@ -160,12 +160,7 @@ public class CreateProcedureGenerator extends AbstractSqlGenerator<CreateProcedu
                 }
 
                 if (!originalSearchPath.equals(schemaName) && !originalSearchPath.startsWith(schemaName + ",") && !originalSearchPath.startsWith("\"" + schemaName + "\",")) {
-                    if (database instanceof EnterpriseDBDatabase){
-                        sql.add(0, new UnparsedSql("SET LOCAL SEARCH_PATH TO " + database.escapeObjectName(defaultSchema, Schema.class) + ", " + originalSearchPath));
-                        sql.add(new UnparsedSql("ALTER SESSION SET CURRENT SCHEMA " + originalSearchPath));
-                    } else {
-                        sql.add(0, new UnparsedSql("SET LOCAL SEARCH_PATH TO " + database.escapeObjectName(schemaName, Schema.class) + ", " + originalSearchPath));
-                    }
+                    sql.add(0, new UnparsedSql("SET LOCAL SEARCH_PATH TO " + database.escapeObjectName(schemaName, Schema.class) + ", " + originalSearchPath));
                 }
             }
         }
