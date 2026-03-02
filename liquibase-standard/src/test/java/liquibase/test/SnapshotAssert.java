bbc14449063bb7089e3ddc5add94d421dc6c7479
Marc R. Hoffmann <hoffmann@mountainminds.com>
2026-03-02 20:11:38 +0000

    FIXUP: Minimize code changes to SnapshotAssert

--- liquibase-standard/src/test/java/liquibase/test/SnapshotAssert.java
+++ liquibase-standard/src/test/java/liquibase/test/SnapshotAssert.java
@@ -1,5 +1,9 @@
 package liquibase.test;
 
+import static org.junit.Assert.fail;
+
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import liquibase.snapshot.DatabaseSnapshot;
 import liquibase.structure.DatabaseObject;
@@ -15,18 +19,26 @@ public class SnapshotAssert {
 		return new SnapshotAssert(snapshot);
 	}
 
-	public void containsObject(DatabaseObject object) {
-		checkContainsObject(object.getClass(), object::equals, object + " not found");
+	public SnapshotAssert containsObject(DatabaseObject object) {
+		return checkContainsObject(object.getClass(), object::equals, ignore -> {}, object + " not found");
 	}
 
-	public <T extends DatabaseObject> T containsObject(Class<T> type, Predicate<T> condition) {
-		return checkContainsObject(type, condition, type.getSimpleName() + " satisfying condition not found");
+	public <T extends DatabaseObject> SnapshotAssert containsObject(Class<T> type, Predicate<T> condition) {
+		return containsObject(type, condition, ignore -> {});
 	}
 
-	private <T extends DatabaseObject> T checkContainsObject(Class<T> type, Predicate<T> condition, String failMessage) {
-		return snapshot.get(type).stream()
+	public <T extends DatabaseObject> SnapshotAssert containsObject(Class<T> type, Predicate<T> condition, Consumer<T> verfifier) {
+		return checkContainsObject(type, condition, verfifier, type.getSimpleName() + " satisfying condition not found");
+	}
+
+	private <T extends DatabaseObject> SnapshotAssert checkContainsObject(Class<T> type, Predicate<T> condition, Consumer<T> verfifier, String failMessage) {
+		Optional<? extends DatabaseObject> first = snapshot.get(type).stream()
 			.filter(condition)
-			.findFirst()
-			.orElseThrow(() -> new AssertionError(failMessage));
+			.peek(verfifier)
+			.findFirst();
+		if (!first.isPresent()) {
+			fail(failMessage);
+		}
+		return this;
 	}
 }
