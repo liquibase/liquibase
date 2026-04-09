6a6582d8df1361573545651977f912b5dfe3842f
Matrix Dai <matrix.dai@aurea.com>
2026-04-09 13:23:07 +0800

    test: update expected help output for diffColumnDefaultValueConstraintName option

--- liquibase-cli/src/test/groovy/liquibase/integration/commandline/LiquibaseCommandLineTest.groovy
+++ liquibase-cli/src/test/groovy/liquibase/integration/commandline/LiquibaseCommandLineTest.groovy
@@ -158,6 +158,15 @@ Global Options
                              (defaults file: 'liquibase.defaultsFile',
                                environment variable: 'LIQUIBASE_DEFAULTS_FILE')
 
+      --diff-column-default-value-constraint-name=PARAM
+                             Should Liquibase compare column default value
+                               constraint name in diff operation?
+                             DEFAULT: true
+                             (defaults file: 'liquibase.
+                               diffColumnDefaultValueConstraintName',
+                               environment variable:
+                               'LIQUIBASE_DIFF_COLUMN_DEFAULT_VALUE_CONSTRAINT_NAME')
+
       --diff-column-order=PARAM
                              Should Liquibase compare column order in diff
                                operation?
